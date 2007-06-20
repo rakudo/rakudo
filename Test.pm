f090ae0072607e5baa64f14f481dbad8c50bbedb
Patrick R. Michaud <pmichaud@pobox.com>
2007-06-20 22:19:05 +0000

    [perl6]: * Simple version of a Test.pm module
    git-svn-id: http://svn.perl.org/parrot/trunk/languages/perl6@19195 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ Test.pm
@@ -0,0 +1,48 @@
+# Copyright (C) 2007, The Perl Foundation.
+# $Id$
+
+## This is a temporary Test.pm to get us started until we get pugs's Test.pm
+## working. It's shamelessly stolen & adapted from MiniPerl6 in the pugs repo.
+
+# globals to keep track of our tests
+my $num_of_tests_run = 0;
+my $num_of_tests_failed = 0;
+my $num_of_tests_planned;
+
+# for running the test suite multiple times in the same process
+my $testing_started;
+
+
+## test functions
+
+sub plan($number_of_tests) {
+    $testing_started      = 1;
+    $num_of_tests_planned = $number_of_tests;
+
+    say '1..' ~ $number_of_tests;
+}
+
+
+sub ok($cond, $desc) {
+    proclaim($cond, 'ok! ' ~ $desc);
+}
+
+
+sub is($got, $expected, $desc) {
+    my $test = $got eq $expected;
+    proclaim($test, 'is! ' ~ $desc);
+}
+
+
+## 'private' subs
+
+sub proclaim($cond, $desc) {
+    $testing_started  = 1;
+    $num_of_tests_run = $num_of_tests_run + 1;
+
+    if ( $cond ) {
+        say "ok ", $num_of_tests_run;
+    } else {
+        say "not ok ", $num_of_tests_run;
+    }
+}

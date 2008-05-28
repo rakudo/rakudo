68d81e1578990e6808fc8d0ab224af605d457915
Jerry Gay a.k.a. particle <Jerry.Gay@gmail.com>
2008-05-28 16:32:49 +0000

    [rakudo] this ugly piece of perl 5 code runs 'make spectest' and looks for passing test files that are not yet included in the 'spectest_regression' target. The output is an evil mess, but still useful.
    ~ not cross-platform
    ~ needs to be generalized
    ~ patches welcome
    Courtesy of Moritz Lenz (moritz++)
    
    git-svn-id: http://svn.perl.org/parrot/trunk/languages/perl6@27887 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

---
+++ tools/update_passing_test_data.pl
@@ -0,0 +1,47 @@
+#! perl
+use strict;
+use warnings;
+
+# this ugly piece of perl 5 code runs 'make spectest' and looks
+# for passing test files that are not yet included in the
+# 'spectest_regression' target. The output is an evil mess, but still useful.
+
+my $test_file = $ARGV[0] || 't/spectest_regression.data';
+
+open my $f, '<', $test_file
+    or die "Can't open file '$test_file' for reading: $!";
+
+my %known_tests;
+while (<$f>){
+    chomp;
+    next if m/^\s*(?:#|$)/;
+    $known_tests{$_}++;
+}
+close $f;
+
+print "Running the test suite...";
+$| = 1;
+
+my @new;
+
+open my $t, 'make spectest 2>&1 |'
+    or die "Can't run make: $!";
+
+while(<$t>){
+    # hack until the looping parse is fixed
+    unlink glob 't/spec/S29-conversions/ord_and_chr.*';
+    print '.';
+    if (m{^t/spec/(.*?)\.+ok$}){
+        my $test = $1 . '.t';
+        if (!$known_tests{$test}){
+            push @new, $test;
+            print "\n<< new: $test>>\n";
+        }
+    }
+}
+close $t;
+if (@new){
+    print "\nNewly passing tests:\n";
+    print "$_\n" for @new;
+}
+

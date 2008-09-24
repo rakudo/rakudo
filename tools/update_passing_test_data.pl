ed13e4bce3037f31b70170eea53a43ea6dca1ac4
Moritz A Lenz <moritz@faui2k3.org>
2008-09-24 22:00:31 +0000

    [rakudo] complete rewrite of tools/update_passing_test_data.pl, now the output is much more informative (but less copy&paste frindly)
    git-svn-id: http://svn.perl.org/parrot/trunk/languages/perl6@31390 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- tools/update_passing_test_data.pl
+++ tools/update_passing_test_data.pl
@@ -1,52 +1,93 @@
 #! perl
-# Copyright (C) 2001-2008, The Perl Foundation.
+# Copyright (C) 2008, The Perl Foundation.
 # $Id$
+
+=head1 DESCRIPTION
+
+This tool runs the same test that C<make spectest> would, except those that
+C<make spectest_regression> runs.
+
+For each file that passes at least one test (criterion might change in future)
+it prints out a short summary about the status of this file.
+
+This is primarily used to identify tests that could be added to
+F<t/spectest_regression.data>, and those that are worth a closer look. But
+please don't add them blindly just because they all pass - chances are that
+there's a good reason for them not already being included.
+
+This script should be called from the main Rakudo directory (ie
+C<languages/perl6/> relative to parrot).
+
+=cut
+
 use strict;
 use warnings;
+use TAP::Harness;
+use TAP::Parser::Aggregator 3.01;
 
-# this ugly piece of perl 5 code runs 'make spectest' and looks
-# for passing test files that are not yet included in the
-# 'spectest_regression' target. The output is an evil mess, but still useful.
+use File::Find;
+use Data::Dumper;
 
-my $test_file = $ARGV[0] || 't/spectest_regression.data';
 
-open my $f, '<', $test_file
-    or die "Can't open file '$test_file' for reading: $!";
-
-my %known_tests;
-while (<$f>){
-    chomp;
-    next if m/^\s*(?:#|$)/;
-    # ignore end-of-line comments
-    s/\s+#.*$//g;
-    $known_tests{$_}++;
+my %not_process;
+{
+    my @not_process = read_specfile('t/spectest_regression.data');
+    @not_process{@not_process}  = (1) x  @not_process;
 }
-close $f;
 
-print "Running the test suite";
-$| = 1;
+print <<KEY;
+Key:
+[S  ]   = some tests passed
+[ P ]   = plan ok (ran all tests)
+[  A]   = all passed
+      ( passed / ran )
+==================================
+KEY
 
-my @new;
+find({ wanted => \&go, no_chdir => 1 }, 't/spec/');
 
-open my $t, 'make spectest 2>&1 |'
-    or die "Can't run make: $!";
+sub go {
+    return if -d $_;
+    return if m/\.sv[nk]/;
+    return unless m/\.t$/;
+    return if $not_process{$_};
+    my $fudged = qx{t/spec/fudge --keep-exit-code rakudo $_};
+    chomp $fudged;
+    my $H = get_harness();
+    my $agg = TAP::Parser::Aggregator->new();
+    $agg->start();
+    $H->aggregate_tests($agg, $fudged);
+    $agg->stop();
 
-while(<$t>){
-    # hack until the looping parse is fixed
-    unlink glob 't/spec/S29-conversions/ord_and_chr.*';
-    print '.';
-    if (m{^t/spec/(.*?)\.+ok$}){
-        my $test = $1 . '.t';
-        if (!$known_tests{$test}){
-            push @new, $test;
-            print "\n<< new: $test>>\n";
-        }
+    my ($some_passed, $plan_ok, $all_passed) = (' ', ' ', ' ');
+    my $actually_passed = $agg->passed - $agg->skipped - $agg->todo;
+    $some_passed = 'S' if $actually_passed;
+    $plan_ok     = 'P' if !scalar($agg->parse_errors);
+    $all_passed  = 'A' if !       $agg->has_errors;
+    printf "[%s%s%s] (% 3d/%-3d) %s\n", $some_passed, $plan_ok, $all_passed,
+           $actually_passed, $agg->total, $_
+                if $actually_passed;
+}
+
+sub read_specfile {
+    my $fn = shift;
+    my @res;
+    open (my $f, '<', $fn) or die "Can't open file '$fn' for reading: $!";
+    while (<$f>){
+        next if m/#/;
+        next unless m/\S/;
+        s/\s+\z//;
+        push @res, "t/spec/$_";
     }
+    return @res;
 }
-close $t;
-if (@new){
-    print "\nNewly passing tests:\n";
-    print "$_\n" for @new;
+
+sub get_harness {
+    return TAP::Harness->new({
+            verbosity   => -2,
+            exec        => ['../../parrot', '-G', 'perl6.pbc'],
+            merge       => 1,
+    });
 }
 
 # Local Variables:

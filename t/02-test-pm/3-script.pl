a5a8853c5292301c2d2be7fff6224fae93d5b2af
Patrick R. Michaud <pmichaud@pobox.com>
2008-10-21 15:55:19 +0000

    [rakudo]:  Remove obsolete t/02-test-pm/ .
    git-svn-id: http://svn.perl.org/parrot/trunk/languages/perl6@32073 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- t/02-test-pm/3-script.pl
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2007, The Perl Foundation.
-# $Id$
-
-use Test;
-
-plan 1;
-
-# here is a TODO test that passes
-ok(1, "TODO that passes", :todo);
--- t/02-test-pm/use_ok_test.pm
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2007, The Perl Foundation.
-# $Id$
-
-module use_ok_test-0.0.1;
-
-sub it_worked { 1 }
-
-1;
-

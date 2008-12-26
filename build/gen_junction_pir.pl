173aea0aad57d03d3c487a4447b7878bfde6e851
Patrick R. Michaud <pmichaud@pobox.com>
2008-12-26 18:40:42 +0000

    [rakudo]:  Undo autothreading of infix:<===>, reopening RT #61692. Autothreading infix:<===> causes Junction creation to fail, and we should check if it indeed should autothread at all.
    git-svn-id: http://svn.perl.org/parrot/trunk/languages/perl6@34394 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

--- build/gen_junction_pir.pl
+++ build/gen_junction_pir.pl
@@ -11,7 +11,7 @@ my @binary = qw(
   infix:+ infix:-
   infix:== infix:!= infix:<  infix:>  infix:<= infix:>=
   infix:eq infix:ne infix:lt infix:gt infix:le infix:ge
-  infix:<=> infix:leg infix:cmp infix:eqv infix:=== infix:=:=
+  infix:<=> infix:leg infix:cmp infix:eqv infix:=:=
 );
 
 my @unary = qw(

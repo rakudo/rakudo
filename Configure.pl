9f84067ae241ef1dc6ee161c94f1cd612b2a0fb1
Patrick R. Michaud <pmichaud@pmichaud.com>
2009-01-29 11:44:06 -0600

    Modify Configure.pl to use "rakudo" instead of "perl6"

--- Configure.pl
+++ Configure.pl
@@ -6,4 +6,4 @@ use strict;
 use warnings;
 
 chdir '../..';
-`$^X -Ilib tools/dev/reconfigure.pl --step=gen::languages --languages=perl6`;
+`$^X -Ilib tools/dev/reconfigure.pl --step=gen::languages --languages=rakudo`;

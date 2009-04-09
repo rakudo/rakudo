7b2c47c49476916e5d4e44519fd11cbd8f1b0b2a
Moritz Lenz <moritz@faui2k3.org>
2009-04-09 17:52:00 +0200

    Implement Hash.push, and add S32-hash/push.t to spectest.data
    All of these new tests pass except one, which trips over a Null PMC access in
    type() from is_deeply in Test.pm

--- src/setting/Hash.pm
+++ src/setting/Hash.pm
@@ -4,6 +4,38 @@ class Hash is also {
         (%result).{%hash.values} = %hash.keys;
         %result;
     }
+
+    multi method push (*@values) {
+        my $previous;
+        my $has_previous;
+        for @values -> $e {
+            if $has_previous {
+                self!push_construct($previous, $e);
+                $has_previous = 0;
+            } elsif $e ~~ Pair {
+                self!push_construct($e.key, $e.value);
+            } else {
+                $previous = $e;
+                $has_previous = 1;
+            }
+        }
+        if $has_previous {
+            warn "Trailing item in Hash.push";
+        }
+    }
+
+    # push a value onto a hash item, constructing an array if necessary
+    method !push_construct (Object $key, Object $value) {
+        if self.exists($key) {
+            if self.{$key} ~~ Array {
+                self.{$key}.push: $value;
+            } else {
+                self.{$key} = [ self.{$key}, $value];
+            }
+        } else {
+            self.{$key} = $value;
+        }
+    }
 }
 
 multi reverse(%hash) {

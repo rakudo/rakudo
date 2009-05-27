b6640c252cdbc1a3026ae508864fd20834046f54
Moritz Lenz <moritz@faui2k3.org>
2009-05-27 17:25:55 +0200

    [Safe.pm] remove debugging statement; add documentation

--- lib/Safe.pm
+++ lib/Safe.pm
@@ -1,5 +1,27 @@
+=begin pod
+
+=head1 NAME
+
+Safe - simplistic, crude Safe mode for Rakudo
+
+=head1 Synopsis
+
+    BEGIN { @*INC.push: 'lib' }
+    use Safe;
+    # rest of your code here, which can't use
+    # run(), qx/../ or open()
+    # (at least not easily)
+    
+=head1 Description
+
+C<Safe> crudely disables the most dangerous commands in Rakudo, right now
+C<run()>, C<qx/.../> and the C<open()> function (opening sockets is still
+allowed, though). Don't rely on it now, embedded PIR might still do very nasty
+things.
+
+=end pod
+
 module Safe {
-    say "in module Safe";
     my $s = -> *@a, *%h { die "operation not permitted in safe mode" };
     Q:PIR {
         $P0 = get_hll_namespace

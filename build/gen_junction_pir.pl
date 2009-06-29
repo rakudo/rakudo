13b1ff5c8ab2495161bf182d1e44152f4ef923c9
Moritz Lenz <mlenz@wvbh074.physik.uni-wuerzburg.de>
2009-06-29 10:10:59 +0200

    don't generate junction for infix:<eqv> which lives in the setting anyway. jnthn++

--- build/gen_junction_pir.pl
+++ build/gen_junction_pir.pl
@@ -12,7 +12,7 @@ my @binary = qw(
   infix:~
   infix:== infix:!= infix:<  infix:>  infix:<= infix:>=
   infix:eq infix:ne infix:lt infix:gt infix:le infix:ge
-  infix:<=> infix:leg infix:cmp infix:eqv infix:=:=
+  infix:<=> infix:leg infix:cmp infix:=:=
 );
 
 my @unary = qw(

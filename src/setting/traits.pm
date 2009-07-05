3e5adcb6cc26e1969f473c0f4dcf2ce55e918ecf
jnthn <jnthn@jnthn.net>
2009-07-05 23:20:32 +0200

    Fix/workaround in trait_mod:<of> so that variables typed with Code types work.

--- src/setting/traits.pm
+++ src/setting/traits.pm
@@ -125,7 +125,7 @@ multi trait_mod:<does>(Hash $var, $role) {
     $var does $role;
 }
 
-multi trait_mod:<of>(Code $block is rw, Object $type is rw) {
+multi trait_mod:<of>(Code $block is rw where { .defined }, Object $type is rw) {
     $block does Callable[$type];
 }
 

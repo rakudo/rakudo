0bca00c45869ba2e67625733d05c3e5e712b1991
Carl Masak <cmasak@gmail.com>
2009-07-12 20:55:46 +0200

    implemented skeletal Buf type
    The current implementation does not do anything useful, it just
    adds stubbed .encode and .decode methods to Str and Buf, respectively.

---
+++ src/setting/Buf.pm
@@ -0,0 +1,11 @@
+class Buf does Positional {
+    method decode($encoding = 'UTF-8') {
+        return "";
+    }
+
+    method list() {
+        return [];
+    }
+}
+
+# vim: ft=perl6
---
+++ src/setting/Str.pm
@@ -0,0 +1,5 @@
+class Str is also {
+    method encode($encoding = 'UTF-8', $nf = '') {
+        return Buf.new();
+    }
+}

bb1c2bef5f5bd5f184df484a98ced69796c4409e
Moritz Lenz <moritz@faui2k3.org>
2009-07-24 22:32:40 +0200

    initial NYI.pm implementation
    This commit introduces some code/constant duplication; my attempts to factor it out
    into a lexical sub or a lexical variable holding a block were not successful -
    there former aren't found (see RT #67868), the latter produce Null PMC access
    in invoke()

---
+++ src/setting/NYI.pm
@@ -0,0 +1,14 @@
+multi caller(){
+    die 'caller() is not yet implemented in Rakudo, sorry';
+}
+multi context(){
+    die 'context() is not yet implemented in Rakudo, sorry';
+}
+multi want(){
+    die 'want() is not yet implemented in Rakudo, sorry';
+}
+multi runinstead(){
+    die 'runinstead() is not yet implemented in Rakudo, sorry';
+}
+
+# vim: ft=perl6

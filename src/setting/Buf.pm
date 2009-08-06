4665d75269f43c8255fa4d2b4723c48b53bab077
Carl Masak <cmasak@gmail.com>
2009-08-06 18:44:08 +0100

    [src/setting/Str.pm] a first shot at .encode
    Currently, it dies when fed non-ASCII characters, because the relevant
    Parrot parts haven't been fully exposed yet.

--- src/setting/Buf.pm
+++ src/setting/Buf.pm
@@ -1,10 +1,16 @@
 class Buf does Positional {
+    has @!values;
+
+    method new(*@values) {
+        self.bless(*, :values(@values));
+    }
+
     method decode($encoding = 'UTF-8') {
         return "";
     }
 
     method list() {
-        return [];
+        return @!values;
     }
 }
 
--- src/setting/Str.pm
+++ src/setting/Str.pm
@@ -1,5 +1,24 @@
 class Str is also {
     method encode($encoding = 'UTF-8', $nf = '') {
-        return Buf.new();
+        my @bytes = Q:PIR {
+            .local int bin_coding, i, max, byte
+            .local string bin_string
+            .local pmc it, result
+            $S0 = self
+            bin_coding = find_encoding 'fixed_8'
+            bin_string = trans_encoding $S0, bin_coding
+            result = new ['ResizablePMCArray']
+            i = 0
+            max = length bin_string
+          bytes_loop:
+            if i >= max goto bytes_done
+            byte = ord bin_string, i
+            push result, byte
+            inc i
+            goto bytes_loop
+          bytes_done:
+            %r = result
+        };
+        return Buf.new(|@bytes);
     }
 }

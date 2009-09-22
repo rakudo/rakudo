5d3d3a359061c922ef7080ba9b53e7bd224e6397
pmichaud <pmichaud@pobox.com>
2009-09-21 20:48:14 -0500

    Add --optimize to default --gen-parrot build.

--- Configure.pl
+++ Configure.pl
@@ -31,7 +31,7 @@ MAIN: {
         my $prefix  = $options{'gen-parrot-prefix'} || cwd()."/parrot_install";
         # parrot's Configure.pl mishandles win32 backslashes in --prefix
         $prefix =~ s{\\}{/}g;
-        my @command = ($^X, "build/gen_parrot.pl", "--prefix=$prefix", @opts);
+        my @command = ($^X, "build/gen_parrot.pl", "--prefix=$prefix", "--optimize", @opts);
 
         print "Generating Parrot ...\n";
         print "@command\n\n";

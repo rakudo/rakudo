78932e8a7b13b4d0dcab35155327322c0ec6ffe7
Jonathan Worthington <jnthn@jnthn.net>
2009-11-07 00:47:16 +0100

    Perl6::Compiler::Role needs accessor for setting a parametric role signature.

--- src/Perl6/Compiler/Role.pm
+++ src/Perl6/Compiler/Role.pm
@@ -1,3 +1,10 @@
 class Perl6::Compiler::Role is Perl6::Compiler::Package;
 
+# Holds the signautre for this parametric role, if any.
 has $!signature;
+
+# Accessor for signature.
+method signature($signature?) {
+    if pir::defined__IP($signature) { $!signature := $signature }
+    $!signature
+}

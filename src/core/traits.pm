f3b547e43028403ff167c902e2b4f255830a20fb
Jonathan Worthington <jnthn@jnthn.net>
2009-11-06 16:25:37 +0100

    Add a traits.pm with just one thingy in it for now.

---
+++ src/core/traits.pm
@@ -0,0 +1,5 @@
+# XXX Signature is wrong really - will fix once we can parse other things.
+# XXX Use .^ once we learn how to parse it.
+multi trait_mod:<is>(Object $child, Object $parent) {
+    $child.HOW.add_parent($child, $parent);
+}

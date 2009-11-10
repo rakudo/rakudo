a31087f00738962db07405de9f1a2432543a2566
pmichaud <pmichaud@pobox.com>
2009-11-10 10:15:38 -0600

    Get trait export to properly export methods and multis.

--- src/cheats/trait-export.pm
+++ src/cheats/trait-export.pm
@@ -5,23 +5,49 @@ our multi trait_mod:<is>(Code $block, $arg?, :$export!) {
     block = find_lex '$block'
     arg = find_lex '$arg'
 
-    # Multis that are exported need to be whole-sale exported as multis.
-    .local pmc blockns
+    # Get the name from the underlying Parrot Sub.
     .local string blockname
-    block = block.'do'()
-    blockns = block.'get_namespace'()
-    blockname = block
-    $P0 = blockns[blockname]
-    $I0 = isa $P0, 'MultiSub'
-    unless $I0 goto multi_handled
-    block = $P0
-  multi_handled:
+    blockname = block.'do'()
 
-    .local pmc exportns, tagns
+    # Methods get a & added to their name.
+    $I0 = isa block, ['Method']
+    unless $I0 goto method_done
+    blockname = concat '&', blockname
+  method_done:
+
+    # Create the EXPORT namespace if it doesn't already exist.
+    # Assume our caller is coming from the namespace where exporting
+    # is to take place.
+    .local pmc blockns, exportns
+    $P0 = getinterp
+    blockns = $P0['namespace';1]
     exportns = blockns.'make_namespace'('EXPORT')
-    tagns = exportns.'make_namespace'('DEFAULT')
+
+    # Multisubs/multimethods export as Perl6MultiSub -- keep a flag
+    # that we can use to test this later.
+    .local int ismulti
+    $P0 = block.'multi'()
+    ismulti = istrue $P0
+
+    .local pmc tags, tags_it
+    tags = split ' ', 'ALL DEFAULT'
+    tags_it = iter tags
+  tags_loop:
+    unless tags_it goto tags_done
+    .local string tagname
+    .local pmc tagns
+    tagname = shift tags_it
+    tagns = exportns.'make_namespace'(tagname)
+    if ismulti goto export_multi
+    # Non-multi, so just export the block directly.
     tagns[blockname] = block
-    tagns = exportns.'make_namespace'('ALL')
-    tagns[blockname] = block
+    goto tags_loop
+  export_multi:
+    # MultiSub candidate, so vivify a Perl6MultiSub in the export
+    # namespace and push this candidate onto it.
+    $P0 = vivify tagns, blockname, ['Perl6MultiSub']
+    push $P0, block
+    goto tags_loop
+  tags_done:
     }
 }

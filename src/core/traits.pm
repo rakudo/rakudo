1f1e2596bd78b3936b05c0eaa891ae1d7cbc5a9e
jnthn <jnthn@jnthn.net>
2009-12-05 17:43:18 +0000

    Allow my $x does Foo to work again.

--- src/core/traits.pm
+++ src/core/traits.pm
@@ -11,3 +11,7 @@ our multi trait_mod:<of>(ContainerDeclarand $cont, Mu \$type) {
 our multi trait_mod:<does>(Mu $target, Role $r) {
     $target.add_composable($target, $r);
 }
+
+our multi trait_mod:<does>(ContainerDeclarand $c, Role $r) {
+    $c.container does $r;
+}

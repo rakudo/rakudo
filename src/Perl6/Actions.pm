8a7d1c5ea27dcab467b99f4fdd7ff7b9ca99f008
Stephen Weeks <tene@allalone.org>
2009-12-15 20:05:07 -0700

    try {} should not catch control exceptions

--- src/Perl6/Actions.pm
+++ src/Perl6/Actions.pm
@@ -381,23 +381,28 @@ method statement_prefix:sym<gather>($/) {
 method statement_prefix:sym<try>($/) {
     my $block := $<blorst>.ast;
     $block.blocktype('immediate');
-    my $past := PAST::Op.new( :pasttype('try'), $block );
-
-    # On failure, capture the exception object into $!.
-    $past.push(PAST::Op.new(
-        :inline( '    .get_results (%r)',
-                 '    $P0 = new ["Perl6Exception"]',
-                 '    setattribute $P0, "$!exception", %r',
-                 '    store_lex "$!", $P0' )
-    ));
+    unless $block.handlers() {
+        $block.handlers([]);
+    }
+    $block.handlers().push(
+        PAST::Control.new(
+            :handle_types_except('CONTROL'),
+            PAST::Op.new(
+                    :inline( '    .get_results (%r)',
+                             '    $P0 = new ["Perl6Exception"]',
+                             '    setattribute $P0, "$!exception", %r',
+                             '    store_lex "$!", $P0' )
+                ),
+        )
+    );
 
     # Otherwise, put a failure into $!.
-    $past.push(PAST::Op.new( :pasttype('bind'),
+    $block.push(PAST::Op.new( :pasttype('bind'),
         PAST::Var.new( :name('$!'), :scope('lexical') ),
         PAST::Op.new( :pasttype('call'), :name('!FAIL') )
     ));
 
-    make $past;
+    make $block;
 }
 
 method blorst($/) {

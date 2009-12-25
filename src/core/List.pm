7729e653cce81e86f2dee2d2c48537323820047a
David Romano <unobe@cpan.org>
2009-12-25 14:44:31 -0800

    * Using map makes List.perl simpler! :-)
    modified:   src/core/List.pm

--- src/core/List.pm
+++ src/core/List.pm
@@ -1,13 +1,8 @@
 augment class List {
     method perl() {
-        if self.elems() == 0 { return Nil.WHAT };
-        # '(' ~ self.map({ $_.perl }).join(', ') ~ ')';
-        my $result = '(';
-        for self -> $x {
-            $result ~= ', ' if $result.chars > 1;
-            $result ~= (~$x.WHAT eq 'Array()' ?? gather { for 0 ... $x.end { take $x.[$_] } } !! $x).perl;
-        }
-        return $result ~ ')';
+        if !self.elems { return Nil.WHAT };
+        # XXX: $_.perl and .perl don't work here yet :-(
+        '(' ~ self.map({ $^a.perl }).join(', ') ~ ')';
     }
 }
 

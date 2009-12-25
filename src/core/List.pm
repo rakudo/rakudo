c9b87200f72e28ace471db4ecd235bb385e55059
David Romano <unobe@cpan.org>
2009-12-25 02:40:57 -0800

    * Allow for .perl to output nested arrays and empty arrays 	modified:   src/core/List.pm

--- src/core/List.pm
+++ src/core/List.pm
@@ -1,12 +1,13 @@
 augment class List {
     method perl() {
+        if self.elems() == 0 { return Nil.WHAT };
         # '(' ~ self.map({ $_.perl }).join(', ') ~ ')';
-        my $result = "(";
+        my $result = '(';
         for self -> $x {
-            $result ~= ", " if $result.chars > 1;
-            $result ~= $x.perl;
+            $result ~= ', ' if $result.chars > 1;
+            $result ~= (~$x.WHAT eq 'Array()' ?? gather { for 0 ... $x.end { take $x.[$_] } } !! $x).perl;
         }
-        return $result ~ ")";
+        return $result ~ ')';
     }
 }
 

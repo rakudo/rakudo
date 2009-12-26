8a8ab97a324d2a7a9c8e333f1f6b83e3253cbc0e
David Romano <unobe@cpan.org>
2009-12-25 16:44:43 -0800

    * Converted tab to spaces modified:   src/core/List.pm

--- src/core/List.pm
+++ src/core/List.pm
@@ -1,7 +1,7 @@
 augment class List {
     method perl() {
         if self.elems() == 0 { return Nil.WHAT };
-	# XXX: $_.perl and .perl don't work, but this does...
+        # XXX: $_.perl and .perl don't work, but this does...
         '(' ~ self.map({ $^a.perl }).join(', ') ~ ')';
     }
 }

9174831c0ff277b550789f05811512d3adb7375a
Solomon Foster <colomon@gmail.com>
2010-01-23 15:30:04 -0500

    Revert "Start throwing iterator example code into List.pm.  Compiles but not tested or used for anything." because it appears to break ng badly.
    This reverts commit 8aedd0c5eea27b37f7019c5117afc9897dc04e5a.

--- src/core/List.pm
+++ src/core/List.pm
@@ -18,37 +18,4 @@ augment class List {
     }
 }
 
-# Throw together an iterator interface, just so we have something to talk about
-
-role Iterator {
-    multi method get() {
-        Nil; # probably should use a special terminator instead
-    }
-
-    multi method list() {
-        self;
-    }
-}
-
-class ScalarIterator does Iterator {
-    has $.value;
-    has $.done;
-
-    multi method new($value) {
-        self.bless(*, :value($value),
-                      :done(Bool::False));
-    }
-
-    multi method get() {
-        if $.done {
-            Nil;
-        }
-        else {
-            $.done = Bool::True;
-            $.value;
-        }
-    }
-}
-
-
 # vim: ft=perl6

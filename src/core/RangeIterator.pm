bd353ed6e9789cac6c222b91e5fe9a352130e785
pmichaud <pmichaud@pobox.com>
2010-01-27 09:46:29 -0600

    Add RangeIterator draft code.

---
+++ src/core/RangeIterator.pm
@@ -0,0 +1,22 @@
+
+
+class RangeIterator {
+    has $!value;
+    has $!max;
+    has $!excludes_max;
+
+    multi method new(Range $r) {
+        $!value        = $r.min;
+        $!max          = $r.max;
+        $!excludes_max = $r.excludes_max;
+        self.get if $r.excludes_min;
+    }
+
+    method get() {
+        my $return = $!value;
+        return IterDone if $return after $!max 
+                           || $!excludes_max && !($return before $!max);
+        $value .= succ;
+        $return;
+    }
+}

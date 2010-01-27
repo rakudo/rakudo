5c20ac2eb3c270f8a41725f49e119268973d9632
Solomon Foster <colomon@gmail.com>
2010-01-27 14:25:29 -0500

    Get RangeIterator compiling, and make RangeIterator.new call bless.

--- src/core/RangeIterator.pm
+++ src/core/RangeIterator.pm
@@ -6,17 +6,18 @@ class RangeIterator {
     has $!excludes_max;
 
     multi method new(Range $r) {
-        $!value        = $r.min;
-        $!max          = $r.max;
-        $!excludes_max = $r.excludes_max;
-        self.get if $r.excludes_min;
+        self.bless(*, :value($r.excludes_min ?? $r.min.succ !! $r.min),
+                      :max($r.max),
+                      :excludes_max($r.excludes_max));
     }
-
+    
     method get() {
-        my $return = $!value;
-        return IterDone if $return after $!max 
-                           || $!excludes_max && !($return before $!max);
-        $value .= succ;
-        $return;
+        my $current = $!value;
+        if $current after $!max
+           || $!excludes_max && !($current before $!max) {
+            return IterDone;
+        }
+        $!value .= succ;
+        $current;
     }
 }

caea56eed6fc6e961af25d121738242e21828e1d
pmichaud <pmichaud@pobox.com>
2010-01-29 23:52:10 -0600

    Updated versions of Array.push, .pop, .shift, .unshift.

--- src/core/Array.pm
+++ src/core/Array.pm
@@ -6,40 +6,27 @@ augment class Array {
  
 
     our method push(*@values) is export {
-        my @a = @values;
-        Q:PIR {
-            $P0 = find_lex '@a'
-            $P0 = getattribute $P0, '$!values'
-            $P1 = find_lex 'self'
-            $P2 = $P1.'!generate'(0)
-            $I0 = elements $P2
-            splice $P2, $P0, $I0, 0
-        };
+        self!fill;
+        pir::splice__0PPii( @!items, [@values].iterator.eager, 
+                            pir::elements(@!items), 0);
         self;
     }
 
     our method unshift(*@values) is export {
-        my @a = @values;
-        Q:PIR {
-            $P0 = find_lex '@a'
-            $P0 = getattribute $P0, '$!values'
-            $P1 = find_lex 'self'
-            $P2 = $P1.'!generate'(0)
-            splice $P2, $P0, 0, 0
-            $P0 = box 0
-            setattribute $P1, '$!gen', $P0
-        };
+        # Reify self, then splice @values at the beginning.
+        # We really should make this lazy, however.
+        pir::splice__0PPii( self!fill, [@values].iterator.eager, 0, 0);
         self;
     }
 
     our multi method pop() {
-        self.elems > 0 ?? pir::pop__PP($!values)
-                       !! fail('Undefined value popped from empty array');
+        self!fill ?? pir::pop(@!items)
+                  !! fail('Undefined value popped from empty array');
     }
 
     our multi method shift() {
-        self.elems > 0 ?? pir::shift__PP($!values)
-                       !! fail('Undefined value shifted from empty array');
+        self!fill(1) ?? pir::shift(@!items)
+                     !! fail('Undefined value shifted from empty array');
     }
 }
 

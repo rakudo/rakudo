b0b94b7f784552fb3542bc980712f0950a26043d
Solomon Foster <colomon@gmail.com>
2010-02-02 22:36:19 -0500

    Primitive postcircumfix:<[ ]> for Iterator.

--- src/core/Iterator.pm
+++ src/core/Iterator.pm
@@ -16,4 +16,12 @@ augment class Iterator {
     multi method Str() {
         self.Seq.Str;
     }
+
+    # TimToady suggests this should be on Cool,
+    # but it makes more sense to me here.  Also
+    # should support slices, but I don't know 
+    # if that's implemented in ng1 yet.
+    multi method postcircumfix:<[ ]>($a) {
+        self.Seq[$a];
+    }
 }

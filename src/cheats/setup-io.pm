3a94b963ea45e66343756d7b2fee13031cfaae34
Jonathan Worthington <jnthn@jnthn.net>
2010-02-10 23:09:39 +0100

    Implement $*OS, $*OSVER and a hugely cheating and lying $*PID.

--- src/cheats/setup-io.pm
+++ src/cheats/setup-io.pm
@@ -11,5 +11,22 @@ INIT {
         set_hll_global ['PROCESS'], "$OUT", $P0
         $P0 = find_lex '$ERR'
         set_hll_global ['PROCESS'], '$ERR', $P0
+
+        ##  Parrot doesn't give us the PID for now. Well, this file *is*
+        ##  in the cheats directory... :-)
+        $P0 = box 0
+        set_hll_global ['PROCESS'], '$PID', $P0
+
+        ##  set up $*OS, $*OSVER $*EXECUTABLE_NAME
+        .include 'sysinfo.pasm'
+        .local string info
+        info = sysinfo .SYSINFO_PARROT_OS
+        $P0 = new ['Str']
+        $P0 = info
+        set_hll_global ['PROCESS'], '$OS', $P0
+        info = sysinfo .SYSINFO_PARROT_OS_VERSION
+        $P0 = new ['Str']
+        $P0 = info
+        set_hll_global ['PROCESS'], '$OSVER', $P0
     }
 }

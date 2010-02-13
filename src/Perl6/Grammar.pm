b14892ba93b2d246de80fb8db8b87f8bb58c0871
pmichaud <pmichaud@pobox.com>
2010-02-12 20:21:32 -0600

    Add NYI messages for := and ::= bindings.

--- src/Perl6/Grammar.pm
+++ src/Perl6/Grammar.pm
@@ -1097,8 +1097,15 @@ token infix:sym<?? !!> {
     <O('%conditional, :reducecheck<ternary>, :pasttype<if>')> 
 }
 
-#token infix:sym<:=>   { <sym>  <O('%assignment, :pasttype<bind>')> }
-#token infix:sym<::=>  { <sym>  <O('%assignment, :pasttype<bind>')> }
+token infix:sym<:=>   { 
+    <sym>  <O('%item_assignment')> 
+    <.panic: ":= binding not yet implemented">
+}
+
+token infix:sym<::=>   { 
+    <sym>  <O('%item_assignment')> 
+    <.panic: "::= binding not yet implemented">
+}
 
 token infix:sym<.=> { <sym> <O('%item_assignment, :nextterm<dottyopish>')> }
 

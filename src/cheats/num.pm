6090323be886c31ac85fb12bb61c76e66f005293
Jonathan Worthington <jnthn@jnthn.net>
2010-02-13 14:58:47 +0100

    Make sure NaN, Inf etc are Perl 6 Nums, not Parrot Floats, so we dispatch to the right sin, etc.

--- src/cheats/num.pm
+++ src/cheats/num.pm
@@ -6,7 +6,20 @@ augment class Num {
 
 INIT {
     pir::set_hll_global__vSP('NaN', pir::set__NS('NaN'));
-    pir::set_hll_global__vSP('Inf', pir::set__NS('Inf'));
-    pir::set_hll_global__vSP('+Inf', pir::set__NS('Inf'));
-    pir::set_hll_global__vSP('-Inf', -pir::set__NS('Inf'));
+        pir::set_hll_global__vSP('NaN', Q:PIR {
+        %r = new ['Num']
+        $N0 = 'NaN'
+        assign %r, $N0
+    });
+    pir::set_hll_global__vSP('Inf', Q:PIR {
+        %r = new ['Num']
+        $N0 = 'Inf'
+        assign %r, $N0
+    });
+    pir::set_hll_global__vSP('+Inf', Q:PIR {
+        %r = new ['Num']
+        $N0 = 'Inf'
+        assign %r, $N0
+    });
+    pir::set_hll_global__vSP('-Inf', -pir::get_hll_global__PS('Inf'));
 }

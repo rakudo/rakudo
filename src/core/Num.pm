40e481e92f874fc0f5c6a7130bc287186f4d3c41
Solomon Foster <colomon@gmail.com>
2010-02-16 14:13:58 -0500

    Fix the definition of cosech.

--- src/core/Num.pm
+++ src/core/Num.pm
@@ -135,7 +135,7 @@ augment class Num {
     }
 
     multi method cosech($base = 'radians') {
-        1 / self.cosh($base);
+        1 / self.sinh($base);
     }
 
     multi method acosech($base = 'radians') {

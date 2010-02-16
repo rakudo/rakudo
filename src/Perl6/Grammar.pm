74bddc2bf9757beda90c2be4680512cab0894aab
pmichaud <pmichaud@pobox.com>
2010-02-16 14:21:57 -0600

    Correct misspelled supersede (bkeeler++).

--- src/Perl6/Grammar.pm
+++ src/Perl6/Grammar.pm
@@ -556,8 +556,8 @@ token scope_declarator:sym<my>        { <sym> <scoped('my')> }
 token scope_declarator:sym<our>       { <sym> <scoped('our')> }
 token scope_declarator:sym<has>       { <sym> <scoped('has')> }
 token scope_declarator:sym<augment>   { <sym> <scoped('augment')> }
-token scope_declarator:sym<supercede> { 
-    <sym> <.panic: '"supercede" not yet implemented'>
+token scope_declarator:sym<supersede> { 
+    <sym> <.panic: '"supersede" not yet implemented'>
 }
 
 rule scoped($*SCOPE) {

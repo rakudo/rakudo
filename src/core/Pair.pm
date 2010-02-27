fe8c56ef6aee7270d463b77444b3678fea4fb322
Jonathan Worthington <jnthn@jnthn.net>
2010-02-27 14:57:19 +0100

    Make Pair.perl less verbose.

--- src/core/Pair.pm
+++ src/core/Pair.pm
@@ -1,6 +1,5 @@
 augment class Pair {
     multi method perl() {
-        # $.key.perl ~ ' => ' ~ $.value.perl;
-        "Pair.new(:key({$.key.perl}), :value({$.value.perl}))";
+        $.key.perl ~ ' => ' ~ $.value.perl;
     }
 }

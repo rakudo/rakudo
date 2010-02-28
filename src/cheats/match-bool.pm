67f51f786c3f8440fccba4c76fb0201b264b1ee9
Moritz Lenz <moritz@faui2k3.org>
2010-02-28 10:12:26 +0100

    cheat in Regex::Match.defined

--- src/cheats/match-bool.pm
+++ src/cheats/match-bool.pm
@@ -2,4 +2,7 @@ INIT {
     Regex::Match.^add_method('Bool', method () {
         self.to >= self.from
     });
+    Regex::Match.^add_method('defined', method () {
+        self.to >= self.from
+    });
 }

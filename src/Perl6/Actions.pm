a5720bba924666f404dd528164bbfb286c6a3787
Solomon Foster <colomon@gmail.com>
2010-03-19 17:53:01 -0400

    Actually create Xop as a function instead of just using it as syntactic sugar for .crosswith.

--- src/Perl6/Actions.pm
+++ src/Perl6/Actions.pm
@@ -1939,9 +1939,21 @@ method infixish($/) {
         }
         else {
             if $metaop eq 'X' {
-                make PAST::Op.new( :name("&crosswith"), :pasttype('call'),
-                                   PAST::Op.new( :pirop('find_sub_not_null__Ps'),
-                                                 $base_opsub ) );
+                unless %*METAOPGEN{$opsub} {
+                    @BLOCK[0].loadinit.push(
+                        PAST::Op.new( :pasttype('bind'),
+                                      PAST::Var.new( :name($opsub), :scope('package') ),
+                                      PAST::Op.new( :pasttype('callmethod'),
+                                                    :name('assuming'),
+                                                    PAST::Op.new( :pirop('find_sub_not_null__Ps'),
+                                                                  '&crosswith' ),
+                                                    PAST::Op.new( :pirop('find_sub_not_null__Ps'),
+                                                                   $base_opsub ) ) ) );
+
+                    %*METAOPGEN{$opsub} := 1;
+                }
+
+                make PAST::Op.new( :name($opsub), :pasttype('call') );
             }
             if $metaop eq 'Z' {
                 make PAST::Op.new( :name("&zipwith"), :pasttype('call'),

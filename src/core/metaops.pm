1c5de1cfb574e234ebb388647d64958c3788df6d
Solomon Foster <colomon@gmail.com>
2010-03-20 04:13:54 -0400

    Create helper crosswith and zipwith functions that call .list on their arguments, so that single scalar arguments can be promoted to Iterables and work with the normal crosswith / zipwith functions.

--- src/core/metaops.pm
+++ src/core/metaops.pm
@@ -9,6 +9,10 @@ our multi sub zipwith(&op, Iterable $a-iterable, Iterable $b-iterable) {
     }
 }
 
+our multi sub zipwith(&op, $a, $b) {
+    zipwith(&op, $a.list, $b.list);
+}
+
 our multi sub crosswith(&op, Iterable $a-iterable, Iterable $b-iterable) {
     my $ai = $a-iterable.iterator;
     my @b = $b-iterable.Seq;
@@ -19,6 +23,10 @@ our multi sub crosswith(&op, Iterable $a-iterable, Iterable $b-iterable) {
             take &op($a, $b);
         }
     }
+}
+
+our multi sub crosswith(&op, $a, $b) {
+    crosswith(&op, $a.list, $b.list);
 }
 
 our multi reduce(&op, $list) {

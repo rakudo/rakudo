d952fc4b1d052c0b2f6cce2ae2f26ab813776a61
quester <quester.pm@gmail.com>
2010-04-20 00:20:12 -1000

    Fix Seq.perl to not say Seq.new(). Interim fix for Iterator.perl infinite loops.

--- src/core/Iterator.pm
+++ src/core/Iterator.pm
@@ -1,6 +1,15 @@
 augment class Iterator {
-    multi method perl() {
-        $.eager.perl;
+    multi method perl(:$limit = 40) {
+        # Doing this right is probably beyond mortal dwimmery,
+        # and even in simple cases it needs the ability to
+        # serialize code to have any hope of producing
+        # output like (1, 1, * + * ... *)
+        # rather than (1, 1, 2, 3, 5, {???} ... ???).
+        # Try to do something useful for debugging, for now.
+        my @a = self.batch($limit);
+        my $and_more = '';
+        $and_more = ', {???} ... ???' if @a == $limit;
+        '(' ~ @a.map({ $^a.perl }).join(', ') ~ $and_more ~ ')';
     }
 
     multi method elems() {

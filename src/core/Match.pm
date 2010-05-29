5c62b61ee886172070a5eb7ee2fbb61f4bddd478
Moritz Lenz <moritz@faui2k3.org>
2010-05-29 23:24:50 +0200

    implement Match.perl; fix Match.ast to return Any if not set (not Undef)

--- src/core/Match.pm
+++ src/core/Match.pm
@@ -1,4 +1,10 @@
 class Match is Regex::Match is Cool does Associative {
+
+    method ast() {
+        my $x = self.Regex::Match::ast;
+        pir::isa__IPs($x, 'Undef') ?? Any !! $x;
+    }
+
     multi method postcircumfix:<{ }>($key) {
         Q:PIR {
             $P0 = find_lex 'self'
@@ -70,6 +76,63 @@ class Match is Regex::Match is Cool does Associative {
             take ('~' => self.substr($prev - $.from)) if $prev < $.to;
         }
     }
+
+    multi method perl() {
+        self!_perl(0);
+    }
+
+    method !_perl(Int $indent) {
+        return [~] gather {
+            my $sp = ' ' x $indent;
+            take $sp;
+            take "Match.new(\n";
+            if $indent == 0 {
+                take " # WARNING: this is not working perl code\n";
+                take " # and for debugging purposes only\n";
+            }
+            take "$sp ast  => {$.ast.perl},\n";
+            take "$sp Str  => {$.Str.perl},\n";
+            take "$sp from => $.from,\n";
+            take "$sp orig => $.orig.perl(),\n";
+            take "$sp to   => $.to,\n";
+            if @(self) {
+                take "$sp positional => [\n";
+                # work around RT #64952
+                for ^self.list {
+                    self!_perl_quant(self.[$_], $indent);
+                    take ",\n";
+                }
+                take "$sp  ],\n";
+            }
+            if %(self) {
+                take "$sp named => \{\n";
+                for %(self).pairs {
+                    take "$sp  {.key} => ";
+                    self!_perl_quant(.value, $indent);
+                    take ",\n";
+                }
+                take "$sp \},\n";
+            }
+            take "$sp)";
+        }
+    }
+
+    method !_perl_quant($obj, $indent) {
+        my $sp = ' ' x $indent;
+        if !defined($obj) {
+            # do nothing; we only get here when something's not quite right...
+        } elsif $obj ~~ Match {
+            take $obj!_perl($indent + 3);
+        } else {
+            take "[\n";
+            for $obj.list {
+                take $_!_perl($indent + 4);
+                take ",\n";
+            }
+            take "$sp  ]";
+        }
+    }
+
 }
 
 # vim: ft=perl6

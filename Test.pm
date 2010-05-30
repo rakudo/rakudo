749fe91a9abf2a787a05dbe9b707c29e73a91960
Moritz Lenz <moritz@faui2k3.org>
2010-05-30 17:46:59 +0200

    [Test.pm] simplify eval_exception
    The days when eval() would leak exceptions through are luckily long gone, no
    need for nesting try {  eval() } anymore.

--- Test.pm
+++ Test.pm
@@ -1,6 +1,5 @@
 module Test;
-# Copyright (C) 2007, The Perl Foundation.
-# $Id: Test.pm 34904 2009-01-03 23:24:38Z masak $
+# Copyright (C) 2007 - 2010 The Perl Foundation.
 
 ## This is a temporary Test.pm to get us started until we get pugs's Test.pm
 ## working. It's shamelessly stolen & adapted from MiniPerl6 in the pugs repo.
@@ -207,9 +206,8 @@ sub _is_deeply(Mu $got, Mu $expected) {
 ## 'private' subs
 
 sub eval_exception($code) {
-    my $eval_exception;
-    try { eval ($code); $eval_exception = $! }
-    $eval_exception // $!;
+    eval ($code);
+    $!;
 }
 
 sub proclaim($cond, $desc) {

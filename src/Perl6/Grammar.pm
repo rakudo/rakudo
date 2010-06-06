rakudo/rakudo

commit 6ba4b221a993e8dc3a4cd378f555b29488f44d28

carp about 'package Foo;'

src/Perl6/Grammar.pm
@@ -815,6 +815,11 @@ rule package_def {
     [
     || ';'
         <.finishpad>
+        {
+            if $*PKGDECL eq 'package' {
+                $/.CURSOR.panic('This appears to be Perl 5 code. If you intended it to be Perl 6 code, please use a package declaration that is not of the form "package Foo;".');
+            }
+        }
         <statementlist>
     || <?[{]> <blockoid>
     || <.panic: 'Malformed package declaration'>

commit a3de5241817078ebdcd10e4627d932247de20454

Fix 'package Foo;' error so that it suggest a Perl 6 alternative
The error message for 'package Foo' introduced in 6ba4b221a993e8dc3a4c
didn't suggest what syntax to use instead. Explain that you should use
package blocks, or the module declaration syntax.

Signed-off-by: Moritz Lenz <moritz@faui2k3.org>

src/Perl6/Grammar.pm
@@ -817,7 +817,7 @@ rule package_def {
         <.finishpad>
         {
             if $*PKGDECL eq 'package' {
-                $/.CURSOR.panic('This appears to be Perl 5 code. If you intended it to be Perl 6 code, please use a package declaration that is not of the form "package Foo;".');
+                $/.CURSOR.panic('This appears to be Perl 5 code. If you intended it to be Perl 6 code, please use a Perl 6 style package block like "package Foo { ... }", or "module Foo; ...".');
             }
         }
         <statementlist>
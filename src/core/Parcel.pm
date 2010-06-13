82255b8b71f92f51fd105874f51c4fed903bc5e6
pmichaud <pmichaud@pobox.com>
2010-06-13 01:26:19 -0500

    Add Parcel.Str .

--- src/core/Parcel.pm
+++ src/core/Parcel.pm
@@ -1,4 +1,6 @@
 augment class Parcel does Positional {
+    method Str() { self.flat.Str }
+
     method elems() { self.flat.elems }
 
     multi method postcircumfix:<[ ]>($index) { self.flat.[$index] }

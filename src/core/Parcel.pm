18674c5b34e87712b7c6e78f530f12be87a74850
pmichaud <pmichaud@pobox.com>
2010-06-14 21:09:58 -0500

    Restore Parcel.ACCEPTS, although I'm not sure how/why it's defined like this.

--- src/core/Parcel.pm
+++ src/core/Parcel.pm
@@ -5,4 +5,10 @@ augment class Parcel does Positional {
 
     multi method postcircumfix:<[ ]>($index) { self.flat.[$index] }
     multi method postcircumfix:<[ ]>(@index) { self.flat.[@index] }
+
+    multi method ACCEPTS($x) {
+        self.elems == 0
+            ?? $x.notdef || ($x ~~ Positional && $x == 0)
+            !! self.Seq.ACCEPTS($x)
+    }
 }

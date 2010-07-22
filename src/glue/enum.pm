5fe193902ce23a683e10ebb708beaead35218d23
Carl Masak <cmasak@gmail.com>
2010-07-23 01:46:17 +0200

    [src/glue/enum.pm] return the right things
    enum Foo <a b c>;
    
    Previously, requesting Foo yielded an EnumMap, whereas it now yields an
    object of some kind whose .enums method returns an EnumMap. Similarly,
    requesting b yielded an Int, whereas now it returns an object that responds
    to a number of methods, as per S12.

--- src/glue/enum.pm
+++ src/glue/enum.pm
@@ -4,11 +4,27 @@ our sub SETUP_NAMED_ENUM($name, $values) {
     # For now, just install EnumMap under the main name.
     my @full_ns = Perl6::Grammar::parse_name($name);
     my ($shortname, @base_ns) = @full_ns;
-    pir::set_hll_global__vPSP(@base_ns, $shortname, $values);
-    
+    my $enumeration-object = (class {
+        method WHAT { $enumeration-object }
+        method enums { $values }
+        method Str { $name }
+    }).new;
+    pir::set_hll_global__vPSP(@base_ns, $shortname, $enumeration-object);
+
     for $values.kv -> $key, $value {
-        pir::set_hll_global__vPSP(@full_ns, $key, $value);
-        pir::set_hll_global__vPSP(@base_ns, $key, $value);
+        my $enum-object = $value but role {
+            method WHAT { $enumeration-object }
+            method perl { $name ~ '::' ~ $key }
+            method Str { $name ~ '::' ~ $key }
+            method Stringy { $key }
+            method key { $key }
+            method value { $value }
+            method pair { $key => $value }
+            method kv { $key, $value }
+            method defined { True }
+        };
+        pir::set_hll_global__vPSP(@full_ns, $key, $enum-object);
+        pir::set_hll_global__vPSP(@base_ns, $key, $enum-object);
     }
 }
 

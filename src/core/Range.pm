176069306b8a0992761ef4c41c14f86b53b8db78
pmichaud <pmichaud@pobox.com>
2010-08-07 01:00:08 -0500

    Some initial Range refactors.

--- src/core/Range.pm
+++ src/core/Range.pm
@@ -4,159 +4,57 @@ class Range is Iterable does Positional {
     has $.max;
     has $.excludes_max = Bool::False;
 
-    multi method new($min,
-                     $max,
-                     Bool :$excludes_min = Bool::False,
+    multi method new(::T $min, T $max, 
+                     Bool :$excludes_min = Bool::False, 
                      Bool :$excludes_max = Bool::False) {
-        self.bless(*, :min($min ~~ Whatever ?? -Inf !! $min),
-                      :max($max ~~ Whatever ?? Inf !! $max),
-                      :excludes_min($excludes_min),
-                      :excludes_max($excludes_max));
+        self.bless(*, :$min, :$max, :$excludes_min, :$excludes_max);
     }
 
-    our Bool multi method ACCEPTS(Range $topic) {
-        ?(($.min == $topic.min)
-          && ($.max == $topic.max)
-          && ($.excludes_min == $topic.excludes_min)
-          && ($.excludes_max == $topic.excludes_min));
+    multi method new(Real $min, $max,
+                     Bool :$excludes_min = Bool::False, 
+                     Bool :$excludes_max = Bool::False) {
+        self.bless(*, :$min, :max($max.Numeric), :$excludes_min, :$excludes_max);
     }
 
-    my Bool multi method !min_test($topic) {
-        $.min == -Inf || $.min before $topic || (!$.excludes_min && !($.min after $topic));
+    multi method new($min, Real $max,
+                     Bool :$excludes_min = Bool::False, 
+                     Bool :$excludes_max = Bool::False) {
+        self.bless(*, :min($min.Numeric), :$max, :$excludes_min, :$excludes_max);
     }
 
-    my Bool multi method !max_test($topic) {
-        $.max == Inf || $topic before $.max || (!$.excludes_max && !($topic after $.max));
+    multi method new(Whatever $min, Real $max,
+                     Bool :$excludes_min = Bool::False, 
+                     Bool :$excludes_max = Bool::False) {
+        self.new(-Inf, $max, :$excludes_min, :$excludes_max);
+    }
+
+    multi method new(Real $min, Whatever $max,
+                     Bool :$excludes_min = Bool::False, 
+                     Bool :$excludes_max = Bool::False) {
+        self.new($min, +Inf, :$excludes_min, :$excludes_max);
+    }
+
+    multi method iterator() {
+        RangeIter.new(:value($!excludes_min ?? $!min.succ !! $!min), 
+                      :$!max, :$!excludes_max);
+    }
+
+    our Str multi method perl() {
+        ( $.min.perl, 
+          ('^' if $.excludes_min), 
+          '..', 
+          ('^' if $.excludes_max), 
+          $.max.perl
+        ).join('');
     }
 
     our Bool multi method ACCEPTS($topic) {
         ?(self!min_test($topic) && self!max_test($topic))
     }
 
-    multi method bounds() {
-        ($.min, $.max)
-    }
-
-    multi method from() { $.min; }
-    multi method to() { $.max; }
-
-    # Beautiful implementation which does not work yet in ng
-    # our Str multi method perl() {
-    #     [~]
-    #         $.min.perl,
-    #         ("^" if $.excludes_min),
-    #         "..",
-    #         ("^" if $.excludes_max),
-    #         $.max.perl;
-    # }
-
-    our Str multi method perl() {
-        my $min = $.min ~~ ::Whatever ?? "*" !! $.min.perl;
-        my $emin = $.excludes_min ?? "^" !! "";
-        my $max = $.max ~~ ::Whatever ?? "*" !! $.max.perl;
-        my $emax = $.excludes_max ?? "^" !! "";
-        $min ~ $emin ~ ".." ~ $emax ~ $max;
-    }
-
-    multi method fmt($format = '%s', $separator = ' ') {
-        self.map({ .fmt($format)}).join($separator);
-    }
-
-    multi method postcircumfix:<[ ]>(Int $index) { self.Seq[$index] }
-    multi method postcircumfix:<[ ]>(@slice)     { self.Seq[@slice] }
-
-    class InfiniteIntRangeIter is Iterator {
-        has $!value;
-        has $!nextIter;
-
-        method infinite() { True; }
-
-        method reify() {
-            unless $!nextIter.defined {
-                $!nextIter = InfiniteIntRangeIter.new( :value($!value + 8) );
-            }
-            $!value, $!value + 1, $!value + 2, $!value + 3,
-            $!value + 4, $!value + 5, $!value + 6, $!value + 7, $!nextIter;
-        }
-    }
-
-    class FiniteIntRangeQuadIter is Iterator {
-        has $!value;
-        has $!max;
-        has $!nextIter;
-
-        method infinite() { False }
-
-        method reify() {
-            return ($!value,) if $!value ~~ EMPTY;
-            unless $!nextIter.defined || $!nextIter ~~ EMPTY {
-                if $!value != $!max {
-                    my $s = $!value + 4;
-                    $!nextIter = $s < $!max
-                                    ?? FiniteIntRangeQuadIter.new( :value($s),
-                                                                   :max($!max) )
-                                    !! EMPTY;
-                } else {
-                    $!nextIter = EMPTY;
-                }
-            }
-            $!value, $!value + 1, $!value + 2, $!value + 3, $!nextIter;
-        }
-    }
-
-    class ConsIter is Iterator {
-        has @!values;
-        has $!nextIter;
-
-        method infinite() { $!nextIter ~~ EMPTY ?? False !! $!nextIter.infinite; }
-
-        method reify() {
-            &infix:<,>(|@!values, $!nextIter);
-        }
-    }
-
-    our method iterator() {
-        if $.min ~~ Int && $.max == Inf {
-                return InfiniteIntRangeIter.new( :value($!excludes_min ?? $.min + 1 !! $.min) );
-        }
-
-        my $start = $.min;
-        $start .= succ if $.excludes_min;
-
-        if $.min ~~ Int && $.max ~~ Int {
-            my $end = $.max;
-            $end .= pred if $.excludes_max;
-
-            if $start > $end {
-                return RangeIter.new( :value( EMPTY ),
-                                      :max($.max),
-                                      :excludes_max($.excludes_max));
-            }
-
-            my $mod = ($end - $start + 1) % 4;
-            if $mod == 0 {
-                FiniteIntRangeQuadIter.new(:value($start), :max($end));
-            } else {
-                my @values;
-                my $i;
-                loop ($i = 0; $i < $mod; $i++) {
-                     @values.push: $start + $i;
-                }
-
-                my $next-iter = $start + $mod < $end
-                    ?? FiniteIntRangeQuadIter.new(:value($start + $mod),
-                                                  :max($end))
-                    !! EMPTY;
-                ConsIter.new(:values(@values),
-                             :nextIter($next-iter));
-            }
-        } else {
-            RangeIter.new( :value( self!max_test($start) ?? $start !! EMPTY ),
-                           :max($.max),
-                           :excludes_max($.excludes_max));
-        }
-    }
+    multi method postcircumfix:<[ ]>(\$parcel) { self.Seq[$parcel]; }
 }
+
 
 our multi sub infix:<..>($min, $max) {
     Range.new($min, $max);
--- src/core/RangeIter.pm
+++ src/core/RangeIter.pm
@@ -2,26 +2,43 @@ class RangeIter is Iterator {
     has $!value;
     has $!max;
     has $!excludes_max;
-    has $!nextIter;
-
-    method infinite() { $!max ~~ Inf }
+    has $!reify;
 
     method reify() {
-        return ($!value,) if $!value ~~ EMPTY;
-        unless $!nextIter.defined || $!nextIter ~~ EMPTY {
-            if $!value cmp $!max != 0 {
-                my $s = $!value.succ;
-                $!nextIter =
-                    $!max == Inf || $s before $!max || (! $!excludes_max && !($s after $!max))
-                        ?? RangeIter.new( :value($s),
-                                        :max($!max),
-                                        :excludes_max($!excludes_max) )
-                        !! EMPTY;
-            } else {
-                $!nextIter = EMPTY;
-            }
+        Q:PIR {
+            .local pmc self, reify, value, max, excl_max
+            self = find_lex 'self'
+            reify = getattribute self, '$!reify'
+            if null reify goto reify_start
+            $I0 = defined reify
+            if $I0 goto reify_done
+          reify_start:
+            reify = new ['Parcel']
+            setattribute self, '$!reify', reify
+            value    = getattribute self, '$!value'
+            max      = getattribute self, '$!max'
+            excl_max = getattribute self, '$!excludes_max'
+            .local int count
+            count = 8
+          reify_loop:
+            $P0 = '&infix:<after>'(value, max)
+            if $P0 goto reify_done
+            unless excl_max goto reify_value
+            $P0 = '&infix:<eqv>'(value, max)
+            if $P0 goto reify_done
+          reify_value:
+            push reify, value
+            value = value.'succ'()
+            dec count
+            if count > 0 goto reify_loop
+            .local pmc nextiter
+            nextiter = new ['RangeIter']
+            setattribute nextiter, '$!value', value
+            setattribute nextiter, '$!max', max
+            setattribute nextiter, '$!excludes_max', excl_max
+            push reify, nextiter
+          reify_done:
+            %r = reify
         }
-        $!value, $!nextIter;
     }
 }
-

9d7428fe4f498c1a86efe37eb5f9f4f3053b9092
Moritz Lenz <moritz@faui2k3.org>
2010-08-12 11:20:18 +0200

    fix interaction between :g and :p regex modifiers in Cool.match
    At least there are some tests that expect them to work together this way,
    similar to m/\G$regex/g in p5

--- src/core/Cool-str.pm
+++ src/core/Cool-str.pm
@@ -303,8 +303,13 @@ augment class Cool {
                 } else {
                     if $m.to == $m.from {
                         %opts<c> = $m.to + 1;
+                        if $p.defined {
+                            warn "multiple matches with :p terminated by zero-width match\n";
+                            last;
+                        }
                     } else {
                         %opts<c> = $m.to;
+                        %opts<p> = $m.to if $p.defined;
                     }
                 }
 

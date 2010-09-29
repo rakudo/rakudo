100b868e436c785dba0fe1ba5678db70340c374a
Moritz Lenz <moritz@faui2k3.org>
2010-09-29 22:48:08 +0200

    implement Attribute.{get,set}_value

--- src/core/Attribute.pm
+++ src/core/Attribute.pm
@@ -3,4 +3,11 @@ augment class Attribute {
         $!has_accessor ?? True !! False
     }
     method Str() { self.name }
+
+    method get_value(Mu $obj) {
+        pir::getattribute__PPS($obj, self.name);
+    }
+    method set_value(Mu $obj, Mu $new_val) {
+        pir::setattribute__vPSP($obj,  self.name, $new_val);
+    }
 }

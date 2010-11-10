rakudo/rakudo

commit 5ce8fcfe541b6eb2426ea64247bf01998b0d5208

Make sure Parcel.item returns an itemized Seq.  Addresses RT #78284, but it also seems to expose a bug in either .trans or S05-transliteration/trans.t .

src/builtins/Parcel.pir
@@ -42,6 +42,7 @@ A Parcel in item context becomes a Seq.
     seq = get_hll_global 'Seq'
     seq = seq.'new'(self)
     seq.'eager'()
+    seq = seq.'item'()
     .return (seq)
 .end
 

commit 037775ebc6a7d6f2cf5835a4bd00e9099b3dcf22

Tweaked Parcel.item.
This fixes the regression in trans.t introduced by 5ce8fcf.

src/builtins/Parcel.pir
@@ -38,10 +38,9 @@ A Parcel in item context becomes a Seq.
 =cut
 
 .sub 'item' :method
-    .local pmc seq, flat, rest
+    .local pmc seq
     seq = get_hll_global 'Seq'
-    seq = seq.'new'(self)
-    seq.'eager'()
+    seq = seq.'new_from_RPA'(self)
     seq = seq.'item'()
     .return (seq)
 .end

src/builtins/Seq.pir
@@ -37,6 +37,23 @@ Seqs are Lists of (immutable) values.
     .return (list)
 .end
 
+=item new_from_RPA
+
+Creates a new Seq from a ResizablePMCArray.
+
+=cut
+
+.namespace ['Seq']
+.sub 'new_from_RPA' :method
+    .param pmc rpa
+    .local pmc seq
+    seq = self.'new'()
+    setattribute seq, '@!rest', rpa
+    $P0 = get_hll_global 'True'
+    setattribute seq, '$!flat', $P0
+    .return (seq)
+.end
+
 =back
 
 =head2 Private methods
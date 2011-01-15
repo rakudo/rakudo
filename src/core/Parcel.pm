1014912a8a8cd64ba951e1dbcd809243c41ca355
Jonathan Worthington <jnthn@jnthn.net>
2011-01-15 23:07:26 +0100

    Add a workaround from bacek++ to avoid hitting things from the NQP Setting that get added to ResizablePMCArray, and also notes on the way that we'll end up ridding ourselves of this problem in the future.

--- src/core/Parcel.pm
+++ src/core/Parcel.pm
@@ -3,8 +3,15 @@ augment class Parcel does Positional {
 
     method elems() { self.flat.elems }
 
-    # Need this method here to avoid ResizablePMCArray.sort from Parrot.
+    # XXX Need these methods here to avoid ResizablePMCArray.sort and
+    # ResizablePMCArray.join leaking in from Parrot (we inherit
+    # multiply from RPA and Any, and RPA has to come first, so we
+    # hit the "wrong" sort; this will go away in the 6model integration
+    # refactors, when we'll has-a RPA in Parcel or some such...we sure
+    # won't inherit from a PMC as that likely won't be implemented in
+    # 6model anyway).
     method sort(&by = &infix:<cmp>) { self.list.sort(&by) }
+    method join(Str $joiner = '') { self.list.join($joiner) }
 
     method at_pos($pos) { self.flat.[$pos] }
 

0bcaa3b7d0cdb23000c2422bf0525994d36bfd26
NXTangl <NXTangl@gmail.com>
2011-02-16 16:00:24 -0800

    Added a bit to Bag.
    Again, this implementation is just a place to start.

--- src/core/Bag.pm
+++ src/core/Bag.pm
@@ -1,8 +1,11 @@
 use v6;
 # Dummy implementation of Bag.
 # Not really done, or functional; just a place to start.
-class Bag does Associative {
-	has %!storage of Int;
+class Bag does Associative is Cool {
+	has %!storage of Int handles <elems keys values kv>
+        method !weight() {
+               %!storage.kv.map: {$^k xx $^v}
+        }
 	sub bag(*@elems) {
 		my %keys of Int;
 		for @elems -> $key{
@@ -10,11 +13,17 @@ class Bag does Associative {
 		}
 		Bag.new(:storage(%keys))
 	}
-	multi method pick($i = Whatever) {
-		%!storage.kv.map: {$^a xx $^b;}.pick($i);
+	multi method pick($i = *) {
+		@!weight.pick($i);
 	}
-	multi sub postcircumfix:<{ }>(Bag $bag: *@indices) {
-		$bag!storage.{@indices};
+        multi method roll($i = *) {
+                @!weight.roll($i);
+        }
+	multi sub postcircumfix:<{ }>(Bag $bag: *@indices, Bool :$exists = 0) {
+		$bag!storage.{@indices} :$exists;
 	}
+        multi method Hash() {
+                %!storage
+        }
 }
 

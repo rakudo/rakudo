rakudo/rakudo

commit 6ca6140a4683a66b9f7d54e9a336051d5f564924

Simplify and optimize the trivial case of Str.comb(). RT #85602

src/core/Cool-str.pm
@@ -54,7 +54,13 @@ augment class Cool {
         $result;
     }
 
-    multi method comb(Regex $matcher = /./, $limit = *, :$match) {
+    multi method comb() {
+        gather for 1..self.chars {
+            take self.substr($_ - 1, 1);
+        }
+    }
+
+    multi method comb(Regex $matcher, $limit = *, :$match) {
         my $self-string = ~self;
         my $c = 0;
         my $l = $limit ~~ ::Whatever ?? Inf !! $limit;

commit 632f813ea7d4c15ecf4dc8e1a4fe5ca7cc3a47f1

Fix a regression introduced by 6ca6140a. This doesn't look like the best way to do it, but there are no tests to prove it incorrect

src/core/Cool-str.pm
@@ -54,8 +54,9 @@ augment class Cool {
         $result;
     }
 
-    multi method comb() {
-        gather for 1..self.chars {
+    multi method comb(:$limit) {
+        my $to = $limit // self.chars;
+        gather for 1..$to {
             take self.substr($_ - 1, 1);
         }
     }
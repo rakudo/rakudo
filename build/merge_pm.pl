a04f6bf7cb90e127565848e1fb2895d3a2e6510d
Jonathan Worthington <jnthn@jnthn.net>
2011-05-04 15:58:19 +0200

    Simplify gen_core_pm.pl, then just rename it to indicate it's a generic way to merge multiple .pm files into one.

--- build/merge_pm.pl
+++ build/merge_pm.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# Copyright (C) 2008, The Perl Foundation.
+# Copyright (C) 2008-2011, The Perl Foundation.
 # $Id$
 
 use strict;
@@ -15,7 +15,6 @@ print <<"END_SETTING";
 
 END_SETTING
 
-my %classnames;
 foreach my $file (@files) {
     print "# From $file\n\n";
     open(my $fh, "<:utf8",  $file) or die "$file: $!";
@@ -23,13 +22,6 @@ foreach my $file (@files) {
     my $x = <$fh>;
     close $fh;
     print $x;
-    foreach ($x =~ /\bclass\s+(\S+)/g) { $classnames{$_}++; }
 }
-
-print "CHECK {\n";
-foreach (keys(%classnames)) {
-    print "    Perl6::Compiler.import('$_');\n";
-}
-print "}\n";
 
 print "\n# vim: set ft=perl6 nomodifiable :\n";

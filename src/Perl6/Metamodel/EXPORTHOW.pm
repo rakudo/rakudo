7e411b34a84ee26b010b3b78ce6727a92ab8a4da
Jonathan Worthington <jnthn@jnthn.net>
2011-05-08 16:24:15 +0200

    Correct HOW exports.

--- src/Perl6/Metamodel/EXPORTHOW.pm
+++ src/Perl6/Metamodel/EXPORTHOW.pm
@@ -1,7 +1,7 @@
 # Bind the HOWs into the EXPORTHOW package under the package declarator
 # names.
 my module EXPORTHOW {
-    ($?PACKAGE.WHO)<package>      := PackageHOW;
-    ($?PACKAGE.WHO)<module>       := ModuleHOW;
-    ($?PACKAGE.WHO)<class>        := ClassHOW;
+    ($?PACKAGE.WHO)<package>      := Perl6::Metamodel::PackageHOW;
+    ($?PACKAGE.WHO)<module>       := Perl6::Metamodel::ModuleHOW;
+    ($?PACKAGE.WHO)<class>        := Perl6::Metamodel::ClassHOW;
 }

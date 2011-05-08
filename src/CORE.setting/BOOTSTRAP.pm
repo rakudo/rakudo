ddeadfd281aed1bc5b6d26bb1266c048c0c06d0b
Jonathan Worthington <jnthn@jnthn.net>
2011-05-08 18:13:21 +0200

    Simplify BOOTSTRAP.pm so it's something we can at least produce code for now.

--- src/CORE.setting/BOOTSTRAP.pm
+++ src/CORE.setting/BOOTSTRAP.pm
@@ -2,12 +2,7 @@ use Perl6::Metamodel;
 
 # Top level classes.
 my class Mu { ... }
-my class Any is Mu { ... }
-my class Cool is Any { ... }
-
-# Attribute.
-# XXX Needs to get all its methods from a role in Perl6::Metamodel so they are
-# available to the compiler.
-my class Attribute is Cool { ... }
+my class Any { ... }
+my class Cool { ... }
 
 {YOU_ARE_HERE}

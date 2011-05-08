b2fea874ef19d360c90ff2b5d30fe414b8f99098
Jonathan Worthington <jnthn@jnthn.net>
2011-05-09 01:02:08 +0200

    Fix inheritance bug.

--- src/Perl6/Metamodel/MultipleInheritance.pm
+++ src/Perl6/Metamodel/MultipleInheritance.pm
@@ -4,7 +4,7 @@ role Perl6::Metamodel::MultipleInheritance {
 
     # Adds a parent.
     method add_parent($obj, $parent) {
-        if self.composed($obj) {
+        if self.is_composed($obj) {
             pir::die("Parents cannot be added to a class after it has been composed");
         }
         for @!parents {

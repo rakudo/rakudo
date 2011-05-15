67b2264f3c2db6780654dfb171d879a285861141
Jonathan Worthington <jnthn@jnthn.net>
2011-05-15 10:36:07 +0200

    Simply proto - don't need {*} but just *.

--- src/CORE.setting/traits.pm
+++ src/CORE.setting/traits.pm
@@ -1,6 +1,6 @@
 use Perl6::Metamodel;
 
-proto trait_mod:<is>(|$c) { {*} }
+proto trait_mod:<is>(|$c) { * }
 
 multi trait_mod:<is>(Mu:U $child, Mu:U $parent) {
     $child.add_parent($child, $parent);

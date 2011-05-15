6dd9027d0f5896555c69b2c0364f262064ac8c58
Jonathan Worthington <jnthn@jnthn.net>
2011-05-15 12:11:10 +0200

    Just use a single dispatch for our first trait handler for now - easier to get to a point where something works.

--- src/CORE.setting/traits.pm
+++ src/CORE.setting/traits.pm
@@ -1,11 +1,16 @@
 use Perl6::Metamodel;
 
-proto trait_mod:<is>(|$c) { * }
-
-multi trait_mod:<is>(Mu:U $child, Mu:U $parent) {
+# XXX Until we get multi-dispatch in, just have one single dispatch
+# for adding a parent; let's us get something working at all easier.
+#proto trait_mod:<is>(|$c) { * }
+#
+#multi trait_mod:<is>(Mu:U $child, Mu:U $parent) {
+#    $child.add_parent($child, $parent);
+#}
+#
+#multi trait_mod:<is>(Attribute $attr, :$rw!) {
+#    $attr.set_rw(1);
+#}
+sub trait_mod:<is>(Mu:U $child, Mu:U $parent) {
     $child.add_parent($child, $parent);
-}
-
-multi trait_mod:<is>(Attribute $attr, :$rw!) {
-    $attr.set_rw(1);
 }

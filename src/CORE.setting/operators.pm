308072ec14be10a32c67a20d75dd355bd95a74e0
Jonathan Worthington <jnthn@jnthn.net>
2011-06-03 00:07:20 +0200

    Get a few very basic ops in place, mostly to check we can.

--- src/CORE.setting/operators.pm
+++ src/CORE.setting/operators.pm
@@ -1,3 +1,38 @@
 sub infix:<=>(Mu \$a, Mu \$b) {
     pir::perl6_container_store__0PP($a, pir::perl6_decontainerize__PP($b))
 }
+
+proto infix:<+>(Mu |$) { * }
+multi infix:<+>(Int $a, Int $b) {
+    pir::repr_box_int__PIP(
+        pir::add__III(pir::repr_unbox_int__IP($a), pir::repr_unbox_int__IP($b)),
+        Int)
+}
+
+proto infix:<->(Mu |$) { * }
+multi infix:<->(Int $a, Int $b) {
+    pir::repr_box_int__PIP(
+        pir::sub__III(pir::repr_unbox_int__IP($a), pir::repr_unbox_int__IP($b)),
+        Int)
+}
+
+proto infix:<*>(Mu |$) { * }
+multi infix:<*>(Int $a, Int $b) {
+    pir::repr_box_int__PIP(
+        pir::mul__III(pir::repr_unbox_int__IP($a), pir::repr_unbox_int__IP($b)),
+        Int)
+}
+
+proto infix:</>(Mu |$) { * }
+multi infix:</>(Int $a, Int $b) {
+    pir::repr_box_int__PIP(
+        pir::div__III(pir::repr_unbox_int__IP($a), pir::repr_unbox_int__IP($b)),
+        Int)
+}
+
+proto infix:<~>(Mu |$) { * }
+multi infix:<~>(Str $a, Str $b) {
+    pir::repr_box_str__PSP(
+        pir::concat__SSS(pir::repr_unbox_str__SP($a), pir::repr_unbox_str__SP($b)),
+        Str)
+}

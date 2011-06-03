4d39aa61ab5d2b7ca9ec439bba75c9887a6fcee6
Jonathan Worthington <jnthn@jnthn.net>
2011-06-04 01:51:21 +0200

    First few relational ops.

--- src/CORE.setting/operators.pm
+++ src/CORE.setting/operators.pm
@@ -36,3 +36,27 @@ multi infix:<~>(Str $a, Str $b) {
         pir::concat__SSS(pir::repr_unbox_str__SP($a), pir::repr_unbox_str__SP($b)),
         Str)
 }
+
+proto infix:<==>(Mu |$) { * }
+multi infix:<==>(Int $a, Int $b) {
+    pir::perl6_booleanize__PI(
+        pir::iseq__III(pir::repr_unbox_int__IP($a), pir::repr_unbox_int__IP($b)))
+}
+
+proto infix:<!=>(Mu |$) { * }
+multi infix:<!=>(Int $a, Int $b) {
+    pir::perl6_booleanize__PI(
+        pir::isne__III(pir::repr_unbox_int__IP($a), pir::repr_unbox_int__IP($b)))
+}
+
+proto infix:<eq>(Mu |$) { * }
+multi infix:<eq>(Str $a, Str $b) {
+    pir::perl6_booleanize__PI(
+        pir::iseq__ISS(pir::repr_unbox_str__SP($a), pir::repr_unbox_str__SP($b)))
+}
+
+proto infix:<ne>(Mu |$) { * }
+multi infix:<ne>(Str $a, Str $b) {
+    pir::perl6_booleanize__PI(
+        pir::isne__ISS(pir::repr_unbox_str__SP($a), pir::repr_unbox_str__SP($b)))
+}

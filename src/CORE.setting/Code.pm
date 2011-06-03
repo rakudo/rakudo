6e94078a1b4cabde9c3124bf36d0cdd07bf5eeff
Jonathan Worthington <jnthn@jnthn.net>
2011-06-03 23:48:02 +0200

    A cheaty Code.clone.

--- src/CORE.setting/Code.pm
+++ src/CORE.setting/Code.pm
@@ -1,2 +1,15 @@
 my class Code {
+    method clone() {
+        my $cloned := pir::repr_clone__PP(self);
+        # XXX Kinda want to write what's below...but '$!do' won't
+        # Parrot stringify just yet.
+        # pir::set_attribute__0PPsP($cloned, Code, '$!do', pir::clone__PP($!do))
+        Q:PIR {
+            %r = find_lex '$cloned'
+            $P0 = find_lex 'Code'
+            $P1 = getattribute %r, $P0, '$!do'
+            $P1 = clone $P1
+            setattribute %r, $P0, '$!do', $P1
+        }
+    }
 }

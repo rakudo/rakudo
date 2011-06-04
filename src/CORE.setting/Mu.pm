a7901341644f69a70cef8f1c91c8cd80642362e1
Jonathan Worthington <jnthn@jnthn.net>
2011-06-04 17:52:59 +0200

    Mu.Bool, Mu.defined.

--- src/CORE.setting/Mu.pm
+++ src/CORE.setting/Mu.pm
@@ -3,6 +3,14 @@ my class Mu {
         self.bless(self.CREATE());
     }
     
+    method Bool() {
+        self.defined
+    }
+    
+    method defined() {
+        pir::perl6_booleanize__PI(pir::repr_defined__IP(self))
+    }
+    
     method CREATE() {
         pir::repr_instance_of__PP(self.WHAT)
     }

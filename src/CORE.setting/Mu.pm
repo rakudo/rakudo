9120d6c597b36ef0628bead43fab0bbc615cc1eb
Jonathan Worthington <jnthn@jnthn.net>
2011-06-04 17:15:23 +0200

    Stub new/CREATE/bless.

--- src/CORE.setting/Mu.pm
+++ src/CORE.setting/Mu.pm
@@ -1,4 +1,16 @@
 my class Mu {
+    method new() {
+        self.bless(self.CREATE());
+    }
+    
+    method CREATE() {
+        pir::repr_instance_of__PP(self.WHAT)
+    }
+    
+    method bless(Mu $candidate) {
+        $candidate
+    }
+    
     method item() { self }
     
     method say() { say(self) }

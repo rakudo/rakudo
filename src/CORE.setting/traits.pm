a49224b24c963b0a82f8aa1a49b8b2b6430b61f6
Jonathan Worthington <jnthn@jnthn.net>
2011-06-04 16:08:06 +0200

    Oops, left in debugging code.

--- src/CORE.setting/traits.pm
+++ src/CORE.setting/traits.pm
@@ -2,7 +2,5 @@ use Perl6::Metamodel;
 
 proto trait_mod:<is>(|$) { * }
 multi trait_mod:<is>(Mu $child, Mu $parent) {
-    Q:PIR { say "in tmi" };
     $child.HOW.add_parent($child, $parent);
-    Q:PIR { say "survived tmi" };
 }

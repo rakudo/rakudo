74248010fede9c8406324d8e066ddaee3830a024
Jonathan Worthington <jnthn@jnthn.net>
2011-06-05 16:00:19 +0200

    Start putting back Cool.chars, Cool.substr. Various others simple string methods (.bytes, .chop, .lc, .uc...) can also be put back by some enterprising soul with tuits; follow the style shown here.

--- src/CORE.setting/Cool.pm
+++ src/CORE.setting/Cool.pm
@@ -1,2 +1,27 @@
 my class Cool {
+    method chars() {
+        pir::perl6_box_int__PI(
+            pir::length__IS(pir::repr_unbox_str__SP(self.Str)));
+    }
+
+    method substr($start as Int, $length?) {
+        my Str $str := self.Str;
+        my Int $len := $length.Int // $str.chars;
+        if ($len < 0) {
+            if ($start >= 0) {
+                $len := $len + $str.chars;
+            }
+            $len := $len - $start;
+        }
+
+        # XXX no prefix:<-> yet...
+        #if ($start > self.chars || $start < -self.chars) {
+        #    return Mu;
+        #}
+
+        pir::perl6_box_str__PS(pir::substr(
+            pir::repr_unbox_str__SP($str),
+            pir::repr_unbox_int__IP($start),
+            pir::repr_unbox_int__IP($len)));
+    }
 }

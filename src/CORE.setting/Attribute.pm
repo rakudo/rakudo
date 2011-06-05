8ace1f158f78b71e673fb7ae4c913bf992fbd5e3
Jonathan Worthington <jnthn@jnthn.net>
2011-06-05 17:09:09 +0200

    Auto-generated attribute accessors, both readonly (default) and rw if 'is rw' trait is applied. Happily, handled by real multi-dispatch trait mods, not special-cased in the compiler like master.

--- src/CORE.setting/Attribute.pm
+++ src/CORE.setting/Attribute.pm
@@ -1,5 +1,27 @@
 # Attributes for this are already set up in bootstrap.
 my class Attribute {
     method compose(Mu $package) {
+        # Generate accessor method, if we're meant to have one.
+        if self.has_accessor {
+            my $name  := self.name;
+            my $dcpkg := pir::perl6_decontainerize__PP($package);
+            my $meth  := self.rw
+                ??
+                method (Mu $self:) {
+                    pir::getattribute__PPPs(
+                        pir::perl6_decontainerize__PP($self),
+                        $dcpkg,
+                        pir::repr_unbox_str__SP($name))
+                }
+                !!
+                method (Mu $self:) {
+                    pir::perl6_decontainerize__PP(
+                        pir::getattribute__PPPs(
+                            pir::perl6_decontainerize__PP($self),
+                            $dcpkg,
+                            pir::repr_unbox_str__SP($name)))
+                };
+            $package.HOW.add_method($package, $name.substr(2), $meth);
+        }
     }
 }

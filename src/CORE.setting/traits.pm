696d78b6c09a7c7d062407c112942db92b97cab0
Jonathan Worthington <jnthn@jnthn.net>
2011-06-05 02:20:50 +0200

    Couple of Attribute trait handlers.

--- src/CORE.setting/traits.pm
+++ src/CORE.setting/traits.pm
@@ -4,3 +4,12 @@ proto trait_mod:<is>(|$) { * }
 multi trait_mod:<is>(Mu $child, Mu $parent) {
     $child.HOW.add_parent($child, $parent);
 }
+multi trait_mod:<is>(Attribute $attr, :$rw!) {
+    $attr.set_rw();
+}
+
+proto trait_mod:<will>(|$) { * }
+multi trait_mod:<will>(Attribute $attr, Block $closure, :$build!) {
+    Q:PIR { say "in tmw" };
+    $attr.set_build_closure($closure)
+}

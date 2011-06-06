a240642647ac10be2672d58f9d1e5e8f3e5d3968
Moritz Lenz <moritz@faui2k3.org>
2011-06-06 21:57:57 +0200

    Num.sqrt and .log

--- src/CORE.setting/Num.pm
+++ src/CORE.setting/Num.pm
@@ -8,4 +8,17 @@ my class Num {
     multi method Str(Num:D:) {
         pir::perl6_box_str__PS(pir::repr_unbox_num__NP(self));
     }
+
+    proto method log(|$) {*}
+    multi method log() {
+        pir::perl6_box_num__PN(pir::ln__NN(pir::repr_unbox_num__NP(self)));
+    }
+    multi method log(Num \$base) {
+        self.log() / $base.log();
+    }
+
+    proto method sqrt(|$) {*}
+    multi method sqrt() {
+        pir::perl6_box_num__PN(pir::sqrt__NN(pir::repr_unbox_num__NP(self)));
+    }
 }

eeeb5cf178eca0e023f35ebddc8192ba7b20d634
Moritz Lenz <moritz@faui2k3.org>
2011-06-07 08:20:24 +0200

    more Num methods

--- src/CORE.setting/Num.pm
+++ src/CORE.setting/Num.pm
@@ -21,4 +21,48 @@ my class Num {
     multi method sqrt() {
         pir::perl6_box_num__PN(pir::sqrt__NN(pir::repr_unbox_num__NP(self)));
     }
+
+    method rand() {
+        pir::perl6_box_num__PN(pir::rand__NN(pir::repr_unbox_num__NP(self)));
+    }
+
+    method ceil() {
+        pir::perl6_box_num__PN(pir::ceil__NN(pir::repr_unbox_num__NP(self)));
+    }
+    method floor() {
+        pir::perl6_box_num__PN(pir::floor__NN(pir::repr_unbox_num__NP(self)));
+    }
+
+    proto method sin(|$) {*}
+    multi method sin() {
+        pir::perl6_box_num__PN(pir::sin__NN(pir::repr_unbox_num__NP(self)));
+    }
+    proto method asin(|$) {*}
+    multi method asin() {
+        pir::perl6_box_num__PN(pir::asin__NN(pir::repr_unbox_num__NP(self)));
+    }
+    proto method cos(|$) {*}
+    multi method cos() {
+        pir::perl6_box_num__PN(pir::cos__NN(pir::repr_unbox_num__NP(self)));
+    }
+    proto method acos(|$) {*}
+    multi method acos() {
+        pir::perl6_box_num__PN(pir::acos__NN(pir::repr_unbox_num__NP(self)));
+    }
+    proto method tan(|$) {*}
+    multi method tan() {
+        pir::perl6_box_num__PN(pir::tan__NN(pir::repr_unbox_num__NP(self)));
+    }
+    proto method atan(|$) {*}
+    multi method atan() {
+        pir::perl6_box_num__PN(pir::atan__NN(pir::repr_unbox_num__NP(self)));
+    }
+    proto method sec(|$) {*}
+    multi method sec() {
+        pir::perl6_box_num__PN(pir::sec__NN(pir::repr_unbox_num__NP(self)));
+    }
+    proto method asec(|$) {*}
+    multi method asec() {
+        pir::perl6_box_num__PN(pir::asec__NN(pir::repr_unbox_num__NP(self)));
+    }
 }

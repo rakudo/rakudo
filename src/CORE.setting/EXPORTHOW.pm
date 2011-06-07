6df35c0fe1f3eb86c0216b22dc30f0a7082d9a4b
Jonathan Worthington <jnthn@jnthn.net>
2011-06-07 18:09:53 +0200

    Add a HOW for representing a generic type. Get it and the various role-related HOWs (which are just stubs so far) into the build.

--- src/CORE.setting/EXPORTHOW.pm
+++ src/CORE.setting/EXPORTHOW.pm
@@ -4,7 +4,10 @@
 my module EXPORTHOW {
     pir::set__vQsPP($?PACKAGE.WHO, Q:PIR{ %r = box 'package' }, Perl6::Metamodel::PackageHOW);
     pir::set__vQsPP($?PACKAGE.WHO, Q:PIR{ %r = box 'module' }, Perl6::Metamodel::ModuleHOW);
+    pir::set__vQsPP($?PACKAGE.WHO, Q:PIR{ %r = box 'generic' }, Perl6::Metamodel::GenericHOW);
     pir::set__vQsPP($?PACKAGE.WHO, Q:PIR{ %r = box 'class' }, Perl6::Metamodel::ClassHOW);
     pir::set__vQsPP($?PACKAGE.WHO, Q:PIR{ %r = box 'class-attr' }, Attribute);
+    pir::set__vQsPP($?PACKAGE.WHO, Q:PIR{ %r = box 'role' }, Perl6::Metamodel::ParametricRoleHOW);
+    pir::set__vQsPP($?PACKAGE.WHO, Q:PIR{ %r = box 'role-attr' }, Attribute);
     pir::set__vQsPP($?PACKAGE.WHO, Q:PIR{ %r = box 'native' }, Perl6::Metamodel::NativeHOW);
 }

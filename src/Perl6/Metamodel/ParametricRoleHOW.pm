f3efa32cc3cdfb1ab343a6315304b2275cb3cd18
Jonathan Worthington <jnthn@jnthn.net>
2011-06-08 21:11:09 +0200

    Just enough to get us able to write a role body.

--- src/Perl6/Metamodel/ParametricRoleHOW.pm
+++ src/Perl6/Metamodel/ParametricRoleHOW.pm
@@ -6,7 +6,20 @@ class Perl6::Metamodel::ParametricRoleHOW
     does Perl6::Metamodel::AttributeContainer
     does Perl6::Metamodel::RoleContainer
     does Perl6::Metamodel::MultipleInheritance
+    does Perl6::Metamodel::Stashing
 {
+    has $!composed;
+
+    method new_type(:$name = '<anon>', :$ver, :$auth, :$repr) {
+        my $metarole := self.new(:name($name), :ver($ver), :auth($auth));
+        self.add_stash(pir::repr_type_object_for__PPS($metarole, 'Uninstantiable'));
+    }
+    
+    method compose($obj) {
+        $!composed := 1;
+        $obj
+    }
+    
     method is_generic($obj) {
         1
     }

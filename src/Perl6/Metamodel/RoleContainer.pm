24d6b2350a037f4426f82392e3dfe1f83fae4ff7
Jonathan Worthington <jnthn@jnthn.net>
2011-06-09 17:46:03 +0200

    Fill out RoleContainer role a bit, and a add a tentative idea of what ClassHOW will do with roles that it's going to compose.

--- src/Perl6/Metamodel/RoleContainer.pm
+++ src/Perl6/Metamodel/RoleContainer.pm
@@ -1,2 +1,11 @@
 role Perl6::Metamodel::RoleContainer {
+    has @!roles_to_compose;
+    
+    method add_role($obj, $role) {
+        @!roles_to_compose.push($role)
+    }
+    
+    method roles_to_compose($obj) {
+        @!roles_to_compose
+    }
 }

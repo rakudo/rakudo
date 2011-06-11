2a503109eebe08dd2453ea28f268fc4396d32001
Moritz Lenz <moritz@faui2k3.org>
2011-06-11 08:41:39 +0200

    add a lot of coercive numeric operators

--- src/core/operators.pm
+++ src/core/operators.pm
@@ -53,6 +53,9 @@ multi prefix:<abs>(Int \$a) {
 multi prefix:<abs>(Num \$a) {
     pir::perl6_box_num__PN(pir::abs__II(pir::repr_unbox_num__NP($a)))
 }
+multi prefix:<abs>(\$a) {
+    abs $a.Numeric
+}
 
 proto infix:<*>(|$) { * }
 multi infix:<*>(Int \$a, Int \$b) {
@@ -62,6 +65,12 @@ multi infix:<*>(Int \$a, Int \$b) {
 multi infix:<*>(Num \$a, Num \$b) {
     pir::perl6_box_num__PN(
         pir::mul__NNN(pir::repr_unbox_num__NP($a), pir::repr_unbox_num__NP($b)))
+}
+multi infix:<*>(\$a, \$b) {
+    $a.Numeric * $b.Numeric
+}
+multi infix:<*>(Real \$a, Real \$b) {
+    $a.Bridge * $b.Bridge
 }
 
 proto infix:</>(|$) { * }
@@ -73,6 +82,12 @@ multi infix:</>(Num \$a, Num \$b) {
     pir::perl6_box_num__PN(
         pir::div__NNN(pir::repr_unbox_num__NP($a), pir::repr_unbox_num__NP($b)))
 }
+multi infix:</>(\$a, \$b) {
+    $a.Numeric / $b.Numeric
+}
+multi infix:</>(Real \$a, Real \$b) {
+    $a.Bridge / $b.Bridge
+}
 
 proto infix:<%>(|$) { * }
 multi infix:<%>(Int \$a, Int \$b) {
@@ -82,6 +97,12 @@ multi infix:<%>(Int \$a, Int \$b) {
 multi infix:<%>(Num \$a, Num \$b) {
     pir::perl6_box_num__PN(
         pir::mod__NNN(pir::repr_unbox_num__NP($a), pir::repr_unbox_num__NP($b)))
+}
+multi infix:<%>(\$a, \$b) {
+    $a.Numeric % $b.Numeric
+}
+multi infix:<%>(Real \$a, Real \$b) {
+    $a.Bridge % $b.Bridge
 }
 
 proto infix:<~>(|$) { * }
@@ -103,6 +124,12 @@ multi infix:<==>(Num \$a, Num \$b) {
     pir::perl6_booleanize__PI(
         pir::iseq__INN(pir::repr_unbox_num__NP($a), pir::repr_unbox_num__NP($b)))
 }
+multi infix:<==>(\$a, \$b) {
+    $a.Numeric == $b.Numeric
+}
+multi infix:<==>(Real \$a, Real \$b) {
+    $a.Bridge == $b.Bridge
+}
 
 proto infix:<!=>(|$) { * }
 multi infix:<!=>(Int \$a, Int \$b) {
@@ -112,6 +139,12 @@ multi infix:<!=>(Int \$a, Int \$b) {
 multi infix:<!=>(Num \$a, Num \$b) {
     pir::perl6_booleanize__PI(
         pir::isne__INN(pir::repr_unbox_num__NP($a), pir::repr_unbox_num__NP($b)))
+}
+multi infix:<!=>(\$a, \$b) {
+    $a.Numeric != $b.Numeric
+}
+multi infix:<!=>(Real \$a, Real \$b) {
+    $a.Bridge != $b.Bridge
 }
 
 proto infix:«<»(|$) { * }
@@ -123,6 +156,12 @@ multi infix:«<»(Num \$a, Num \$b) {
     pir::perl6_booleanize__PI(
         pir::islt__INN(pir::repr_unbox_num__NP($a), pir::repr_unbox_num__NP($b)))
 }
+multi infix:«<»(\$a, \$b) {
+    $a.Numeric < $b.Numeric
+}
+multi infix:«<»(Real \$a, Real \$b) {
+    $a.Bridge < $b.Bridge
+}
 
 proto infix:«<=»(|$) { * }
 multi infix:«<=»(Int \$a, Int \$b) {
@@ -132,6 +171,12 @@ multi infix:«<=»(Int \$a, Int \$b) {
 multi infix:«<=»(Num \$a, Num \$b) {
     pir::perl6_booleanize__PI(
         pir::isle__INN(pir::repr_unbox_num__NP($a), pir::repr_unbox_num__NP($b)))
+}
+multi infix:«<=»(\$a, \$b) {
+    $a.Numeric <= $b.Numeric
+}
+multi infix:«<=»(Real \$a, Real \$b) {
+    $a.Bridge <= $b.Bridge
 }
 
 proto infix:«>»(|$) { * }
@@ -143,6 +188,12 @@ multi infix:«>»(Num \$a, Num \$b) {
     pir::perl6_booleanize__PI(
         pir::isgt__INN(pir::repr_unbox_num__NP($a), pir::repr_unbox_num__NP($b)))
 }
+multi infix:«>»(\$a, \$b) {
+    $a.Numeric > $b.Numeric
+}
+multi infix:«>»(Real \$a, Real \$b) {
+    $a.Bridge > $b.Bridge
+}
 
 proto infix:«>=»(|$) { * }
 multi infix:«>=»(Int \$a, Int \$b) {
@@ -152,6 +203,12 @@ multi infix:«>=»(Int \$a, Int \$b) {
 multi infix:«>=»(Num \$a, Num \$b) {
     pir::perl6_booleanize__PI(
         pir::isge__INN(pir::repr_unbox_num__NP($a), pir::repr_unbox_num__NP($b)))
+}
+multi infix:«>=»(\$a, \$b) {
+    $a.Numeric >= $b.Numeric
+}
+multi infix:«>=»(Real \$a, Real \$b) {
+    $a.Bridge >= $b.Bridge
 }
 
 proto infix:<eq>(|$) { * }
@@ -328,4 +385,10 @@ multi infix:<**>(Num \$a, Num \$b) {
 }
 multi infix:<**>(Int \$a, Int \$b) {
     pir::perl6_box_int__PI(pir::set__IN(pir::pow__NNN(pir::repr_unbox_int__IP($a), pir::repr_unbox_int__IP($b))));
+}
+multi infix:<**>(\$a, \$b) {
+    $a.Numeric ** $b.Numeric
+}
+multi infix:<**>(Real \$a, Real \$b) {
+    $a.Bridge ** $b.Bridge
 }

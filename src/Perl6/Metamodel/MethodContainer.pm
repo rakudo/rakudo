275ab3ddac6a745cf1471efe2e4b32f2e3e070b9
Jonathan Worthington <jnthn@jnthn.net>
2011-06-11 19:31:25 +0200

    Implement submethod handling in MethodContainer role (so it's available to all HOWs that do that role).

--- src/Perl6/Metamodel/MethodContainer.pm
+++ src/Perl6/Metamodel/MethodContainer.pm
@@ -1,18 +1,31 @@
 role Perl6::Metamodel::MethodContainer {
     # Lookup table of the methods.
     has %!methods;
+    has %!submethods;
 
     # The order that the methods were added in.
     has @!method_order;
 
     # Add a method.
     method add_method($obj, $name, $code_obj) {
-        if %!methods{$name} {
+        # Ensure we haven't already got it.
+        if %!methods{$name} || %!submethods{$name} {
             pir::die("Package '" ~ self.name($obj) ~ "' already has a method '" ~
                 $name ~ "'; did you mean to declare a multi-method?");
         }
+        
+        # Add to correct table depending on if it's a Submethod. Note, we
+        # may get Parrot subs in here during BOOTSTRAP; the try is to cope
+        # with them.
+        my $is_submethod := 0;
+        try { $is_submethod := $code_obj.HOW.name($code_obj) eq 'Submethod' }
+        if $is_submethod {
+            %!submethods{$name} := $code_obj;
+        }
+        else {
+            %!methods{$name} := $code_obj;
+        }
         @!method_order[+@!method_order] := $code_obj;
-        %!methods{$name} := $code_obj;
     }
 
     # Gets the method hierarchy.
@@ -38,8 +51,13 @@ role Perl6::Metamodel::MethodContainer {
         @meths
     }
 
-    # Get the method table. Only contains methods directly declared here.
+    # Get the method table. Only contains methods directly declared here,
+    # and excludes submethods.
     method method_table($obj) {
         %!methods
+    }
+    
+    method submethod_table($obj) {
+        %!submethods
     }
 }

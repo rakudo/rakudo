f17f3126d19a514435275578bb89e257feccddf2
pmichaud <pmichaud@pobox.com>
2011-06-10 22:06:42 -0500

    Add a preliminary Array.STORE.

--- src/core/Array.pm
+++ src/core/Array.pm
@@ -1,15 +1,25 @@
 class Array is List {
     has $!descriptor;
     
-    method BIND_POS(\$pos, $v is copy) {
-        pir::find_method__PPs(List, 'BIND_POS')(self, $pos, $v);
-    }
-
     method at_pos($pos) {
         self.exists($pos)
           ?? pir::find_method__PPs(List, 'at_pos')(self, $pos)
           !! pir::setattribute__0PPsP(my $v, Scalar, '$!whence',
                  -> { pir::find_method__PPs(List, 'BIND_POS')(self, $pos, $v) } )
+    }
+
+    method BIND_POS(\$pos, $v is copy) {
+        pir::find_method__PPs(List, 'BIND_POS')(self, $pos, $v);
+    }
+
+    method STORE(|$) {
+        pir::setattribute__vPPsP(self, List, '$!items', Mu);
+        pir::shift__PP(
+            pir::setattribute__3PPsP(self, List, '$!rest',
+                pir::perl6_current_args_rpa__P())
+        );
+        self.gimme(*);
+        self
     }
 }
 

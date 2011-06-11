ecdc63ca767d6ab0dc1c1cc0775ddf394953f958
Jonathan Worthington <jnthn@jnthn.net>
2011-06-11 21:32:01 +0200

    Start to fill out just a little bit of signature and parameter introspection. Much more to do, much of it LHF.

--- src/core/Parameter.pm
+++ src/core/Parameter.pm
@@ -1,2 +1,33 @@
 my class Parameter {
+    # XXX constant...
+    my $SIG_ELEM_BIND_CAPTURE       := 1;
+    my $SIG_ELEM_BIND_PRIVATE_ATTR  := 2;
+    my $SIG_ELEM_BIND_PUBLIC_ATTR   := 4;
+    my $SIG_ELEM_SLURPY_POS         := 8;
+    my $SIG_ELEM_SLURPY_NAMED       := 16;
+    my $SIG_ELEM_SLURPY_BLOCK       := 32;
+    my $SIG_ELEM_INVOCANT           := 64;
+    my $SIG_ELEM_MULTI_INVOCANT     := 128;
+    my $SIG_ELEM_IS_RW              := 256;
+    my $SIG_ELEM_IS_COPY            := 512;
+    my $SIG_ELEM_IS_PARCEL          := 1024;
+    my $SIG_ELEM_IS_OPTIONAL        := 2048;
+    my $SIG_ELEM_ARRAY_SIGIL        := 4096;
+    my $SIG_ELEM_HASH_SIGIL         := 8192;
+    my $SIG_ELEM_IS_CAPTURE         := 32768;
+    my $SIG_ELEM_UNDEFINED_ONLY     := 65536;
+    my $SIG_ELEM_DEFINED_ONLY       := 131072;
+
+    method name() {
+        $!variable_name
+    }
+    
+    # XXX TODO: Many more bits :-)
+    method perl() {
+        my $perl = $!nominal_type.HOW.name($!nominal_type);
+        if $!variable_name {
+            $perl = $perl ~ " " ~ $!variable_name;
+        }
+        $perl
+    }
 }
--- src/core/Signature.pm
+++ src/core/Signature.pm
@@ -1,2 +1,26 @@
 my class Signature {
+    method params() {
+        pir::setattribute__0PPsP(
+            pir::repr_instance_of__PP(List),
+            List, '$!rest', $!params);
+    }
+    
+    # XXX TODO: Parameter separators.
+    method perl() {
+        # Opening.
+        my $perl = ':(';
+        
+        # Parameters.
+        my $params = self.params();
+        my $sep = '';
+        my $i = 0;
+        while $i < $params.elems {
+            $perl = $perl ~ $sep ~ $params[$i].perl;
+            $sep = ', ';
+            $i = $i + 1;
+        }
+        
+        # Closer.
+        $perl ~ ')'
+    }
 }

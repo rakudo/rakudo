c0312cdc96a956a43d01411be133f909419c8151
Jonathan Worthington <jnthn@jnthn.net>
2011-06-12 18:06:05 +0200

    Use slurpies for print and say now we have them.

--- src/core/IO.pm
+++ src/core/IO.pm
@@ -1,12 +1,12 @@
-# XXX Very cheaty, just to get us able to output something.
-sub say(\$x) {
-    my $list = ($x,).list;
-    pir::print($list.shift) while $list.gimme(1);
+# XXX Relatively cheaty, just to get us able to output something.
+# But you should see what USED to be here! O.O
+sub say(*@list) {
+    pir::print(@list.shift) while @list.gimme(1);
     pir::print("\n");
+    1.Bool
 }
 
-sub print(\$x) {
-    my $list = ($x,).list;
-    pir::print($list.shift) while $list.gimme(1);
+sub print(*@list) {
+    pir::print(@list.shift) while @list.gimme(1);
+    1.Bool
 }
-

cb6bc8302cc3c7f117abe51829d656cc41c0a38b
Jonathan Worthington <jnthn@jnthn.net>
2011-06-15 00:43:30 +0200

    More twiddling with SubsetHOW's checking code.

--- src/Perl6/Metamodel/SubsetHOW.pm
+++ src/Perl6/Metamodel/SubsetHOW.pm
@@ -28,7 +28,7 @@ class Perl6::Metamodel::SubsetHOW
     
     # Do check when we're on LHS of smartmatch (e.g. Even ~~ Int).
     method type_check($obj, $checkee) {
-        pir::perl6_booleanize__PI($checkee.HOW =:= self ||
+        pir::perl6_booleanize__PI(pir::istrue($checkee.HOW =:= self) ||
             pir::type_check__IPP($checkee, $!refinee))
     }
     
@@ -36,6 +36,6 @@ class Perl6::Metamodel::SubsetHOW
     method accepts_type($obj, $checkee) {
         pir::perl6_booleanize__PI(
             pir::type_check__IPP($checkee, $!refinee) &&
-            $!refinement.ACCEPTS($checkee))
+            pir::istrue__IP($!refinement.ACCEPTS($checkee)))
     }
 }

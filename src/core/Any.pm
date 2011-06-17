cc35faeccfc29aabfdcec5feacce93e50f19944b
pmichaud <pmichaud@pobox.com>
2011-06-17 06:49:14 -0500

    Implement Any.join .

--- src/core/Any.pm
+++ src/core/Any.pm
@@ -1,11 +1,18 @@
 my class Any {
 
     # List-like methods for Any.
-
     method flat() { self.list.flat }
-
     method list() { (self,).list }
 
+    method join($separator = ' ') {
+        my $list = (self,).flat.eager;
+        my Mu $rsa := pir::new__Ps('ResizableStringArray');
+        pir::push__vPS($rsa, pir::repr_unbox_str__SP($list.shift.Stringy)) 
+            while $list;
+        pir::perl6_box_str__PS(
+            pir::join(pir::repr_unbox_str__SP($separator.Stringy), $rsa))
+    }
+         
     proto method postcircumfix:<[ ]>(|$) { * }
     multi method postcircumfix:<[ ]>(\$pos) is rw {
         self.at_pos($pos)

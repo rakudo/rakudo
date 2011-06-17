c40446707f0e4ec4478ab2eb0a713ecc7f6b5bd5
pmichaud <pmichaud@pobox.com>
2011-06-17 04:59:50 -0500

    Restore List.at_pos and List.exists.

--- src/core/List.pm
+++ src/core/List.pm
@@ -2,6 +2,18 @@ class List {
     # has Mu $!items    # BOOTSTRAP.pm
     # has Mu $!nextiter # BOOTSTRAP.pm
 
+    method at_pos(\$pos) {
+        self.exists($pos)
+          ?? pir::set__PQi($!items, pir::repr_unbox_int__IP($pos))
+          !! Mu
+    }
+
+    method exists(\$pos) {
+        self.gimme($pos + 1);
+        pir::perl6_booleanize__PI(
+            pir::exists__IQI($!items, pir::repr_unbox_int__IP($pos)))
+    }
+
     method gimme($n) {
         pir::defined($!items) or 
             pir::setattribute__3PPsP(self, List, '$!items', pir::new__Ps('ResizablePMCArray'));

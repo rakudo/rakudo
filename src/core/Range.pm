66c1021eabb7b5ec8cd40a4c45cead6ca7cfb9e7
pmichaud <pmichaud@pobox.com>
2011-06-19 18:59:51 -0500

    Refactor Range, add the ability to exclude endpoints.  Also remove the RangeIter class.
    Since Range is immutable, it can serve directly as its own iterator
    and doesn't need a RangeIter helper to keep track of generated
    elements.  This eliminates some duplication of code, and provides
    some much nicer optimization possibilities.

--- src/core/Range.pm
+++ src/core/Range.pm
@@ -1,31 +1,71 @@
 class Range is Iterable {
     has $.min;
     has $.max;
+    has $.excludes_min;
+    has $.excludes_max;
 
     proto method new(|$) { * }
-    multi method new($min, $max) {
-        self.CREATE.BUILD($min, $max)
+    multi method new($min, $max, :$excludes_min, :$excludes_max) {
+        my $new = self.CREATE;
+        $new.BUILD($min, $max, $excludes_min, $excludes_max)
     }
-    multi method new($min, Whatever $max) {
-        self.CREATE.BUILD($min, $Inf)
+    multi method new($min, Whatever $max, :$excludes_min, :$excludes_max) {
+        my $new = self.CREATE;
+        $new.BUILD($min, $Inf, $excludes_min, $excludes_max)
     }
 
-    method BUILD($min, $max) {
+    method BUILD($min, $max, $excludes_min, $excludes_max) {
         $!min = $min;
         $!max = $max;
+        $!excludes_min = $excludes_min;
+        $!excludes_max = $excludes_max;
         self;
     }
 
-    method flat()     { self.iterator.list }
     method infinite() { $.max == $Inf }
-    method iterator() { RangeIter.new(:value($.min), :max($.max)) }
-    method list()     { self.iterator.list }
+    method iterator() { self }
+
+    method reify($n is copy = 10) {
+        $n = $Inf if Whatever.ACCEPTS($n);
+        fail "request for infinite elements from range"
+          if $n == $Inf && self.infinite;
+        my $value = $!excludes_min ?? $!min.succ !! $!min;
+        my $cmpstop = $!excludes_max ?? 0 !! 1;
+        my Mu $rpa := pir::new__Ps('ResizablePMCArray');
+        (pir::push__vPP($rpa, $value++); $n--)
+          while $n > 0 && ($value cmp $!max) < $cmpstop;
+        ($value cmp $!max) < $cmpstop
+            && pir::push__vPP($rpa,
+                   ($value.succ cmp $!max < $cmpstop)
+                      ?? self.CREATE.BUILD($value, $!max, 0, $!excludes_max)
+                      !! $value);
+        pir__perl6_box_rpa__PP($rpa)
+    }
 
     multi method gist(Range:D:) { self.perl }
-    multi method perl(Range:D:) { $.min ~ '..' ~ $.max }
+    multi method perl(Range:D:) { 
+        $.min 
+          ~ ('^' if $.excludes_min)
+          ~ '..'
+          ~ ('^' if $.excludes_max)
+          ~ $.max
+    }
 }
 
 
-sub infix:<..>($min, $max) { Range.new($min, $max) }
-
-
+###  XXX remove the (1) from :excludes_min and :excludes_max below
+sub infix:<..>($min, $max) { 
+    Range.new($min, $max) 
+}
+sub infix:<^..>($min, $max) { 
+    Range.new($min, $max, :excludes_min(1)) 
+}
+sub infix:<..^>($min, $max) { 
+    Range.new($min, $max, :excludes_max(1)) 
+}
+sub infix:<^..^>($min, $max) {
+    Range.new($min, $max, :excludes_min(1), :excludes_max(1)) 
+}
+sub prefix:<^>($max) {
+    Range.new(0, $max, :excludes_max(1)) 
+}

af530138913b5a6bb96bb22d2fc579d6236a92b6
pmichaud <pmichaud@pobox.com>
2011-06-19 09:03:06 -0500

    Interim implementation of list slicing syntax (full implementation requires several core types to be "does Positional").

--- src/core/Any.pm
+++ src/core/Any.pm
@@ -1,8 +1,12 @@
 class MapIter { ... }
+class Parcel { ... }
 
 my class Any {
 
+    ########
     # List-like methods for Any.
+    ########
+
     method flat() { self.list.flat }
     method list() { (self,).list }
     method elems() { self.list.elems }
@@ -22,12 +26,20 @@ my class Any {
     }
          
     proto method postcircumfix:<[ ]>(|$) { * }
-    multi method postcircumfix:<[ ]>(\$pos) is rw {
+    multi method postcircumfix:<[ ]>($pos) is rw {
+        fail "Cannot use negative index $pos on {self.WHAT.perl}" if $pos < 0;
         self.at_pos($pos)
     }
+    # XXX: Temporary hack to allow slicing; the below method(s) should be
+    #      (@pos) when List, Range, Parcel, etc. "does Positional".
+    multi method postcircumfix:<[ ]>(Iterable \$pos) {
+        $pos.map({self[$_]}).Parcel
+    }
+    multi method postcircumfix:<[ ]>(Parcel \$pos) { self[$pos.flat] }
 
+    ########
     # Hash-like methods for Any.
-
+    ########
     proto method postcircumfix:<{ }>(|$) { * }
     multi method postcircumfix:<{ }>(\$key) is rw {
         self.at_key($key)

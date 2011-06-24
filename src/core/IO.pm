5307d26a4eb2c2050f4a57a83d69b3e6faac5cf5
Moritz Lenz <moritz@faui2k3.org>
2011-06-24 22:34:28 +0200

    start with implementing basic IO

--- src/core/IO.pm
+++ src/core/IO.pm
@@ -12,3 +12,52 @@ sub say(|$) {
 sub gist(|$) {
     pir__perl6_box_rpa__PP(pir::perl6_current_args_rpa__P()).gist
 }
+
+class IO {
+    has $!PIO;
+    has Int $.ins = 0;
+    has $.chomp = True;
+
+    proto method open(|$) { * }
+    multi method open($filename, :$r, :$w, :$a, :$bin) {
+        my $mode = $w ?? 'w' !! ($a ?? 'wa' !! 'r');
+        # TODO: catch error, and fail()
+        nqp::bindattr(self, IO, '$!PIO',
+             $filename eq '-'
+                ?? pir::getstdin__P()
+                !! pir::open__PSS(nqp::unbox_s($filename), nqp::unbox_s($mode))
+        );
+        $!PIO.encoding($bin ?? 'binary' !! 'utf8');
+        self;
+    }
+
+    method close() {
+        # TODO:b catch errors
+        $!PIO.close;
+        1.Bool;
+    }
+
+    method eof() {
+        nqp::p6bool($!PIO.eof);
+    }
+
+    method get() {
+        my Str $x = nqp::p6box_s($!PIO.readline);
+        # XXX don't fail() as long as it's fatal
+        # fail('end of file') if self.eof && $x eq '';
+        return Str if self.eof && $x eq '';
+
+        $!ins++;
+        # XXX
+        # comment out as long as initiliaztion of attributes is a no-op
+#        $!chomp ?? $x.chomp !! $x;
+        $x.chomp;
+    }
+
+    method lines() {
+        gather while (my $line = self.get).defined {
+            take $line;
+        }
+
+    }
+}

30f5cc9624b9b46ecbff88a2c234e5c9383e21ce
pmichaud <pmichaud@pobox.com>
2011-06-27 04:12:39 -0500

    Add last/next/redo handling to map loops in MapIter.  Unfortunately, getting the exception handlers to work cleanly means going back to Q:PIR for much of the MapIter body, although it's very straightforward code. Fortunately, this gives us another huge speed boost, such that using map to iterate over a range is twice as fast as it was before (about 80% faster than master). while loop with an increment variable.

--- src/core/MapIter.pm
+++ src/core/MapIter.pm
@@ -16,29 +16,60 @@ my class MapIter is Iterator {
 
     method reify($n is copy = 1, :$sink) {
         if !$!reified.defined {
-            ## we don't have &prefix:<|> or good control blocks yet,
-            ## so we'll temporarily implement MapIter with Q:PIR blocks.
+            ## we don't have good control blocks yet, so we'll 
+            ## temporarily implement MapIter with Q:PIR blocks.
             my $count = $!block.count;
-            my $block := pir::perl6_decontainerize__PP($!block); ### TODO: Why?
             $n = nqp::istype($n, Whatever) ?? 1000 !! $n.Int;
             $!list.gimme($count * $n);
             my Mu $rpa := nqp::list();
-            my Mu $args;
-            repeat {
-                $args := nqp::getattr($!list.munch($!block.count), 
-                                      Parcel, '$!storage');
-                nqp::push($rpa,
-                    Q:PIR {
-                        $P0 = find_lex '$args'
-                        $P1 = find_lex '$block'
-                        %r = $P1($P0 :flat)
-                    }) if $args;
-            } while $args 
-                    && nqp::islt_i(nqp::elems($rpa), nqp::unbox_i($n));
+            my $block := pir::perl6_decontainerize__PP($!block); ### TODO: Why?
+            my $list := pir::perl6_decontainerize__PP($!list);
+            my Mu $args := Q:PIR {
+                .local int n
+                .local pmc rpa, block, count, list, Parcel, List, result
+                rpa    = find_lex '$rpa'
+                block  = find_lex '$block'
+                count  = find_lex '$count'
+                list   = find_lex '$list'
+                Parcel = find_lex 'Parcel'
+                List   = find_lex 'List'
+                $P0    = find_lex '$n'
+                n      = repr_unbox_int $P0
+                .local pmc handler
+                handler = root_new ['parrot';'ExceptionHandler']
+                set_addr handler, catch
+                handler.'handle_types'(.CONTROL_LOOP_LAST, .CONTROL_LOOP_NEXT, .CONTROL_LOOP_REDO)
+                push_eh handler
+              next:
+                $I0 = elements rpa
+                unless $I0 < n goto last
+                .local pmc args
+                args = list.'munch'(count)
+                args = getattribute args, Parcel, '$!storage'
+                unless args goto last
+                result = block(args :flat)
+                push rpa, result
+                goto next
+              catch:
+                .local pmc exception, type
+                .get_results (exception)
+                result = getattribute exception, 'payload'
+                push rpa, result
+                type = getattribute exception, 'type'
+                if type == .CONTROL_LOOP_LAST goto last
+                if type != .CONTROL_LOOP_REDO goto next
+              redo:
+                $P0 = getattribute list, List, '$!items'
+                splice $P0, args, 0, 0
+                goto next
+              last:
+                pop_eh
+                %r = args
+            };
             # create the next MapIter if we haven't reached the end
             nqp::push($rpa, self.CREATE.BUILD($!list, $!block))
               if $args;
-            $!reified := pir__perl6_box_rpa__PP($rpa);
+            $!reified := nqp::p6parcel($rpa, nqp::null());
             $!list = Any;
             $!block = Any;
         }

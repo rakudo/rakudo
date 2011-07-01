83ad0a8931341be10ee1d309fd9f419d748b8f96
pmichaud <pmichaud@pobox.com>
2011-07-01 14:32:43 -0500

    Add NYI.pm -- a place where we can explicitly put things we know are NYI.

---
+++ src/core/NYI.pm
@@ -0,0 +1,10 @@
+# things that are known to be NYI
+
+sub NYI(*@msg) { die @msg };
+
+sub callsame(|$) { NYI "callsame not yet implemented" };
+sub callwith(|$) { NYI "callwith not yet implemented" };
+sub nextsame(|$) { NYI "nextsame not yet implemented" };
+sub nextwith(|$) { NYI "nextwith not yet implemented" };
+
+

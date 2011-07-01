0b7280663b3e787daf803872988be493a9b8d238
Tadeusz Sośnierz <tadzikes@gmail.com>
2011-07-01 11:41:28 +0200

    Implement Pair.hash, fixing S06-signature/slurpy-and-interpolation.t

--- src/core/Pair.pm
+++ src/core/Pair.pm
@@ -1,8 +1,10 @@
 my class Pair is Enum does Associative {
     # method value() is rw { ... }
+    method hash {
+        { self.key => self.value }
+    }
 }
 
 sub infix:«=>»($key, Mu $value) { 
     Pair.new(:key($key), :value($value))
 }
-

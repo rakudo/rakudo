59ec8c6a6b134b094c0c099d5648c3df8e2a4b96
Moritz Lenz <moritz@faui2k3.org>
2011-07-04 22:16:45 +0200

    infix cmp for Pairs

--- src/core/Pair.pm
+++ src/core/Pair.pm
@@ -5,3 +5,7 @@ my class Pair is Enum does Associative {
 sub infix:«=>»($key, Mu $value) { 
     Pair.new(:key($key), :value($value))
 }
+
+multi infix:<cmp>(Pair \$a, Pair \$b) {
+    ($a.key cmp $b.key) || ($a.value cmp $b.value)
+}

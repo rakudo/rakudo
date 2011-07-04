3cae1b46d88cea5c4600aeb5ce8edfb6631a4ac4
Moritz Lenz <moritz@faui2k3.org>
2011-07-04 10:06:24 +0200

    constants should be "my", jnthn++

--- src/core/Num.pm
+++ src/core/Num.pm
@@ -140,8 +140,8 @@ my class Num {
     }
 }
 
-constant pi = 3.14159265e0;
-constant e  = 2.71828183e0;
+my constant pi = 3.14159265e0;
+my constant e  = 2.71828183e0;
 
 multi prefix:<++>(Num:D \$a is rw) {   # XXX
     $a = nqp::p6box_n(nqp::add_n(nqp::unbox_n($a), 1))

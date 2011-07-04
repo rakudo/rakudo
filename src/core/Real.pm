ce2882f06ca30f62387d21194f032f284e0c9d26
Moritz Lenz <moritz@faui2k3.org>
2011-07-04 20:37:47 +0200

    add infix mod

--- src/core/Real.pm
+++ src/core/Real.pm
@@ -54,3 +54,8 @@ multi infix:«>=»(Real \$a, Real \$b)  { $a.Bridge >= $b.Bridge }
 
 multi infix:<cmp>(Real \$a, Real \$b) { $a.Bridge cmp $b.Bridge }
 
+proto sub infix:<mod>(|$) {*}
+multi sub infix:<mod>(Real $a, Real $b) {
+    $a - ($a div $b) * $b;
+}
+

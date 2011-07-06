1f8fe70bf32e3416e3da803fceced76a03d8d156
Moritz Lenz <moritz@faui2k3.org>
2011-07-06 20:00:53 +0200

    Pair.ACCEPTS

--- src/core/Pair.pm
+++ src/core/Pair.pm
@@ -1,5 +1,12 @@
 my class Pair is Enum does Associative {
     # method value() is rw { ... }
+
+    multi method ACCEPTS(Pair:D: %h) {
+        $.value.ACCEPTS(%h{%.key});
+    }
+    multi method ACCEPTS(Pair:D: Mu $other) {
+        $other."$.key"().Bool === $.value.Bool
+    }
 }
 
 sub infix:«=>»($key, Mu $value) { 

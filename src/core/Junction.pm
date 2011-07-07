b4d5e56a078dfb20d738feb706332c238265ad9a
pmichaud <pmichaud@pobox.com>
2011-07-07 11:12:31 -0500

    Add Junction.ACCEPTS.

--- src/core/Junction.pm
+++ src/core/Junction.pm
@@ -19,6 +19,25 @@ my class Junction is Mu {
         $count == 1;
     }
 
+    multi method Str(Junction:D:) {
+        self.new($!storage.map({$_.Str}), :type($!type))
+    }
+
+    multi method ACCEPTS(Junction:D: Mu \$topic) {
+        ($!storage.map({return True if $_.ACCEPTS($topic)}).gimme(*); return False)
+            if $!type eq 'any';
+        ($!storage.map({return False unless $_.ACCEPTS($topic)}).gimme(*); return True) 
+            if $!type eq 'all';
+        ($!storage.map({return False if $_.ACCEPTS($topic)}).gimme(*); return True)
+            if $!type eq 'none';
+        # 'one' junction
+        my $count = 0;
+        $!storage.map({ $count++ if $_.ACCEPTS($topic); return False if $count > 1 }).gimme(*);
+        $count == 1;
+    }
+
+    
+
     submethod BUILD(:$!storage, :$!type) { }
 
     multi method perl(Junction:D:) { 

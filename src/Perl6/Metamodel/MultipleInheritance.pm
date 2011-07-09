98baf0f9dd8ed03b486df45cf37d2834be55b020
Jonathan Worthington <jnthn@jnthn.net>
2011-07-09 20:41:01 +0200

    Implement :tree in .^parents.

--- src/Perl6/Metamodel/MultipleInheritance.pm
+++ src/Perl6/Metamodel/MultipleInheritance.pm
@@ -21,9 +21,18 @@ role Perl6::Metamodel::MultipleInheritance {
     }
 
     # Introspects the parents.
-    method parents($obj, :$local) {
+    method parents($obj, :$local, :$tree) {
         if $local {
             @!parents
+        }
+        elsif $tree {
+            my @result;
+            for @!parents {
+                my @pt := [$_];
+                @pt.push($_.HOW.parents($_, :tree(1)));
+                @result.push(pir::perl6ize_type__PP(@pt).Array.item);
+            }
+            return pir::perl6ize_type__PP(@result);
         }
         else {
             # All parents is MRO minus the first thing (which is us).

b58cea24dde1b7a1a6702a6f72d0a40276feec42
Jonathan Worthington <jnthn@jnthn.net>
2011-07-13 23:56:37 +0200

    Factor MRO-based type checking out into a role.

--- src/Perl6/Metamodel/ClassHOW.pm
+++ src/Perl6/Metamodel/ClassHOW.pm
@@ -10,6 +10,7 @@ class Perl6::Metamodel::ClassHOW
     does Perl6::Metamodel::DefaultParent
     does Perl6::Metamodel::C3MRO
     does Perl6::Metamodel::MROBasedMethodDispatch
+    does Perl6::Metamodel::MROBasedTypeChecking
     does Perl6::Metamodel::BUILDPLAN
     does Perl6::Metamodel::Mixins
     does Perl6::Metamodel::NonGeneric
@@ -69,53 +70,11 @@ class Perl6::Metamodel::ClassHOW
         $obj
     }
     
-    method publish_type_cache($obj) {
-        my @tc;
-        for self.mro($obj) {
-            @tc.push($_);
-            if pir::can($_.HOW, 'does_list') {
-                my @does_list := $_.HOW.does_list($_);
-                for @does_list {
-                    @tc.push($_);
-                }
-            }
-        }
-        pir::publish_type_check_cache($obj, @tc)
-    }
-    
     method does_list($obj) {
         @!does_list
     }
     
     method is_composed($obj) {
         $!composed
-    }
-    
-    method isa($obj, $type) {
-        my $decont := pir::nqp_decontainerize__PP($type);
-        for self.mro($obj) {
-            if pir::nqp_decontainerize__PP($_) =:= $decont { return 1 }
-        }
-        0
-    }
-    
-    method type_check($obj, $checkee) {
-        # The only time we end up in here is if the type check cache was
-        # not yet published, which means the class isn't yet fully composed.
-        # Just hunt through MRO.
-        for self.mro($obj) {
-            if $_ =:= $checkee {
-                return 1;
-            }
-            if pir::can($_.HOW, 'does_list') {
-                my @does_list := $_.HOW.does_list($_);
-                for @does_list {
-                    if $_ =:= $checkee {
-                        return 1;
-                    }
-                }
-            }
-        }
-        0
     }
 }

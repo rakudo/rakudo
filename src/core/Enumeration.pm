1084674c48a3eb07047f1e5dd6555c7009f94313
Jonathan Worthington <jnthn@jnthn.net>
2011-07-13 14:01:52 +0200

    Stub in roles where we'll put extra methods that enumerations carry.

---
+++ src/core/Enumeration.pm
@@ -0,0 +1,8 @@
+# Method that we have on enumeration types.
+my role Enumeration {
+}
+
+# Methods that we also have if the base type of an enumeration is
+# Numeric.
+my role NumericEnumeration {
+}

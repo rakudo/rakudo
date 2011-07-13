a8d4770f3f64b93db623d36dc1a61f6eb3469bab
Jonathan Worthington <jnthn@jnthn.net>
2011-07-13 14:02:24 +0200

    Get BaseType to compute an MRO.

--- src/Perl6/Metamodel/BaseType.pm
+++ src/Perl6/Metamodel/BaseType.pm
@@ -3,8 +3,25 @@
 # like they support it.
 role Perl6::Metamodel::BaseType {
     has $!base_type;
+    has $!base_type_set;
+    has @!mro;
     
     method set_base_type($obj, $base_type) {
+        if $!base_type_set {
+            pir::die("Base type has already been set for " ~ self.name($obj));
+        }
         $!base_type := $base_type;
+        $!base_type_set := 1;
+    }
+    
+    # Our MRO is just that of base type.
+    method mro($obj) {
+        unless @!mro {
+            @!mro[0] := $!base_type;
+            for $!base_type.HOW.mro($!base_type) {
+                @!mro.push($_);
+            }
+        }
+        @!mro
     }
 }

rakudo/rakudo

commit ef31ceff9b788b0e0ac3bcd464673fb5aed626e1

Make n/1 Rats be Ints in sequence generation

src/core/operators.pm
@@ -39,6 +39,9 @@ sub SEQUENCE($left, $right, :$exclude_end) {
             $tail.munch($tail.elems - $count);
             $value := $code(|$tail);
             last if $value ~~ $endpoint;
+            if $value.isa(Rat) && $value.denominator == 1 {
+                $value := $value.Int
+            }
             $tail.push($value);
             take $value;
         }

commit 6b3b6011c3c11386f165fe33793023c1ce2412b2

Fix regression introduced in ef31cef.  According to S03-sequence/basic.t, a sequence of Rats should remain a sequence of Rats, even if the denominators are 1.  Instead of narrowing values, narrow the geometric factor whenever its denominator is 1.

src/core/operators.pm
@@ -39,9 +39,6 @@ sub SEQUENCE($left, $right, :$exclude_end) {
             $tail.munch($tail.elems - $count);
             $value := $code(|$tail);
             last if $value ~~ $endpoint;
-            if $value.isa(Rat) && $value.denominator == 1 {
-                $value := $value.Int
-            }
             $tail.push($value);
             take $value;
         }
@@ -83,6 +80,7 @@ sub SEQUENCE($left, $right, :$exclude_end) {
                 elsif $a != 0 && $b != 0 && $c != 0 {
                     $ab = $b / $a;
                     if $ab == $c / $b {
+                        $ab = $ab.Int if $ab ~~ Rat && $ab.denominator == 1;
                         $code = { $^x * $ab }
                     }
                 }
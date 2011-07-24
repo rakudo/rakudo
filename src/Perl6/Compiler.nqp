fedd5b3f76e1eb6afa0932ae4a3b0e58db298033
pmichaud <pmichaud@pobox.com>
2011-07-24 16:58:20 -0500

    Add initial version of @*ARGS.

--- src/Perl6/Compiler.nqp
+++ src/Perl6/Compiler.nqp
@@ -2,6 +2,12 @@ use NQPP6Regex;
 use QRegex;
 
 class Perl6::Compiler is HLL::Compiler {
+    method command_eval(*@args, *%options) {
+        my $hll_ns := pir::get_root_global__Ps('perl6');
+        $hll_ns<@!ARGS> := @args;
+        my $super := pir::find_method__PPs(HLL::Compiler, 'command_eval');
+        $super(self, |@args, |%options);
+    }
 }
 
 
--- src/core/terms.pm
+++ src/core/terms.pm
@@ -1,2 +1,11 @@
 sub term:<time>() { nqp::p6box_i(pir::time__I()) }
 
+{
+    my @ARGS;
+    my Mu $argiter := nqp::iterator(pir::get_hll_global__Ps('@!ARGS'));
+    $argiter := nqp::iterator(pir::get_hll_global__Ps('@!ARGS'));
+    nqp::shift($argiter) if $argiter;
+    @ARGS.push(nqp::p6box_s(nqp::shift($argiter))) while $argiter;
+    nqp::bindkey(pir::get_who__PP(PROCESS), '@ARGS', @ARGS);
+}
+

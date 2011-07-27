8c37086967c336985a4cb5bb088a22fe7459784f
kboga <kristof@PCHOME.(none)>
2011-07-27 17:35:08 +0200

    Implemented @*INC.

--- src/core/terms.pm
+++ src/core/terms.pm
@@ -16,5 +16,23 @@ sub term:<time>() { nqp::p6box_i(pir::time__I()) }
         %ENV{$key} = nqp::p6box_s(nqp::atkey($env, nqp::unbox_s($key)));
     }
     nqp::bindkey(pir::get_who__PP(PROCESS), '%ENV', %ENV);
+
+    my @INC;
+    my Mu $config := nqp::atpos(pir::getinterp, pir::const::IGLOBALS_CONFIG_HASH);
+    # add PERL6LIB
+    @INC.push(%ENV<PERL6LIB>.split(
+        nqp::p6box_s(nqp::atkey($config, 'osname')) eq 'MSWin32' ?? ':' !! ';'
+    )) if %ENV<PERL6LIB>;
+    # add ~/.perl6/lib
+    @INC.push((%ENV<HOME> // %ENV<HOMEDRIVE> ~ %ENV<HOMEPATH>) ~ '/.perl6/lib');
+    # add the installed Parrot languages/perl6/lib directory
+    @INC.push(
+       nqp::p6box_s(nqp::atkey($config, 'libdir')) ~
+       nqp::p6box_s(nqp::atkey($config, 'versiondir')) ~
+       'languages/perl6/lib'
+    );
+    # add current directory
+    @INC.push('.'); # remove this when 'use lib' works fine
+    nqp::bindkey(pir::get_who__PP(PROCESS), '@INC', @INC);
 }
 

31cb7bc2fe4858ebdb5a13bd5a96bf379e78ff52
Jonathan Worthington <jnthn@jnthn.net>
2011-07-28 00:11:11 +0200

    A couple more methods in Enumeration/NumericEnumeration.

--- src/core/Enumeration.pm
+++ src/core/Enumeration.pm
@@ -2,6 +2,10 @@
 my role Enumeration {
     has $.key;
     
+    multi method gist(::?CLASS:D:) {
+        self.^name ~ '::' ~ $!key
+    }
+    
     method perl() {
         self.defined ??
             (self.^name ~ '::' ~ $!key) !!
@@ -12,4 +16,7 @@ my role Enumeration {
 # Methods that we also have if the base type of an enumeration is
 # Numeric.
 my role NumericEnumeration {
+    multi method Str(::?CLASS:D:) {
+        self.key
+    }
 }

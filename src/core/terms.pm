ad8d7a6a172a7c47732f0d0548e82b9073e4a919
pmichaud <pmichaud@pobox.com>
2011-07-27 00:36:20 -0500

    Add %*ENV.

--- src/core/terms.pm
+++ src/core/terms.pm
@@ -6,5 +6,15 @@ sub term:<time>() { nqp::p6box_i(pir::time__I()) }
     @ARGS.push(nqp::p6box_s(nqp::shift($argiter))) while $argiter;
     nqp::bindkey(pir::get_who__PP(PROCESS), '@ARGS', @ARGS);
     $PROCESS::ARGFILES = ArgFiles.new(:args(@ARGS));
+
+    my %ENV;
+    my Mu $env := pir::new__Ps('Env');
+    my Mu $enviter := nqp::iterator($env);
+    my $key;
+    while $enviter {
+        $key = nqp::p6box_s(pir::shift__SP($enviter));
+        %ENV{$key} = nqp::p6box_s(nqp::atkey($env, nqp::unbox_s($key)));
+    }
+    nqp::bindkey(pir::get_who__PP(PROCESS), '%ENV', %ENV);
 }
 

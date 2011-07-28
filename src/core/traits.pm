56e2229935bdd90c6ff1572a9f2bc6e5878e114b
Jonathan Worthington <jnthn@jnthn.net>
2011-07-28 19:45:57 +0200

    Make $x is readonly work on parameters.

--- src/core/traits.pm
+++ src/core/traits.pm
@@ -17,6 +17,9 @@ multi trait_mod:<is>(Routine:D $r, :$rw!) {
     $r.set_rw();
 }
 
+multi trait_mod:<is>(Parameter:D $param, :$readonly!) {
+    # This is the default.
+}
 multi trait_mod:<is>(Parameter:D $param, :$rw!) {
     $param.set_rw();
 }

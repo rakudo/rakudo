01c72868dc8c4ca713c2963f0bc0bbfc4022e940
Jonathan Worthington <jnthn@jnthn.net>
2011-08-06 00:31:07 +0200

    Turns out that some tests relied on the Parcel.Capture returning Parcel hack, because certain things in Capture were not implemented. This adds .elems, .Str and .gist on Capture.

--- src/core/Capture.pm
+++ src/core/Capture.pm
@@ -28,8 +28,48 @@ my class Capture {
         nqp::p6list($!list, List, Mu);
     }
     
+    method elems(Capture:D:) {
+        nqp::p6box_i(nqp::elems($!list))
+    }
+    
+    method gist(Capture:D:) {
+        my Mu $gist := nqp::list();
+        if $!list {
+            my Mu $iter := nqp::iterator($!list);
+            nqp::push($gist, nqp::unbox_s(nqp::shift($iter).gist)) while $iter;
+        }
+        if $!hash {
+            my Mu $iter := nqp::iterator($!hash);
+            while $iter {
+                my $kv := nqp::shift($iter);
+                nqp::push($gist, nqp::unbox_s((nqp::p6box_s($kv) => $kv.value).gist));
+            }
+        }
+        nqp::p6box_s(nqp::join(' ', $gist))
+    }
+    
+    multi method Str(Capture:D:) {
+        my Mu $str := nqp::list();
+        if $!list {
+            my Mu $iter := nqp::iterator($!list);
+            nqp::push($str, nqp::unbox_s(nqp::shift($iter).Str)) while $iter;
+        }
+        if $!hash {
+            my Mu $iter := nqp::iterator($!hash);
+            while $iter {
+                my $kv := nqp::shift($iter);
+                nqp::push($str, nqp::unbox_s((nqp::p6box_s($kv) => $kv.value).Str));
+            }
+        }
+        nqp::p6box_s(nqp::join(' ', $str))
+    }
+    
     method Capture(Capture:D:) {
         self
+    }
+    
+    method Numeric(Capture:D:) {
+        self.elems
     }
     
     # XXX TODO: Should include the hash part too...

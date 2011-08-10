4aab0e5334e16ac34b14f825db4c67945e7bb1d3
Moritz Lenz <moritz@faui2k3.org>
2011-08-10 22:26:05 +0200

    tweak BackTrace so that you can generate and print a bt easily from an Exception object

--- src/core/BackTrace.pm
+++ src/core/BackTrace.pm
@@ -14,24 +14,38 @@ class BackTraceLine {
 }
 
 class BackTrace is List {
-    method new(Int $offset = 1) {
+    proto method new(|$) {*}
+
+    multi method new(Exception $e, Int $offset = 0) {
+        self.new(nqp::getattr(pir::perl6_decontainerize__PP($e), Exception, '$!ex').backtrace, $offset);
+    }
+
+    # note that parrot backtraces are RPAs, marshalled to us as Parcel
+    multi method new(Parcel $bt, Int $offset = 0) {
         my $new = self.bless(*);
-        my $bt = nqp::atkey(pir::getinterp, 'context').backtrace;
         for $offset .. $bt.elems - 1 {
             next if pir::isnull($bt[$_]<sub>);
-            my Mu $p6sub =
+            my $code = try {
                 pir::perl6_code_object_from_parrot_sub__PP($bt[$_]<sub>);
+            };
             my $line     = $bt[$_]<annotations><line>;
             my $file     = $bt[$_]<annotations><file>;
+            next unless $line && $file;
+            # now *that's* an evil hack
+            last if $file eq 'src/stage2/gen/NQPHLL.pm';
             my $subname  = nqp::p6box_s($bt[$_]<sub>);
             $subname = '<anon>' if $subname.substr(0, 6) eq '_block';
             $new.push: BackTraceLine.new(
                 :$line,
                 :$file,
                 :$subname,
-                :code($p6sub),
+                :$code,
             );
         }
+        # another evil hack:
+        # it seems that the backtrace always starts at line 1
+        # of the script, but we're not interested in that
+        $new.pop if $new[*-1].line == 1;
         $new;
     }
 

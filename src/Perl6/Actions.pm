1d3f337511bb1b990a7fa311266f35764d34146b
Jonathan Worthington <jnthn@jnthn.net>
2011-08-13 22:05:31 +0200

    Factor autogenerate_proto out of routine_def.

--- src/Perl6/Actions.pm
+++ src/Perl6/Actions.pm
@@ -1328,16 +1328,7 @@ class Perl6::Actions is HLL::Actions {
                         $new_proto := $*ST.derive_dispatcher($proto);
                     }
                     else {
-                        # Generate a proto foo(|$) { * }
-                        my $p_past := PAST::Block.new(
-                            :name($block.name), :nsentry(''),
-                            PAST::Stmts.new(),
-                            PAST::Op.new( :pirop('perl6_enter_multi_dispatch_from_onlystar_block P') ));
-                        $outer[0].push(PAST::Stmt.new($p_past));
-                        my @p_params := [hash(is_capture => 1, nominal_type => $*ST.find_symbol(['Mu']) )];
-                        my $p_sig := $*ST.create_signature([$*ST.create_parameter(@p_params[0])]);
-                        add_signature_binding_code($p_past, $p_sig, @p_params);
-                        $new_proto := $*ST.create_code_object($p_past, 'Sub', $p_sig, 1);
+                        $new_proto := self.autogenerate_proto($/, $block.name, $outer[0]);
                     }
 
                     # Install in current scope.
@@ -1390,6 +1381,18 @@ class Perl6::Actions is HLL::Actions {
         my $closure := block_closure(reference_to_code_object($code, $past));
         $closure<sink_past> := PAST::Op.new( :pasttype('null') );
         make $closure;
+    }
+    
+    method autogenerate_proto($/, $name, $install_in) {
+        my $p_past := PAST::Block.new(
+            :name($name), :nsentry(''),
+            PAST::Stmts.new(),
+            PAST::Op.new( :pirop('perl6_enter_multi_dispatch_from_onlystar_block P') ));
+        $install_in.push(PAST::Stmt.new($p_past));
+        my @p_params := [hash(is_capture => 1, nominal_type => $*ST.find_symbol(['Mu']) )];
+        my $p_sig := $*ST.create_signature([$*ST.create_parameter(@p_params[0])]);
+        add_signature_binding_code($p_past, $p_sig, @p_params);
+        $*ST.create_code_object($p_past, 'Sub', $p_sig, 1);
     }
 
     method method_def($/) {

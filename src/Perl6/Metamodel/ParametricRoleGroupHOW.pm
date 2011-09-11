6238f80a9aa2b87df46187c545c5b4c1075356c4
Jonathan Worthington <jnthn@jnthn.net>
2011-09-11 17:53:27 +0200

    Various corrections to the role group meta-object.

--- src/Perl6/Metamodel/ParametricRoleGroupHOW.pm
+++ src/Perl6/Metamodel/ParametricRoleGroupHOW.pm
@@ -12,6 +12,7 @@
 # a particular candidate.
 class Perl6::Metamodel::ParametricRoleGroupHOW
     does Perl6::Metamodel::Naming
+    does Perl6::Metamodel::Stashing
 {
     has @!possibilities;
     has @!add_to_selector;
@@ -28,13 +29,13 @@ class Perl6::Metamodel::ParametricRoleGroupHOW
     }
     
     method new_type(:$name!, :$repr) {
-        my $meta := self.new(:name($name), :selector($selector_creator));
-        pir::repr_type_object_for__PPS($meta, 'Uninstantiable');
+        my $meta := self.new(:name($name), :selector($selector_creator()));
+        self.add_stash(pir::repr_type_object_for__PPS($meta, 'Uninstantiable'));
     }
     
     method add_possibility($obj, $possible) {
-        @!possibilities.push($possible);
-        @!add_to_selector.push($possible);
+        @!possibilities[+@!possibilities] := $possible;
+        @!add_to_selector[+@!add_to_selector] := $possible;
     }
     
     method specialize($obj, *@pos_args, *%named_args) {

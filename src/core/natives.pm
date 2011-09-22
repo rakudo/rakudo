60e9ad7b4d41bd402eb41cba2d00200e2d99fb1c
jnthn <jnthn@jnthn.net>
2011-09-22 13:10:46 +0200

    Fix copy-paste fail noticed by JimmyZ++.

--- src/core/natives.pm
+++ src/core/natives.pm
@@ -7,7 +7,7 @@ my native int16 is repr('P6int') is Int is nativesize(16) { }
 my native int32 is repr('P6int') is Int is nativesize(32) { }
 
 my native num is repr('P6num') is Num { }
-my native num32 is repr('P6int') is Int is nativesize(32) { }
-my native num64 is repr('P6int') is Int is nativesize(64) { }
+my native num32 is repr('P6num') is Num is nativesize(32) { }
+my native num64 is repr('P6num') is Num is nativesize(64) { }
 
 my native str is repr('P6str') is Str { }

f7cc8666092296160c5e60da51de297e5a1d75b1
Moritz Lenz <moritz@faui2k3.org>
2011-10-02 09:34:03 +0200

    infix === is an Any operator

--- src/core/Mu.pm
+++ src/core/Mu.pm
@@ -280,10 +280,6 @@ sub infix:<=:=>(Mu \$x, Mu \$y) {
     nqp::p6bool(nqp::iseq_i(nqp::where($x), nqp::where($y)));
 }
 
-proto infix:<===>(Mu $a?, Mu $b?) { * }
-multi infix:<===>(Mu $a?)         { Bool::True }
-multi infix:<===>(Mu $a, Mu $b)   { $a.defined eq $b.defined && $a.WHICH === $b.WHICH }
-
 proto sub infix:<eqv>(Mu $, Mu $) { * }
 multi sub infix:<eqv>(Mu $a, Mu $b) {
     $a.WHAT === $b.WHAT && $a === $b

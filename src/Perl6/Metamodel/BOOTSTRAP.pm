3c6b5e5b3fe287157082bfbdc1b65375e5d4c884
jnthn <jnthn@jnthn.net>
2011-12-03 23:11:54 +0100

    Fix a bug in generic instantiation of parameters; the container descriptor also needs to be instantiated.

--- src/Perl6/Metamodel/BOOTSTRAP.pm
+++ src/Perl6/Metamodel/BOOTSTRAP.pm
@@ -330,7 +330,9 @@ Parameter.HOW.add_method(Parameter, 'instantiate_generic', sub ($self, $type_env
         # Clone with the type instantiated.
         my $ins      := pir::repr_clone__PP($self);
         my $type     := pir::getattribute__PPPs($self, Parameter, '$!nominal_type');
+        my $cd       := pir::getattribute__PPPs($self, Parameter, '$!container_descriptor');
         my $ins_type := $type.HOW.instantiate_generic($type, $type_environment);
+        my $ins_cd   := $cd ?? $cd.instantiate_generic($type_environment) !! $cd;
         unless $ins_type.HOW.archetypes.generic {
             my $flags := pir::repr_get_attr_int__IPPs($ins, Parameter, '$!flags');
             if $flags +& $SIG_ELEM_NOMINAL_GENERIC {
@@ -338,7 +340,8 @@ Parameter.HOW.add_method(Parameter, 'instantiate_generic', sub ($self, $type_env
                     $flags - $SIG_ELEM_NOMINAL_GENERIC)
             }
         }
-        pir::setattribute__0PPsP($ins, Parameter, '$!nominal_type', $ins_type)
+        pir::setattribute__0PPsP($ins, Parameter, '$!nominal_type', $ins_type);
+        pir::setattribute__0PPsP($ins, Parameter, '$!container_descriptor', $ins_cd)
     });
 Parameter.HOW.add_method(Parameter, 'set_rw', sub ($self) {
         my $dcself := pir::perl6_decontainerize__PP($self);

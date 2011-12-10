1f9310d73d4bfd5773110c5c1b05b8a67cf2eefd
jnthn <jnthn@jnthn.net>
2011-12-10 15:22:29 +0100

    Include variable name (at least, in the scalar case) in uninitialized warnings.

--- src/core/Mu.pm
+++ src/core/Mu.pm
@@ -122,19 +122,28 @@ my class Mu {
     }
     
     proto method Numeric(|$) { * }
-    multi method Numeric(Mu:U:) {
-        note "Use of uninitialized value of type {self.^name} in numeric context";
+    multi method Numeric(Mu:U \$v:) {
+        my $varname := $v.VAR.?name;
+        note "Use of uninitialized " ~ 
+            ($varname ?? "variable $varname" !! "value") ~
+            " of type {self.^name} in numeric context";
         0
     }
     proto method Real(|$) { * }
-    multi method Real(Mu:U:) {
-        note "Use of uninitialized value in of type {self.^name} numeric context";
+    multi method Real(Mu:U \$v:) {
+        my $varname := $v.VAR.?name;
+        note "Use of uninitialized " ~ 
+            ($varname ?? "variable $varname" !! "value") ~
+            " of type {self.^name} in numeric context";
         0
     }
     
     proto method Str(|$) { * }
-    multi method Str(Mu:U:) {
-        note "Use of uninitialized value in of type {self.^name} string context";
+    multi method Str(Mu:U \$v:) {
+        my $varname := $v.VAR.?name;
+        note "Use of uninitialized " ~ 
+            ($varname ?? "variable $varname" !! "value") ~
+            " of type {self.^name} in string context";
         ''
     }
     multi method Str(Mu:D:) {

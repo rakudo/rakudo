8ea0b2b0baadc4949b70f6fd67bbca970814c1ef
Moritz Lenz <moritz@faui2k3.org>
2011-12-15 08:34:27 +0100

    ObjAt needs its own WHICH method, since it is a value type

--- src/core/ObjAt.pm
+++ src/core/ObjAt.pm
@@ -1,4 +1,13 @@
 my class ObjAt {
+    multi method WHICH(ObjAt:D:) {
+        nqp::box_s(
+            nqp::concat_s(
+                nqp::concat_s(nqp::unbox_s(self.^name), '|'),
+                $!value
+            ),
+            ObjAt
+        );
+    }
     multi method Str(ObjAt:D:) {
         nqp::p6box_s(nqp::unbox_s(self));
     }

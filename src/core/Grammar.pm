07374c2eea66ad72f9f9fec8cfd4fa6dbf9a0296
jnthn <jnthn@jnthn.net>
2012-01-05 18:58:29 +0100

    Grammar.parse and Grammar.parsefile should set $/. Resolves RT#107236.

--- src/core/Grammar.pm
+++ src/core/Grammar.pm
@@ -1,12 +1,13 @@
 my class Grammar is Cursor { 
     method parse($target, :$rule = 'TOP', :$actions, *%opt) {
         my $*ACTIONS = $actions;
-        self."!cursor_init"($target, |%opt)."$rule"().MATCH;
+        pir::find_caller_lex__Ps('$/') =
+            self."!cursor_init"($target, |%opt)."$rule"().MATCH;
     }
     method parsefile(Cool $filename as Str, *%opts) {
         my $fh    := open($filename);
         my $match := self.parse($fh.slurp, |%opts);
         $fh.close;
-        $match;
+        pir::find_caller_lex__Ps('$/') = $match;
     }
 }

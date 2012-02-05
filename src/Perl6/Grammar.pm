a177364f19341e4e0d6bdc297202b97e11129f0f
Tadeusz Sośnierz <tadzikes@gmail.com>
2012-02-05 17:54:23 +0100

    Remove trailing whitespace in declarator blocks

--- src/Perl6/Grammar.pm
+++ src/Perl6/Grammar.pm
@@ -164,7 +164,7 @@ grammar Perl6::Grammar is HLL::Grammar {
         {
             if ~$*DOC ne '' {
                 my $cont  := Perl6::Pod::serialize_aos(
-                    [~$*DOC]
+                    [Perl6::Pod::formatted_text(~$*DOC)]
                 )<compile_time_value>;
                 my $block := $*W.add_constant(
                     'Pod::Block::Declarator', 'type_new',

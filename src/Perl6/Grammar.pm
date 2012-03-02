rakudo/rakudo

commit 6588dd3980d073eeff4bd0b99d863fef5f76a32f

X::Syntax::{Missing,Malformed}

src/Perl6/Grammar.pm
@@ -553,13 +553,13 @@ grammar Perl6::Grammar is HLL::Grammar {
         | <?[{]>
             <.newpad>
             <blockoid>
-        | <.panic: 'Missing block'>
+        || { $*W.throw($/, 'X::Syntax::Missing', what =>'block') }
     }
 
     token lambda { '->' | '<->' }
 
     token block($*IMPLICIT = 0) {
-        [ <?[{]> || <.panic: 'Missing block'> ]
+        [ <?[{]> || { $*W.throw($/, 'X::Syntax::Missing', what =>'block') }]
         <.newpad>
         <blockoid>
     }
@@ -570,8 +570,8 @@ grammar Perl6::Grammar is HLL::Grammar {
         [
         | '{YOU_ARE_HERE}' <you_are_here>
         | '{' ~ '}' <statementlist> <?ENDSTMT>
-        | <?terminator> <.panic: 'Missing block'>
-        | <?> <.panic: 'Malformed block'>
+        | <?terminator> || { $*W.throw($/, 'X::Syntax::Missing', what =>'block') }
+        | <?> { $*W.throw($/, 'X::Syntax::Missing', what => 'block') }
         ]
         { $*CURPAD := $*W.pop_lexpad() }
     }
@@ -1348,7 +1348,7 @@ grammar Perl6::Grammar is HLL::Grammar {
                 ]
             || <.panic: "Unable to parse $*PKGDECL definition">
             ]
-        ] || <.panic: "Malformed $*PKGDECL">
+        ] || { $*W.throw($/, 'X::Syntax::Malformed', what => $*PKGDECL) }
     }
 
     token declarator {

src/core/Exceptions.pm
@@ -242,6 +242,15 @@ my class X::Syntax::Confused does X::Syntax {
     method message() { 'Confused' }
 }
 
+my class X::Syntax::Malformed does X::Syntax {
+    has $.what;
+    method message() { "Malformed $.what" }
+}
+my class X::Syntax::Missing does X::Syntax {
+    has $.what;
+    method message() { "Missing $.what" }
+}
+
 my class X::Syntax::SigilWithoutName does X::Syntax {
     method message() { 'Non-declarative sigil is missing its name' }
 }

commit d4dc7d7d95506a56ceae32b4615a51a590d66481

fix regression introduced in 6588dd39, fixes bug #111492

src/Perl6/Grammar.pm
@@ -572,7 +572,7 @@ grammar Perl6::Grammar is HLL::Grammar {
         [
         | '{YOU_ARE_HERE}' <you_are_here>
         | '{' ~ '}' <statementlist> <?ENDSTMT>
-        | <?terminator> || { $*W.throw($/, 'X::Syntax::Missing', what =>'block') }
+        | <?terminator> { $*W.throw($/, 'X::Syntax::Missing', what =>'block') }
         | <?> { $*W.throw($/, 'X::Syntax::Missing', what => 'block') }
         ]
         { $*CURPAD := $*W.pop_lexpad() }
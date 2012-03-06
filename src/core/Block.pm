a5b3ede64787cc95694754afe5bb6a001fa6edb3
Jonathan Worthington <jnthn@jnthn.net>
2012-03-06 10:04:02 +0100

    Keep a list that knows the order we should run LEAVE/KEEP/UNDO, since they should be considered as a single queue.

--- src/core/Block.pm
+++ src/core/Block.pm
@@ -5,6 +5,9 @@ my class Block {
         nqp::existskey($!phasers, nqp::unbox_s($name)) ||
             nqp::bindkey($!phasers, nqp::unbox_s($name), nqp::list());
         nqp::push(nqp::atkey($!phasers, nqp::unbox_s($name)), &block);
+        if $name eq any(<LEAVE KEEP UNDO>) {
+            self.add_phaser('!LEAVE-ORDER', &block);
+        }
     }
     
     method phasers(Str $name) {

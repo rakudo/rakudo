754e4a052bb4366c10751fa6310cf1bbc7cb203d
Moritz Lenz <moritz@faui2k3.org>
2012-04-21 14:54:15 +0200

    succeed in creating error message much more often

--- src/core/Backtrace.pm
+++ src/core/Backtrace.pm
@@ -14,7 +14,7 @@ my class Backtrace is List {
         }
 
         multi method Str(Backtrace::Frame:D:) {
-            my $s = $.subtype;
+            my $s = self.subtype;
             $s ~= ' ' if $s.chars;
             "  in {$s}$.subname at {$.file}:$.line\n"
         }
@@ -67,8 +67,8 @@ my class Backtrace is List {
         # but it turns out that a simple   perl6 -e 'die "foo"'
         # has two bt frames from the mainline; so it's OK to never
         # consider the last one
-        loop (; $idx < $.end; ++$idx) {
-            my $cand = $.at_pos($idx);
+        loop (; $idx < self.end; ++$idx) {
+            my $cand = self.at_pos($idx);
             return $idx unless $cand.is-hidden;
         }
         Int;
@@ -76,7 +76,7 @@ my class Backtrace is List {
 
     method outer-caller-idx(Int $startidx is copy) {
         my %print;
-        my $start   = $.at_pos($startidx).code;
+        my $start   = self.at_pos($startidx).code;
         return $startidx.list unless $start;
         my $current = $start.outer;
         my %outers;
@@ -86,9 +86,9 @@ my class Backtrace is List {
         }
         my @outers;
         loop (my Int $i = $startidx; $i < $.end; ++$i) {
-            if $.at_pos($i).code && %outers{%.at_pos($i).code.static_id} {
+            if self.at_pos($i).code && %outers{self.at_pos($i).code.static_id} {
                 @outers.push: $i;
-                return @outers if $.at_pos($i).is-routine;
+                return @outers if self.at_pos($i).is-routine;
             }
         }
 

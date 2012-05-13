f5288fc65fa6c8132ac6e0cbb713549cb19ff5e3
Moritz Lenz <moritz@faui2k3.org>
2012-05-13 07:54:57 +0200

    flesh out Version a bit
    * fixed constructor to properly handle :plus
    * .Str now properly handles * wildcards and trailing +
    * first shot at ACCEPTS

--- src/core/Version.pm
+++ src/core/Version.pm
@@ -1,16 +1,29 @@
 class Version is List {
     has Bool $.plus = False;
     method new(*@positional, :$plus) {
-        my $new := nextwith(@positional);
-        nqp::bindattr($new, Version, '$!plus', so $plus);
-        $new;
+        self.List::new(@positional).MYBUILD(?$plus);
+    }
+    submethod MYBUILD(Bool $plus) {
+        $!plus = $plus;
+        self;
     }
     multi method Str(Version:D:) {
-        'v' ~ self.join('.');
+        'v' ~ self.map({ $_ ~~ Whatever ?? '*' !! $_}).join('.') ~ ($!plus ?? '+' !! '');
     }
     multi method gist(Version:D:) { self.Str }
     multi method perl(Version:D:) {
         self.^name ~ '.new(' ~ self.List::perl ~ ', :plus(' ~ $!plus.perl ~ '))';
 
+    }
+    multi method ACCEPTS(Version:D: Version:D $other) {
+        for self.kv -> $i, $v {
+            next if $v ~~ Whatever;
+            my $o = $other[$i];
+            return True unless defined $o;
+            next if $o ~~ Whatever;
+            return $.plus if $o after  $v;
+            return False  if $o before $v;
+        }
+        True;
     }
 }

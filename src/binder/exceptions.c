531b9d83a57b7f11b55e4173d586ca84069198fc
Moritz Lenz <moritz@faui2k3.org>
2012-06-06 14:20:11 +0200

    move C infrastructure for typed exception to a separate file
    I want to reuse it later in the binder

---
+++ src/binder/exceptions.c
@@ -0,0 +1,14 @@
+#include "parrot/parrot.h"
+#include "parrot/extend.h"
+#include "exceptions.h"
+PMC * Rakudo_get_thrower(PARROT_INTERP, const char * name) {
+        PMC * const hll_ns = Parrot_hll_get_ctx_HLL_namespace(interp);
+        PMC * const ex_hash = Parrot_ns_find_namespace_global(interp,
+                    hll_ns, Parrot_str_new_constant(interp, "P6EX"));
+        PMC * const thrower = PMC_IS_NULL(ex_hash)
+            ? PMCNULL
+            : VTABLE_get_pmc_keyed_str(interp,
+                    ex_hash, Parrot_str_new(interp, name, 0));
+        return thrower;
+}
+
---
+++ src/binder/exceptions.h
@@ -0,0 +1,1 @@
+PMC * Rakudo_get_thrower(PARROT_INTERP, const char * name);

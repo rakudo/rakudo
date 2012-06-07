242220441742350ef8f8f5b9bea41d8f520013f0
pmichaud <pmichaud@pobox.com>
2012-06-07 13:20:50 -0500

    Allow $flattens param to take Mu as an argument.

--- src/core/MapIter.pm
+++ src/core/MapIter.pm
@@ -6,14 +6,14 @@ my class MapIter is Iterator {
     has $!first;               # Is this the first iterator in the sequence?
     has Mu $!items;            # reified items we haven't consumed yet
 
-    method new($list, $block, $flattens = Bool::True) { 
+    method new($list, $block, Mu $flattens = Bool::True) { 
         my $new := nqp::create(self);
         $new.BUILD(nqp::p6listiter(nqp::list(nqp::p6decont($list)), $new), 
                    $block, $flattens, True);
         $new;
     }
 
-    method BUILD(Mu \$listiter, \$block, \$flattens, $first = False) { 
+    method BUILD(Mu \$listiter, \$block, Mu \$flattens, $first = False) { 
         nqp::bindattr($listiter, ListIter, '$!list', self) if nqp::isconcrete($listiter);
         $!listiter := $listiter; 
         $!block = $block; 

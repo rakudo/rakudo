a56bf7567abb4fd97430191f28ab67cb57a62df3
jnthn <jnthn@jnthn.net>
2012-06-22 15:19:39 +0200

    Provide a way to cleanly set name/ver/auth.

--- src/Perl6/Metamodel/Naming.pm
+++ src/Perl6/Metamodel/Naming.pm
@@ -1,5 +1,8 @@
 role Perl6::Metamodel::Naming {
     has $!name;
+    method set_name($obj, $name) {
+        $!name := $name
+    }
     method name($obj) {
         $!name
     }
--- src/Perl6/Metamodel/Versioning.pm
+++ src/Perl6/Metamodel/Versioning.pm
@@ -2,6 +2,9 @@ role Perl6::Metamodel::Versioning {
     has $!ver;
     has $!auth;
 
-    method ver() { $!ver }
-    method auth() { $!auth }
+    method ver($obj) { $!ver }
+    method auth($obj) { $!auth }
+    
+    method set_ver($obj, $ver) { $!ver := $ver }
+    method set_auth($obj, $auth) { $!auth := $auth }
 }

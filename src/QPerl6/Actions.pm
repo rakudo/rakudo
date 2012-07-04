e1bf5db1c6b258bb32a89450fd4c527ea71a769b
jnthn <jnthn@jnthn.net>
2012-07-04 22:07:57 +0200

    Fix [...], to get 08-var-array.t passing again.

--- src/QPerl6/Actions.pm
+++ src/QPerl6/Actions.pm
@@ -3541,7 +3541,7 @@ class QPerl6::Actions is HLL::Actions {
     }
 
     method circumfix:sym<[ ]>($/) {
-        make PAST::Op.new( :name('&circumfix:<[ ]>'), $<semilist>.ast, :node($/) );
+        make QAST::Op.new( :op('call'), :name('&circumfix:<[ ]>'), $<semilist>.ast, :node($/) );
     }
 
     method circumfix:sym<sigil>($/) {

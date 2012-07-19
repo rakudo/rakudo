c4dd87c44e7bbc6f1b8ffb358d9abe7a8a3a74da
jnthn <jnthn@jnthn.net>
2012-07-19 22:35:39 +0200

    Fix some protos.

--- src/core/Bool.pm
+++ src/core/Bool.pm
@@ -25,23 +25,23 @@ my class Bool {
 }
 
 
-proto prefix:<?>($) { * }
+proto prefix:<?>(Mu $) { * }
 multi prefix:<?>(Bool:D \$a) { $a }
 multi prefix:<?>(Mu \$a) { $a.Bool }
 
-proto prefix:<so>($) { * }
+proto prefix:<so>(Mu $) { * }
 multi prefix:<so>(Bool:D \$a) { $a }
 multi prefix:<so>(Mu \$a) { $a.Bool }
 
-proto prefix:<!>($) { * }
+proto prefix:<!>(Mu $) { * }
 multi prefix:<!>(Bool \$a) { nqp::p6bool($a ?? 0 !! 1) }
 multi prefix:<!>(Mu \$a) { nqp::p6bool($a.Bool ?? 0 !! 1) }
 
-proto prefix:<not>($) { * }
+proto prefix:<not>(Mu $) { * }
 multi prefix:<not>(Bool \$a) { nqp::p6bool($a ?? 0 !! 1) }
 multi prefix:<not>(Mu \$a) { nqp::p6bool($a.Bool ?? 0 !! 1) }
 
-proto prefix:<?^>($) { * }
+proto prefix:<?^>(Mu $) { * }
 multi prefix:<?^>(Mu \$a) { not $a }
 
 proto infix:<?&>(|$)                  { * }

edf1a73cbe35a773e6af4474c494ea2b3957c671
jnthn <jnthn@jnthn.net>
2012-07-21 14:45:29 +0200

    Pair/Whatever/WhateverCode need to be stubbed really early now.

--- src/core/traits.pm
+++ src/core/traits.pm
@@ -1,12 +1,14 @@
 use Perl6::BOOTSTRAP;
 
+# Stub a few things the compiler wants to have really early on.
+my class Pair { ... }
+my class Whatever { ... }
+my class WhateverCode { ... }
+
 # Stub these or we can't use any sigil other than $.
 my role Positional { ... }
 my role Associative { ... }
 my role Callable { ... }
-
-# This is needed for the export trait.
-my class Pair { ... }
 
 # for errors
 my class X::Inheritance::Unsupported { ... }

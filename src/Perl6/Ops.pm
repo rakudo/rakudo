a9dbdeff505db362d05e99d86494c7243220f4df
jnthn <jnthn@jnthn.net>
2012-07-24 23:59:05 +0200

    Add tclc op (could go in NQP also; feel free to move).

--- src/Perl6/Ops.pm
+++ src/Perl6/Ops.pm
@@ -43,6 +43,7 @@ $ops.add_hll_pirop_mapping('perl6', 'p6assoccode', 'perl6_associate_sub_code_obj
 $ops.add_hll_pirop_mapping('perl6', 'p6getouterctx', 'perl6_get_outer_ctx', 'PP');
 $ops.add_hll_pirop_mapping('perl6', 'p6mdthunk', 'perl6_multi_dispatch_thunk', 'PP');
 $ops.add_hll_pirop_mapping('perl6', 'p6mdcandthunk', 'perl6_multi_dispatch_cand_thunk', 'PPi');
+$ops.add_hll_pirop_mapping('perl6', 'tclc', 'titlecase', 'Ss');
 $ops.add_hll_op('perl6', 'p6getcallsig', -> $qastcomp, $op {
     my $reg := $*REGALLOC.fresh_p();
     my $ops := $qastcomp.post_new('Ops', :result($reg));

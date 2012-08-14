217d1e3e331c2bec5969cb1f2f2015507963e941
Moritz Lenz <moritz@faui2k3.org>
2012-08-14 12:06:40 +0200

    switch Bool to sigilless params

--- src/core/Bool.pm
+++ src/core/Bool.pm
@@ -15,7 +15,7 @@ my class Bool {
     method pick(Bool:U: $n = 1) { (Bool::True, Bool::False).pick($n) }
     method roll(Bool:U: $n = 1) { (Bool::True, Bool::False).roll($n) }
 
-    multi method ACCEPTS(Bool:D: Mu \$topic) { self }
+    multi method ACCEPTS(Bool:D: Mu \topic ) { self }
 
     multi method perl(Bool:D:) { self ?? 'Bool::True' !! 'Bool::False' }
 
@@ -24,55 +24,55 @@ my class Bool {
     }
 }
 
-multi prefix:<++>(Bool:U \$a is rw) { $a = True; }
-multi prefix:<-->(Bool:U \$a is rw) { $a = False; }
-multi postfix:<++>(Bool:U \$a is rw) { $a = True; False; }
-multi postfix:<-->(Bool:U \$a is rw) { $a = False; }
+multi prefix:<++>(Bool:U \a is rw)  { a = True; }
+multi prefix:<-->(Bool:U \a is rw)  { a = False; }
+multi postfix:<++>(Bool:U \a is rw) { a = True; False; }
+multi postfix:<-->(Bool:U \a is rw) { a = False; }
 
 proto prefix:<?>(Mu $) { * }
-multi prefix:<?>(Bool:D \$a) { $a }
-multi prefix:<?>(Mu \$a) { $a.Bool }
+multi prefix:<?>(Bool:D \a) { a }
+multi prefix:<?>(Mu \a) { a.Bool }
 
 proto prefix:<so>(Mu $) { * }
-multi prefix:<so>(Bool:D \$a) { $a }
-multi prefix:<so>(Mu \$a) { $a.Bool }
+multi prefix:<so>(Bool:D \a) { a }
+multi prefix:<so>(Mu \a) { a.Bool }
 
 proto prefix:<!>(Mu $) { * }
-multi prefix:<!>(Bool \$a) { nqp::p6bool($a ?? 0 !! 1) }
-multi prefix:<!>(Mu \$a) { nqp::p6bool($a.Bool ?? 0 !! 1) }
+multi prefix:<!>(Bool \a) { nqp::p6bool(a ?? 0 !! 1) }
+multi prefix:<!>(Mu \a) { nqp::p6bool(a.Bool ?? 0 !! 1) }
 
 proto prefix:<not>(Mu $) { * }
-multi prefix:<not>(Bool \$a) { nqp::p6bool($a ?? 0 !! 1) }
-multi prefix:<not>(Mu \$a) { nqp::p6bool($a.Bool ?? 0 !! 1) }
+multi prefix:<not>(Bool \a) { nqp::p6bool(a ?? 0 !! 1) }
+multi prefix:<not>(Mu \a) { nqp::p6bool(a.Bool ?? 0 !! 1) }
 
 proto prefix:<?^>(Mu $) { * }
-multi prefix:<?^>(Mu \$a) { not $a }
+multi prefix:<?^>(Mu \a) { not a }
 
 proto infix:<?&>(|)                  { * }
-multi infix:<?&>(Mu $x = Bool::True)  { $x.Bool }
-multi infix:<?&>(Mu \$a, Mu \$b)      { $a.Bool && $b.Bool }
+multi infix:<?&>(Mu $x = Bool::True) { $x.Bool }
+multi infix:<?&>(Mu \a, Mu \b)       { a.Bool && b.Bool }
 
-proto infix:<?|>(|)                  { * }
+proto infix:<?|>(|)                   { * }
 multi infix:<?|>(Mu $x = Bool::False) { $x.Bool }
-multi infix:<?|>(Mu \$a, Mu \$b)      { $a.Bool || $b.Bool }
+multi infix:<?|>(Mu \a, Mu \b)        { a.Bool || b.Bool }
 
-proto infix:<?^>(|)                  { * }
+proto infix:<?^>(|)                   { * }
 multi infix:<?^>(Mu $x = Bool::False) { $x.Bool }
-multi infix:<?^>(Mu \$a, Mu \$b)      { $a.Bool ^^ $b.Bool }
+multi infix:<?^>(Mu \a, Mu \b)        { a.Bool ^^ b.Bool }
 
 # These operators are normally handled as macros in the compiler;
 # we define them here for use as arguments to functions.
-proto infix:<&&>(|)                  { * }
+proto infix:<&&>(|)                   { * }
 multi infix:<&&>(Mu $x = Bool::True)  { $x }
-multi infix:<&&>(Mu \$a, Mu \$b)      { $a && $b }
+multi infix:<&&>(Mu \a, Mu \b)        { a && b }
 
-proto infix:<||>(|)                  { * }
+proto infix:<||>(|)                   { * }
 multi infix:<||>(Mu $x = Bool::False) { $x }
-multi infix:<||>(Mu \$a, Mu \$b)      { $a || $b }
+multi infix:<||>(Mu \a, Mu \b)        { a || b }
 
-proto infix:<^^>(|)                  { * }
+proto infix:<^^>(|)                   { * }
 multi infix:<^^>(Mu $x = Bool::False) { $x }
-multi infix:<^^>(Mu \$a, Mu \$b)      { $a ^^ $b }
+multi infix:<^^>(Mu \a, Mu \b)        { a ^^ b }
 multi infix:<^^>(*@a) {
     my $a = shift @a;
     while @a {
@@ -84,23 +84,23 @@ multi infix:<^^>(*@a) {
     $a;
 }
 
-proto infix:<//>(|)                  { * }
+proto infix:<//>(|)                   { * }
 multi infix:<//>(Mu $x = Any)         { $x }
-multi infix:<//>(Mu \$a, Mu \$b)      { $a // $b }
+multi infix:<//>(Mu \a, Mu \b)        { a // b }
 
-proto infix:<and>(|)                 { * }
+proto infix:<and>(|)                  { * }
 multi infix:<and>(Mu $x = Bool::True) { $x }
-multi infix:<and>(Mu \$a, Mu \$b)     { $a && $b }
+multi infix:<and>(Mu \a, Mu \b)       { a && b }
 
-proto infix:<or>(|)                  { * }
+proto infix:<or>(|)                   { * }
 multi infix:<or>(Mu $x = Bool::False) { $x }
-multi infix:<or>(Mu \$a, Mu \$b)      { $a || $b }
+multi infix:<or>(Mu \a, Mu \b)        { a || b }
 
-proto infix:<xor>(|)                  { * }
+proto infix:<xor>(|)                   { * }
 multi infix:<xor>(Mu $x = Bool::False) { $x }
-multi infix:<xor>(Mu \$a, Mu \$b)      { $a ^^ $b }
+multi infix:<xor>(Mu \a, Mu \b)        { a ^^ b }
 multi infix:<xor>(*@a)                 { &infix:<^^>(@a); }
 
-proto infix:<orelse>(|)              { * }
+proto infix:<orelse>(|)               { * }
 multi infix:<orelse>(Mu $x = Any)     { $x }
-multi infix:<orelse>(Mu \$a, Mu \$b)  { $a // $b }
+multi infix:<orelse>(Mu \a, Mu \b)    { a // b }

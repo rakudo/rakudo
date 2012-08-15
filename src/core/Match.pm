rakudo/rakudo

commit 94aa2b79d6d68022634b5867e99ab65f05bdd527

switch Match.gist from q[...] to ｢...｣, TimToady++

src/core/Match.pm
@@ -62,7 +62,7 @@ my class Match is Capture is Cool {
     multi method gist (Match:D: $d = 0) {
         return "#<failed match>" unless self;
         my $s = ' ' x ($d + 1);
-        my $r = ("=> " if $d) ~ "q[{self}]\n";
+        my $r = ("=> " if $d) ~ "｢{self}｣\n";
         for @.caps {
             $r ~= $s ~ (.key // '?') ~ ' ' ~ .value.gist($d + 1)
         }

commit e326148fd9f3fa22231406435c02485f5de48aae

avoid codepoints above 255 in the setting
reverts the factor ~2 parse slowdown of the setting introduced in 94aa2b7

src/core/Match.pm
@@ -62,7 +62,7 @@ my class Match is Capture is Cool {
     multi method gist (Match:D: $d = 0) {
         return "#<failed match>" unless self;
         my $s = ' ' x ($d + 1);
-        my $r = ("=> " if $d) ~ "｢{self}｣\n";
+        my $r = ("=> " if $d) ~ "\x[FF62]{self}\x[FF63]\n";
         for @.caps {
             $r ~= $s ~ (.key // '?') ~ ' ' ~ .value.gist($d + 1)
         }
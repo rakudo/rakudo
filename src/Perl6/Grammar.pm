4925c91d7bdc94c473e72b60cc7fb2752cf1e662
jnthn <jnthn@jnthn.net>
2012-11-03 13:02:14 +0100

    Eliminate leftover quoting code; unused since Q.

--- src/Perl6/Grammar.pm
+++ src/Perl6/Grammar.pm
@@ -2585,33 +2585,6 @@ grammar Perl6::Grammar is HLL::Grammar does STD {
         <block>
     }
 
-    token quote_escape:sym<$> {
-        <?[$]>
-        :my $*QSIGIL := '$';
-        <?quotemod_check('s')>
-        [ <EXPR('y=')> || <.panic: "Non-variable \$ must be backslashed"> ]
-    }
-
-    token quote_escape:sym<array> {
-        <?[@]>
-        :my $*QSIGIL := '@';
-        <?quotemod_check('a')> <EXPR('y=')>
-    }
-
-    token quote_escape:sym<%> {
-        <?[%]>
-        :my $*QSIGIL := '%';
-        <?quotemod_check('h')> <EXPR('y=')>
-    }
-
-    token quote_escape:sym<&> {
-        <?[&]>
-        :my $*QSIGIL := '&';
-        <?quotemod_check('f')> <EXPR('y=')>
-    }
-
-    token quote_escape:sym<{ }> { <?[{]> <?quotemod_check('c')> <block> }
-
     token circumfix:sym<( )> { :dba('parenthesized expression') '(' ~ ')' <semilist> }
     token circumfix:sym<[ ]> { :dba('array composer') '[' ~ ']' <semilist> }
     token circumfix:sym<ang> {

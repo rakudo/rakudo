5f588e48d580105f3d18b88ad10a172776793606
jnthn <jnthn@jnthn.net>
2012-11-06 19:43:10 +0100

    Add nqp::p6setfirstflag to replace a pir:: op.

--- src/Perl6/Ops.pm
+++ src/Perl6/Ops.pm
@@ -38,6 +38,7 @@ $ops.add_hll_pirop_mapping('perl6', 'p6getpackage', 'perl6_get_package_through_w
 $ops.add_hll_pirop_mapping('perl6', 'p6stateinit', 'perl6_state_needs_init', 'I');
 $ops.add_hll_pirop_mapping('perl6', 'p6setpre', 'perl6_set_checking_pre', 'v');
 $ops.add_hll_pirop_mapping('perl6', 'p6clearpre', 'perl6_clear_checking_pre', 'v');
+$ops.add_hll_pirop_mapping('perl6', 'p6setfirstflag', 'perl6_set_block_first_flag', '0P');
 $ops.add_hll_pirop_mapping('perl6', 'p6takefirstflag', 'perl6_take_block_first_flag', 'I');
 $ops.add_hll_pirop_mapping('perl6', 'p6return', 'perl6_returncc', '0P');
 $ops.add_hll_pirop_mapping('perl6', 'p6routinereturn', 'perl6_return_from_routine', '0P');

cecc9faa609eb0963331f7280a591457709d78f3
Moritz Lenz <moritz@faui2k3.org>
2012-11-18 11:58:03 +0100

    replace QAST::VM with QAST::Op

--- src/Perl6/Actions.pm
+++ src/Perl6/Actions.pm
@@ -339,8 +339,8 @@ class Perl6::Actions is HLL::Actions does STDActions {
                 ),
                 QAST::Op.new(
                     :op('if'),
-                    QAST::VM.new(
-                        :pirop('can IPs'),
+                    QAST::Op.new(
+                        :op<can>,
                         QAST::Var.new( :name('ctxsave'), :scope('local') ),
                         QAST::SVal.new( :value('ctxsave') )
                     ),

17bcd9649369c2f059ec6158f4ecbc3ef90bb8d3
Moritz Lenz <moritz@faui2k3.org>
2012-12-02 22:11:18 +0100

    switch to real constants in Parameter.pm

--- src/core/Parameter.pm
+++ src/core/Parameter.pm
@@ -1,22 +1,22 @@
 my class Parameter {
     # XXX constant...
-    my $SIG_ELEM_BIND_CAPTURE       := 1;
-    my $SIG_ELEM_BIND_PRIVATE_ATTR  := 2;
-    my $SIG_ELEM_BIND_PUBLIC_ATTR   := 4;
-    my $SIG_ELEM_SLURPY_POS         := 8;
-    my $SIG_ELEM_SLURPY_NAMED       := 16;
-    my $SIG_ELEM_SLURPY_BLOCK       := 32;
-    my $SIG_ELEM_INVOCANT           := 64;
-    my $SIG_ELEM_MULTI_INVOCANT     := 128;
-    my $SIG_ELEM_IS_RW              := 256;
-    my $SIG_ELEM_IS_COPY            := 512;
-    my $SIG_ELEM_IS_PARCEL          := 1024;
-    my $SIG_ELEM_IS_OPTIONAL        := 2048;
-    my $SIG_ELEM_ARRAY_SIGIL        := 4096;
-    my $SIG_ELEM_HASH_SIGIL         := 8192;
-    my $SIG_ELEM_IS_CAPTURE         := 32768;
-    my $SIG_ELEM_UNDEFINED_ONLY     := 65536;
-    my $SIG_ELEM_DEFINED_ONLY       := 131072;
+    constant $SIG_ELEM_BIND_CAPTURE       = 1;
+    constant $SIG_ELEM_BIND_PRIVATE_ATTR  = 2;
+    constant $SIG_ELEM_BIND_PUBLIC_ATTR   = 4;
+    constant $SIG_ELEM_SLURPY_POS         = 8;
+    constant $SIG_ELEM_SLURPY_NAMED       = 16;
+    constant $SIG_ELEM_SLURPY_BLOCK       = 32;
+    constant $SIG_ELEM_INVOCANT           = 64;
+    constant $SIG_ELEM_MULTI_INVOCANT     = 128;
+    constant $SIG_ELEM_IS_RW              = 256;
+    constant $SIG_ELEM_IS_COPY            = 512;
+    constant $SIG_ELEM_IS_PARCEL          = 1024;
+    constant $SIG_ELEM_IS_OPTIONAL        = 2048;
+    constant $SIG_ELEM_ARRAY_SIGIL        = 4096;
+    constant $SIG_ELEM_HASH_SIGIL         = 8192;
+    constant $SIG_ELEM_IS_CAPTURE         = 32768;
+    constant $SIG_ELEM_UNDEFINED_ONLY     = 65536;
+    constant $SIG_ELEM_DEFINED_ONLY       = 131072;
 
     method name() {
         $!variable_name

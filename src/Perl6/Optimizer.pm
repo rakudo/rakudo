94949561a225e43874b9b607d7762ad4e52c1068
Timo Paulssen <timonator@perpetuum-immobile.de>
2013-01-27 20:45:14 +0100

    why not make this a level 2 optimization? :)

--- src/Perl6/Optimizer.pm
+++ src/Perl6/Optimizer.pm
@@ -216,7 +216,7 @@ class Perl6::Optimizer {
         }
 
         # we may be able to unfold a junction at compile time.
-        if $*LEVEL >= 3 && is_outer_foldable() && nqp::istype($op[0], QAST::Op) && $op[0].op eq "chain" {
+        if $*LEVEL >= 2 && is_outer_foldable() && nqp::istype($op[0], QAST::Op) && $op[0].op eq "chain" {
             my $exp-side := self.can_chain_junction_be_warped($op[0]);
             if $exp-side != -1 {
                 my $juncop := $op[0][$exp-side].name eq '&infix:<&>' ?? 'if' !! 'unless';

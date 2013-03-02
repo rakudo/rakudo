ec1c81ae2d893b13422b95dc94ea4f9230208a26
jnthn <jnthn@jnthn.net>
2013-03-02 13:46:56 +0100

    Toss dead code.

--- src/Perl6/Ops.pm
+++ src/Perl6/Ops.pm
@@ -11,7 +11,6 @@ $ops.add_hll_pirop_mapping('perl6', 'p6list', 'perl6_list_from_rpa', 'PPPP', :in
 $ops.add_hll_pirop_mapping('perl6', 'p6listitems', 'perl6_listitems', 'PP', :inlinable(1));
 $ops.add_hll_pirop_mapping('perl6', 'p6decont', 'perl6_decontainerize', 'PP', :inlinable(1));
 $ops.add_hll_pirop_mapping('perl6', 'p6recont_ro', 'perl6_recontainerize_to_ro', 'PP', :inlinable(1));
-$ops.add_hll_pirop_mapping('perl6', 'callerid', 'perl6_callerid', 'I');
 $ops.add_hll_pirop_mapping('perl6', 'ishash', 'perl6_is_hash', 'IP', :inlinable(1));
 $ops.add_hll_pirop_mapping('perl6', 'p6store', 'perl6_container_store', '0PP', :inlinable(1));
 $ops.add_hll_pirop_mapping('perl6', 'p6type', 'perl6ize_type', 'PP', :inlinable(1));

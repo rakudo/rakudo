e93d1c97736faf241f87875b3dee6143fbcd88f9
kboga <kristofbogaerts@gmail.com>
2013-03-03 16:21:24 +0100

    pir::compreg__Ps -> nqp::getcomp

--- src/Perl6/ModuleLoader.pm
+++ src/Perl6/ModuleLoader.pm
@@ -171,7 +171,7 @@ class Perl6::ModuleLoader {
                 # Get the compiler and compile the code, then run it
                 # (which runs the mainline and captures UNIT).
                 my $?FILES   := %chosen<pm>;
-                my $eval     := pir::compreg__Ps('perl6').compile($source);
+                my $eval     := nqp::getcomp('perl6').compile($source);
                 my $*CTXSAVE := self;
                 my $*MAIN_CTX;
                 $eval();

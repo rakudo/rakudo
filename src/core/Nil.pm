af2e7e1b9c1516d44fbeeb50ff4c027424d71e33
Moritz Lenz <moritz@faui2k3.org>
2013-03-05 21:07:58 +0100

    bring Nil more in line with newest spec

--- src/core/Nil.pm
+++ src/core/Nil.pm
@@ -3,7 +3,10 @@ my class Nil is Iterator is Cool {
     method iterator() { self }
     method reify($n?) { () }
     method gist() { 'Nil' }
+    method perl() { 'Nil' }
     multi method Str() { '' }
+    method ACCEPTS(|) { Nil }
+    method Bool(|) { False }
 }
 
 

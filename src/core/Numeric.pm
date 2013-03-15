5c491115aa8ce81a40e35e509a3a568a4806a4c8
jnthn <jnthn@jnthn.net>
2013-03-15 20:50:00 +0100

    Fix a proto.

--- src/core/Numeric.pm
+++ src/core/Numeric.pm
@@ -170,9 +170,9 @@ proto sub ceiling($) is pure   { * }
 multi sub ceiling($a)          { $a.Numeric.ceiling }
 multi sub ceiling(Numeric $a)  { $a.ceiling }
 
-proto sub round($) is pure   { * }
-multi sub round($a)          { $a.Numeric.round }
-multi sub round(Numeric $a)  { $a.round }
+proto sub round($, $?) is pure      { * }
+multi sub round($a)                 { $a.Numeric.round }
+multi sub round(Numeric $a)         { $a.round }
 multi sub round(Numeric $a, $scale) { $a.round($scale) }
 
 proto infix:<+>($a?, $b?) is pure   { * }

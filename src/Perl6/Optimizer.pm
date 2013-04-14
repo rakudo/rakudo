05ec31b973390b9e6fc6b6baa7b895c921a16c95
Timo Paulssen <timonator@perpetuum-immobile.de>
2013-04-14 12:40:17 +0200

    cache symbols found in SETTING.

--- src/Perl6/Optimizer.pm
+++ src/Perl6/Optimizer.pm
@@ -35,6 +35,7 @@ class Perl6::Optimizer {
 
     # The Setting, which contains things like Signature and Parameter.
     has $!SETTING;
+    has %!SETTING_CACHE;
 
     has %!foldable_junction;
     has %!foldable_outer;
@@ -174,21 +175,32 @@ class Perl6::Optimizer {
         return 0;
     }
 
-    method find_setting() {
-        if nqp::defined($!SETTING) {
-            return $!SETTING;
-        }
-        my int $i := +@!block_stack;
-        while $i > 0 {
-            $i := $i - 1;
-            my $block := @!block_stack[$i];
-            my %sym := $block.symbol("!CORE_MARKER");
-            if +%sym {
-                $!SETTING := $block;
-                return $block;
+    method find_in_setting($symbol) {
+        if !nqp::defined($!SETTING) {
+            my int $i := +@!block_stack;
+            while $i > 0 && !nqp::defined($!SETTING) {
+                $i := $i - 1;
+                my $block := @!block_stack[$i];
+                my %sym := $block.symbol("!CORE_MARKER");
+                if +%sym {
+                    $!SETTING := $block;
+                }
+            }
+            nqp::die("Optimizer couldn't find CORE while looking for $symbol.");
+        } else {
+            if nqp::existskey(%!SETTING_CACHE, $symbol) {
+                return %!SETTING_CACHE{$symbol};
             }
         }
-        nqp::die("Optimizer couldn't find CORE.");
+        my %sym := $!SETTING.symbol($symbol);
+        if +%sym {
+            if nqp::existskey(%sym, 'value') {
+                %!SETTING_CACHE{$symbol} := %sym<value>;
+                return %sym<value>;
+            } else {
+                nqp::die("Optimizer: cannot find $symbol in SETTING.");
+            }
+        }
     }
 
     method can_chain_junction_be_warped($node) {
@@ -254,7 +266,7 @@ class Perl6::Optimizer {
                 $found := 1;
             }
             if $found == 1 {
-                my $signature := self.find_setting().symbol("Signature")<value>;
+                my $signature := self.find_in_setting("Signature");
                 my $iter := nqp::iterator(nqp::getattr($obj.signature, $signature, '$!params'));
                 while $iter {
                     my $p := nqp::shift($iter);

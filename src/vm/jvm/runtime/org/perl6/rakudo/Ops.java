eba83b58119c0cdaed2989a63a7a02198ff3ce42
jnthn <jnthn@jnthn.net>
2013-05-11 17:12:45 +0200

    Implement nqp::p6decontrv.

--- src/vm/jvm/runtime/org/perl6/rakudo/Ops.java
+++ src/vm/jvm/runtime/org/perl6/rakudo/Ops.java
@@ -9,6 +9,7 @@ import org.perl6.nqp.sixmodel.*;
 public final class Ops {
     private static SixModelObject Parcel;
     private static SixModelObject Code;
+    private static SixModelObject Routine;
     private static SixModelObject Signature;
     public static SixModelObject Parameter;
     public static SixModelObject Int;
@@ -16,6 +17,7 @@ public final class Ops {
     public static SixModelObject Str;
     public static SixModelObject Junction;
     public static SixModelObject Scalar;
+    private static SixModelObject ContainerDescriptor;
     private static SixModelObject False;
     private static SixModelObject True;
     private static boolean initialized = false;
@@ -24,7 +26,9 @@ public final class Ops {
     private static final int HINT_PARCEL_STORAGE = 0;
     private static final int HINT_CODE_DO = 0;
     private static final int HINT_CODE_SIG = 1;
+    private static final int HINT_ROUTINE_RW = 7;
     private static final int HINT_SIG_PARAMS = 0;
+    private static final int HINT_CD_RW = 1;
     
     public static SixModelObject p6init(ThreadContext tc) {
         if (!initialized) {
@@ -37,6 +41,7 @@ public final class Ops {
     public static SixModelObject p6settypes(SixModelObject conf, ThreadContext tc) {
         Parcel = conf.at_key_boxed(tc, "Parcel");
         Code = conf.at_key_boxed(tc, "Code");
+        Routine = conf.at_key_boxed(tc, "Routine");
         Signature = conf.at_key_boxed(tc, "Signature");
         Parameter = conf.at_key_boxed(tc, "Parameter");
         Int = conf.at_key_boxed(tc, "Int");
@@ -44,6 +49,7 @@ public final class Ops {
         Str = conf.at_key_boxed(tc, "Str");
         Junction = conf.at_key_boxed(tc, "Junction");
         Scalar = conf.at_key_boxed(tc, "Scalar");
+        ContainerDescriptor = conf.at_key_boxed(tc, "ContainerDescriptor");
         False = conf.at_key_boxed(tc, "False");
         True = conf.at_key_boxed(tc, "True");
         return conf;
@@ -101,5 +107,30 @@ public final class Ops {
         }
 
         return parcel;
+    }
+    
+    public static SixModelObject p6decontrv(SixModelObject cont, ThreadContext tc) {
+        if (isRWScalar(tc, cont)) {
+            tc.curFrame.codeRef.codeObject.get_attribute_native(tc, Routine, "$!rw", HINT_ROUTINE_RW);
+            if (tc.native_i == 0) {
+                /* Recontainerize to RO. */
+                SixModelObject roCont = Scalar.st.REPR.allocate(tc, Scalar.st);
+                roCont.bind_attribute_boxed(tc, Ops.Scalar, "$!value",
+                    RakudoContainerSpec.HINT_value,
+                    cont.st.ContainerSpec.fetch(tc, cont));
+                return roCont;
+            }
+        }
+        return cont;
+    }
+    
+    private static boolean isRWScalar(ThreadContext tc, SixModelObject check) {
+        if (!(check instanceof TypeObject) && check.st.WHAT == Scalar) {
+            SixModelObject desc = check.get_attribute_boxed(tc, Scalar, "$!descriptor",
+                RakudoContainerSpec.HINT_descriptor);
+            desc.get_attribute_native(tc, ContainerDescriptor, "$!rw", HINT_CD_RW);
+            return tc.native_i != 0;
+        }
+        return false;
     }
 }

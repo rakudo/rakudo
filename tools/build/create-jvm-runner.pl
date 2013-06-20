d1b3d1f49120862a943c77a543e04204c975c6a9
Stefan O'Rear <stefanor@cox.net>
2013-06-20 04:54:49 -0700

    Work-in-progress test scripts

--- tools/build/create-jvm-runner.pl
+++ tools/build/create-jvm-runner.pl
@@ -5,22 +5,32 @@ use strict;
 use warnings;
 use 5.008;
 use File::Spec;
+use File::Copy 'cp';
 
 my ($prefix, $nqpprefix, $thirdpartyjars) = @ARGV;
 
 my $cpsep = $^O eq 'MSWin32' ? ';' : ':';
-my $install_to = File::Spec->catfile($prefix, $^O eq 'MSWin32' ? 'perl6.bat' : 'perl6');
+my $bat   = $^O eq 'MSWin32' ? '.bat' : '';
 
 my $preamble = $^O eq 'MSWin32' ? '@' : '#!/bin/sh
 exec ';
-my $postamble = $^O eq 'MSWin32' ? '%*' : '"$@"';
+my $postamble = $^O eq 'MSWin32' ? ' %*' : ' "$@"';
 
-open my $fh, ">", $install_to;
-print $fh $preamble;
-print $fh 'java -Xms100m -Xbootclasspath/a:' . $prefix . $cpsep . $thirdpartyjars . $cpsep . 'rakudo-runtime.jar'
-        . ' -cp ' . $nqpprefix . ' perl6 ';
-print $fh $postamble;
-print $fh "\n";
-close $fh;
+sub install {
+    my ($name, $command) = @_;
 
-chmod 0755, $install_to if $^O ne 'MSWin32';
+    my $install_to = File::Spec->catfile($prefix, "$name$bat");
+
+    open my $fh, ">", $install_to or die "open: $!";
+    print $fh $preamble, $command, $postamble, "\n" or die "print: $!";
+    close $fh or die "close: $!";
+
+    chmod 0755, $install_to if $^O ne 'MSWin32';
+}
+
+my $jopts = '-Xms100m -Xbootclasspath/a:' . $prefix . $cpsep . $thirdpartyjars . $cpsep . 'rakudo-runtime.jar -cp ' . $nqpprefix;
+
+install "perl6", "java $jopts perl6";
+install "perl6-jdb-server", "java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n $jopts perl6";
+install "perl6-eval-server", "java $jopts org.perl6.nqp.runtime.EvalServer TESTTOKEN " . File::Spec->catfile($prefix,"perl6.class");
+cp(File::Spec->catfile($nqpprefix,"eval-client.pl"), ".");

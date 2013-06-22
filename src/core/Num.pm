30c82c76c3c7ab7eee932fac3a3cb1435b9cfb44
Jonathan Worthington <jnthn@jnthn.net>
2013-06-22 03:19:38 +0200

    Fix Num.new

--- src/core/Num.pm
+++ src/core/Num.pm
@@ -19,7 +19,7 @@ my class Num does Real {
             nqp::fromnum_I(nqp::unbox_n(self), Int);
     }
 
-    multi method new() { nqp::box_n(0, self) }
+    multi method new() { nqp::box_n(0e0, self) }
     multi method new($n) { nqp::box_n($n.Num, self) }
     
     multi method perl(Num:D:) {

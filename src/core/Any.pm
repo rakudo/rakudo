9799d8854fc5f63e7c54ae1415c6b89153c9e461
Timo Paulssen <timonator@perpetuum-immobile.de>
2013-06-23 04:05:53 +0200

    a first attempt at postcircumfix:<[ ]> with LoL.

--- src/core/Any.pm
+++ src/core/Any.pm
@@ -275,6 +275,16 @@ my class Any {
         X::Bind::Slice.new(type => self.WHAT).throw;
     }
 
+    multi method postcircumfix:<[ ]>(\SELF: LoL $coords) is rw {
+        my $subcoords = $coords[1..^*].lol;
+        if +@$subcoords == 1 {
+            $subcoords = $subcoords[0].list;
+        }
+        return-rw do for $coords[0].list {
+            SELF[$_][@$subcoords];
+        }
+    }
+
     proto method at_pos(|) {*}
     multi method at_pos(Any:D: $pos) {
         fail X::OutOfRange.new(

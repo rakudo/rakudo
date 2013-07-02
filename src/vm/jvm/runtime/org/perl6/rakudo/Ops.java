284a76f0a8b5a2af1aaa310f9c5a256a1c74fec5
jnthn <jnthn@jnthn.net>
2013-07-02 18:02:09 +0200

    Implement nqp::p6decodelocaltime.

--- src/vm/jvm/runtime/org/perl6/rakudo/Ops.java
+++ src/vm/jvm/runtime/org/perl6/rakudo/Ops.java
@@ -1,6 +1,7 @@
 package org.perl6.rakudo;
 
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Comparator;
 import org.perl6.nqp.runtime.*;
 import org.perl6.nqp.sixmodel.*;
@@ -681,5 +682,29 @@ public final class Ops {
         }
         
         return dispatcher;
+    }
+    
+    public static SixModelObject p6decodelocaltime(long sinceEpoch, ThreadContext tc) {
+        // Get calendar for current local host's timezone.
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(sinceEpoch * 1000);
+        
+        // Populate result int array.
+        SixModelObject BOOTIntArray = tc.gc.BOOTIntArray;
+        SixModelObject result = BOOTIntArray.st.REPR.allocate(tc, BOOTIntArray.st);
+        tc.native_i = c.get(Calendar.SECOND);
+        result.bind_pos_native(tc, 0);
+        tc.native_i = c.get(Calendar.MINUTE);
+        result.bind_pos_native(tc, 1);
+        tc.native_i = c.get(Calendar.HOUR_OF_DAY);
+        result.bind_pos_native(tc, 2);
+        tc.native_i = c.get(Calendar.DAY_OF_MONTH);
+        result.bind_pos_native(tc, 3);
+        tc.native_i = c.get(Calendar.MONTH) + 1;
+        result.bind_pos_native(tc, 4);
+        tc.native_i = c.get(Calendar.YEAR);
+        result.bind_pos_native(tc, 5);
+        
+        return result;
     }
 }

9bdb5bc6a595737be8951821731713af6d92fea4
Elizabeth Mattijsen <liz@dijkmat.nl>
2013-07-15 13:41:24 +0200

    Move definition of $/, $! and $_ so "is default" might actually work in the future.

--- src/core/traits.pm
+++ src/core/traits.pm
@@ -1,11 +1,5 @@
 use Perl6::BOOTSTRAP;
 
-# Declare these, as setting mainline doesn't get them automatically (as the
-# Mu/Any/Scalar are not loaded).
-my $! is default(Nil);
-my $/ is default(Nil);
-my $_ is default(Nil);
-
 # Stub a few things the compiler wants to have really early on.
 my class Pair { ... }
 my class Whatever { ... }
@@ -150,6 +144,12 @@ multi trait_mod:<is>(Parameter:D $param, :$required!) {
 multi trait_mod:<is>(Parameter:D $param, :$parcel!) {
     $param.set_parcel();
 }
+
+# Declare these, as setting mainline doesn't get them automatically (as the
+# Mu/Any/Scalar are not loaded).
+my $! is default(Nil);
+my $/ is default(Nil);
+my $_ is default(Nil);
 
 sub EXPORT_SYMBOL(\exp_name, @tags, Mu \sym) {
     my @export_packages = $*EXPORT;

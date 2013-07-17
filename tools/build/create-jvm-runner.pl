965beb5658bc07496993b94527d7bcfca6e2ba9b
Donald Hunter <donald@sealgair.com>
2013-07-17 22:36:10 +0100

    Added an install target for JVM.

--- tools/build/create-jvm-runner.pl
+++ tools/build/create-jvm-runner.pl
@@ -7,10 +7,12 @@ use 5.008;
 use File::Spec;
 use File::Copy 'cp';
 
-my $USAGE = "Usage: $0 <prefix> <nqp prefix> <third party jars>\n";
+my $USAGE = "Usage: $0 <type> <prefix> <nqp prefix> <third party jars>\n";
 
-my ($prefix, $nqpprefix, $thirdpartyjars) = @ARGV
+my ($type, $prefix, $nqpprefix, $thirdpartyjars) = @ARGV
     or die $USAGE;
+
+die "Invalid target type $type" unless $type eq 'dev' || $type eq 'install';
 
 my $cpsep = $^O eq 'MSWin32' ? ';' : ':';
 my $bat   = $^O eq 'MSWin32' ? '.bat' : '';
@@ -19,10 +21,14 @@ my $preamble = $^O eq 'MSWin32' ? '@' : '#!/bin/sh
 exec ';
 my $postamble = $^O eq 'MSWin32' ? ' %*' : ' "$@"';
 
+my $bindir = $type eq 'install' ? File::Spec->catfile($prefix, 'bin') : $prefix;
+my $jardir = $type eq 'install' ? File::Spec->catfile($prefix, 'languages', 'perl6', 'runtime') : $prefix;
+my $libdir = $type eq 'install' ? File::Spec->catfile($prefix, 'languages', 'perl6', 'lib') : 'blib';
+
 sub install {
     my ($name, $command) = @_;
 
-    my $install_to = File::Spec->catfile($prefix, "$name$bat");
+    my $install_to = File::Spec->catfile($bindir, "$name$bat");
 
     open my $fh, ">", $install_to or die "open: $!";
     print $fh $preamble, $command, $postamble, "\n" or die "print: $!";
@@ -31,7 +37,16 @@ sub install {
     chmod 0755, $install_to if $^O ne 'MSWin32';
 }
 
-my $jopts = '-Xms100m -Xbootclasspath/a:' . $prefix . $cpsep . $thirdpartyjars . $cpsep . "rakudo-runtime.jar${cpsep}perl6.jar -cp " . $nqpprefix;
+my $bootclasspath = join($cpsep,
+    ($thirdpartyjars,
+    File::Spec->catfile($jardir, 'rakudo-runtime.jar'),
+    File::Spec->catfile($jardir, 'perl6.jar'),
+    $jardir,
+    $libdir));
+    
+my $classpath = join($cpsep, ($jardir, $libdir, $nqpprefix));
+
+my $jopts = '-Xms100m -Xbootclasspath/a:' . $bootclasspath . ' -cp ' . $classpath;
 
 install "perl6", "java $jopts perl6";
 install "perl6-jdb-server", "java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n $jopts perl6";

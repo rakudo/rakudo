cd4831d5dc43223baf6fa860c97fd733f5b7e400
jnthn <jnthn@jnthn.net>
2013-07-26 00:06:10 +0200

    Implement default trait on variables.
    Again, a first pass, but seems to basically work.

--- src/core/Variable.pm
+++ src/core/Variable.pm
@@ -4,3 +4,10 @@ my class Variable {
     has $.var is rw;
     has $.block;
 }
+
+# Variable traits come here, not in traits.pm, since we declare Variable
+# in the setting rather than BOOTSTRAP.
+multi trait_mod:<is>(Variable:D $v, :$default!) {
+    $v.var = $default;
+    nqp::getattr($v.var, $v.VAR.WHAT, '$!descriptor').set_default($default);
+}

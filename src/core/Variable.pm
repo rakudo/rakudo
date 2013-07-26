367739fa6b2ab9f6099a515cccd0bab5b3ad4b67
Elizabeth Mattijsen <liz@dijkmat.nl>
2013-07-26 15:26:05 +0200

    Make "my $a will foo" die with a more awesome error message

--- src/core/Variable.pm
+++ src/core/Variable.pm
@@ -34,6 +34,9 @@ multi trait_mod:<is>(Variable:D $v, :$dynamic!) {
 }
 
 # phaser traits
+multi trait_mod:<will>(Variable:D $v, $block, |c ) {
+    die "You can not 'will {c.hash.keys}' on a variable";
+}
 multi trait_mod:<will>(Variable:D $v, $block, :$begin! ) {
     $v.block.add_phaser('BEGIN', $block)
 }

b9ac7a463da3d0cef9c997d7682010e497c3b7c7
Elizabeth Mattijsen <liz@dijkmat.nl>
2013-07-28 17:03:50 +0200

    Make CORE.setting's files' names a bit more descriptive at either end

--- src/core/traits.pm
+++ src/core/traits.pm
@@ -1,15 +1,3 @@
-use Perl6::BOOTSTRAP;
-
-# Stub a few things the compiler wants to have really early on.
-my class Pair { ... }
-my class Whatever { ... }
-my class WhateverCode { ... }
-
-# Stub these or we can't use any sigil other than $.
-my role Positional { ... }
-my role Associative { ... }
-my role Callable { ... }
-
 # for errors
 my class X::Inheritance::Unsupported { ... }
 my class X::Inheritance::UnknownParent { ... }

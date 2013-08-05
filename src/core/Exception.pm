rakudo/rakudo

commit 156de3be59136b3b75f4eb51c7ee1697667a5cfe

Inlcude file name after the SORRY line
that way bulk messages (like for missing subroutines) get the file
name exactly once (not 0 times as before, and not repeated for every occurrence either)

src/core/Exception.pm
@@ -132,51 +132,6 @@ sub COMP_EXCEPTION(|) {
     }
 }
 
-my class X::Comp::Group is Exception {
-    has $.panic;
-    has @.sorrows;
-    has @.worries;
-    
-    method is-compile-time() { True }
-    
-    method gist(::?CLASS:D:) {
-        my $r = "";
-        if $.panic || @.sorrows {
-            my $color = %*ENV<RAKUDO_ERROR_COLOR> // $*OS ne 'MSWin32';
-            my ($red, $clear) = $color ?? ("\e[31m", "\e[0m") !! ("", "");
-            $r ~= "$red==={$clear}SORRY!$red===$clear\n";
-            for @.sorrows {
-                $r ~= .gist(:!sorry, :!expect) ~ "\n";
-            }
-            if $.panic {
-                $r ~= $.panic.gist(:!sorry) ~ "\n";
-            }
-        }
-        if @.worries {
-            $r ~= $.panic || @.sorrows
-                ?? "Other potential difficulties:\n"
-                !! "Potential difficulties:\n";
-            for @.worries {
-                $r ~= .gist(:!sorry, :!expect).indent(4) ~ "\n";
-            }
-        }
-        $r
-    }
-    
-    method message() {
-        my @m;
-        for @.sorrows {
-            @m.push(.message);
-        }
-        if $.panic {
-            @m.push($.panic.message);
-        }
-        for @.worries {
-            @m.push(.message);
-        }
-        @m.join("\n")
-    }
-}
 
 do {
     sub is_runtime($bt) {
@@ -428,12 +383,58 @@ my role X::Comp is Exception {
     method sorry_heading() {
         my $color = %*ENV<RAKUDO_ERROR_COLOR> // $*OS ne 'MSWin32';
         my ($red, $clear) = $color ?? ("\e[31m", "\e[0m") !! ("", "");
-        "$red==={$clear}SORRY!$red===$clear\n"
+        "$red==={$clear}SORRY!$red===$clear Error while compiling $.filename\n"
     }
     method SET_FILE_LINE($file, $line) {
         $!filename = $file;
         $!line     = $line;
         $!is-compile-time = True;
+    }
+}
+
+my class X::Comp::Group does X::Comp {
+    has $.panic;
+    has @.sorrows;
+    has @.worries;
+
+    method is-compile-time() { True }
+
+    multi method gist(::?CLASS:D:) {
+        my $r = "";
+        if $.panic || @.sorrows {
+            my $color = %*ENV<RAKUDO_ERROR_COLOR> // $*OS ne 'MSWin32';
+            my ($red, $clear) = $color ?? ("\e[31m", "\e[0m") !! ("", "");
+            $r ~= "$red==={$clear}SORRY!$red===$clear Error while compiling $.filename\n";
+            for @.sorrows {
+                $r ~= .gist(:!sorry, :!expect) ~ "\n";
+            }
+            if $.panic {
+                $r ~= $.panic.gist(:!sorry) ~ "\n";
+            }
+        }
+        if @.worries {
+            $r ~= $.panic || @.sorrows
+                ?? "Other potential difficulties:\n"
+                !! "Potential difficulties:\n";
+            for @.worries {
+                $r ~= .gist(:!sorry, :!expect).indent(4) ~ "\n";
+            }
+        }
+        $r
+    }
+
+    method message() {
+        my @m;
+        for @.sorrows {
+            @m.push(.message);
+        }
+        if $.panic {
+            @m.push($.panic.message);
+        }
+        for @.worries {
+            @m.push(.message);
+        }
+        @m.join("\n")
     }
 }
 

src/Perl6/World.nqp
@@ -2384,6 +2384,7 @@ class Perl6::World is HLL::World {
         %opts<panic> := @panic[0] if @panic;
         %opts<sorrows> := p6ize_recursive(@*SORROWS) if @*SORROWS;
         %opts<worries> := p6ize_recursive(@*WORRIES) if @*WORRIES;
+        %opts<filename> := nqp::box_s(nqp::getlexdyn('$?FILES'), self.find_symbol(['Str']));
         try {
             my $group_type := self.find_symbol(['X', 'Comp', 'Group']);
             return $group_type.new(|%opts);

commit df814a21966e78ab96bc2f8c4f838c7cf2cf3c00

Fixes spectests that were broken by 156de3be59136b3b75f4eb51c7ee1697667a5cfe

src/core/Exception.pm
@@ -392,7 +392,7 @@ my role X::Comp is Exception {
     }
 }
 
-my class X::Comp::Group does X::Comp {
+my class X::Comp::Group is Exception {
     has $.panic;
     has @.sorrows;
     has @.worries;
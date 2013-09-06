b03eced72be8fff97e700dd09c749f9392aacc2a
Elizabeth Mattijsen <liz@dijkmat.nl>
2013-09-06 21:06:08 +0200

    Remove superfluous stub (already stubbed in core_prologue)

--- src/core/stubs.pm
+++ src/core/stubs.pm
@@ -3,7 +3,6 @@
 # Code/Block/Routine/Sub/Method and Str/Int/Num. They are built in BOOTSTRAP.pm
 # in Perl6::Metamodel for now, though should be a BEGIN block in CORE.setting
 # in the end.
-my class Whatever { ... }
 my class Bag { ... }
 my class KeyBag { ... }
 my class KeySet { ... }

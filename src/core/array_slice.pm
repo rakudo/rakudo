25eddf504dc5b6036f83fc0eb742098c5f5c6cb1
Elizabeth Mattijsen <liz@dijkmat.nl>
2013-09-21 13:48:18 +0200

    Make all optional named param version handle no named param version
    Because of a bug (#119929) the no-named parameter is never selected, unlike
    Niecza.  Feels to me that a candidate with *no* named parameters is narrower
    than a candidate with *only* optional named parameters.
    
    Fixing this bug should allow better optimization of all {} and [] accesses
    without adverbs.

--- src/core/array_slice.pm
+++ src/core/array_slice.pm
@@ -3,10 +3,10 @@
 proto sub postcircumfix:<[ ]>(|) { * }
 
 # @a[1]
-multi sub postcircumfix:<[ ]>( \SELF, int $pos ) is rw {
-    fail "Cannot use negative index $pos on {SELF.WHAT.perl}" if $pos < 0;
-    SELF.at_pos($pos);
-}
+#multi sub postcircumfix:<[ ]>( \SELF, int $pos ) is rw {  # NOT SELECTED
+#    fail "Cannot use negative index $pos on {SELF.WHAT.perl}" if $pos < 0;
+#    SELF.at_pos($pos);
+#}
 multi sub postcircumfix:<[ ]>(\SELF, int $pos, Mu :$BIND! is parcel) is rw {
     fail "Cannot use negative index $pos on {SELF.WHAT.perl}" if $pos < 0;
     SELF.bind_pos($pos, $BIND);
@@ -22,15 +22,17 @@ multi sub postcircumfix:<[ ]>(
   :$v      = $default
 ) is rw {
     fail "Cannot use negative index $pos on {SELF.WHAT.perl}" if $pos < 0;
-    SLICE_ONE( SELF, $pos,
-      True, $delete, $exists, $kv, $p, $k, $v );
+    $delete & $exists & $kv & $p & $k & $v === $default
+      ?? SELF.at_pos($pos)
+      !!  SLICE_ONE( SELF, $pos,
+            True, $delete, $exists, $kv, $p, $k, $v );
 }
 
 # @a[$x]
-multi sub postcircumfix:<[ ]>( \SELF, $pos ) is rw {
-    fail "Cannot use negative index $pos on {SELF.WHAT.perl}" if $pos < 0;
-    SELF.at_pos($pos);
-}
+#multi sub postcircumfix:<[ ]>( \SELF, $pos ) is rw {    # NOT SELECTED
+#    fail "Cannot use negative index $pos on {SELF.WHAT.perl}" if $pos < 0;
+#    SELF.at_pos($pos);
+#}
 multi sub postcircumfix:<[ ]>(\SELF, $pos, Mu :$BIND! is parcel) is rw {
     fail "Cannot use negative index $pos on {SELF.WHAT.perl}" if $pos < 0;
     SELF.bind_pos($pos, $BIND);
@@ -46,20 +48,22 @@ multi sub postcircumfix:<[ ]>(
   :$v      = $default
 ) is rw {
     fail "Cannot use negative index $pos on {SELF.WHAT.perl}" if $pos < 0;
-    SLICE_ONE( SELF, $pos,
-      True, $delete, $exists, $kv, $p, $k, $v );
+    $delete & $exists & $kv & $p & $k & $v === $default
+      ?? SELF.at_pos($pos)
+      !! SLICE_ONE( SELF, $pos,
+           True, $delete, $exists, $kv, $p, $k, $v );
 }
 
 # @a[@i]
-multi sub postcircumfix:<[ ]>( \SELF, Positional \pos ) is rw {
-    if nqp::iscont(pos)  {
-        fail "Cannot use negative index {pos} on {SELF.WHAT.perl}" if pos < 0;
-        SELF.at_pos(pos);
-    }
-    else {
-        pos.map({ SELF[$_] }).eager.Parcel;
-    }
-}
+#multi sub postcircumfix:<[ ]>( \SELF, Positional \pos ) is rw {  # NOT SELECTED
+#    if nqp::iscont(pos)  {
+#        fail "Cannot use negative index {pos} on {SELF.WHAT.perl}" if pos < 0;
+#        SELF.at_pos(pos);
+#    }
+#    else {
+#        pos.map({ SELF[$_] }).eager.Parcel;
+#    }
+#}
 multi sub postcircumfix:<[ ]>(\SELF, Positional \pos, :$BIND!) is rw {
     X::Bind::Slice.new(type => SELF.WHAT).throw;
 }
@@ -73,14 +77,26 @@ multi sub postcircumfix:<[ ]>(
   :$k      = $default,
   :$v      = $default
 ) is rw {
-    SLICE_MORE( SELF, pos,
-      True, $delete, $exists, $kv, $p, $k, $v );
+    if $delete & $exists & $kv & $p & $k & $v === $default {
+        if nqp::iscont(pos)  {
+            fail "Cannot use negative index {pos} on {SELF.WHAT.perl}"
+              if pos < 0;
+            SELF.at_pos(pos);
+        }
+        else {
+            pos.map({ SELF[$_] }).eager.Parcel;
+        }
+    }
+    else {
+        SLICE_MORE( SELF, pos,
+          True, $delete, $exists, $kv, $p, $k, $v );
+    }
 }
 
 # @a[->{}]
-multi sub postcircumfix:<[ ]>( \SELF, Callable $block ) is rw {
-    SELF[$block(|(SELF.elems xx $block.count))];
-}
+#multi sub postcircumfix:<[ ]>( \SELF, Callable $block ) is rw { # NOT SELECTED
+#    SELF[$block(|(SELF.elems xx $block.count))];
+#}
 multi sub postcircumfix:<[ ]>(\SELF, Callable $block, :$BIND!) is rw {
     X::Bind::Slice.new(type => SELF.WHAT).throw;
 }
@@ -94,14 +110,16 @@ multi sub postcircumfix:<[ ]>(
   :$k      = $default,
   :$v      = $default
 ) is rw {
-    SLICE_MORE( SELF, $block(|(SELF.elems xx $block.count)),
-      True, $delete, $exists, $kv, $p, $k, $v );
+    $delete & $exists & $kv & $p & $k & $v === $default
+      ?? SELF[$block(|(SELF.elems xx $block.count))]
+      !! SLICE_MORE( SELF, $block(|(SELF.elems xx $block.count)),
+           True, $delete, $exists, $kv, $p, $k, $v );
 }
 
 # @a[*]
-multi sub postcircumfix:<[ ]>( \SELF, Whatever ) is rw {
-    SELF[SELF.keys];
-}
+#multi sub postcircumfix:<[ ]>( \SELF, Whatever ) is rw {  # NOT SELECTED
+#    SELF[SELF.keys];
+#}
 multi sub postcircumfix:<[ ]>(\SELF, Whatever, :$BIND!) is rw {
     X::Bind::Slice.new(type => SELF.WHAT).throw;
 }
@@ -115,14 +133,16 @@ multi sub postcircumfix:<[ ]>(
   :$k      = $default,
   :$v      = $default
 ) is rw {
-    SLICE_MORE( SELF, SELF.keys,
-      True, $delete, $exists, $kv, $p, $k, $v );
+    $delete & $exists & $kv & $p & $k & $v === $default
+      ?? SELF[SELF.keys]
+      !! SLICE_MORE( SELF, SELF.keys,
+           True, $delete, $exists, $kv, $p, $k, $v );
 }
 
 # @a[]
-multi sub postcircumfix:<[ ]>( \SELF ) is rw {
-    SELF.list;
-}
+#multi sub postcircumfix:<[ ]>( \SELF ) is rw {  # NOT SELECTED
+#    SELF.list;
+#}
 multi sub postcircumfix:<[ ]>(\SELF, :$BIND!) is rw {
     X::Bind::ZenSlice.new(type => SELF.WHAT).throw;
 }
@@ -135,6 +155,8 @@ multi sub postcircumfix:<[ ]>(
   :$k      = $default,
   :$v      = $default
 ) is rw {
-    SLICE_MORE( SELF, SELF.keys,
-      True, $delete, $exists, $kv, $p, $k, $v );
+    $delete & $exists & $kv & $p & $k & $v === $default
+      ?? SELF.list
+      !! SLICE_MORE( SELF, SELF.keys,
+           True, $delete, $exists, $kv, $p, $k, $v );
 }
--- src/core/hash_slice.pm
+++ src/core/hash_slice.pm
@@ -3,9 +3,9 @@
 proto sub postcircumfix:<{ }>(|) { * }
 
 # %h<key>
-multi sub postcircumfix:<{ }>( \SELF, $key ) is rw {
-    SELF.at_key($key);
-}
+#multi sub postcircumfix:<{ }>( \SELF, $key ) is rw {   # NOT SELECTED
+#    SELF.at_key($key);
+#}
 multi sub postcircumfix:<{ }>(\SELF, $key, Mu :$BIND! is parcel) is rw {
     SELF.bind_key($key, $BIND);
 }
@@ -19,16 +19,18 @@ multi sub postcircumfix:<{ }>(
   :$k      = $default,
   :$v      = $default
 ) is rw {
-    SLICE_ONE( SELF, $key,
-      False, $delete, $exists, $kv, $p, $k, $v );
+    $delete & $exists & $kv & $p & $k & $v === $default
+      ?? SELF.at_key($key)
+      !! SLICE_ONE( SELF, $key,
+           False, $delete, $exists, $kv, $p, $k, $v );
 }
 
 # %h<a b c>
-multi sub postcircumfix:<{ }>( \SELF, Positional \key ) is rw {
-    nqp::iscont(key) 
-      ?? SELF.at_key(key) 
-      !! key.map({ SELF{$_} }).eager.Parcel;
-}
+#multi sub postcircumfix:<{ }>( \SELF, Positional \key ) is rw {  # NOT SELECTED
+#    nqp::iscont(key) 
+#      ?? SELF.at_key(key) 
+#      !! key.map({ SELF{$_} }).eager.Parcel;
+#}
 multi sub postcircumfix:<{ }>(\SELF, Positional \key, :$BIND!) is rw {
     X::Bind::Slice.new(type => SELF.WHAT).throw;
 }
@@ -42,14 +44,21 @@ multi sub postcircumfix:<{ }>(
   :$k      = $default,
   :$v      = $default
 ) is rw {
-    SLICE_MORE( SELF, key,
-      False, $delete, $exists, $kv, $p, $k, $v );
+    if $delete & $exists & $kv & $p & $k & $v === $default {
+        nqp::iscont(key) 
+          ?? SELF.at_key(key) 
+          !! key.map({ SELF{$_} }).eager.Parcel;
+    }
+    else {
+        SLICE_MORE( SELF, key,
+          False, $delete, $exists, $kv, $p, $k, $v );
+    }
 }
 
 # %h{*}
-multi sub postcircumfix:<{ }>( \SELF, Whatever ) is rw {
-    SELF{SELF.keys};
-}
+#multi sub postcircumfix:<{ }>( \SELF, Whatever ) is rw {  # NOT SELECTED
+#    SELF{SELF.keys};
+#}
 multi sub postcircumfix:<{ }>(\SELF, Whatever, :$BIND!) is rw {
     X::Bind::Slice.new(type => SELF.WHAT).throw;
 }
@@ -63,14 +72,16 @@ multi sub postcircumfix:<{ }>(
   :$k      = $default,
   :$v      = $default
 ) is rw {
-    SLICE_MORE( SELF, SELF.keys,
-      False, $delete, $exists, $kv, $p, $k, $v );
+    $delete & $exists & $kv & $p & $k & $v === $default
+      ?? SELF{SELF.keys}
+      !! SLICE_MORE( SELF, SELF.keys,
+           False, $delete, $exists, $kv, $p, $k, $v );
 }
 
 # %h{}
-multi sub postcircumfix:<{ }>( \SELF ) is rw {
-    SELF;
-}
+#multi sub postcircumfix:<{ }>( \SELF ) is rw {  # NOT SELECTED
+#    SELF;
+#}
 multi sub postcircumfix:<{ }>(\SELF, :$BIND!) is rw {
     X::Bind::ZenSlice.new(type => SELF.WHAT).throw;
 }
@@ -83,6 +94,8 @@ multi sub postcircumfix:<{ }>(
   :$k      = $default,
   :$v      = $default
 ) is rw {
-    SLICE_MORE( SELF, SELF.keys,
-      False, $delete, $exists, $kv, $p, $k, $v );
+    $delete & $exists & $kv & $p & $k & $v === $default
+      ?? SELF
+      !! SLICE_MORE( SELF, SELF.keys,
+           False, $delete, $exists, $kv, $p, $k, $v );
 }

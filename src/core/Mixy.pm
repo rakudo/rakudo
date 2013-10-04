be96b97389740d41ccd7ccdea2bd96ae1c2b56cf
Elizabeth Mattijsen <liz@dijkmat.nl>
2013-10-04 23:58:03 +0200

    Make sure that Mixy allows for negative values when building

--- src/core/Mixy.pm
+++ src/core/Mixy.pm
@@ -3,6 +3,22 @@ my role Mixy does Baggy  {  # should really be QuantHash, but that's for later
     method default(--> Real) { 0 }
     method total(--> Real) { [+] self.values }
 
+    method new-fp(*@pairs --> Mixy) {
+        my %e;
+        for @pairs {
+            when Pair {
+                (%e{$_.key.WHICH} //= ($_.key => 0)).value += $_.value;
+            }
+            default {
+                (%e{$_.WHICH} //= ($_ => 0)).value++;
+            }
+        }
+        for %e -> $p {
+            %e.delete_key($p.key) if $p.value.value == 0;
+        }
+        self.bless(:elems(%e));
+    }
+
     multi method gist(Mixy:D $ : --> Str) {
         my $name := self.^name;
         ( $name eq 'Mix' ?? 'mix' !! "$name.new" )

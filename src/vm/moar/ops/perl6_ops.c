fec3db7d695ef18054a6e42efe9a3ee451afd6e7
jnthn <jnthn@jnthn.net>
2013-10-24 18:09:21 +0200

    Try adding ext-op; comment out what won't link.

--- src/vm/moar/ops/perl6_ops.c
+++ src/vm/moar/ops/perl6_ops.c
@@ -1,4 +1,10 @@
 #include "moar.h"
 
+/* Initializes the Perl 6 extension ops. */
+static void p6init(MVMThreadContext *tc) {
+}
 
-
+/* Registers the extops with MoarVM. */
+void Rakudo_ops_init(MVMThreadContext *tc) {
+    /*MVM_ext_register_extop(tc, "p6init",  p6init, 0, NULL);*/
+}

ab490e1db0f5013fbc09aabb9a6c88a03c6f75ec
jnthn <jnthn@jnthn.net>
2013-10-24 22:57:50 +0200

    Uncomment extop registration of p6init.
    Builds on Win32 now. Hopefully it still builds elsewhere too.

--- src/vm/moar/ops/perl6_ops.c
+++ src/vm/moar/ops/perl6_ops.c
@@ -1,3 +1,4 @@
+#define MVM_SHARED 1
 #include "moar.h"
 
 /* Initializes the Perl 6 extension ops. */
@@ -6,5 +7,5 @@ static void p6init(MVMThreadContext *tc) {
 
 /* Registers the extops with MoarVM. */
 void Rakudo_ops_init(MVMThreadContext *tc) {
-    /*MVM_ext_register_extop(tc, "p6init",  p6init, 0, NULL);*/
+    MVM_ext_register_extop(tc, "p6init",  p6init, 0, NULL);
 }

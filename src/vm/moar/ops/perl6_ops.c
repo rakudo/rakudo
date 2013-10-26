e0ad5cd098d52ad6ca5159bd58fd5feabca2292b
jnthn <jnthn@jnthn.net>
2013-10-26 15:30:54 +0200

    Stub p6settypes.

--- src/vm/moar/ops/perl6_ops.c
+++ src/vm/moar/ops/perl6_ops.c
@@ -12,7 +12,16 @@ static void p6init(MVMThreadContext *tc) {
     }
 }
 
+/* Stashes away various type references. */
+static MVMuint8 s_p6settypes[] = {
+    MVM_operand_obj | MVM_operand_read_reg
+};
+static void p6settypes(MVMThreadContext *tc) {
+    /* XXX TO DO */
+}
+
 /* Registers the extops with MoarVM. */
 MVM_DLL_EXPORT void Rakudo_ops_init(MVMThreadContext *tc) {
     MVM_ext_register_extop(tc, "p6init",  p6init, 0, NULL);
+    MVM_ext_register_extop(tc, "p6settypes",  p6settypes, 1, s_p6settypes);
 }

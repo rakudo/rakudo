166048e83eacfa544641bd5775e96ecc13c33f0a
Andrew Egeler <root+github@retupmoca.com>
2013-10-26 15:41:37 -0400

    Simplification to sub gethostname( --> Str)
    All we're getting is a single string, so we don't need all the
    complication of S29's gethost.

--- src/core/OS.pm
+++ src/core/OS.pm
@@ -1,4 +1,3 @@
-proto sub gethost(|) {*}
-multi sub gethost(){
-    return OS::Name.new(name => nqp::p6box_s(nqp::gethostname()));
+sub gethostname( --> Str){
+    return nqp::p6box_s(nqp::gethostname());
 }

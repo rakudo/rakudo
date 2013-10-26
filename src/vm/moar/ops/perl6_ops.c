1185c34883dd53c4a6a16c5fbab763391418621e
Tobias Leich <email@froggs.de>
2013-10-26 21:36:19 +0200

    stub p6bool

--- src/vm/moar/ops/perl6_ops.c
+++ src/vm/moar/ops/perl6_ops.c
@@ -20,8 +20,18 @@ static void p6settypes(MVMThreadContext *tc) {
     /* XXX TO DO */
 }
 
+/* Stashes away various type references. */
+static MVMuint8 s_p6bool[] = {
+    MVM_operand_obj | MVM_operand_write_reg,
+    MVM_operand_int64 | MVM_operand_read_reg,
+};
+static void p6bool(MVMThreadContext *tc) {
+    /* XXX TO DO */
+}
+
 /* Registers the extops with MoarVM. */
 MVM_DLL_EXPORT void Rakudo_ops_init(MVMThreadContext *tc) {
     MVM_ext_register_extop(tc, "p6init",  p6init, 0, NULL);
     MVM_ext_register_extop(tc, "p6settypes",  p6settypes, 1, s_p6settypes);
+    MVM_ext_register_extop(tc, "p6bool",  p6bool, 2, s_p6bool);
 }

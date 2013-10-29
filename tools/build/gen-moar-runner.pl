95476c27545f2b997bd689457d3ac5b55f55b3a0
Moritz Lenz <moritz@faui2k3.org>
2013-10-29 22:29:28 +0100

    import and adapot gen-moar-runner.pl from nqp
    pretty pointless so far, because we do not get that far in the compilation.

---
+++ tools/build/gen-moar-runner.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+# Copyright (C) 2013, The Perl Foundation.
+
+use strict;
+use warnings;
+use 5.008;
+use File::Spec;
+
+my ($moar, $libpath) = @ARGV;
+
+if ($^O eq 'MSWin32') {
+    my $install_to = 'perl6-m.bat';    
+    open my $fh, ">", $install_to
+        or die "Could not open $install_to: $!";
+    printf q[@ "%s" --libpath="%s" perl6.moarvm %*] . "\n",
+            $libpath, $moar;
+    close $fh
+        or die "Could not close $install_to: $!";
+}
+else {
+    my $install_to = 'perl6-m';
+    open my $fh, ">", $install_to
+        or die "Could not open $install_to: $!";
+    printf $fh <<'EOS', $moar, $libpath;
+#!/bin/sh
+exec %s --libpath=%s perl6.moarvm "$@"
+EOS
+    close $fh
+        or die "Could not close $install_to: $!";
+    chmod 0755, $install_to;
+}

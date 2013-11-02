a58b13560ec1000e30ce56b872689a1c1f8124f6
Jonathan Worthington <jnthn@jnthn.net>
2013-11-02 16:40:01 +0100

    Implement Publish.interval.

--- src/vm/jvm/core/Threading.pm
+++ src/vm/jvm/core/Threading.pm
@@ -903,6 +903,29 @@ my class Publish {
         }
         ForSubscribable.new(:@values, :$scheduler)
     }
+
+    method interval($interval, $delay = 0, :$scheduler = $*SCHEDULER) {
+        my class IntervalSubscribable does Subscribable {
+            has $!scheduler;
+            has $!interval;
+            has $!delay;
+
+            submethod BUILD(:$!scheduler, :$!interval, :$!delay) {}
+
+            method subscribe(|c) {
+                my $sub = self.Subscribable::subscribe(|c);
+                $!scheduler.schedule_every(
+                    {
+                        state $i = 0;
+                        $sub.next().($i++);
+                    },
+                    $!interval, $!delay
+                );
+                $sub
+            }
+        }
+        IntervalSubscribable.new(:$interval, :$delay, :$scheduler)
+    }
 }
 
 # A KeyReducer provides a thread-safe way to compose a hash from multiple

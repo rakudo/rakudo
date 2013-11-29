d01d6dc0d1b39f48860f13f50715b8e87a9e9a3e
Tobias Leich <email@froggs.de>
2013-11-29 15:00:42 +0100

    correctly create maor runner on windows

--- tools/build/gen-moar-runner.pl
+++ tools/build/gen-moar-runner.pl
@@ -12,8 +12,8 @@ if ($^O eq 'MSWin32') {
     my $install_to = 'perl6-m.bat';    
     open my $fh, ">", $install_to
         or die "Could not open $install_to: $!";
-    printf q[@ "%s" --libpath="%s" perl6.moarvm %*] . "\n",
-            $libpath, $moar;
+    printf $fh q[@ "%s" --libpath="%s" perl6.moarvm %%*] . "\n",
+            $moar, $libpath;
     close $fh
         or die "Could not close $install_to: $!";
 }

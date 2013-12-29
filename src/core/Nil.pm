174727377f56acca7d6d148cdf33689bdadbef88
Timo Paulssen <timonator@perpetuum-immobile.de>
2013-12-29 18:26:33 +0100

    method calls and hash/array access on Nil gives Nil.

--- src/core/Nil.pm
+++ src/core/Nil.pm
@@ -6,6 +6,24 @@ my class Nil is Cool { # declared in BOOTSTRAP
     method reify($n?) { () }
     method gist() { 'Nil' }
     multi method Str() { '' }
+
+    method at_pos($pos) {
+        return self
+    }
+    method at_key($pos) {
+        return self
+    }
+    method bind_pos($pos, $bind) {
+        die "Attempted to bind_pos to Nil.";
+    }
+    method bind_key($pos, $bind) {
+        die "Attempted to bind_key to Nil.";
+    }
 }
 
-
+Nil.^add_fallback(
+    -> $, $name { True },
+    -> $, $name {
+        anon sub (|) { Nil }
+    }
+);

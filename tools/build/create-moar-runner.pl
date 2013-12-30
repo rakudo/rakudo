23932887fffc982659b8a02bcb152d2af204f33a
Tobias Leich <email@froggs.de>
2013-12-30 22:46:46 +0100

    pass several --libpath options

--- tools/build/create-moar-runner.pl
+++ tools/build/create-moar-runner.pl
@@ -6,14 +6,15 @@ use warnings;
 use 5.008;
 use File::Spec;
 
-my ($moar, $libpath) = @ARGV;
+my ($moar, $p6_mbc_path, @libpaths) = @ARGV;
+$p6_mbc_path = File::Spec->rel2abs($p6_mbc_path || '.');
 
 if ($^O eq 'MSWin32') {
-    my $install_to = 'perl6-m.bat';    
+    my $install_to = 'perl6-m.bat';
     open my $fh, ">", $install_to
         or die "Could not open $install_to: $!";
-    printf $fh q[@ "%s" --libpath="%s" perl6.moarvm %%*] . "\n",
-            $moar, $libpath;
+    printf $fh q[@ "%s" --libpath="%s" %s\\perl6.moarvm %%*] . "\n",
+            $moar, join('" --libpath="', @libpaths), $p6_mbc_path;
     close $fh
         or die "Could not close $install_to: $!";
 }
@@ -21,9 +22,9 @@ else {
     my $install_to = 'perl6-m';
     open my $fh, ">", $install_to
         or die "Could not open $install_to: $!";
-    printf $fh <<'EOS', $moar, $libpath;
+    printf $fh <<'EOS', $moar, join('" --libpath="', @libpaths), $p6_mbc_path;
 #!/bin/sh
-exec %s --libpath=%s perl6.moarvm "$@"
+exec %s --libpath="%s" %s/perl6.moarvm "$@"
 EOS
     close $fh
         or die "Could not close $install_to: $!";

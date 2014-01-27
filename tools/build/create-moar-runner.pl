cb1c86f612e4034f28a4783a735a9d55d0212056
Jonathan Worthington <jnthn@jnthn.net>
2014-01-27 22:04:23 +0100

    Generalize create-moar-runner.
    Means we'll be able to use it to create a perl6-debug one also.

--- tools/build/create-moar-runner.pl
+++ tools/build/create-moar-runner.pl
@@ -6,25 +6,24 @@ use warnings;
 use 5.008;
 use File::Spec;
 
-my ($moar, $p6_mbc_path, @libpaths) = @ARGV;
+my ($moar, $mbc, $install_to, $p6_mbc_path, @libpaths) = @ARGV;
 $p6_mbc_path = File::Spec->rel2abs($p6_mbc_path || '.');
 
 if ($^O eq 'MSWin32') {
-    my $install_to = 'perl6-m.bat';
+    $install_to .= '.bat';
     open my $fh, ">", $install_to
         or die "Could not open $install_to: $!";
-    printf $fh q[@ "%s" --libpath="%s" %s\\perl6.moarvm %%*] . "\n",
-            $moar, join('" --libpath="', @libpaths), $p6_mbc_path;
+    printf $fh q[@ "%s" --libpath="%s" %s\\%s %%*] . "\n",
+            $moar, join('" --libpath="', @libpaths), $p6_mbc_path, $mbc;
     close $fh
         or die "Could not close $install_to: $!";
 }
 else {
-    my $install_to = 'perl6-m';
     open my $fh, ">", $install_to
         or die "Could not open $install_to: $!";
-    printf $fh <<'EOS', $moar, join('" --libpath="', @libpaths), $p6_mbc_path;
+    printf $fh <<'EOS', $moar, join('" --libpath="', @libpaths), $p6_mbc_path, $mbc;
 #!/bin/sh
-exec %s --libpath="%s" %s/perl6.moarvm "$@"
+exec %s --libpath="%s" %s/%s "$@"
 EOS
     close $fh
         or die "Could not close $install_to: $!";

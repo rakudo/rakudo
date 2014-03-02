4d8734d4bbdfbe71dc350ed51c520e092c27c0ff
Larry Wall <larry@wall.org>
2014-03-02 10:53:38 -0800

    implement subparse, redef parse to require $

--- src/core/Grammar.pm
+++ src/core/Grammar.pm
@@ -1,6 +1,13 @@
 my class Grammar is Cursor { 
     method parse($target, :$rule = 'TOP', Mu :$actions = Mu, *%opt) {
         my $*ACTIONS = $actions;
+        my $result = 
+            self."!cursor_init"($target, |%opt)."$rule"().MATCH;
+        $result = Nil unless $result.to == $target.chars;
+        nqp::getlexcaller('$/') = $result;
+    }
+    method subparse($target, :$rule = 'TOP', Mu :$actions = Mu, *%opt) {
+        my $*ACTIONS = $actions;
         nqp::getlexcaller('$/') =
             self."!cursor_init"($target, |%opt)."$rule"().MATCH;
     }

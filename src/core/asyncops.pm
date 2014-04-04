6768b727019bf49671552764a945ed351acb6299
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-04-04 15:20:53 +0200

    Re-implement WINNER, support "done @promises"
    Much of the logic existed twice in the sub, this has now been simplified.  Also,
    any parameter sanity tests are now performed *before* looking for a Promise/Channel
    so that problems in parameters do not stay hidden if they just did not happen to hit
    that case.

--- src/core/asyncops.pm
+++ src/core/asyncops.pm
@@ -17,10 +17,9 @@ multi sub await(Channel $c) {
 my constant $WINNER_KIND_DONE = 0;
 my constant $WINNER_KIND_MORE = 1;
 
-sub WINNER(@winner_args, *@pieces, :$wild_done, :$wild_more, :$wait, :$wait_time is copy) {
-    my num $start_time = nqp::time_n();
-    my Int $num_pieces = +@pieces div 3;
-    my $timeout_promise;
+sub WINNER(@winner, *@other, :$wild_done, :$wild_more, :$wait, :$wait_time is copy) {
+    my Num $until = $wait ?? nqp::time_n() + $wait !! Nil;
+
     sub invoke_right(&block, $key, $value?) {
         my @names = map *.name, &block.signature.params;
         return do if @names eqv ['$k', '$v'] || @names eqv ['$v', '$k'] {
@@ -34,112 +33,120 @@ sub WINNER(@winner_args, *@pieces, :$wild_done, :$wild_more, :$wait, :$wait_time
         } elsif +@names == 0 {
             return &block();
         } else {
-            die "couldn't figure out how to invoke {&block.signature().perl}";
+            die "Couldn't figure out how to invoke {&block.signature().perl}";
         }
     }
-    # if we don't have a last block, we need to retry until we
-    # have a winner.
+
+    my @todo;
+#       |-- [ kind, contestant, block, alternate_block? ]
+
+    # sanity check and transmogrify possibly multiple promises into things to do
+    while +@other {
+        my $kind = @other.shift;
+        if $kind != $WINNER_KIND_DONE && $kind != $WINNER_KIND_MORE {
+            die "Got a {$kind.WHAT.perl}, but expected $WINNER_KIND_DONE or $WINNER_KIND_MORE";
+        }
+
+        my @contestant = @other.shift;
+        while @other[0] !~~ Block {
+            my $next := @other.shift;
+            if $next !~~ Promise && $next !~~ Channel {
+                die "Got a {$next.WHAT.perl}, but expected a Promise or Channel";
+            }
+            elsif $kind == $WINNER_KIND_MORE && $next ~~ Promise {
+                die "Cannot use 'more' on a Promise";
+            }
+            push @contestant, $next;
+        }
+        my &block = @other.shift;
+
+        @todo.push: [ $kind, $_, &block ] for @contestant;
+    }
+
+    # transmogrify any winner spec if nothing to do so far
+    if !@todo {
+        for @winner {
+            when Promise {
+                @todo.push: [ $WINNER_KIND_DONE, $_, $wild_done ];
+            }
+            when Channel {
+                @todo.push: [ $WINNER_KIND_MORE, $_, $wild_more, $wild_done ];
+            }
+            default {
+                die "Got a {$_.WHAT.perl}, but expected a Promise or Channel";
+            }
+        }
+    }
+
+    if !@todo {
+        die "Nothing todo for winner";
+    }
+
     my $action;
-    loop {
-        my @promises_only;
-        my Bool $has_channels = False;
-        if $num_pieces > 0 {
-            for (^$num_pieces).pick(*) -> $index {
-                my ($kind, $arg, &block) = @pieces[$index * 3, $index * 3 + 1, $index * 3 + 2];
-                if $kind == $WINNER_KIND_DONE {
-                    if $arg ~~ Promise {
-                        if $arg {
-                            $action = { invoke_right(&block, $arg, $arg.result) };
-                            last;
-                        }
-                        @promises_only.push: $arg;
-                    } elsif $arg ~~ Channel {
-                        if $arg.closed {
-                            $action = { invoke_right(&block, $arg); }
-                            last;
-                        }
-                        $has_channels = True;
-                    } else {
-                        die "Got a {$arg.WHAT.perl}, but expected a Channel or Promise.";
+    my $timeout_promise;
+
+    CHECK:
+    loop {  # until something to return
+        my @promises;
+        my Bool $must_yield;
+
+        for @todo.pick(*) -> $todo {
+            my $kind       := $todo[0];
+            my $contestant := $todo[1];
+
+            if $kind == $WINNER_KIND_DONE {
+
+                if $contestant ~~ Promise {
+                    if $contestant {   # kept/broken
+                        $action = 
+                          {invoke_right($todo[2],$contestant,$contestant.result)};
+                        last; # CHECK;
                     }
-                } elsif $kind == $WINNER_KIND_MORE {
-                    if $arg ~~ Channel {
-                        if (my $val := $arg.poll) !~~ Nil {
-                            $action = { invoke_right(&block, $arg, $val); }
-                            last;
-                        }
-                        $has_channels = True;
-                    } elsif $arg ~~ Promise {
-                        die "cannot use 'more' on a Promise.";
-                    } else {
-                        die "Got a {$arg.WHAT.perl}, but expected a Channel or Promise.";
+                    @promises.push: $contestant;
+                }
+
+                else {   # Channel
+                    if $contestant.closed {
+                        $action = {invoke_right($todo[2], $contestant)};
+                        last; # CHECK;
                     }
                 }
             }
-            last if $action;
-            if $wait {
-                $wait_time -= (nqp::time_n() - $start_time);
-                if $wait_time <= 0 || $timeout_promise {
-                    $action = $wait;
-                    last;
-                } elsif !$timeout_promise {
-                    $timeout_promise = Promise.in($wait_time);
-                    @promises_only.push: $timeout_promise;
-                    $num_pieces++;
-                    @pieces.push: 0;
-                    @pieces.push: $timeout_promise;
-                    @pieces.push: $wait;
+
+            else { # $kind == $WINNER_KIND_MORE && $contestant ~~ Channel
+
+                if (my $value := $contestant.poll) !~~ Nil {
+                    $action = {invoke_right($todo[2], $contestant, $value)};
+                    last; # CHECK;
                 }
+
+                elsif $contestant.closed && $todo[3] {
+                    $action = {invoke_right($todo[3], $contestant)};
+                    last; # CHECK;
+                }
+                $must_yield = True;
             }
-        } else {
-            for @winner_args.pick(*) {
-                when Channel {
-                    if (my $val := $_.poll()) !~~ Nil {
-                        $action = { invoke_right($wild_more, $_, $val) };
-                        last;
-                    } elsif $_.closed {
-                        $action = { invoke_right($wild_done, $_); }
-                        last;
-                    }
-                    $has_channels = True;
-                }
-                when Promise {
-                    if $_ eqv $timeout_promise && $_ {
-                        $action = $wait;
-                        last;
-                    } elsif $_ {
-                        $action = { invoke_right($wild_done, $_, $_.result); }
-                        last;
-                    }
-                    @promises_only.push: $_;
-                }
-                default {
-                    die "Got a {$_.WHAT.perl}, but expected a Channel or Promise.";
-                }
-            }
-            last if $action;
-            # when we hit this, none of the promises or channels
-            # have given us a result. if we have a wait closure,
-            # we immediately return, otherwise we block on any
-            # of the promises of our args.
-            if $wait {
-                $wait_time -= (nqp::time_n() - $start_time);
-                if $wait_time <= 0 || $timeout_promise {
-                    return $wait();
-                } elsif !$timeout_promise {
-                    $timeout_promise = Promise.in($wait_time);
-                    @promises_only.push: $timeout_promise;
-                    $num_pieces++;
-                    @winner_args.push: $timeout_promise;
-                }
-            }
-            # if we only had promises, we can block on "anyof".
         }
-        if $has_channels || (@promises_only == 0) {
-            Thread.yield();
-        } else {
-            Promise.anyof(|@promises_only).result;
+
+        last if $action; # remove if we can last to CHECK:
+
+        # we have to wait
+        if $until {
+            if $nqp::time_n() >= $until {  # we're done waiting
+                $action = $wait;
+                last; # CHECK;
+            }
+            
+            # make sure we wait next time
+            @promises.push: $timeout_promise //= Promise.at($until);
         }
+
+        # yield the thread only if we must
+        $must_yield
+          ?? Thread.yield()
+          !! Promise.anyof(|@promises).result;
     }
-    $action()
+
+    # must do action outside above loop to make any "last" in block find the right loop
+    $action();
 }

f64f40f64c258f11ca3ae5c1af4de9ba94707966
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-04-14 16:00:16 +0200

    Implement (Set|Bag|Mix)(|Hash).(minpairs|maxpairs)
    As an alternative to .min/.max

--- src/core/QuantHash.pm
+++ src/core/QuantHash.pm
@@ -3,6 +3,44 @@ my role QuantHash does Associative {
     method Num     ( --> Num)     { self.total.Num }
     method Numeric ( --> Numeric) { self.total.Numeric }
     method Real    ( --> Real)    { self.total.Real }
+
+    method minpairs ( --> List ) {
+        my @found;
+        my $min = Inf;
+        for self.pairs {
+            my $value := .value;
+            if $value > $min {
+                next;
+            }
+            elsif $value < $min {
+                @found = $_;
+                $min = $value;
+            }
+            else {
+                @found.push: $_;
+            }
+        }
+        @found;
+    }
+
+    method maxpairs ( --> List ) {
+        my @found;
+        my $max = -Inf;
+        for self.pairs {
+            my $value := .value;
+            if $value < $max {
+                next;
+            }
+            elsif $value > $max {
+                @found = $_;
+                $max = $value;
+            }
+            else {
+                @found.push: $_;
+            }
+        }
+        @found;
+    }
 }
 
 # vim: ft=perl6 expandtab sw=4

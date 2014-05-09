698d2085e7cde23ad5ccbe217f1d468f233d05cd
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-05-09 23:35:31 +0200

    Make signal() a bit more picky on what to accept
    At compile time, if only 1 signal

--- src/core/signals.pm
+++ src/core/signals.pm
@@ -1,14 +1,20 @@
 my enum Signal ( :SIGINT(1), :SIGBREAK(2), :SIGHUP(3), :SIGWINCH(4) );
 
-sub signal(*@signals, :$scheduler = $*SCHEDULER) {
-    my class SignalCancellation is repr('AsyncTask') { }
+sub signal(Signal $signal, *@signals, :$scheduler = $*SCHEDULER) {
+
+    if @signals.grep( { $_ !~~ Signal } ) -> @invalid {
+        die "Found invalid signals: {@invalid}";
+    }
+    @signals.unshift: $signal;
+
     state %sigmap =
         SIGINT,   nqp::const::SIG_INT,
         SIGBREAK, nqp::const::SIG_BREAK,
         SIGHUP,   nqp::const::SIG_HUP,
         SIGWINCH, nqp::const::SIG_WINCH;
+
+    my class SignalCancellation is repr('AsyncTask') { }
     Supply.merge( @signals.map(-> $sig {
-        die "Invalid signal" unless $sig ~~ Signal;
         my $s = Supply.new;
         nqp::signal($scheduler.queue,
             -> $signum { $s.more($signum) },

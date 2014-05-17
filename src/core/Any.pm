a672ad9e6d56a1ea2b18e1a27a5660b640874600
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-05-17 12:45:08 +0200

    Add MMD candidates for simple Callables
    for grep, grep-index, first, first-index, last-index

--- src/core/Any.pm
+++ src/core/Any.pm
@@ -128,23 +128,53 @@ my class Any { # declared in BOOTSTRAP
         SELF.unshift(@values);
     }
 
-    method grep(Mu $test) is rw {
+    proto method grep(|) { * }
+    multi method grep(Callable &test) is rw {
+        self.map({ $_ if test($_) });
+    }
+    multi method grep(Mu $test) is rw {
         self.map({ $_ if $_ ~~ $test });
     }
-    method grep-index(Mu $test) {
+
+    proto method grep-index(|) { * }
+    multi method grep-index(Callable &test) {
+        my $index = -1;
+        self.map: { $index++; +$index if test($_) };
+    }
+    multi method grep-index(Mu $test) {
         my $index = -1;
         self.map: { $index++; +$index if $_ ~~ $test };
     }
-    method first(Mu $test) is rw {
+
+    proto method first(|) { * }
+    multi method first(Callable &test) is rw {
+        self.map({ return $_ if test($_) });
+        Nil;
+    }
+    multi method first(Mu $test) is rw {
         self.map({ return $_ if $_ ~~ $test });
         Nil;
     }
-    method first-index(Mu $test) {
+
+    proto method first-index(|) { * }
+    multi method first-index(Callable &test) {
+        my $index = -1;
+        self.map: { $index++; return $index if test($_) };
+        Nil;
+    }
+    multi method first-index(Mu $test) {
         my $index = -1;
         self.map: { $index++; return $index if $_ ~~ $test };
         Nil;
     }
-    method last-index(Mu $test) {
+
+    proto method last-index(|) { * }
+    multi method last-index(Callable &test) {
+        my $index = self.elems;
+        self.reverse.map: { $index--; return $index if test($_) };
+        Nil;
+    }
+    multi method last-index(Mu $test) {
         my $index = self.elems;
         self.reverse.map: { $index--; return $index if $_ ~~ $test };
         Nil;

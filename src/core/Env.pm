0a70378044fc4c8a4953a10e296a945322a2c9b9
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-05-28 13:24:22 +0200

    We need $*CWD when we need %*ENV

--- src/core/Env.pm
+++ src/core/Env.pm
@@ -10,6 +10,19 @@
         %ENV{$key} = nqp::p6box_s(nqp::iterval($envelem));
     }
     PROCESS::<%ENV> := %ENV;
+
+    ## duplicate src/core/IO.pm::cwd
+    my $CWD = IO::Path.new(nqp::p6box_s(
+#?if parrot
+        pir::trans_encoding__Ssi(
+            nqp::cwd(),
+            pir::find_encoding__Is('utf8'))
+#?endif
+#?if !parrot
+            nqp::cwd(),
+#?endif
+    ));
+    PROCESS::<$CWD> = $CWD;
 }
 
 # vim: ft=perl6 expandtab sw=4

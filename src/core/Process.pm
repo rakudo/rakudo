6cd50fa0c259a91b9267511baf75a9ddfd4a79a7
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-06-02 00:20:48 +0200

    Implement $*USER and $*GROUP (not yet specced)
    In numeric context, they provide uid/guid and in string context they provide
    the user's name and group.  Works by executing "id" where available, but only
    when $*USER and $*GROUP are actually used.

--- src/core/Process.pm
+++ src/core/Process.pm
@@ -22,6 +22,42 @@
     PROCESS::<$PROGRAM_NAME> = $PROGRAM_NAME;
 
     PROCESS::<$TMPDIR> = IO::Spec.tmpdir().path;
+
+    class IdName {
+        has Int $.id;
+        has Str $.name;
+
+        method Numeric { $!id }
+        method Str     { $!name }
+        method gist    { "$!name ($!id)" }
+    }
+
+    # probably needs some nqp::op, LHF for someone?
+    my $on_demand_fetch = Proxy.new(
+      FETCH => {
+          if try qx/id/ -> $id {
+              $id ~~ m/^
+                [ uid "=" $<uid>=(\d+) ]
+                [ "(" $<user>=(\w+) ")" ]
+                \s+
+                [ gid "=" $<gid>=(\d+) ]
+                [ "(" $<group>=(\w+) ")" ]
+              /;
+
+              PROCESS::<$USER>  := IdName.new( :id(+$<uid>), :name(~$<user>) );
+              PROCESS::<$GROUP> := IdName.new( :id(+$<gid>), :name(~$<group>) );
+          }
+
+          # alas, no support yet
+          else {
+              PROCESS::<$USER>     := Nil;
+              PROCESS::<$GROUP>    := Nil;
+          }
+      }
+    );
+    
+    PROCESS::<$USER>     //= $on_demand_fetch;
+    PROCESS::<$GROUP>    //= $on_demand_fetch;
 }
 
 # vim: ft=perl6 expandtab sw=4

57555c8d665d0144a5bcb23a9b0390be4749e5ca
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-06-02 00:22:35 +0200

    Make $*EXECUTABLE and $*EXECUTABLE_NAME immutable

--- src/core/Process.pm
+++ src/core/Process.pm
@@ -14,8 +14,8 @@
         or ($*VM.config<prefix> ~ '/bin/' ~ ($*VM.config<osname> eq 'MSWin32' ?? 'perl6-m.bat' !! 'perl6-m'));
 #?endif
     $EXECUTABLE := $EXECUTABLE.path.absolute;
-    PROCESS::<$EXECUTABLE>      = $EXECUTABLE;
-    PROCESS::<$EXECUTABLE_NAME> = $EXECUTABLE.basename;
+    PROCESS::<$EXECUTABLE>      := $EXECUTABLE;
+    PROCESS::<$EXECUTABLE_NAME> := $EXECUTABLE.basename;
 
     my Mu $comp := nqp::getcomp('perl6');
     my $PROGRAM_NAME = $comp.user-progname();

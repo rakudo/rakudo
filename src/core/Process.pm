17f02304af7a9445af59aae9c721241d852de4c1
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-06-03 11:17:21 +0200

    Take #2 on implementing $*USER and $*GROUP
    Please note that this adds one qx() for every start.  Somehow, I can't get
    this to be on-demand using Proxy.  Pointers in that direction welcome!

--- src/core/Process.pm
+++ src/core/Process.pm
@@ -22,6 +22,41 @@
     PROCESS::<$PROGRAM_NAME> = $PROGRAM_NAME;
 
     PROCESS::<$TMPDIR> = IO::Spec.tmpdir().path;
+
+    class IdName {
+        has Int $.id;
+        has Str $.name;
+
+        method Numeric { $!id }
+        method Str     { $!name }
+        method gist    { "$!name ($!id)" }
+    }
+
+    # probably needs some nqp::op, LHF for someone?
+    if try { qx/id/ } -> $id {
+        if $id ~~ m/^
+          [ uid "=" $<uid>=(\d+) ]
+          [ "(" $<user>=(<-[ ) ]>+) ")" ]
+          \s+
+          [ gid "=" $<gid>=(\d+) ]
+          [ "(" $<group>=(<-[ ) ]>+) ")" ]
+        / {
+            PROCESS::<$USER>  := IdName.new( :id(+$<uid>), :name(~$<user>) );
+            PROCESS::<$GROUP> := IdName.new( :id(+$<gid>), :name(~$<group>) );
+        }
+
+        # alas, no support yet
+        else {
+            PROCESS::<$USER>  := Nil;
+            PROCESS::<$GROUP> := Nil;
+        }
+    }
+
+    # alas, no support yet
+    else {
+        PROCESS::<$USER>  := Nil;
+        PROCESS::<$GROUP> := Nil;
+    }
 }
 
 # vim: ft=perl6 expandtab sw=4

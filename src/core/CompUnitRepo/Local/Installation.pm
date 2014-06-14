4d0bdd9d5ca40e3119ee674f59aef6f136a6c715
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-06-14 10:33:29 +0200

    Move CompUnitRepo::Distribution to its own file

--- src/core/CompUnitRepo/Local/Installation.pm
+++ src/core/CompUnitRepo/Local/Installation.pm
@@ -1,33 +1,3 @@
-class CompUnitRepo::Distribution {
-    has $.id is rw;
-    has $.name;
-    has $.auth;
-    has $.author;
-    has $.authority;
-    has $.ver;
-    has $.version;
-    has $.description;
-    has @.depends;
-    has %.provides;
-    has %.files;
-    has $.source-url;
-    method auth { $!auth // $!author // $!authority }
-    method ver  { $!ver // $!version }
-    method Hash {
-        {
-            :$!id,
-            :$!name,
-            :$.auth,
-            :$.ver,
-            :$!description,
-            :@!depends,
-            :%!provides,
-            :%!files,
-            :$!source-url,
-        }
-    }
-}
-
 class CompUnitRepo::Local::Installation {
     has %!dists;
     has $!cver = nqp::hllize(nqp::atkey(nqp::gethllsym('perl6', '$COMPILER_CONFIG'), 'version'));

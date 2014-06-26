f2060eac5054ec1c1e68053523bb44c9574b9d14
jnthn <jnthn@jnthn.net>
2014-06-26 22:18:50 +0200

    Ensure thread counts are Ints.

--- src/core/ThreadPoolScheduler.pm
+++ src/core/ThreadPoolScheduler.pm
@@ -54,8 +54,8 @@ my class ThreadPoolScheduler does Scheduler {
     }
     
     submethod BUILD(
-        :$!initial_threads = 0,
-        :$!max_threads = %*ENV<RAKUDO_MAX_THREADS> // 16,
+        Int :$!initial_threads = 0,
+        Int :$!max_threads = (%*ENV<RAKUDO_MAX_THREADS> // 16).Int,
     ) {
         die "Initial thread pool threads ($!initial_threads) must be less than or equal to maximum threads ($!max_threads)"
             if $!initial_threads > $!max_threads;

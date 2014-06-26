ef3bb15a41acdf450e12f6d5d5f5f659a66bec81
jnthn <jnthn@jnthn.net>
2014-06-26 17:12:29 +0200

    wrap should not force decontainerization.

--- src/core/Routine.pm
+++ src/core/Routine.pm
@@ -92,7 +92,7 @@ my class Routine { # declared in BOOTSTRAP
                 nqp::bindattr($handle, WrapHandle, '$!wrapper', &wrapper);
                 $handle
             }
-            method postcircumfix:<( )>($c) {
+            method postcircumfix:<( )>($c) is rw {
                 $!dispatcher.enter(|$c);
             }
             method soft() { True }

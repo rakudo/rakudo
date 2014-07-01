e371c34206800c777fb7e06e11427acbd492a0f5
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-07-01 20:41:51 +0200

    Implement "is cached" on methods properly

--- src/core/Method.pm
+++ src/core/Method.pm
@@ -4,4 +4,14 @@ my class Method { # declared in BOOTSTRAP
     multi method gist(Method:D:) { self.name }
 }
 
+multi sub trait_mod:<is>(Method $m, :$cached!) {
+    my %cache;
+    $m.wrap(-> $self, |c {
+        my $key := $self.WHICH ~ '|' ~ c.gist;
+        %cache{$key}:exists
+          ?? %cache{$key}
+          !! (%cache{$key} = callsame);
+    });
+}
+
 # vim: ft=perl6 expandtab sw=4

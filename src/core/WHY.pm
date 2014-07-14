39c6b7ba4a60e4677146dc3a13781b8d5228b8e2
Rob Hoelz <rob@hoelz.ro>
2014-07-14 18:36:10 -0500

    Flesh out WHY helper class a bit

--- src/core/WHY.pm
+++ src/core/WHY.pm
@@ -1,7 +1,44 @@
 my class WHY {
-    has $!leading;
-    has $!trailing;
+    has Str $!leading;
+    has Str $!trailing;
+
+    method new(:$leading, :$trailing) {
+        my $obj := nqp::create(self);
+        nqp::bindattr($obj, WHY, '$!leading',  nqp::p6box_s(~$leading))  if $leading.defined;
+        nqp::bindattr($obj, WHY, '$!trailing', nqp::p6box_s(~$trailing)) if $trailing.defined;
+        $obj
+    }
 
     method leading  { $!leading }
     method trailing { $!trailing }
+
+    method _add_leading($addition) {
+        if $!leading.defined {
+            $!leading ~= "\n$addition";
+        } else {
+            $!leading = ~$addition;
+        }
+    }
+
+    method _add_trailing($addition) {
+        if $!trailing.defined {
+            $!trailing ~= "\n$addition";
+        } else {
+            $!trailing = ~$addition;
+        }
+    }
+
+    method Str {
+        my $result = '';
+
+        $result ~= $!leading  if $!leading.defined;
+        $result ~= "\n"       if $!leading.defined && $!trailing.defined;
+        $result ~= $!trailing if $!trailing.defined;
+
+        $result
+    }
+
+    method gist {
+        $.Str
+    }
 }

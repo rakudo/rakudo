10142b22446cbca0cb3ba6bf881f029340628146
Rob Hoelz <rob@hoelz.ro>
2014-07-23 21:42:02 -0500

    Use the block for documentation that we're passed instead of creating a fresh one

--- src/core/traits.pm
+++ src/core/traits.pm
@@ -13,12 +13,12 @@ my sub set_leading_docs($obj, $type, $docs) {
     my $dc := nqp::decont($obj);
     my $current_why := nqp::getattr($dc, $type, '$!why');
 
-    unless $current_why {
-        $current_why := Pod::Block::Declarator.new();
-        $*POD_BLOCKS.push($current_why);
-        nqp::bindattr($dc, $type, '$!why', $current_why);
+    if $current_why {
+        # XXX remove $docs from $*POD_BLOCKS (best way to do this?)
+        $current_why._add_leading(~$docs);
+    } else {
+        nqp::bindattr($dc, $type, '$!why', $docs);
     }
-    $current_why._add_leading($docs);
 }
 
 my sub set_trailing_docs($obj, $type, $docs) {

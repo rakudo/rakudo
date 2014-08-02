4481fa986dac33cb3535637feb110edfbf46e4d8
Rob Hoelz <rob@hoelz.ro>
2014-08-01 22:45:43 -0500

    Eliminate redundant add to POD_BLOCKS

--- src/core/traits.pm
+++ src/core/traits.pm
@@ -27,7 +27,6 @@ my sub set_trailing_docs($obj, $type, $docs) {
 
     unless $current_why {
         $current_why := Pod::Block::Declarator.new();
-        $*POD_BLOCKS.push($current_why);
         nqp::bindattr($dc, $type, '$!why', $current_why);
     }
     $current_why._add_trailing($docs);

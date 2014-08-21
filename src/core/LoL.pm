47d315aa9490e48c7268dbf35d93886cc92b87ec
TimToady <larry@wall.org>
2014-08-20 23:47:06 -0700

    optimize X operator, esp for ranges

--- src/core/LoL.pm
+++ src/core/LoL.pm
@@ -44,22 +44,91 @@ sub infix:<X>(|lol) {
     my @l;
     my \elem = lol[0];
     @l[0] = nqp::iscont(elem) ?? (elem,).list.item !! (elem,).flat.item;
-    my int $i = 0;
     my int $n = lol.elems - 1;
+    my Mu $end := nqp::list_i();
+    for 1..$n -> $i {
+        my \elem = lol[$i];
+        @l[$i] = nqp::iscont(elem) ?? (elem,).list.item !! (elem,).flat.item;
+        nqp::bindpos_i($end,$i,@l[$i].elems);  # presumably eagerifies 2nd and subsequent lists
+    }
+
     my Mu $v := nqp::list();
-    gather {
-        while $i >= 0 {
-            if @l[$i].gimme(1) {
-                nqp::bindpos($v, $i, @l[$i].shift);
-                if $i >= $n { take nqp::p6parcel(nqp::clone($v), nqp::null()) }
-                else {
-                    $i = $i + 1;
-                    my \elem = lol[$i];
-                    @l[$i] = nqp::iscont(elem) ?? (elem,).list.item !! (elem,).flat.item;
+
+    # optimize for 2D and 3D crosses
+    my @result := do {
+        if $n == 1 { # 2-dimensional
+            gather {
+                my int $e = nqp::atpos_i($end,1);
+                my $l0 = @l[0];
+                my $l1 = @l[1];
+                while $l0.gimme(1) {
+                    nqp::bindpos($v, 0, @l[0].shift);
+                    loop (my int $j = 0; $j < $e; $j = $j + 1) {
+                        nqp::bindpos($v, 1, $l1[$j]);
+                        take nqp::clone($v);
+                    }
                 }
             }
-            else { $i = $i - 1 }
         }
+        elsif $n == 2 { # 3-dimensional
+            gather {
+                my int $e1 = nqp::atpos_i($end,1);
+                my int $e2 = nqp::atpos_i($end,2);
+                my $l0 = @l[0];
+                my $l1 = @l[1];
+                my $l2 = @l[2];
+                while $l0.gimme(1) {
+                    nqp::bindpos($v, 0, @l[0].shift);
+                    loop (my int $j = 0; $j < $e1; $j = $j + 1) {
+                        nqp::bindpos($v, 1, $l1[$j]);
+                        loop (my int $k = 0; $k < $e2; $k = $k + 1) {
+                            nqp::bindpos($v, 2, $l2[$k]);
+                            take nqp::clone($v);
+                        }
+                    }
+                }
+            }
+        }
+        else {  # general case
+            my int $i = 0;
+            my Mu $jsave := nqp::list_i();
+            gather {
+                while $i == 0 {
+                    if @l[0].gimme(1) {
+                        nqp::bindpos($v, $i, @l[0].shift);
+
+                        if $i >= $n { take nqp::clone($v) }
+                        else { $i = $i + 1; }
+
+                        my int $j = 0;
+                        while $i >= 1 {
+                            if $j < nqp::atpos_i($end,$i) {
+                                nqp::bindpos($v, $i, @l[$i][$j]);
+                                $j = $j + 1;
+
+                                if $i >= $n { take nqp::clone($v) }
+                                else {
+                                    nqp::bindpos_i($jsave, $i, $j);
+                                    $i = $i + 1;
+                                    $j = 0;
+                                }
+                            }
+                            else {
+                                $i = $i - 1;
+                                $j = nqp::atpos_i($jsave,$i);
+                            }
+                        }
+                    }
+                    else { $i = $i - 1 }
+                }
+            }
+        }
+    }
+
+    # Don't care if a Range is lazy
+    given elem {
+        when Range { eager @result }
+        default { @result }
     }
 }
 

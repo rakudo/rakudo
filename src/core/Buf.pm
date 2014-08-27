b613f0a3b0b51f175ab9f0f51751173292ebd716
Tobias Leich <email@froggs.de>
2014-08-27 14:59:57 +0200

    implement Blob.assign_pos

--- src/core/Buf.pm
+++ src/core/Buf.pm
@@ -31,7 +31,23 @@ my role Blob[::T = uint8] does Positional[T] does Stringy is repr('VMArray') is 
     multi method at_pos(Blob:D: int $i) {
         nqp::atpos_i(self, $i)
     }
-    
+
+    multi method assign_pos(Blob:D: \pos, Mu \assignee) is rw {
+#?if !parrot
+        if nqp::istype(pos, Num) && nqp::isnanorinf(pos) {
+#?endif
+#?if parrot
+        if nqp::isnanorinf(pos) {
+#?endif
+            X::Item.new(aggregate => self, index => pos).throw;
+        }
+        my int $p = nqp::unbox_i(nqp::istype(pos, Int) ?? pos !! pos.Int);
+        nqp::bindpos_i(self, $p, assignee)
+    }
+    multi method assign_pos(Blob:D: int $pos, Mu \assignee) is rw {
+        nqp::bindpos_i(self, $pos, assignee)
+    }
+
     multi method Bool(Blob:D:) {
         nqp::p6bool(nqp::elems(self));
     }

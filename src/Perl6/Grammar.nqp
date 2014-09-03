rakudo/rakudo

commit 8ef5c5564cbb69496c5d3b6c19c9a4af0daa72f7

[grammar] use some more native types

src/Perl6/Grammar.nqp
@@ -639,7 +639,7 @@ grammar Perl6::Grammar is HLL::Grammar does STD {
               $*POD_ANGLE_COUNT := -1;
               1
             } else {
-              my $ct := nqp::chars($<begin-tag>);
+              my int $ct := nqp::chars($<begin-tag>);
               $endtag := nqp::x(">", $ct);
               my $rv := $*POD_ANGLE_COUNT <= 0 || $*POD_ANGLE_COUNT >= $ct;
               $*POD_ANGLE_COUNT := $ct;
@@ -1177,12 +1177,13 @@ grammar Perl6::Grammar is HLL::Grammar does STD {
         <identifier> ':' <?[\s]> <.ws>
         {
             $*LABEL       := ~$<identifier>;
-            my $total     := nqp::chars(self.orig());
-            my $from      := self.MATCH.from();
-            my $to        := self.MATCH.to() + nqp::chars($*LABEL);
-            my $line      := HLL::Compiler.lineof(self.orig(), self.from());
-            my $prematch  := nqp::substr(self.orig(), $from > 20 ?? $from - 20 !! 0, 20);
-            my $postmatch := nqp::substr(self.orig(), $to, 20);
+            my str $orig      := self.orig();
+            my int $total     := nqp::chars($orig);
+            my int $from      := self.MATCH.from();
+            my int $to        := self.MATCH.to() + nqp::chars($*LABEL);
+            my int $line      := HLL::Compiler.lineof($orig, self.from());
+            my str $prematch  := nqp::substr($orig, $from > 20 ?? $from - 20 !! 0, 20);
+            my str $postmatch := nqp::substr($orig, $to, 20);
             my $label     := $*W.find_symbol(['Label']).new( :name($*LABEL), :$line, :$prematch, :$postmatch );
             $*W.add_object($label);
             $*W.install_lexical_symbol($*W.cur_lexpad(), $*LABEL, $label);
@@ -3443,9 +3444,9 @@ grammar Perl6::Grammar is HLL::Grammar does STD {
     }
 
     sub bracket_ending($matches) {
-        my $check := $matches[+$matches - 1];
-        my $str   := $check.Str;
-        my $last  := nqp::substr($str, nqp::chars($check) - 1, 1);
+        my $check     := $matches[+$matches - 1];
+        my str $str   := $check.Str;
+        my str $last  := nqp::substr($str, nqp::chars($check) - 1, 1);
         $last eq ')' || $last eq '}' || $last eq ']' || $last eq '>'
     }
 

commit 0be23748ff42960f04ae150e0a69dccedecbce6d

Unbust on (at least) OSX, caused by 8ef5c5564c
No idea why this makes a difference, but suspect the eq on the next line to
have some influence in this.  Error seen was:

Stage start      :   0.000
Stage parse      : moar(51746,0x7fff74dba310) malloc: *** error for object 0x7faa4c7aad90: pointer being freed was not allocated
*** set a breakpoint in malloc_error_break to debug
make: *** [CORE.setting.moarvm] Abort trap: 6

src/Perl6/Grammar.nqp
@@ -3446,7 +3446,7 @@ grammar Perl6::Grammar is HLL::Grammar does STD {
     sub bracket_ending($matches) {
         my $check     := $matches[+$matches - 1];
         my str $str   := $check.Str;
-        my str $last  := nqp::substr($str, nqp::chars($check) - 1, 1);
+        my $last  := nqp::substr($str, nqp::chars($check) - 1, 1);
         $last eq ')' || $last eq '}' || $last eq ']' || $last eq '>'
     }
 
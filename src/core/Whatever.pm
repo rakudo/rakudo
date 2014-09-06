4a74294806853404703d5cb792066b48cb8579ca
TimToady <larry@wall.org>
2014-09-05 23:33:16 -0700

    no longer need new and clone in Whatever

--- src/core/Whatever.pm
+++ src/core/Whatever.pm
@@ -1,16 +1,10 @@
 my class Whatever {
     multi method ACCEPTS(Whatever:D: $topic) { True }
-    method new() { our $star //= nqp::create(self) }
-    method clone() { self }
-
     multi method perl(Whatever:D:) { '*' }
 }
 
 my class HyperWhatever {
     multi method ACCEPTS(HyperWhatever:D: $topic) { True }
-    method new() { our $starstar //= nqp::create(self) }
-    method clone() { self }
-
     multi method perl(HyperWhatever:D:) { '**' }
 }
 

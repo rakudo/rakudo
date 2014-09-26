1613b0bbc6c82b939178c6e7e3a84d8b5c8040d7
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-09-26 22:24:54 +0200

    Remove old parrot wart

--- src/core/Env.pm
+++ src/core/Env.pm
@@ -13,18 +13,7 @@
 }
 
 multi sub INITIALIZE_DYNAMIC('$*CWD') {
-    ## duplicate src/core/IO.pm::cwd
-    my $CWD = IO::Path.new(nqp::p6box_s(
-#?if parrot
-        pir::trans_encoding__Ssi(
-            nqp::cwd(),
-            pir::find_encoding__Is('utf8'))
-#?endif
-#?if !parrot
-            nqp::cwd(),
-#?endif
-    ));
-    PROCESS::<$CWD> := $CWD;
+    PROCESS::<$CWD> = IO::Path.new(nqp::p6box_s(nqp::cwd()));
 }
 
 # vim: ft=perl6 expandtab sw=4

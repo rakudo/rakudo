6cd83e532e939b52f6f5748a01c6b60662ab3d4f
Tobias Leich <email@froggs.de>
2014-09-28 22:30:41 +0200

    stringify $?FILES in Label.new
    There are cases where $?FILES contain an IO::Path object.

--- src/core/Label.pm
+++ src/core/Label.pm
@@ -7,10 +7,8 @@ my class Label {
     method new(:$name, :$line, :$prematch, :$postmatch) {
         # XXX Register in &?BLOCK.labels when we have &?BLOCK.
         my $obj  := nqp::create(self);
-        my $file := nqp::getlexdyn('$?FILES');
         nqp::bindattr($obj, Label, '$!name',      $name);
-        nqp::bindattr($obj, Label, '$!file',      nqp::isnull($file)
-            ?? '<unknown file>' !! nqp::p6box_s(nqp::getlexdyn('$?FILES')));
+        nqp::bindattr($obj, Label, '$!file',     ~nqp::getlexdyn('$?FILES'));
         nqp::bindattr($obj, Label, '$!line',      $line);
         nqp::bindattr($obj, Label, '$!prematch',  nqp::p6box_s($prematch));
         nqp::bindattr($obj, Label, '$!postmatch', nqp::p6box_s($postmatch));

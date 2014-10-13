36d74f606a55a064f523cf1a36328c7c8d90f65a
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-10-13 14:43:33 +0200

    Some code esthetics

--- src/core/IO/Special.pm
+++ src/core/IO/Special.pm
@@ -1,19 +1,21 @@
 class IO::Special {
     has Str $.what;
 
-    submethod BUILD(:$!what!) { }
+    submethod BUILD(:$!what!)    { }
+    method WHICH(IO::Special:D:) { "IO::Special$!what" }
+    method Str(IO::Special:D:)   { $!what }
 
-    method IO { self }
+    method IO(IO::Special:D:) { self }
 
-    method e() { True }
-    method d() { False }
-    method f() { False }
-    method s() { 0 }
-    method l() { False }
-    method r() { $!what eq '<IN>' }
-    method w() { $!what eq '<OUT>' or $!what eq '<ERR>' }
-    method x() { False }
-    method modified() { Instant }
-    method accessed() { Instant }
-    method changed()  { Instant}
+    method e(IO::Special:D:) { True }
+    method d(IO::Special:D:) { False }
+    method f(IO::Special:D:) { False }
+    method s(IO::Special:D:) { 0 }
+    method l(IO::Special:D:) { False }
+    method r(IO::Special:D:) { $!what eq '<IN>' }
+    method w(IO::Special:D:) { $!what eq '<OUT>' or $!what eq '<ERR>' }
+    method x(IO::Special:D:) { False }
+    method modified(IO::Special:D:) { Instant }
+    method accessed(IO::Special:D:) { Instant }
+    method changed(IO::Special:D:)  { Instant}
 }

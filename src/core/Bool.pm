514e41b166e0311c02ebf84b2d7735fa9375fb50
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-10-18 19:39:11 +0200

    Stay in NQP land as much as possible
    Good for another 15% performance increase

--- src/core/Bool.pm
+++ src/core/Bool.pm
@@ -18,15 +18,11 @@ my class Bool { # declared in BOOTSTRAP
 
     proto method pick(|) { * }
     multi method pick(Bool:U:) {
-       nqp::rand_n(2e0) >= 1  # can't use Rat's this early
-         ?? Bool::True        # must be fully qualified this early
-         !! Bool::False;
+       nqp::p6bool(nqp::rand_n(2e0) >= 1);
     }
     multi method pick(Bool:U: 0) { () }
     multi method pick(Bool:U: 1) {
-       nqp::rand_n(2e0) >= 1
-         ?? Bool::True
-         !! Bool::False;
+       nqp::p6bool(nqp::rand_n(2e0) >= 1);
     }
     multi method pick(Bool:U: $n) {
        nqp::rand_n(2e0) >= 1
@@ -36,13 +32,13 @@ my class Bool { # declared in BOOTSTRAP
 
     proto method roll(|) { * }
     multi method roll(Bool:U:) {
-       nqp::rand_n(2e0) >= 1 ?? Bool::True !! Bool::False;
+       nqp::p6bool(nqp::rand_n(2e0) >= 1);
     }
     multi method roll(Bool:U: 1) {
-       nqp::rand_n(2e0) >= 1 ?? Bool::True !! Bool::False;
+       nqp::p6bool(nqp::rand_n(2e0) >= 1);
     }
     multi method roll(Bool:U: $n) {
-       (nqp::rand_n(2e0) >= 1 ?? Bool::True !! Bool::False) xx $n;
+       (nqp::p6bool(nqp::rand_n(2e0) >= 1)) xx $n;
     }
 
     multi method ACCEPTS(Bool:D: Mu \topic ) { self }

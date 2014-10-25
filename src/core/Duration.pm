5f797ed75b5805ee18c760b3f7478f9bca3bb969
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-10-25 15:53:05 +0200

    s/x/tai, as x is so meaningless

--- src/core/Duration.pm
+++ src/core/Duration.pm
@@ -1,42 +1,42 @@
 my class Duration is Cool does Real {
-    has Rat $.x = 0;
+    has Rat $.tai = 0;
       # A linear count of seconds.
 
-    method new($x) { self.bless: x => $x.Rat }
+    method new($tai) { self.bless: tai => $tai.Rat }
 
-    method Bridge(Duration:D:) { $!x.Num }
-    method Rat(Duration:D:)    { $!x     }
-    method Num(Duration:D:)    { $!x.Num }
-    method narrow(Duration:D:) { $!x.narrow }
+    method Bridge(Duration:D:) { $!tai.Num }
+    method Rat(Duration:D:)    { $!tai     }
+    method Num(Duration:D:)    { $!tai.Num }
+    method narrow(Duration:D:) { $!tai.narrow }
 
-    multi method Str(Duration:D:) { ~$.x }
+    multi method Str(Duration:D:) { ~$.tai }
 
-    multi method perl(Duration:D:) { "Duration.new({$.x.perl})" }
+    multi method perl(Duration:D:) { "Duration.new({$.tai.perl})" }
 }
 
 multi sub prefix:<->(Duration:D $a) {
-    Duration.new: -$a.x;
+    Duration.new: -$a.tai;
 }
 
 multi sub infix:<+>(Duration:D $a, Real $b) {
-    Duration.new: $a.x + $b;
+    Duration.new: $a.tai + $b;
 }
 multi sub infix:<+>(Real $a, Duration:D $b) {
-    Duration.new: $a + $b.x;
+    Duration.new: $a + $b.tai;
 }
 multi sub infix:<+>(Duration:D $a, Duration:D $b) {
-    Duration.new: $a.x + $b.x;
+    Duration.new: $a.tai + $b.tai;
 }
 
 multi sub infix:<->(Duration:D $a, Real $b) {
-    Duration.new: $a.x - $b;
+    Duration.new: $a.tai - $b;
 }
 multi sub infix:<->(Duration:D $a, Duration:D $b) {
-    Duration.new: $a.x - $b.x;
+    Duration.new: $a.tai - $b.tai;
 }
 
 multi sub infix:<%>(Duration:D $a, Real $b) {
-    Duration.new: $a.x % $b
+    Duration.new: $a.tai % $b
 }
 
 # vim: ft=perl6 expandtab sw=4
--- src/core/Instant.pm
+++ src/core/Instant.pm
@@ -1,18 +1,18 @@
 my class Duration {... }
 
 my class Instant is Cool does Real {
-    has Rat $.x;
+    has Rat $.tai;
       # A linear count of seconds since 1970-01-01T00:00:00Z, plus
-      # tai-utc::initial-offset. Thus, $.x matches TAI from 1970
+      # tai-utc::initial-offset. Thus, $.tai matches TAI from 1970
       # to the present.
 
-    method new($x) { self.bless: x => $x.Rat }
+    method new($tai) { self.bless: tai => $tai.Rat }
 
     method from-posix($posix, Bool $prefer-leap-second = False) {
     # $posix is in general not expected to be an integer.
     # If $prefer-leap-second is true, 915148800 is interpreted to
     # mean 1998-12-31T23:59:60Z rather than 1999-01-01T00:00:00Z.
-        my $p = floor $posix;
+        my $p = $posix.floor;
         my $offset = tai-utc::initial-offset;
         for tai-utc::leap-second-posix() {
             if $_ < $p {
@@ -29,30 +29,28 @@ my class Instant is Cool does Real {
     # The inverse of .from-posix, except that the second return
     # value is true if *and only if* this Instant is in a leap
     # second.
-        my $n = floor $.x;
+        my $n = $.tai.floor;
         my $offset = tai-utc::initial-offset;
         for tai-utc::leap-second-posix() {
             if $_ < $n - $offset {
                 ++$offset;
             } else {
-                return ($.x - $offset, $n - $offset == $_)
+                return ($.tai - $offset, $n - $offset == $_)
             }
         }
-        ($.x - $offset, False)
+        ($.tai - $offset, False)
     }
 
     multi method Str(Instant:D:) {
-        'Instant:' ~ $.x
+        'Instant:' ~ $.tai
     }
     multi method perl(Instant:D:) {
-        "Instant.new($.x.perl())";
+        "Instant.new($.tai.perl())";
     }
-    method Bridge(Instant:D:) { $.x.Bridge }
-    method Num   (Instant:D:) { $.x.Num    }
-    method Int   (Instant:D:) { $.x.Int    }
-    method narrow(Instant:D:) { $.x.narrow }
-
-
+    method Bridge(Instant:D:) { $.tai.Bridge }
+    method Num   (Instant:D:) { $.tai.Num    }
+    method Int   (Instant:D:) { $.tai.Int    }
+    method narrow(Instant:D:) { $.tai.narrow }
 
 #    TODO: should be the new .gist, probably
 #    method Str() {
@@ -62,55 +60,54 @@ my class Instant is Cool does Real {
 }
 
 multi sub infix:«cmp»(Instant:D $a, Instant:D $b) {
-    $a.x <=> $b.x
-}
+    $a.tai <=> $b.tai }
 
 multi sub infix:«<=>»(Instant:D $a, Instant:D $b) {
-    $a.x <=> $b.x
+    $a.tai <=> $b.tai
 }
 
 multi sub infix:«==»(Instant:D $a, Instant:D $b) {
-    $a.x == $b.x
+    $a.tai == $b.tai
 }
 
 multi sub infix:«!=»(Instant:D $a, Instant:D $b) {
-    $a.x != $b.x
+    $a.tai != $b.tai
 }
 
 multi sub infix:«<»(Instant:D $a, Instant:D $b) {
-    $a.x < $b.x
+    $a.tai < $b.tai
 }
 
 multi sub infix:«>»(Instant:D $a, Instant:D $b) {
-    $a.x > $b.x
+    $a.tai > $b.tai
 }
 
 multi sub infix:«<=»(Instant:D $a, Instant:D $b) {
-    $a.x <= $b.x
+    $a.tai <= $b.tai
 }
 
 multi sub infix:«>=»(Instant:D $a, Instant:D $b) {
-    $a.x >= $b.x
+    $a.tai >= $b.tai
 }
 
 multi sub infix:<+>(Instant:D $a, Real:D $b) {
-    Instant.new: $a.x + $b;
+    Instant.new: $a.tai + $b;
 }
 multi sub infix:<+>(Real:D $a, Instant:D $b) {
-    Instant.new: $a + $b.x;
+    Instant.new: $a + $b.tai;
 }
 multi sub infix:<+>(Instant:D $a, Duration:D $b) {
-    Instant.new: $a.x + $b.x;
+    Instant.new: $a.tai + $b.tai;
 }
 multi sub infix:<+>(Duration:D $a, Instant:D $b) {
-    Instant.new: $a.x + $b.x;
+    Instant.new: $a.tai + $b.tai;
 }
 
 multi sub infix:<->(Instant:D $a, Instant:D $b) {
-    Duration.new: $a.x - $b.x;
+    Duration.new: $a.tai - $b.tai;
 }
 multi sub infix:<->(Instant:D $a, Real:D $b) {
-    Instant.new: $a.x - $b;
+    Instant.new: $a.tai - $b;
 }
 
 sub term:<time>() { nqp::p6box_i(nqp::time_i()) }

2fcc17cf7b541f3a800d42c02049f75d7b29b63d
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-11-03 17:27:39 +0100

    Introducing: IO::File, for existing files
    Well, actually anything that's not a directory, at this stage

---
+++ src/core/IO/File.pm
@@ -0,0 +1,19 @@
+# A class for file(path)s that we know exist
+my class IO::File does IO::Local {
+
+    submethod BUILD(:$!abspath) { }
+
+    method open(IO::File:D: |c) { open( $!abspath, |c ) }
+
+    method copy(IO::File:D: $to as Str, :$createonly) {
+        COPY-FILE($!abspath, MAKE-ABSOLUTE-PATH($to,$*CWD ~ '/'), :$createonly);
+    }
+
+    method unlink(IO::File:D:) { UNLINK-PATH($!abspath) }
+
+    method link(IO::File:D: $name as Str) {
+        LINK-FILE($!abspath, MAKE-ABSOLUTE-PATH($name,$*CWD ~ '/'));
+    }
+
+    method d(IO::File:D:) { False }
+}

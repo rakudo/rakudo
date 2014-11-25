5c669664ba7dc40bd5357c5bed1f87db3d6e71db
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-11-25 20:16:32 +0100

    Introduce SPURT-PATH()

--- src/core/IO.pm
+++ src/core/IO.pm
@@ -201,6 +201,21 @@ sub LINK-FILE(Str $target, Str $name) {
     True;
 }
 
+sub SPURT-PATH(Str $path, $what, :$enc, :$append, :$createonly, :$bin, |c) {
+    if $createonly and FILETEST-E($path) {
+        fail("File '$path' already exists, and :createonly was specified");
+    }
+    my $mode = $append ?? :a !! :w;
+    my $handle = open($path, :enc($enc // 'utf8'), :$bin, |$mode, |c);
+    $handle // $handle.throw;
+
+    my $spurt := $bin
+      ?? $handle.write($what)
+      !! $handle.print($what);
+    $handle.close;  # can't use LEAVE in settings :-(
+    $spurt;
+}
+
 sub MAKE-DIR(Str $path, Int $mode) {
     nqp::mkdir(nqp::unbox_s($path), nqp::unbox_i($mode));
     CATCH { default {

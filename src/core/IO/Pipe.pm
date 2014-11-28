58f79c192ef6b4f800296abd820cec99bb3be4e9
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-11-28 10:43:13 +0100

    Finally found the magic incantation, jnthn++

--- src/core/IO/Pipe.pm
+++ src/core/IO/Pipe.pm
@@ -2,10 +2,9 @@ A:
 class IO::Pipe does IO does PIO {
     has Str $.command;
 
-    method BUILD (:$command,Mu :$PIO,:$chomp,:$bin,:$encoding,:$nl) {
-        $!command = $command;
-        $!PIO := nqp::decont($PIO);
+    method BUILD(:$!command,Mu :$PIO,:$chomp,:$bin,:$encoding,:$nl) {
         $!chomp = $chomp // True;
+        nqp::bind($!PIO, nqp::decont($PIO));
         self.encoding($bin ?? 'binary' !! $encoding // 'utf8');
         self.nl($nl // "\n");
     }

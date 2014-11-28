bb40426359269f509580dc21149c7aa149bf7a68
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-11-28 17:25:23 +0100

    Use the proper way for closing a pipe

--- src/core/IO/Pipe.pm
+++ src/core/IO/Pipe.pm
@@ -3,6 +3,14 @@ class IO::Pipe does IO does PIO {
 
     method BUILD(:$!command,|c) { self!set-PIO-attributes(|c) }
 
+    method close(IO::Pipe:D:) {
+        # TODO:b catch errors
+        my $ps = Proc::Status.new;
+        $ps.status( nqp::closefh_i($!PIO) ) if nqp::defined($!PIO);
+        $!PIO := Mu;
+        $ps;
+    }
+
 #    method seek(|c) {
 #        fail "You cannot seek on an {self.^name}";
 #    }

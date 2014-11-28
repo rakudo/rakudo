cf051eb9c543bce89dbccf7b50aebca668eb635c
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-11-28 12:48:11 +0100

    Cannot mask a multi method from a role (yet)

--- src/core/IO/Pipe.pm
+++ src/core/IO/Pipe.pm
@@ -3,9 +3,9 @@ class IO::Pipe does IO does PIO {
 
     method BUILD(:$!command,|c) { self!set-PIO-attributes(|c) }
 
-    multi method seek(|c) {
-        fail "You cannot seek on an {self.^name}";
-    }
+#    method seek(|c) {
+#        fail "You cannot seek on an {self.^name}";
+#    }
 }
 
 # vim: ft=perl6 expandtab sw=4

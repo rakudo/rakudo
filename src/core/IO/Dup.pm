29e643a5be0ec3e693a2b31d82c7d50bcb2969bc
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-11-29 13:55:05 +0100

    Initial version of IO::Dup

--- src/core/IO/Dup.pm
+++ src/core/IO/Dup.pm
@@ -1,3 +1,44 @@
-# placeholder file to facilitate switching between nom and newio branches
+class IO::Dup does IO does PIO {
+    has int $.fileno;
+
+    my @name = <STDIN STDOUT STDERR>;
+
+    method BUILD(:$fileno,|c) {
+        $!fileno = $fileno;  # natives cannot be set in signature
+
+        my Mu $PIO;
+        if $!fileno == 0 {
+            $PIO := nqp::getstdin();
+        }
+        elsif $!fileno == 1 {
+            $PIO := nqp::getstdout();
+        }
+        elsif $!fileno == 2 {
+            $PIO := nqp::getstderr();
+        }
+        else {
+            die "Cannot dup file descriptor #$!fileno yet";
+        }
+
+        self!set-PIO-attributes(:$PIO,|c);
+    }
+
+    method WHICH(IO::Dup:D:) { "IO::Dup($!fileno)" }
+    method Str(IO::Dup:D:)   { @name[$!fileno] // "Dup#$!fileno" }
+
+    method IO(IO::Dup:D:) { self }
+
+    method e(IO::Dup:D:) { True }
+    method d(IO::Dup:D:) { False }
+    method f(IO::Dup:D:) { False }
+    method s(IO::Dup:D:) { 0 }
+    method l(IO::Dup:D:) { False }
+    method r(IO::Dup:D:) { $!fileno == 0 }
+    method w(IO::Dup:D:) { $!fileno  > 0 }
+    method x(IO::Dup:D:) { False }
+    method modified(IO::Dup:D:) { Instant }
+    method accessed(IO::Dup:D:) { Instant }
+    method changed(IO::Dup:D:)  { Instant }
+}
 
 # vim: ft=perl6 expandtab sw=4

7c43c898519d1d3de3edf95016a164e81dc4822b
Elizabeth Mattijsen <liz@dijkmat.nl>
2014-11-29 13:55:55 +0100

    IO::Special is now a placeholder

--- src/core/IO/Special.pm
+++ src/core/IO/Special.pm
@@ -1,21 +1,3 @@
-class IO::Special {
-    has Str $.what;
+# placeholder file to facilitate switching between nom and newio branches
 
-    submethod BUILD(:$!what!)    { }
-    method WHICH(IO::Special:D:) { "IO::Special$!what" }
-    method Str(IO::Special:D:)   { $!what }
-
-    method IO(IO::Special:D:) { self }
-
-    method e(IO::Special:D:) { True }
-    method d(IO::Special:D:) { False }
-    method f(IO::Special:D:) { False }
-    method s(IO::Special:D:) { 0 }
-    method l(IO::Special:D:) { False }
-    method r(IO::Special:D:) { $!what eq '<IN>' }
-    method w(IO::Special:D:) { $!what eq '<OUT>' or $!what eq '<ERR>' }
-    method x(IO::Special:D:) { False }
-    method modified(IO::Special:D:) { Instant }
-    method accessed(IO::Special:D:) { Instant }
-    method changed(IO::Special:D:)  { Instant}
-}
+# vim: ft=perl6 expandtab sw=4

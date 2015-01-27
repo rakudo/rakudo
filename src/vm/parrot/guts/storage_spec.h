340b7a79e335b605a38dd73bae674d55d1c077e4
Jonathan Worthington <jnthn@jnthn.net>
2015-01-27 21:30:00 +0100

    Sync header files with NQP.
    Should hopefully fix the r-p build.

--- src/vm/parrot/guts/storage_spec.h
+++ src/vm/parrot/guts/storage_spec.h
@@ -25,7 +25,7 @@ typedef struct {
     /* The types that this one can box/unbox to. */
     INTVAL can_box;
     
-    /* For ints, whether it's an usigned value. */
+    /* For ints, whether it's an unsigned value. */
     INTVAL is_unsigned;
 } storage_spec;
 

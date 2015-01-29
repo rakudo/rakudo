13eb6eaa6ffa8d74ad06d9dcb6f467a65ff47153
Brad Gilbert <b2gills@gmail.com>
2015-01-29 13:51:57 -0600

    Remove ICU check from Str
    ICU is now mandatory

--- src/core/Str.pm
+++ src/core/Str.pm
@@ -519,22 +519,11 @@ my class Str does Stringy { # declared in BOOTSTRAP
     multi method gist(Str:D:) { self }
     multi method perl(Str:D:) {
         my $result = '"';
-#?if parrot
-        my $icu = $*VM.config<has_icu>;
-        for ^self.chars -> $i {
-            my $ch = substr(self,$i, 1);
-            $result ~= %esc{$ch}
-                       //  (   ((!$icu && $ch.ord >= 256)
-                               || nqp::iscclass( nqp::const::CCLASS_PRINTING,
-                                                  nqp::unbox_s($ch), 0))
-#?endif
-#?if !parrot
         for ^self.chars -> $i {
             my $ch = substr(self,$i, 1);
             $result ~= %esc{$ch}
                        //  (nqp::iscclass( nqp::const::CCLASS_PRINTING,
                                                   nqp::unbox_s($ch), 0)
-#?endif
                            ?? $ch
                            !! $ch.ord.fmt('\x[%x]')
                            );

c12c52bbfa67ea47504188167132c2055a17788b
Elizabeth Mattijsen <liz@dijkmat.nl>
2015-02-07 11:43:52 +0100

    Make IO::Symlink compile

--- src/core/IO/Symlink.pm
+++ src/core/IO/Symlink.pm
@@ -1,3 +1,8 @@
-# placeholder file to facilitate switching between nom and newio branches
+# A class for symlinks that we know exist
+my class IO::Symlink is Cool does IO::Pathy {
+
+    method l(IO::Symlink:D:) { True }
+    # other filetests to be using lstat rather than stat
+}
 
 # vim: ft=perl6 expandtab sw=4

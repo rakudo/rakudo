bbe942f93301c14e9d3e02873f26f982b1c14f3b
Tobias Leich <email@froggs.de>
2015-02-16 14:48:14 +0100

    consider lib file extension info on jvm backend

--- lib/NativeCall.pm
+++ lib/NativeCall.pm
@@ -118,6 +118,7 @@ sub guess_library_name($libname) {
     elsif $libname ~~ /\.<.alpha>+$/ { $libname }
     elsif $libname ~~ /\.so(\.<.digit>+)+$/ { $libname }
     elsif $*VM.config<load_ext> :exists { $libname ~ $*VM.config<load_ext> }
+    elsif $*VM.config<nativecall.so> :exists { $libname ~ '.' ~ $*VM.config<nativecall.so> }
     elsif $*VM.config<dll> :exists {
         my $ext = $*VM.config<dll>;
         $ext ~~ s/^.*\%s//;

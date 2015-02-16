5899abe6c46e0974abf4c4a8294188034a8b1880
Elizabeth Mattijsen <liz@dijkmat.nl>
2015-02-16 15:44:19 +0100

    Look up $*VM only once

--- t/04-nativecall/CompileTestLib.pm
+++ t/04-nativecall/CompileTestLib.pm
@@ -2,15 +2,16 @@ module CompileTestLib;
 
 sub compile_test_lib($name) is export {
     my ($c_line, $l_line);
-    my $cfg = $*VM.config;
-    if $*VM.name eq 'parrot' {
+    my $VM := $*VM;
+    my $cfg = $VM.config;
+    if $VM.name eq 'parrot' {
         my $o  = $cfg<o>;
         my $so = $cfg<load_ext>;
         $c_line = "$cfg<cc> -c $cfg<cc_shared> $cfg<cc_o_out>$name$o $cfg<ccflags> t/04-nativecall/$name.c";
         $l_line = "$cfg<ld> $cfg<ld_load_flags> $cfg<ldflags> " ~
             "$cfg<libs> $cfg<ld_out>$name$so $name$o";
     }
-    elsif $*VM.name eq 'moar' {
+    elsif $VM.name eq 'moar' {
         my $o  = $cfg<obj>;
         my $so = $cfg<dll>;
         $so ~~ s/^.*\%s//;
@@ -18,7 +19,7 @@ sub compile_test_lib($name) is export {
         $l_line = "$cfg<ld> $cfg<ldshared> $cfg<ldflags> " ~
             "$cfg<ldlibs> $cfg<ldout>$name$so $name$o";
     }
-    elsif $*VM.name eq 'jvm' {
+    elsif $VM.name eq 'jvm' {
         $c_line = "$cfg<nativecall.cc> -c $cfg<nativecall.ccdlflags> -o$name$cfg<nativecall.o> $cfg<nativecall.ccflags> t/04-nativecall/$name.c";
         $l_line = "$cfg<nativecall.ld> $cfg<nativecall.perllibs> $cfg<nativecall.lddlflags> $cfg<nativecall.ldflags> $cfg<nativecall.ldout>$name.$cfg<nativecall.so> $name$cfg<nativecall.o>";
     }

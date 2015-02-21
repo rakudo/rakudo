b34447e0977ebbaa22bcbfaafeed8e868938e550
Paul Cochrane <paul@liekut.de>
2015-02-21 15:23:16 +0100

    Rename test functions to use hyphens instead of underscores
    The old test function names have been left as they are for the time being
    and redirect to the new function names internally.

--- lib/Test.pm
+++ lib/Test.pm
@@ -132,6 +132,10 @@ multi sub isnt(Mu $got, Mu $expected, $desc = '') is export {
 }
 
 multi sub cmp_ok(Mu $got, $op, Mu $expected, $desc = '') is export {
+    cmp-ok($got, $op, $expected, $desc);
+}
+
+multi sub cmp-ok(Mu $got, $op, Mu $expected, $desc = '') is export {
     $time_after = nqp::p6box_n(nqp::time_n);
     $got.defined; # Hack to deal with Failures
     my $ok;
@@ -152,6 +156,10 @@ multi sub cmp_ok(Mu $got, $op, Mu $expected, $desc = '') is export {
 }
 
 multi sub is_approx(Mu $got, Mu $expected, $desc = '') is export {
+    is-approx($got, $expected, $desc);
+}
+
+multi sub is-approx(Mu $got, Mu $expected, $desc = '') is export {
     $time_after = nqp::p6box_n(nqp::time_n);
     my $tol = $expected.abs < 1e-6 ?? 1e-5 !! $expected.abs * 1e-6;
     my $test = ($got - $expected).abs <= $tol;
@@ -185,6 +193,10 @@ multi sub skip($reason, $count = 1) is export {
 }
 
 sub skip_rest($reason = '<unknown>') is export {
+    skip-rest($reason);
+}
+
+sub skip-rest($reason = '<unknown>') is export {
     $time_after = nqp::p6box_n(nqp::time_n);
     die "A plan is required in order to use skip_rest" if $no_plan;
     skip($reason, $num_of_tests_planned - $num_of_tests_run);
@@ -224,6 +236,10 @@ multi sub flunk($reason) is export {
 }
 
 multi sub isa_ok(Mu $var, Mu $type, $msg = ("The object is-a '" ~ $type.perl ~ "'")) is export {
+    isa-ok($var, $type, $msg);
+}
+
+multi sub isa-ok(Mu $var, Mu $type, $msg = ("The object is-a '" ~ $type.perl ~ "'")) is export {
     $time_after = nqp::p6box_n(nqp::time_n);
     my $ok = proclaim($var.isa($type), $msg)
         or diag('Actual type: ' ~ $var.^name);
@@ -268,6 +284,10 @@ multi sub use-ok(Str $code, $msg = ("The module can be use-d ok")) is export {
 }
 
 multi sub dies_ok(Callable $code, $reason = '') is export {
+    dies-ok($code, $reason);
+}
+
+multi sub dies-ok(Callable $code, $reason = '') is export {
     $time_after = nqp::p6box_n(nqp::time_n);
     my $death = 1;
     try {
@@ -280,6 +300,10 @@ multi sub dies_ok(Callable $code, $reason = '') is export {
 }
 
 multi sub lives_ok(Callable $code, $reason = '') is export {
+    lives-ok($code, $reason);
+}
+
+multi sub lives-ok(Callable $code, $reason = '') is export {
     $time_after = nqp::p6box_n(nqp::time_n);
     try {
         $code();
@@ -290,6 +314,10 @@ multi sub lives_ok(Callable $code, $reason = '') is export {
 }
 
 multi sub eval_dies_ok(Str $code, $reason = '') is export {
+    eval-dies-ok($code, $reason);
+}
+
+multi sub eval-dies-ok(Str $code, $reason = '') is export {
     $time_after = nqp::p6box_n(nqp::time_n);
     my $ee = eval_exception($code);
     my $ok = proclaim( $ee.defined, $reason );
@@ -298,6 +326,10 @@ multi sub eval_dies_ok(Str $code, $reason = '') is export {
 }
 
 multi sub eval_lives_ok(Str $code, $reason = '') is export {
+    eval-lives-ok($code, $reason);
+}
+
+multi sub eval-lives-ok(Str $code, $reason = '') is export {
     $time_after = nqp::p6box_n(nqp::time_n);
     my $ee = eval_exception($code);
     my $ok = proclaim((not defined $ee), $reason)
@@ -306,8 +338,11 @@ multi sub eval_lives_ok(Str $code, $reason = '') is export {
     return $ok;
 }
 
-multi sub is_deeply(Mu $got, Mu $expected, $reason = '') is export
-{
+multi sub is_deeply(Mu $got, Mu $expected, $reason = '') is export {
+    is-deeply($got, $expected, $reason);
+}
+
+multi sub is-deeply(Mu $got, Mu $expected, $reason = '') is export {
     $time_after = nqp::p6box_n(nqp::time_n);
     my $test = _is_deeply( $got, $expected );
     my $ok = proclaim($test, $reason);
@@ -323,7 +358,11 @@ multi sub is_deeply(Mu $got, Mu $expected, $reason = '') is export
     return $ok;
 }
 
-sub throws_like($code, $ex_type, $reason?, *%matcher) is export {
+sub throws_like(|capture) is export {
+    throws-like(|capture);
+}
+
+sub throws-like($code, $ex_type, $reason?, *%matcher) is export {
     subtest {
         plan 2 + %matcher.keys.elems;
         my $msg;

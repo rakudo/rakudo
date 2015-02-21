23f9130ccf3ee494f01f404d1679432be936f2d1
Tobias Leich <email@froggs.de>
2015-02-21 23:09:47 +0100

    more CStruct sizeof tests

--- t/04-nativecall/12-sizeof.c
+++ t/04-nativecall/12-sizeof.c
@@ -19,12 +19,34 @@ typedef struct {
     short bar5;
 } Bar;
 
+typedef struct {
+    char  bar1;
+    short bar2;
+    char  bar3;
+    int   bar4;
+    short bar5;
+    long  baz6;
+    int   bar7;
+} Baz;
+
+typedef struct {
+    char  buz1;
+} Buz;
+
 DLLEXPORT int SizeofFoo() {
     return sizeof(Foo);
 }
 
 DLLEXPORT int SizeofBar() {
     return sizeof(Bar);
+}
+
+DLLEXPORT int SizeofBaz() {
+    return sizeof(Baz);
+}
+
+DLLEXPORT int SizeofBuz() {
+    return sizeof(Buz);
 }
 
 DLLEXPORT int SizeofInt() {

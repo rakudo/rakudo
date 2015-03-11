070a1f598edf7325ff6c451de37b0cb1877b4b88
Mouq <alexmoquin@gmail.com>
2015-03-11 07:21:44 -0400

    Throw if two infinite lists are attempted to be hyperop'd

--- src/core/Exception.pm
+++ src/core/Exception.pm
@@ -1444,8 +1444,18 @@ my class X::HyperOp::NonDWIM is Exception {
     has $.left-elems;
     has $.right-elems;
     method message() {
-        "Lists on both side of non-dwimmy hyperop of &.operator.name() are not of the same length\n"
+        "Lists on either side of non-dwimmy hyperop of &.operator.name() are not of the same length\n"
         ~ "left: $.left-elems elements, right: $.right-elems elements";
+    }
+}
+
+my class X::HyperOp::Infinite is Exception {
+    has &.operator;
+    has $.side;
+    method message() {
+        $.side eq "both"
+            ?? "Lists on both sides of hyperop of &.operator.name() are known to be infinite"
+            !! "List on $.side side of hyperop of &.operator.name() is known to be infinite"
     }
 }
 

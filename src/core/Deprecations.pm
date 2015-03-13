0a8bdf23f5c9057662c513bd2f24f3027f1516dd
Elizabeth Mattijsen <liz@dijkmat.nl>
2015-03-13 08:22:08 +0100

    Kebab-case RAKUDO_NO_DEPRECATIONS
    There is no point in adding a deprecation cycle here, as adding it would
    inhibit seeing that particular deprecation message.  Since hardly anybody
    is using this feature, I tought it'd be safe to kebab-case immediately.

--- src/core/Deprecations.pm
+++ src/core/Deprecations.pm
@@ -108,7 +108,7 @@ sub DEPRECATED ( $alternative, $from?, $removed?, :$up = 1, :$what ) {
 }
 
 END {
-    unless %*ENV<RAKUDO_NO_DEPRECATIONS> {
+    unless %*ENV<RAKUDO-NO-DEPRECATIONS> {
         if Deprecation.report -> $message {
             note $message;   # q:to/TEXT/ doesn't work in settings
             note 'Please contact the author to have these calls to deprecated code adapted,

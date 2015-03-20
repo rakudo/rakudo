b7a974462afdb4eb32cd07c5d666cc908bc1df04
Pepe Schwarz <peschwa@gmail.com>
2015-03-20 19:40:13 +0000

    Correct CLASSPATH example

--- src/vm/jvm/runtime/org/perl6/rakudo/RakudoEvalServer.java
+++ src/vm/jvm/runtime/org/perl6/rakudo/RakudoEvalServer.java
@@ -17,12 +17,12 @@ public class RakudoEvalServer extends EvalServer {
     /*
         Neccessary elements in the CLASSPATH are as follows:
 
-        $P6_INSTALL_PREFIX/languages/nqp/lib
-        $P6_INSTALL_PREFIX/languages/nqp/runtime/*
-        $P6_INSTALL_PREFIX/languages/perl6/lib
-        $P6_INSTALL_PREFIX/languages/perl6/lib/Perl6
-        $P6_INSTALL_PREFIX/languages/perl6/runtime
-        $P6_INSTALL_PREFIX/languages/perl6/runtime/*
+        $P6_INSTALL_PREFIX/share/nqp/lib
+        $P6_INSTALL_PREFIX/share/nqp/runtime/*
+        $P6_INSTALL_PREFIX/share/perl6/lib
+        $P6_INSTALL_PREFIX/share/perl6/lib/Perl6
+        $P6_INSTALL_PREFIX/share/perl6/runtime
+        $P6_INSTALL_PREFIX/share/perl6/runtime/*
     */
     public RakudoEvalServer() {
         String[] cps = System.getProperty("java.class.path").split("[;:]");
@@ -36,7 +36,7 @@ public class RakudoEvalServer extends EvalServer {
             throw new RuntimeException("CLASSPATH not set properly, couldn't find perl6.jar");
         }
 
-        System.setProperty("perl6.prefix", appname.substring(0, appname.indexOf("/languages")));
+        System.setProperty("perl6.prefix", appname.substring(0, appname.indexOf("/share")));
         System.setProperty("perl6.execname", "RakudoEvalServer");
     }
 }

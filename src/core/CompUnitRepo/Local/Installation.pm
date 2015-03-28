9cd98ed5def7a91b37747f52e109522b769cbc7d
Tobias Leich <email@froggs.de>
2015-03-28 18:26:14 +0100

    fix installed runners for Windows PowerShell

--- src/core/CompUnitRepo/Local/Installation.pm
+++ src/core/CompUnitRepo/Local/Installation.pm
@@ -20,10 +20,10 @@ class CompUnitRepo::Local::Installation does CompUnitRepo::Locally {
     my $windows_wrapper = '@rem = \'--*-Perl-*--
 @echo off
 if "%OS%" == "Windows_NT" goto WinNT
-#perl# "%~dp0\%0" %1 %2 %3 %4 %5 %6 %7 %8 %9
+#perl# "%~dpn0" %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto endofperl
 :WinNT
-#perl# "%~dp0\%0" %*
+#perl# "%~dpn0" %*
 if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
 if %errorlevel% == 9009 echo You do not have Perl in your PATH.
 if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul

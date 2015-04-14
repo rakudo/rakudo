5bc3954a34d0fdc310d5dbbd1eafe3ec8f4081cf
Brent Laabs <bslaabs@gmail.com>
2015-04-13 19:20:53 -0700

    add infix:<ne>(Str,Str) subs, skip metaop for ne(Any, Any)
    Makes string ne much closer in speed to eq by using nqp::isne_s().
    28-47% faster, depending on how many of the args are strings.

--- src/core/Stringy.pm
+++ src/core/Stringy.pm
@@ -28,6 +28,7 @@ multi sub infix:<eq>(\a, \b)       { a.Stringy eq b.Stringy }
 proto sub infix:<ne>(Mu $?, Mu $?) is pure { * }
 multi sub infix:<ne>($x?)            { Bool::True }
 multi sub infix:<ne>(Mu \a, Mu \b)   { a !eq b }
+multi sub infix:<ne>(Any \a, Any \b) { a.Stringy ne b.Stringy }
 
 proto sub infix:<lt>(Mu $?, Mu $?) is pure { * }
 multi sub infix:<lt>($x?)          { Bool::True }

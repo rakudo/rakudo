51c0852848b7cc0ba99a92acbd8873e07a0481c9
Tobias Leich <email@froggs.de>
2015-05-16 21:50:41 +0200

    put CUR object into %*CUSTOM_LIB for panda
    Panda needs to check whether site or home are under control of a CUR,
    and therefore manage theirself.

--- src/core/Inc.pm
+++ src/core/Inc.pm
@@ -53,8 +53,7 @@ RAKUDO_MODULE_DEBUG("Init @INC with {specs}")
                     for $props.list -> $prop {
                         if nqp::istype($prop,Associative) {
                             for $prop.value.flat -> $path {
-                                @INC.push: PARSE-INCLUDE-SPECS($path);
-                                %CUSTOM_LIB{$prop.key} = $path;
+                                @INC.push: %CUSTOM_LIB{$prop.key} = PARSE-INCLUDE-SPECS($path);
                             }
                         }
                         else {
@@ -76,7 +75,7 @@ RAKUDO_MODULE_DEBUG("Init @INC with {specs}")
                     my $ver := nqp::p6box_s(nqp::atkey($compiler, 'version'));
                     my $path := "$home/.perl6/$ver";
                     @INC.push: "file#$path/lib", "inst#$path";
-                    %CUSTOM_LIB<home> = $path;
+                    %CUSTOM_LIB<home> = INCLUDE-SPEC2CUR("inst#$path");
                 }
             }
             @INC.push:
@@ -87,9 +86,9 @@ RAKUDO_MODULE_DEBUG("Init @INC with {specs}")
               "inst#$prefix/vendor",
               "inst#$prefix/site";
 
-            %CUSTOM_LIB<perl>   =  $prefix;
-            %CUSTOM_LIB<vendor> = "$prefix/vendor";
-            %CUSTOM_LIB<site>   = "$prefix/site";
+            %CUSTOM_LIB<perl>   = INCLUDE-SPEC2CUR("inst#$prefix");
+            %CUSTOM_LIB<vendor> = INCLUDE-SPEC2CUR("inst#$prefix/vendor");
+            %CUSTOM_LIB<site>   = INCLUDE-SPEC2CUR("inst#$prefix/site");
         }
     }
 

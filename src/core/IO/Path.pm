rakudo/rakudo

commit e60f85c275af59e9d42d602399042b8da8acb59c

Don't allow empty path specification, raydiak++
It functioned as 'current directory' so far, but that can have dangerous
implications, especially when trying to do an rm-rf on a directory, and you
lost the directory name somewhere along the way.

src/core/IO/Path.pm
@@ -29,7 +29,8 @@ my class IO::Path is Cool {
         self.bless(:$path, :$SPEC, :$CWD)!fap;
     }
 
-    multi method new(IO::Path: $path, :$SPEC = $*SPEC, :$CWD = $*CWD) {
+    multi method new(IO::Path: $path?, :$SPEC = $*SPEC, :$CWD = $*CWD) {
+        die "Must specify something as a path: did you mean '.' for the current directory?" unless $path;
         self.bless(:$path, :$SPEC, :$CWD);
     }
     multi method new(IO::Path:

commit 00b44eb369b858b0cdd5cb3157090d2ea72a5799

Fix IO::Path(:basename<foo>), spotted by cygx++
Which I broke with e60f85c275af59e9d42d and which did not
cause any spectest breakage :-(

src/core/IO/Path.pm
@@ -29,7 +29,7 @@ my class IO::Path is Cool {
         self.bless(:$path, :$SPEC, :$CWD)!fap;
     }
 
-    multi method new(IO::Path: $path?, :$SPEC = $*SPEC, :$CWD = $*CWD) {
+    multi method new(IO::Path: $path, :$SPEC = $*SPEC, :$CWD = $*CWD) {
         die "Must specify something as a path: did you mean '.' for the current directory?" unless $path;
         self.bless(:$path, :$SPEC, :$CWD);
     }
@@ -46,12 +46,15 @@ my class IO::Path is Cool {
     }
     multi method new(IO::Path:
       :$basename!,
-      :$dirname = '.',
+      :$dirname = '',
       :$volume  = '',
       :$SPEC    = $*SPEC,
       :$CWD     = $*CWD,
     ) {
         self.bless(:path($SPEC.join($volume,$dirname,$basename)),:$SPEC,:$CWD);
+    }
+    multi method new(IO::Path:) {
+        die "Must specify something as a path: did you mean '.' for the current directory?";
     }
 
     method abspath() {
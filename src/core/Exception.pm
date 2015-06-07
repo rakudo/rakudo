043d436af8cfa43c02f3183dd134fa19b6c96190
Rob Hoelz <rob@hoelz.ro>
2015-06-07 14:56:41 -0500

    Add new X::StubCode exception type
    Exceptions of this type are used when stubbed-out code (eg. .../???/!!!)
    are called

--- src/core/Exception.pm
+++ src/core/Exception.pm
@@ -1942,4 +1942,10 @@ my class X::PairMap::NotAllowed is Exception {
     }
 }
 
+my class X::StubCode is Exception {
+    method message() {
+        'Stub code executed'
+    }
+}
+
 # vim: ft=perl6 expandtab sw=4

d9c1e267f50513eaeb9717be01fea97585e4ca85
Timo Paulssen <timonator@perpetuum-immobile.de>
2015-07-06 16:22:30 +0200

    make Failure objects a tiny bit smaller

--- src/core/Failure.pm
+++ src/core/Failure.pm
@@ -1,7 +1,7 @@
 my class Failure {
     has $.exception;
     has $.backtrace;
-    has $!handled;
+    has int $!handled;
 
     method new(Exception $exception) {
          self.bless(:$exception);

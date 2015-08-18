facd0efcbcc08292d7751b0900b126f55cf2f740
Elizabeth Mattijsen <liz@dijkmat.nl>
2015-08-18 13:51:49 +0900

    Give push()/unshift() same semantics as method
    Spotted by skids++

--- src/core/Array.pm
+++ src/core/Array.pm
@@ -543,12 +543,13 @@ proto sub shift(@) {*}
 multi sub shift(@a) { @a.shift }
 
 proto sub unshift(|) {*}
-multi sub unshift(\a, \elem)   { a.unshift: elem }
-multi sub unshift(\a, *@elems) { a.unshift: @elems }
+multi sub unshift(\a, \elem)    { a.unshift: elem }
+multi sub unshift(\a, **@elems) { a.unshift: @elems }
 
 proto sub push(|) {*}
-multi sub push(\a, \elem)   { a.push: elem }
-multi sub push(\a, *@elems) { a.push: @elems }
-sub splice(@arr, |c)        { @arr.splice(|c) }
+multi sub push(\a, \elem)    { a.push: elem }
+multi sub push(\a, **@elems) { a.push: @elems }
+
+sub splice(@arr, |c)         { @arr.splice(|c) }
 
 # vim: ft=perl6 expandtab sw=4

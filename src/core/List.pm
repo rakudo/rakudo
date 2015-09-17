2266f4a5d3700a9f2d37da51e439ff5594756583
TimToady <larry@wall.org>
2015-09-16 21:04:05 -0700

    simplify +args binding code

--- src/core/List.pm
+++ src/core/List.pm
@@ -211,24 +211,23 @@ my class List does Iterable does Positional { # declared in BOOTSTRAP
 
     method from-slurpy-onearg(|) {
         my Mu \vm-tuple = nqp::captureposarg(nqp::usecapture(), 1);
-        my int $n = nqp::elems(vm-tuple);
-        my \future = IterationBuffer.CREATE;
-        if $n == 1 {
-            my \consider = nqp::atpos(vm-tuple, 0);
-            my $no-sink := nqp::push(future, nqp::iscont(consider)
-                ?? consider
-                !! nqp::istype(consider, Iterable) && consider.DEFINITE
-                    ?? consider.list.Slip
-                    !! consider);
-        }
-
         my \result := self.CREATE;
         my \buffer := IterationBuffer.CREATE;
         my \todo := List::Reifier.CREATE;
         nqp::bindattr(result, List, '$!reified', buffer);
         nqp::bindattr(result, List, '$!todo', todo);
         nqp::bindattr(todo, List::Reifier, '$!reified', buffer);
-        nqp::bindattr(todo, List::Reifier, '$!future', $n == 1 ?? future !! vm-tuple);
+        nqp::bindattr(todo, List::Reifier, '$!future',
+            nqp::elems(vm-tuple) != 1
+            ?? vm-tuple
+            !! do {
+                my \consider = nqp::atpos(vm-tuple, 0);
+                nqp::iscont(consider) || !nqp::istype(consider, Iterable) || !consider.DEFINITE
+                ?? vm-tuple
+                !! nqp::list(consider.list.Slip)
+            }
+        );
+
         nqp::bindattr(todo, List::Reifier, '$!reification-target',
             result.reification-target());
         result

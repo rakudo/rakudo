8bc846721acf6d73dced9f102c2eb133d60a6d8f
TimToady <larry@wall.org>
2015-10-01 13:59:52 -0700

    delete incorrect, duplicate code
    (If we really need to dup code for performance, it should do one-arg
    lazy semantics correctly as the new slip.t tests now test for.)

--- src/core/Slip.pm
+++ src/core/Slip.pm
@@ -3,38 +3,12 @@
 my class Slip {
     multi method Slip(Slip:D:) { self }
     method defined () { self.so }
+    method CALL-ME (+args) { args.Slip }
 }
 
 # The slip(...) function creates a Slip.
 proto slip(|) { * }
-multi slip() {
-    my \result = Slip.CREATE;
-    nqp::bindattr(result, List, '$!reified', BEGIN IterationBuffer.CREATE);
-    result
-}
-multi slip(|) {
-    my \result  = Slip.CREATE;
-    my \in      = nqp::p6argvmarray();
-    my \reified = IterationBuffer.CREATE;
-    nqp::bindattr(result, List, '$!reified', reified);
-    while nqp::elems(in) {
-        if nqp::istype(nqp::atpos(in, 0), Slip) {
-            # We saw a Slip, so we'll lazily deal with the rest of the things
-            # (as the Slip may expand to something lazy).
-            my \todo := List::Reifier.CREATE;
-            nqp::bindattr(result, List, '$!todo', todo);
-            nqp::bindattr(todo, List::Reifier, '$!reified', reified);
-            nqp::bindattr(todo, List::Reifier, '$!future', in);
-            nqp::bindattr(todo, List::Reifier, '$!reification-target',
-                result.reification-target());
-            last;
-        }
-        else {
-            nqp::push(reified, nqp::shift(in));
-            Nil # don't Sink the thing above
-        }
-    }
-    result
-}
+multi slip() { Empty }
+multi slip(+args) { args.Slip }
 
 # vim: ft=perl6 expandtab sw=4

rakudo/rakudo

commit 2e92038e92c46fa7ec9e7187081ed24e3e7af22c

make fail, die and Failure.new cheaper

src/core/Failure.pm
@@ -7,8 +7,9 @@ my class Failure {
         self.bless(:$exception);
     }
     multi method new($payload =
-        (CALLER::CALLER::.EXISTS-KEY('$!') and CALLER::CALLER::('$!').DEFINITE)
-         ?? CALLER::CALLER::('$!') !! 'Failed') {
+        nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx))).EXISTS-KEY('$!')
+        && nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!').DEFINITE
+            ?? nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!') !! "Died") {
         if ($payload ~~ Exception) {
             self.bless(:exception($payload));
         }
@@ -78,8 +79,9 @@ multi sub fail(Exception:U $e) {
     $fail
 }
 multi sub fail($payload =
-    (CALLER::CALLER::.EXISTS-KEY('$!') and CALLER::CALLER::('$!').DEFINITE)
-     ?? CALLER::CALLER::('$!') !! 'Failed') {
+        nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx))).EXISTS-KEY('$!')
+        && nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!').DEFINITE
+            ?? nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!') !! "Died") {
 
     my Mu $return := nqp::getlexcaller('RETURN');
 

src/core/control.pm
@@ -172,8 +172,9 @@ multi sub die(Exception:U $e) {
     X::AdHoc.new(:payload("Died with undefined " ~ $e.^name)).throw;
 }
 multi sub die($payload =
-    (CALLER::CALLER::.EXISTS-KEY('$!') and CALLER::CALLER::('$!').DEFINITE)
-     ?? CALLER::CALLER::('$!') !! "Died") {
+        nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx))).EXISTS-KEY('$!')
+        && nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!').DEFINITE
+            ?? nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!') !! "Died") {
     if $payload ~~ Exception {
         $payload.throw;
     }

commit b06d30decf4d4341818b039a095e09fbb5e9bf94

Refactor fail/die again
Alas, 2e92038e92c46fa7ec9e introduced quite some spectest errors.
This commit goes back to using CALLER::CALLER:: PseudoStash Perl6
objects, rather than the lowlevel NQP objects that do not support
things like EXISTS-KEY.  But instead of before, it will only be doing
a CALLER::CALLER:: once.  And to further improve performance, the
parameterless fail/die are not separate candidates, hopefully make it
easier to inline.

Unfortunately, this causes 3 (new) spectest failures, but all failing
tests seem suspect to me, so I'm putting this in rather than just
revert 2e92038e92c46fa7ec9e .

src/core/Failure.pm
@@ -3,19 +3,20 @@ my class Failure {
     has $.backtrace;
     has $!handled;
 
+    multi method new() {
+        my $stash := CALLER::CALLER::;
+        my $payload = $stash<$!>.DEFINITE ?? $stash<$!> !! "Died";
+        self.bless(:exception( $payload ~~ Exception
+          ?? $payload !! X::AdHoc.new(:$payload)
+        ))
+    }
     multi method new(Exception $exception) {
         self.bless(:$exception);
     }
-    multi method new($payload =
-        nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx))).EXISTS-KEY('$!')
-        && nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!').DEFINITE
-            ?? nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!') !! "Died") {
-        if ($payload ~~ Exception) {
-            self.bless(:exception($payload));
-        }
-        else {
-            self.bless(:exception(X::AdHoc.new(:$payload)));
-        }
+    multi method new($payload) {
+        self.bless(:exception( $payload ~~ Exception
+          ?? $payload !! X::AdHoc.new(:$payload)
+        ))
     }
     multi method new(|cap (*@msg)) {
          self.bless(:exception(X::AdHoc.from-slurpy(|cap)));
@@ -70,6 +71,18 @@ my class Failure {
 }
 
 proto sub fail(|) {*};
+multi sub fail() {
+    my $stash := CALLER::CALLER::;
+    my $payload = $stash<$!>.DEFINITE ?? $stash<$!> !! "Died";
+
+    my $fail := Failure.new( $payload ~~ Exception
+      ?? $payload !! X::AdHoc.new(:$payload));
+
+    my Mu $return := nqp::getlexcaller('RETURN');
+    $return($fail) unless nqp::isnull($return);
+
+    $fail
+}
 multi sub fail(Exception:U $e) {
     my $fail := Failure.new(
         X::AdHoc.new(:payload("Failed with undefined " ~ $e.^name))
@@ -78,29 +91,16 @@ multi sub fail(Exception:U $e) {
     $return($fail) unless nqp::isnull($return);
     $fail
 }
-multi sub fail($payload =
-        nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx))).EXISTS-KEY('$!')
-        && nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!').DEFINITE
-            ?? nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!') !! "Died") {
+multi sub fail($payload) {
+    my $fail := Failure.new( $payload ~~ Exception
+      ?? $payload
+      !! X::AdHoc.new(:$payload)
+    );
 
     my Mu $return := nqp::getlexcaller('RETURN');
+    $return($fail) unless nqp::isnull($return);
 
-    # Strange behavior alert:
-    # If you try to if(...) { $fail := ... } else { $fail := ... }
-    # here it behaves sort of as if "use fatal" were in effect even
-    # when it is not, except that's not what is going on because
-    # "die" does not get hit AFAICT.  That took me 4 hours of
-    # fumbling around to figure out what was wrong.
-    if ($payload ~~ Exception) {
-        my $fail := Failure.new($payload);
-        $return($fail) unless nqp::isnull($return);
-        $fail
-    }
-    else {
-        my $fail := Failure.new(X::AdHoc.new(:$payload));
-        $return($fail) unless nqp::isnull($return);
-        $fail
-    }
+    $fail
 }
 multi sub fail(|cap (*@msg)) {
     my $fail := Failure.new(X::AdHoc.from-slurpy(|cap));

src/core/control.pm
@@ -168,19 +168,20 @@ sub done() {
 }
 
 proto sub die(|) {*};
+multi sub die() {
+    my $stash  := CALLER::CALLER::;
+    my $payload = $stash<$!>.DEFINITE ?? $stash<$!> !! "Died";
+    $payload ~~ Exception
+      ?? $payload.throw
+      !! X::AdHoc.new(:$payload).throw
+}
 multi sub die(Exception:U $e) {
     X::AdHoc.new(:payload("Died with undefined " ~ $e.^name)).throw;
 }
-multi sub die($payload =
-        nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx))).EXISTS-KEY('$!')
-        && nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!').DEFINITE
-            ?? nqp::ctxlexpad(nqp::ctxcaller(nqp::ctxcaller(nqp::ctx)))('$!') !! "Died") {
-    if $payload ~~ Exception {
-        $payload.throw;
-    }
-    else {
-        X::AdHoc.new(:$payload).throw
-    }
+multi sub die($payload) {
+    $payload ~~ Exception
+      ?? $payload.throw
+      !! X::AdHoc.new(:$payload).throw
 }
 multi sub die(|cap ( *@msg )) {
     X::AdHoc.from-slurpy(|cap).throw
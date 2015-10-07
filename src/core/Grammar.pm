d5eeb4df892d8e545763e807d774defb36444415
Elizabeth Mattijsen <liz@dijkmat.nl>
2015-10-07 15:59:34 +0200

    Adaptation of PR #546, cognominal++
    Coerce to Capture, rather than just take

--- src/core/Grammar.pm
+++ src/core/Grammar.pm
@@ -1,12 +1,12 @@
 my class Grammar is Cursor {
-    method parse($target, :$rule = 'TOP',  List :$args = (), Mu :$actions = Mu, *%opt) {
+    method parse($target, :$rule = 'TOP',  Capture() :$args = \(), Mu :$actions = Mu, *%opt) {
         my $*ACTIONS = $actions;
         my $result =
             self."!cursor_init"($target, |%opt)."$rule"(|$args).MATCH;
         $result = Nil unless $result.to == $target.chars;
         nqp::getlexcaller('$/') = $result;
     }
-    method subparse($target, :$rule = 'TOP', List :$args = (),  Mu :$actions = Mu, *%opt) {
+    method subparse($target, :$rule = 'TOP', Capture() :$args = \(),  Mu :$actions = Mu, *%opt) {
         my $*ACTIONS = $actions;
         nqp::getlexcaller('$/') =
             self."!cursor_init"($target, |%opt)."$rule"(|$args).MATCH;

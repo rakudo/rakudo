154f44be9bb35dbc5034f986415dd5b4a6ab7fab
Elizabeth Mattijsen <liz@dijkmat.nl>
2015-10-13 16:45:25 +0200

    Make await on Iterable, rather than List, jnthn++

--- src/core/asyncops.pm
+++ src/core/asyncops.pm
@@ -10,10 +10,10 @@ multi sub await() {
 multi sub await(Any $x) {
     die "Must specify a Promise or Channel to await on (got a $x.^name())";
 }
-multi sub await(List:D $list) { $list.eager.map({ await $_ }) }
-multi sub await(Promise:D $p) { $p.result }
-multi sub await(Channel:D $c) { $c.receive }
-multi sub await(*@awaitables) { @awaitables.eager.map({await $_}) }
+multi sub await(Iterable:D $i) { $i.eager.map({ await $_ }) }
+multi sub await(Promise:D $p)  { $p.result }
+multi sub await(Channel:D $c)  { $c.receive }
+multi sub await(*@awaitables)  { @awaitables.eager.map({await $_}) }
 
 sub cas (\val,&code) { val = code(val) } # naive implementation of cas
 

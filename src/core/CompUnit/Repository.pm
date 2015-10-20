d7bcf66fa4829a2edb080eecc038b454efffbeb7
Stefan Seifert <nine@detonation.org>
2015-10-20 21:08:25 +0200

    Add CompUnit::Repository role

---
+++ src/core/CompUnit/Repository.pm
@@ -0,0 +1,26 @@
+role CompUnit::PrecompilationRepository { ... }
+
+role CompUnit::Repository {
+    # Resolves a dependency specification to a concrete dependency. If the
+    # dependency was not already loaded, loads it. Returns a CompUnit
+    # object that represents the selected dependency. If there is no
+    # matching dependency, throws X::CompUnit::UnsatisfiedDependency.
+    method need(CompUnit::DependencySpecification $spec,
+                # If we're first in the chain, our precomp repo is the chosen one.
+                CompUnit::PrecompilationRepository $precomp = self.precomp-repository())
+        returns CompUnit:D
+        { ... }
+
+    # Returns the CompUnit objects describing all of the compilation
+    # units that have been loaded by this repository in the current
+    # process.
+    method loaded()
+        returns Iterable
+        { ... }
+
+    method precomp-repository()
+        returns CompUnit::PrecompilationRepository
+        { CompUnit::PrecompilationRepository::None }
+}
+
+# vim: ft=perl6 expandtab sw=4

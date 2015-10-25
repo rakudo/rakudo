b0b2489a2cff7b56d61e8e4a7b8bc7c51fd852cc
Elizabeth Mattijsen <liz@dijkmat.nl>
2015-10-25 15:28:44 +0100

    Fix #126447 and #125953 (and possibly more)

--- src/core/control.pm
+++ src/core/control.pm
@@ -268,7 +268,7 @@ sub CLONE-HASH-DECONTAINERIZED(\hash) {
     my $e;
     while $iter {
         $e := nqp::shift($iter);
-        nqp::bindkey($clone, nqp::iterkey_s($e), nqp::decont(nqp::iterval($e)));
+        nqp::bindkey($clone,nqp::iterkey_s($e),nqp::decont(~nqp::iterval($e)));
     }
     $clone;
 }

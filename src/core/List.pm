43e9d37de68f35160bc2be54153caad27a25cb4a
jnthn <jnthn@jnthn.net>
2015-11-12 13:41:27 +0100

    Remove backref methods.
    We don't need to construct the entire match object just to backref
    things.

--- src/core/List.pm
+++ src/core/List.pm
@@ -283,7 +283,6 @@ my class List does Iterable does Positional { # declared in BOOTSTRAP
     # Pretend we're a Match assuming we're a list of Matches
     method to()      { self.elems ?? self[self.end].to !! Nil }
     method from()    { self.elems ?? self[0].from !! Nil }
-    method backref() { self.elems ?? self[0].orig.substr(self[0].from ..^ self[self.end].to) !! Nil }
 
     method fmt($format = '%s', $separator = ' ') {
         self.map({ .fmt($format) }).join($separator);

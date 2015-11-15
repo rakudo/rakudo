e383972c5d686009b903a7633a9ebdc353d738df
Stefan Seifert <nine@detonation.org>
2015-11-15 15:15:24 +0100

    Make sure $*PERL.compiler.id changes on recompilation
    Previously this id only changed when recompiling src/Perl6/Compiler.nqp.
    This new implementation can most probably still be improved, but at least
    works for now.

--- src/core/Compiler.pm
+++ src/core/Compiler.pm
@@ -3,6 +3,7 @@ class Compiler does Systemic {
     has Str $.release;
     has Str $!build-date;
     has Str $.codename;
+    BEGIN my $id = $*W.handle.Str ~ '.' ~ nqp::time_n();
 
     submethod BUILD (
       :$!name      = 'rakudo',
@@ -16,7 +17,7 @@ class Compiler does Systemic {
         my Mu $compiler := nqp::getcurhllsym('$COMPILER_CONFIG');
         $!id = nqp::p6box_s( nqp::existskey($compiler,'id')
           ?? nqp::atkey($compiler,'id')
-          !! nqp::getcomp('perl6').compilation-id );
+          !! $id );
         $!version = Version.new(
           $version // nqp::p6box_s(nqp::atkey($compiler, 'version')) );
         $!release =

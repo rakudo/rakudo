b8160db6075d31909557da7efc75a67271826dfd
Elizabeth Mattijsen <liz@dijkmat.nl>
2015-11-19 21:57:04 -0500

    Revert "improve permutations"
    This reverts commit 57b83f4ad7803bb2dc96313c42636229ca76bf6d.
    
    As requested by the original PR author, grondilu++

--- src/core/native_array.pm
+++ src/core/native_array.pm
@@ -519,16 +519,13 @@ class array does Iterable is repr('VMArray') {
 sub permutations(int $n) {
     my int $i;
     $n == 1 ?? ( (0,), ) !!
-    gather {
-        my @permutations = permutations($n - 1);
-        while $i < $n {
-            my Int @i;
-            my int $j;
-            @i.push($j++) while $j < $i;
-            $j = $i + 1;
-            @i.push($j++) while $j < $n;
-            take (nqp::clone($i), |@i[@$_]) for @permutations;
-            $i = $i + 1;
-        }
+    gather while $i < $n {
+        my Int @i;
+        my int $j;
+        @i.push($j++) while $j < $i;
+        $j = $i + 1;
+        @i.push($j++) while $j < $n;
+        take (nqp::clone($i), |@i[@$_]) for permutations($n - 1);
+        $i = $i + 1;
     }
 }

49d8728f19864150bd5d0a46d9d9f24912ac7ff8
grondilu <grondilu@yahoo.fr>
2015-12-05 14:43:16 +0100

    deal with nul case
    if self is actually null, the nul integer should be returned.

--- src/core/Complex.pm
+++ src/core/Complex.pm
@@ -218,6 +218,7 @@ my class Complex is Cool does Numeric {
     }
 
     method narrow(Complex:D:) {
+        self == 0e0 ?? 0 !!
         $!re == 0e0 ?? self !!
         $!im / $!re ≅ 0e0
             ?? $!re.narrow

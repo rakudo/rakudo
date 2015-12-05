cc85f1f33d44fbd52b5046f2342cb3f591a0aab8
Arne Skjærholt <arnsholt@gmail.com>
2015-12-05 20:12:06 +0100

    HLL::Compiler.evalfiles is more appropriate than command_eval.
    The latter does exception handling and calls exit(1) if an exception is
    caught. Using evalfiles lets the exception propagate into the module loader,
    which lets us handle exceptions during precomp.
    
    Currently it'll just propagate into the same error handler a level up, but
    this avoids weird exception behaviour in the future, should we want to handle
    some exceptions.

--- src/core/CompUnit/PrecompilationRepository.pm
+++ src/core/CompUnit/PrecompilationRepository.pm
@@ -256,7 +256,7 @@ class CompUnit::PrecompilationRepository::Default does CompUnit::PrecompilationR
 
         $RMD("Precompiling $path into $bc ($lle $profile $optimize)") if $RMD;
         my $compiler := nqp::getcomp('perl6');
-        $compiler.command_eval: $path, :ll-exception($lle), :target(Rakudo::Internals.PRECOMP-TARGET), :output($io),
+        $compiler.evalfiles: $path, :ll-exception($lle), :target(Rakudo::Internals.PRECOMP-TARGET), :output($io),
             :encoding('utf8'), :transcode('ascii iso-8859-1');
 
         unless $bc.e {

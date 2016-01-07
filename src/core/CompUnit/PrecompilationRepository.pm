40dcf81c8bea91b989418013ee5def21f2fdf892
Elizabeth Mattijsen <liz@dijkmat.nl>
2016-01-07 13:56:02 +0100

    Fewer $*RAKUDO_MODULE_DEBUG lookups please

--- src/core/CompUnit/PrecompilationRepository.pm
+++ src/core/CompUnit/PrecompilationRepository.pm
@@ -22,7 +22,8 @@ class CompUnit::PrecompilationRepository::Default does CompUnit::PrecompilationR
 
     method !load-handle-for-path(IO::Path $path) {
         my $preserve_global := nqp::ifnull(nqp::gethllsym('perl6', 'GLOBAL'), Mu);
-        RAKUDO_MODULE_DEBUG("Loading precompiled $path") if $*RAKUDO_MODULE_DEBUG;
+        RAKUDO_MODULE_DEBUG("Loading precompiled $path")
+          if $*RAKUDO_MODULE_DEBUG;
         my $handle := CompUnit::Loader.load-precompilation-file($path);
         nqp::bindhllsym('perl6', 'GLOBAL', $preserve_global);
         CATCH {
@@ -39,11 +40,12 @@ class CompUnit::PrecompilationRepository::Default does CompUnit::PrecompilationR
 
     method !load-dependencies(IO::Path $path, Instant $since) {
         my $compiler-id = $*PERL.compiler.id;
+        my int $DEBUG = $*RAKUDO_MODULE_DEBUG;
         for ($path ~ '.deps').IO.lines -> $dependency {
             my ($id, $src) = $dependency.words;
             my $file = self.store.path($compiler-id, $id);
             my $modified = $file.modified;
-            RAKUDO_MODULE_DEBUG("$file mtime: $modified since: $since src: {$src.IO.modified}") if $*RAKUDO_MODULE_DEBUG;
+            RAKUDO_MODULE_DEBUG("$file mtime: $modified since: $since src: {$src.IO.modified}") if $DEBUG;
             return False if $modified > $since or not $src.IO.e or $modified <= $src.IO.modified;
             %!loaded{$id} //= self!load-handle-for-path(self.store.load($compiler-id, $id));
 
@@ -61,7 +63,8 @@ class CompUnit::PrecompilationRepository::Default does CompUnit::PrecompilationR
                 %!loaded{$id} //= self!load-handle-for-path($path)
             }
             else {
-                RAKUDO_MODULE_DEBUG("Removing precompiled $path mtime: $modified since: $since") if $*RAKUDO_MODULE_DEBUG;
+                RAKUDO_MODULE_DEBUG("Removing precompiled $path mtime: $modified since: $since")
+                  if $*RAKUDO_MODULE_DEBUG;
                 self.store.delete($*PERL.compiler.id, $id); # remove outdated file so we precompile again
                 self.store.unlock;
                 CompUnit::Handle
@@ -76,8 +79,9 @@ class CompUnit::PrecompilationRepository::Default does CompUnit::PrecompilationR
     method precompile(IO::Path:D $path, CompUnit::PrecompilationId $id, Bool :$force = False) {
         my $compiler-id = $*PERL.compiler.id;
         my $io = self.store.destination($compiler-id, $id);
+        my int $DEBUG = $*RAKUDO_MODULE_DEBUG;
         if not $force and $io.e and $io.s {
-            RAKUDO_MODULE_DEBUG("$path already precompiled into $io") if $*RAKUDO_MODULE_DEBUG;
+            RAKUDO_MODULE_DEBUG("$path already precompiled into $io") if $DEBUG;
             self.store.unlock;
             return True;
         }
@@ -85,7 +89,7 @@ class CompUnit::PrecompilationRepository::Default does CompUnit::PrecompilationR
         my $rev-deps = ($io ~ '.rev-deps').IO;
         if $rev-deps.e {
             for $rev-deps.lines {
-                RAKUDO_MODULE_DEBUG("removing outdated rev-dep $_") if $*RAKUDO_MODULE_DEBUG;
+                RAKUDO_MODULE_DEBUG("removing outdated rev-dep $_") if $DEBUG;
                 self.store.delete($compiler-id, $_);
             }
         }
@@ -99,7 +103,7 @@ class CompUnit::PrecompilationRepository::Default does CompUnit::PrecompilationR
         my $current_dist = %*ENV<RAKUDO_PRECOMP_DIST>;
         %*ENV<RAKUDO_PRECOMP_DIST> = $*RESOURCES ?? $*RESOURCES.Str !! '{}';
 
-        RAKUDO_MODULE_DEBUG("Precompiling $path into $io") if $*RAKUDO_MODULE_DEBUG;
+        RAKUDO_MODULE_DEBUG("Precompiling $path into $io") if $DEBUG;
         my $proc = run($*EXECUTABLE, $lle, "--target={$*VM.precomp-target}", "--output=$io", $path, :out);
         %*ENV<RAKUDO_PRECOMP_WITH>:delete;
         %*ENV<RAKUDO_PRECOMP_LOADING>:delete;
@@ -109,11 +113,11 @@ class CompUnit::PrecompilationRepository::Default does CompUnit::PrecompilationR
         if not $proc.out.close or $proc.status {  # something wrong
             self.store.unlock;
             push @result, "Return status { $proc.status }\n";
-            RAKUDO_MODULE_DEBUG("Precomping $path failed: {@result}") if $*RAKUDO_MODULE_DEBUG;
+            RAKUDO_MODULE_DEBUG("Precomping $path failed: {@result}") if $DEBUG;
             fail @result if @result;
         }
         else {
-            RAKUDO_MODULE_DEBUG("Precompiled $path into $io") if $*RAKUDO_MODULE_DEBUG;
+            RAKUDO_MODULE_DEBUG("Precompiled $path into $io") if $DEBUG;
             my @dependencies;
             my $compiler-id = $*PERL.compiler.id;
             for @result -> $dependency {

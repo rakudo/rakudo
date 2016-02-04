8bd7ee6241c41587680f22cb6d3538bf4211fed5
Elizabeth Mattijsen <liz@dijkmat.nl>
2016-02-05 00:23:28 +0100

    Make %h<a>.push|append about 10% faster
    And stop it from leaking values

--- src/core/Hash.pm
+++ src/core/Hash.pm
@@ -272,29 +272,21 @@ my class Hash { # declared in BOOTSTRAP
     }
 
     # push a value onto a hash slot, constructing an array if necessary
-    method !_push_construct(Mu $key, Mu \value) {
-        if self.EXISTS-KEY($key) {
-            if self.{$key}.^isa(Array) {
-                self.{$key}.push(value);
-            } else {
-                self.{$key} = [ self.{$key}, value ];
-            }
-        } else {
-            self.{$key} = value;
-        }
+    method !_push_construct(Mu $key, Mu \value --> Nil) {
+        self.EXISTS-KEY($key)
+          ?? self.AT-KEY($key).^isa(Array)
+            ?? self.AT-KEY($key).push(value)
+            !! self.ASSIGN-KEY($key,[self.AT-KEY($key),value])
+          !! self.ASSIGN-KEY($key,value)
     }
 
     # append values into a hash slot, constructing an array if necessary
-    method !_append_construct(Mu $key, Mu \value) {
-        if self.EXISTS-KEY($key) {
-            if self.{$key}.^isa(Array) {
-                self.{$key}.append(|value);
-            } else {
-                self.{$key} = [ |self.{$key}, |value ];
-            }
-        } else {
-            self.{$key} = value;
-        }
+    method !_append_construct(Mu $key, Mu \value --> Nil) {
+        self.EXISTS-KEY($key)
+          ?? self.AT-KEY($key).^isa(Array)
+            ?? self.AT-KEY($key).append(|value)
+            !! self.ASSIGN-KEY($key,[|self.AT-KEY($key),|value])
+          !! self.ASSIGN-KEY($key,value)
     }
 
     my role TypedHash[::TValue] does Associative[TValue] {

e375087a9b18e2565f0eae7e0afb3a155d1e28a5
Salvador Ortiz <sog@msg.com.mx>
2016-02-08 00:46:30 -0600

    Follows the spec for Failure.handled

--- src/core/Failure.pm
+++ src/core/Failure.pm
@@ -2,10 +2,10 @@ my class Failure is Nil {
     has $.exception;
     has $.backtrace;
 #?if moar
-    has int $!handled;
+    has int $.handled is rw;
 #?endif
 #?if jvm
-    has $!handled;
+    has $.handled is rw;
 #?endif
 
     multi method new() {
@@ -54,8 +54,6 @@ my class Failure is Nil {
         Bool::False;
     }
     multi method Bool(Failure:D:) { $!handled = 1; Bool::False; }
-
-    method handled(Failure:D:)    { Bool($!handled); }
 
 #?if moar
     method Int(Failure:D:)        { $!handled ?? Int !! self!throw(); }

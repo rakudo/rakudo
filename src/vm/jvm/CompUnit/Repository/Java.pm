b11477f3b5abe82399bf482a52b4040a63659315
Pepe Schwarz <peschwa@gmail.com>
2016-02-21 09:40:51 +0000

    Add basic infrastructure to support JVM interop via CURs.

---
+++ src/vm/jvm/CompUnit/Repository/Java.pm
@@ -0,0 +1,37 @@
+class CompUnit::Repository::Java does CompUnit::Repository {
+    method need(
+        CompUnit::DependencySpecification $spec,
+        CompUnit::PrecompilationRepository $precomp = self.precomp-repository(),
+    )
+        returns CompUnit:D
+    {
+        if $spec.from eq 'Java' {
+            my $nqp := nqp::gethllsym('perl6', 'ModuleLoader');
+
+            return CompUnit.new(
+                :short-name($spec.short-name),
+                :handle(CompUnit::Handle.new($nqp.load_module($spec.short-name, {:from<NQP>}))),
+                :repo(self),
+                :repo-id($spec.short-name),
+                :from($spec.from),
+            );
+        }
+
+        return self.next-repo.need($spec, $precomp) if self.next-repo;
+        X::CompUnit::UnsatisfiedDependency.new(:specification($spec)).throw;
+    }
+
+    method loaded() {
+        []
+    }
+
+    method id() {
+        'Java'
+    }
+
+    method path-spec() {
+        'java#'
+    }
+}
+
+# vim: ft=perl6 expandtab sw=4

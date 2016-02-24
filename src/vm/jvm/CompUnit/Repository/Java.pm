abe034b070db7a2370a91a0d64682b5b328f79e1
Pepe Schwarz <peschwa@gmail.com>
2016-02-24 11:33:14 +0000

    Get basic JVM Interop working again.
    As in, basic (i.e. natives and their box types) work again, as do simple
    callouts, e.g. System.out.println.

--- src/vm/jvm/CompUnit/Repository/Java.pm
+++ src/vm/jvm/CompUnit/Repository/Java.pm
@@ -1,4 +1,13 @@
 class CompUnit::Repository::Java does CompUnit::Repository {
+    has $!interop;
+
+    sub make_package($name, $who) {
+        my $pkg := nqp::knowhow().new_type(:name);
+        $pkg.HOW.compose($pkg);
+        nqp::setwho($pkg, $who);
+        $pkg
+    }
+
     method need(
         CompUnit::DependencySpecification $spec,
         CompUnit::PrecompilationRepository $precomp = self.precomp-repository(),
@@ -6,12 +15,31 @@ class CompUnit::Repository::Java does CompUnit::Repository {
         returns CompUnit:D
     {
         if $spec.from eq 'Java' {
-            my $java := nqp::gethllsym('perl6', 'JavaModuleLoader');
-            say "got hllsym JavaModuleLoader..." if %*ENV<RAKUDO_MODULE_DEBUG>;
+            $!interop = nqp::jvmrakudointerop() unless nqp::isconcrete($!interop);
+
+            my $jtype = $!interop.typeForName($spec.short-name.subst(/'::'/, '.', :g));
+
+            # register the class by its name (cf. Inline::Perl5, nine++)
+            my @parts = $spec.short-name.split('::');
+            my $inner = @parts.pop;
+            my $ns := ::GLOBAL.WHO;
+            for @parts {
+                $ns{$_} := Metamodel::PackageHOW.new_type(name => $_) unless $ns{$_}:exists;
+                $ns := $ns{$_}.WHO;
+            }
+            my @existing = $ns{$inner}.WHO.pairs;
+            $ns{$inner} := $jtype;
+            # $jtype.WHO{$_.key} := $_.value for @existing;
+
+            nqp::setwho(::($spec.short-name), Stash.new());
+            ::($spec.short-name).WHO<EXPORT> := Metamodel::PackageHOW.new();
+            ::($spec.short-name).WHO<&EXPORT> := sub EXPORT(*@args) {
+                Map.new($inner => $jtype);
+            }
 
             return CompUnit.new(
                 :short-name($spec.short-name),
-                :handle(CompUnit::Handle.new($java.load_module($spec.short-name, {:from<Java>}))),
+                :handle(CompUnit::Handle.from-unit(::($spec.short-name).WHO)),
                 :repo(self),
                 :repo-id($spec.short-name),
                 :from($spec.from),

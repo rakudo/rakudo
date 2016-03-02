7ea0881632d5e8fb7135aa39dc446ecc16bcd93e
Rob Hoelz <rob@hoelz.ro>
2016-03-02 08:47:53 -0600

    REPL6: Wire up basic completions
    Completions in CORE now work; new subs and packages introduced into
    the REPL's namespace are not yet included in completions

--- src/core/REPL.pm
+++ src/core/REPL.pm
@@ -1,5 +1,43 @@
 use nqp;
 
+my sub sorted-set-insert(@values, $value) {
+    my $low        = 0;
+    my $high       = @values.end;
+    my $insert_pos = 0;
+
+    while $low <= $high {
+        my $middle = floor($low + ($high - $low) / 2);
+
+        my $middle_elem = @values[$middle];
+
+        if $middle == @values.end {
+            if $value eq $middle_elem {
+                return;
+            } elsif $value lt $middle_elem {
+                $high = $middle - 1;
+            } else {
+                $insert_pos = +@values;
+                last;
+            }
+        } else {
+            my $middle_plus_one_elem = @values[$middle + 1];
+
+            if $value eq $middle_elem || $value eq $middle_plus_one_elem {
+                return;
+            } elsif $value lt $middle_elem {
+                $high = $middle - 1;
+            } elsif $value gt $middle_plus_one_elem {
+                $low = $middle + 1;
+            } else {
+                $insert_pos = $middle + 1;
+                last;
+            }
+        }
+    }
+
+    splice(@values, $insert_pos, 0, $value);
+}
+
 my role ReadlineBehavior[$WHO] {
     method readline(Mu \SELF, Mu \super, Mu \stdin, Mu \stdout, Mu \prompt) {
         say 'readline';
@@ -8,8 +46,18 @@ my role ReadlineBehavior[$WHO] {
 }
 
 my role LinenoiseBehavior[$WHO] {
-    my &linenoise           = $WHO<&linenoise>;
-    my &linenoiseHistoryAdd = $WHO<&linenoiseHistoryAdd>;
+    my &linenoise                      = $WHO<&linenoise>;
+    my &linenoiseHistoryAdd            = $WHO<&linenoiseHistoryAdd>;
+    my &linenoiseSetCompletionCallback = $WHO<&linenoiseSetCompletionCallback>;
+    my &linenoiseAddCompletion         = $WHO<&linenoiseAddCompletion>;
+
+    method completions-for-line(Str $line, int $cursor-index) { ... }
+
+    method init-line-editor {
+        linenoiseSetCompletionCallback(sub ($line, $c) {
+            eager self.completions-for-line($line, $line.chars).map(&linenoiseAddCompletion.assuming($c));
+        });
+    }
 
     method readline(Mu \SELF, Mu \super, Mu \stdin, Mu \stdout, Mu \prompt) {
         linenoise(prompt) // nqp::null_s()
@@ -22,16 +70,50 @@ my role FallbackBehavior {
     }
 }
 
+my role Completions {
+    has @!completions;
+
+    submethod BUILD {
+        @!completions = CORE::.keys.flatmap({
+            /^ "&"? $<word>=[\w* <.lower> \w*] $/ ?? ~$<word> !! []
+        }).sort;
+    }
+
+    method update-completions {
+    }
+
+    method extract-last-word(Str $line) {
+        my $m = $line ~~ /^ $<prefix>=[.*?] <|w>$<last_word>=[\w*]$/;
+
+        return ( ~$m<prefix>, ~$m<last_word> );
+    }
+
+    method completions-for-line(Str $line, int $cursor-index) {
+        # ignore $cursor-index until we have a backend that provides it
+        my ( $prefix, $word-at-cursor ) = self.extract-last-word($line);
+
+        # XXX this could be more efficient if we had a smarter starting index
+        gather for @!completions -> $word {
+            if $word ~~ /^ "$word-at-cursor" / {
+                take $prefix ~ $word;
+            }
+        }
+    }
+}
+
 # XXX print warning when we can't load this file
 # XXX comment about method signatures
 # XXX prevent other methods from getting added
 class REPL is export { # XXX no need for is export later
+    also does Completions;
+
     # XXX print fallback messages
     method !load-line-editor() {
         my $readline = try require Readline;
 
         if $readline.HOW ~~ Metamodel::ModuleHOW {
             self does ReadlineBehavior[$readline.WHO];
+            self.?init-line-editor();
             return;
         }
 
@@ -39,6 +121,7 @@ class REPL is export { # XXX no need for is export later
 
         if $linenoise.HOW ~~ Metamodel::ModuleHOW {
             self does LinenoiseBehavior[$linenoise.WHO];
+            self.?init-line-editor();
             return;
         }
 
@@ -50,6 +133,9 @@ class REPL is export { # XXX no need for is export later
     }
 
     method eval(Mu \SELF, Mu \super, Mu \code, Mu \args, Mu \adverbs) {
-        super.(SELF, code, |@(args), |%(adverbs));
+        my \result = super.(SELF, code, |@(args), |%(adverbs));
+        # XXX we don't get a context until our next readline?
+        self.update-completions;
+        result
     }
 }

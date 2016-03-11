02122e5e67d0c9738c81b5986394681b96982b1d
Elizabeth Mattijsen <liz@dijkmat.nl>
2016-03-11 13:51:32 +0100

    Streamline Version creation / handling and fixes
    Makes it 10% (for v1.foo) to 36x faster (for v6)
    
    - use custom SET-SELF method for faster initialization
    - save parts as native list with deconted values (makes it truly immutable)
    - creates stringification at creation time (faster .perl/.gist/.Str)
    - shares instances for v6, v6.c, v, v*, v+
    - outputs .perl as version string whenever possible

--- src/core/Version.pm
+++ src/core/Version.pm
@@ -1,50 +1,108 @@
 class Version {
-    has @.parts;
-    has Bool $.plus = False;
+    has $!parts;
+    has int $!plus;
+    has str $!string;
 
-    multi method new(Whatever) { self.bless(:parts(*)) }
-    multi method new(Str() $s) {
-        my @parts = $s.comb(/:r '*' || \d+ || <.alpha>+/);
-        for @parts {
-            if $_ eq '*' {
-                $_ = *;
-            }
-            else {
-                my $numeric = .Numeric;
-                $_ = $numeric if $numeric.defined;
-            }
-        }
-        self.bless(
-          :parts(@parts),
-          :plus($s.ends-with("+")),
-        );
-    };
-
-    multi method Str(Version:D:) {
-        @!parts.map({ nqp::istype($_,Whatever) ?? '*' !! $_}).join('.')
-          ~ ($!plus ?? '+' !! '');
+    method !SET-SELF(\parts,\plus,\string) {
+        $!parts := nqp::getattr(parts,List,'$!reified');
+        $!plus   = plus;
+        $!string = $!plus ?? nqp::concat(string,"+") !! string;
+        self
     }
-    multi method gist(Version:D:) { 'v' ~ self.Str }
-    multi method perl(Version:D:) {
-        self.^name ~ ".new('" ~ self.Str ~ "')";
 
+    multi method new(Version:) {
+        # "v" sentinel
+        once nqp::create(self)!SET-SELF(nqp::list,0,"")
+    }
+    multi method new(Version: Whatever) {
+        # "v*" sentinel
+        once nqp::create(self)!SET-SELF(nqp::list(*),0,"*")
+    }
+    multi method new(Version: Str() $s) {
+
+        # sentinel most common
+        if $s eq '6' {
+            once nqp::create(self)!SET-SELF(nqp::list("6"),0,"6")
+        }
+        elsif $s eq '6c' {
+            once nqp::create(self)!SET-SELF(nqp::list("6","c"),0,"6c")
+        }
+
+        # something sensible given
+        elsif $s.comb(/:r '*' || \d+ || <.alpha>+/).eager -> @s {
+            my $strings  := nqp::getattr(@s,List,'$!reified');
+            my int $elems = nqp::elems($strings);
+            my $parts    := nqp::setelems(nqp::list,$elems);
+
+            my int $i = -1;
+            while nqp::islt_i($i = nqp::add_i($i,1),$elems) {
+                my str $s = nqp::atpos($strings,$i);
+                nqp::bindpos($parts,$i, nqp::iseq_s($s,"*")
+                  ?? *
+                  !! (my $numeric = $s.Numeric).defined
+                    ?? nqp::decont($numeric)
+                    !! $s
+                );
+            }
+
+            nqp::create(self)!SET-SELF($parts,$s.ends-with("+"),@s.join("."))
+        }
+
+        # "v+" sentinel
+        elsif $s.ends-with("+") {
+            once nqp::create(self)!SET-SELF(nqp::list,1,"")
+        }
+        # get "v" sentinel
+        else {
+            self.new
+        }
+    }
+
+    multi method Str(Version:D:)  { $!string }
+    multi method gist(Version:D:) { nqp::concat("v",$!string) }
+    multi method perl(Version:D:) {
+        if nqp::chars($!string) {
+            my int $first = nqp::ord($!string);
+            nqp::isge_i($first,48) && nqp::isle_i($first,57) # "0" <= x <= "9"
+              ?? nqp::concat("v",$!string)
+              !! self.^name ~ ".new('$!string')"
+        }
+        else {
+            self.^name ~ ".new"
+        }
     }
     multi method ACCEPTS(Version:D: Version:D $other) {
-        for @!parts.kv -> $i, $v {
-            next if nqp::istype($v,Whatever);
-            my $o = $other.parts[$i];
-            return True unless defined $o;
-            next if nqp::istype($o,Whatever);
-            return $.plus if $o after  $v;
-            return False  if $o before $v;
+        my $oparts := nqp::getattr($other,Version,'$!parts');
+        my $oelems  = nqp::isnull($oparts) ?? 0 !! nqp::elems($oparts);
+
+        my int $elems = nqp::elems($!parts);
+        my int $i     = -1;
+        while nqp::islt_i($i = nqp::add_i($i,1),$elems) {
+            my $v := nqp::atpos($!parts,$i);
+
+            # if whatever here, no more check this iteration
+            unless nqp::istype($v,Whatever) {
+
+                # nothing left to check, so ok
+                return True if nqp::isge_i($i,$oelems);
+
+                # if whatever there, no more to check this iteration
+                my $o := nqp::atpos($oparts,$i);
+                unless nqp::istype($o,Whatever) {
+                    return nqp::p6bool($!plus) if $o after  $v;
+                    return False               if $o before $v;
+                }
+            }
         }
         True;
     }
 
     multi method WHICH(Version:D:) {
-        my $s := join '|', self.^name, self.Str;
-        nqp::box_s(nqp::unbox_s($s), ObjAt);
+        nqp::box_s(nqp::unbox_s(self.^name ~ '|' ~ $!string), ObjAt);
     }
+
+    method parts() { $!parts }
+    method plus()  { nqp::p6bool($!plus) }
 }
 
 

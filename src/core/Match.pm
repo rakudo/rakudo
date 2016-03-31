800b5a4480e21155e6023f933552f4706356b286
Clifton Wood <clifton.wood@gmail.com>
2016-03-31 04:58:54 -0400

    Adds infix:<eqv> for Match objects

--- src/core/Match.pm
+++ src/core/Match.pm
@@ -62,6 +62,17 @@ my class Match is Capture is Cool {
         }
     }
 
+    multi sub infix:<eqv>(Match:D $a, Match:D $b) {
+        [&&] (
+            $a.to   eqv $b.to,
+            $a.from eqv $b.from,
+            $a.orig eqv $b.orig,
+            $a.made eqv $b.made,
+            $a.list eqv $b.list,
+            $a.hash eqv $b.hash
+        );
+    }
+
     multi method perl(Match:D:) {
         my %attrs;
         %attrs.ASSIGN-KEY("orig", self.orig.perl);

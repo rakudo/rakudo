1cb2e8d9e71797f576b3ffc01129a196e30a9bad
Elizabeth Mattijsen <liz@dijkmat.nl>
2016-04-09 22:04:38 +0200

    Fix for RT #127866
    Well, only on Moar, as the JVM doesn't support "unival" yet.

--- src/core/Str.pm
+++ src/core/Str.pm
@@ -37,10 +37,19 @@ my class Str does Stringy { # declared in BOOTSTRAP
     multi method DUMP(Str:D:) { self.perl }
 
     method Int(Str:D:) {
-        my str $s = self;
-        nqp::findnotcclass(nqp::const::CCLASS_NUMERIC, $s, 0, nqp::chars($s)) >= nqp::chars($s)
-            ?? nqp::atpos(nqp::radix_I(10, $s, 0, 0, Int), 0)
+        my str $s     = self;
+        my int $chars = nqp::chars($s);
+        nqp::isge_i(nqp::findnotcclass(
+          nqp::const::CCLASS_NUMERIC,$s,0,$chars),$chars)
+            ?? nqp::atpos(nqp::radix_I(10,$s,0,0,Int),0)
+#?if moar
+            !! nqp::iseq_i($chars,1)
+              ?? unival(nqp::ord($s)) // self.Numeric.Int
+              !! self.Numeric.Int
+#?endif
+#?if !moar
             !! self.Numeric.Int
+#?endif
     }
     method Num(Str:D:) { self.Numeric.Num; }
 

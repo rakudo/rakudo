rakudo/rakudo

commit 48cc6b5f469e915333d82e24b8bf54bee8385110

Preliminary fix for RT #127968
I'm not sure why a Channel would need to create a Supply first to be
able to create a .list.  So I re-imagined Channel.list to be a Seq
with a simple Iterator that is polling the Channel.  This causes one
Channel spectest to fail, but I feel the test is wrong as it apparently
assumes there is a Supply underlying Channel.list.  Comments / Patches
welcome!

src/core/Channel.pm
@@ -129,7 +129,15 @@ my class Channel {
     }
 
     multi method list(Channel:D:) {
-        self.Supply.list
+        Seq.new( class :: does Iterator {
+            has $!channel;
+            method !SET-SELF($!channel) { self }
+            method new(\c) { nqp::create(self)!SET-SELF(c) }
+            method pull-one() {
+                my Mu \got = $!channel.poll;
+                nqp::eqaddr(got, Nil) ?? IterationEnd !! got
+            }
+        }.new(self))
     }
 
     method close() {

commit 5a1416233b0ca7a63c6888d88acf69eb33b90a00

Fix test breakage caused by 48cc6b5f469e915333
Implements:
- Channel.receive-nil-on-close
- Channel.iterator
- Channel.Seq
Re-Implements:
- Channel.list

Also: RT #127968 was a dup of RT #127960

src/core/Channel.pm
@@ -46,20 +46,27 @@ my class Channel {
         Nil
     }
 
-    method receive(Channel:D:) {
+    method !receive(Channel:D: $fail-on-close) {
         my \msg := nqp::shift($!queue);
         if nqp::istype(msg, CHANNEL_CLOSE) {
             nqp::push($!queue, msg);  # make sure other readers see it
             $!closed_promise_vow.keep(Nil);
             X::Channel::ReceiveOnClosed.new(channel => self).throw
+              if $fail-on-close;
+            Nil
         }
         elsif nqp::istype(msg, CHANNEL_FAIL) {
             nqp::push($!queue, msg);  # make sure other readers see it
             $!closed_promise_vow.break(msg.error);
             die msg.error;
         }
-        msg
+        else {
+            msg
+        }
     }
+
+    method receive(Channel:D:)              { self!receive(1) }
+    method receive-nil-on-close(Channel:D:) { self!receive(0) }
 
     method poll(Channel:D:) {
         my \msg := nqp::queuepoll($!queue);
@@ -128,17 +135,20 @@ my class Channel {
         }
     }
 
-    multi method list(Channel:D:) {
-        Seq.new( class :: does Iterator {
+    method iterator(Channel:D:) {
+        class :: does Iterator {
             has $!channel;
             method !SET-SELF($!channel) { self }
             method new(\c) { nqp::create(self)!SET-SELF(c) }
             method pull-one() {
-                my Mu \got = $!channel.poll;
+                my Mu \got = $!channel.receive-nil-on-close;
                 nqp::eqaddr(got, Nil) ?? IterationEnd !! got
             }
-        }.new(self))
+        }.new(self)
     }
+
+    method Seq(Channel:D:)  { Seq.new(self.iterator) }
+    method list(Channel:D:) { self.Seq.list }
 
     method close() {
         $!closed = 1;
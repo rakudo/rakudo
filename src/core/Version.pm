00e71c8181d43b5259ee9fb0a56a314d9a75544b
Elizabeth Mattijsen <liz@dijkmat.nl>
2016-04-24 01:26:09 +0100

    Add another way of creating Version object
    This will basically allow alternate ways of parsing Version strings
    that generate an array of parts to compare, and possibly an alternate
    string representation.
    
      class Version::Reverse is Version {
          method new(Str $s) {
              Version::Reverse.Version::new($s.split(".").reverse,$s)
          }
      }

--- src/core/Version.pm
+++ src/core/Version.pm
@@ -18,6 +18,9 @@ class Version {
         # "v*" sentinel
         once nqp::create(self)!SET-SELF(nqp::list(*),0,"*")
     }
+    multi method new(Version: @parts, Str:D $string) {
+        nqp::create(self)!SET-SELF(@parts,0,$string)
+    }
     multi method new(Version: Str() $s) {
 
         # sentinel most common

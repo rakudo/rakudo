3122ce7ca22e8299b3c6a903f6030033e5494b11
Stefan Seifert <nine@detonation.org>
2016-05-08 23:24:03 +0200

    Split off repo-id from precomp files
    The repo-id is the identity of $*REPO (representing its contents and the whole
    repo chain) at the time when a precomp file's dependencies were last resolved.
    As long as the repo-id is the same, we can just load the dependencies by their
    ids and do not have to resolve them from their DependencySpecifications.
    
    When the repo-id is different, we need to re-resolve and check if the
    dependencies still resolve to the same precomp files. If they do, we can still
    use them and now store the updated repo-id in a precomp store. In other words:
    the repo-id is volatile information compared to the byte code and the
    dependency information.
    
    Also, we probably don't need to protect this repo-id file with a lock, since
    re-resolving dependencies in multiple processes is just a little wasteful but
    harmless.

--- src/core/CompUnit/PrecompilationStore.pm
+++ src/core/CompUnit/PrecompilationStore.pm
@@ -1,16 +1,23 @@
 role CompUnit::PrecompilationStore {
     # Load the precompilation identified by the pairing of the specified
     # compiler and precompilation ID.
+
     method load(CompUnit::PrecompilationId $compiler-id,
+                CompUnit::PrecompilationId $precomp-id)
+    { ... }
+
+    # Return the repository id for which the specified precomp file's
+    # dependencies have been validated
+    method load-repo-id(CompUnit::PrecompilationId $compiler-id,
                 CompUnit::PrecompilationId $precomp-id)
     { ... }
 
     # Store the file at the specified path in the precompilation store,
     # under the given compiler ID and precompilation ID.
-    method store(CompUnit::PrecompilationId $compiler-id,
-                 CompUnit::PrecompilationId $precomp-id,
-                 IO::Path:D $path)
-    { ... }
+    #method store(CompUnit::PrecompilationId $compiler-id,
+    #             CompUnit::PrecompilationId $precomp-id,
+    #             IO::Path:D $path)
+    #{ ... }
 
     # Delete an individual precompilation.
     method delete(CompUnit::PrecompilationId $compiler-id,

bf346147007c1c54c2ca42236c71583d0829c68f
Pawel Murias <pawelmurias@gmail.com>
2016-05-13 10:16:27 +0200

    [js] Add the js runtime for the p6* ops.

--- src/vm/js/perl6-runtime/runtime.js
+++ src/vm/js/perl6-runtime/runtime.js
@@ -1,2 +1,12 @@
-console.log("loading up the rakudo runtime");
+var nqp = require("nqp-runtime");
+var op = {};
+var perl6Types;
+op.p6settypes = function(types) {
+  p6Types = types.content;
+  return types;
+};
+op.p6bool = function(value) {
+  return value ? p6Types.get('True') : p6Types.get('False');
+}
+nqp.loadOps({op:op});
 module.exports = null;

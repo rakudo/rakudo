7afeb68bee12d593df2c6ad6bb2599a80136fc13
Elizabeth Mattijsen <liz@dijkmat.nl>
2016-05-26 12:56:06 +0200

    Simplify Rational.new
    Part of a cunning plan to handle Inf.Rat better

--- src/core/Rational.pm
+++ src/core/Rational.pm
@@ -16,17 +16,28 @@ my role Rational[::NuT, ::DeT] does Real {
     }
 
     method new(NuT \nu = 0, DeT \de = 1) {
-        my $new     := nqp::create(self);
-        my $gcd     := de == 0 ?? 1 !! nu gcd de;
-        my $numerator   = nu div $gcd;
-        my $denominator = de div $gcd;
-        if $denominator < 0 {
-            $numerator   = -$numerator;
-            $denominator = -$denominator;
+        my $new := nqp::create(self);
+
+        # 0 denominator take it verbatim to support Inf/-Inf/NaN
+        if de == 0 {
+            nqp::bindattr($new,self.WHAT,'$!numerator',  nqp::decont(nu));
+            nqp::bindattr($new,self.WHAT,'$!denominator',nqp::decont(de));
         }
-        nqp::bindattr($new, self.WHAT, '$!numerator',     nqp::decont($numerator));
-        nqp::bindattr($new, self.WHAT, '$!denominator',   nqp::decont($denominator));
-        $new;
+
+        # normalize
+        else {
+            my $gcd     := de == 0 ?? 1 !! nu gcd de;
+            my $numerator   = nu div $gcd;
+            my $denominator = de div $gcd;
+            if $denominator < 0 {
+                $numerator   = -$numerator;
+                $denominator = -$denominator;
+            }
+            nqp::bindattr($new,self.WHAT,'$!numerator',  nqp::decont($numerator));
+            nqp::bindattr($new,self.WHAT,'$!denominator',nqp::decont($denominator));
+        }
+
+        $new
     }
 
     method nude() { self.REDUCE-ME; $!numerator, $!denominator }

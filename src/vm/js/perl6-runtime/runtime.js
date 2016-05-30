a80a5ae5f84a5edbb7861590d2296ed6fbbdd5db
Pawel Murias <pawelmurias@gmail.com>
2016-05-30 23:57:35 +0200

    [js] Fetching stuff out of variables.

--- src/vm/js/perl6-runtime/runtime.js
+++ src/vm/js/perl6-runtime/runtime.js
@@ -1,13 +1,19 @@
-var nqp = require("nqp-runtime");
+var nqp = require('nqp-runtime');
 var op = {};
-var perl6Types;
+
+var Scalar;
+var True;
+var False;
+
 op.p6settypes = function(types) {
-  p6Types = types.content;
+  Scalar = types.content.get('Scalar');
+  True = types.content.get('True');
+  False = types.content.get('False');
   return types;
 };
 
 op.p6bool = function(value) {
-  return value ? p6Types.get('True') : p6Types.get('False');
+  return value ? True : False;
 };
 
 op.p6typecheckrv = function(rv, routine, bypassType) {
@@ -15,11 +21,54 @@ op.p6typecheckrv = function(rv, routine, bypassType) {
   return rv;
 };
 
-op.p6decontrv = function(rountine, contr) {
+op.p6decontrv = function(rountine, cont) {
   // STUB
-  return rv;
+  return cont;
 };
 
-nqp.loadOps({op:op});
+var containerSpecs = require('nqp-runtime/container-specs.js');
+
+function RakudoScalar(STable) {
+  this.STable = STable;
+}
+
+RakudoScalar.prototype.configure = function(conf) {
+  this.setupSTable();
+};
+
+RakudoScalar.prototype.setupSTable = function() {
+  this.STable.addInternalMethod('$$assignunchecked', function(ctx, value) {
+    console.log('storing into rakudo_scalar unchecked');
+    process.exit();
+  });
+
+  this.STable.addInternalMethod('$$assign', function(ctx, value) {
+    console.log('storing into rakudo_scalar');
+    process.exit();
+  });
+
+  this.STable.addInternalMethod('$$decont', function(ctx) {
+    return this.$$getattr(Scalar, '$!value');
+  });
+
+  this.STable.addInternalMethod('$$getInt', function(ctx) {
+    return this.$$decont().$$getInt();
+  });
+};
+
+RakudoScalar.prototype.serialize = function(cursor) {
+  console.log('serializing rakudo_scalar');
+};
+
+RakudoScalar.prototype.deserialize = function(cursor) {
+  console.log('* deserializing rakudo_scalar');
+  this.setupSTable();
+};
+
+RakudoScalar.prototype.name = 'rakudo_scalar';
+
+containerSpecs.rakudo_scalar = RakudoScalar;
+
+nqp.loadOps({op: op});
 module.exports = null;
 

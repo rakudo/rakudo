rakudo/rakudo

commit 848add944fe6de9090f0abc3b17e9a3a8e2cc42b

Fix -Ilib and friends not overriding installed dependencies
When checking the dependencies of a precomp file, we did not take development
version of modules in FileSystem repositories into account. I.e. with A
depending on B putting a new version of B into the lib directory and running
perl6 -Ilib -MA you would still get the precompiled version of A that uses the
installed version of B.

Fixed by having the identity of a FileSystem repository reflect its complete
content. So by changing the content we detect a change in the repository chain
and know to re-resolve dependencies.

src/core/CompUnit/Repository/FileSystem.pm
@@ -1,6 +1,7 @@
 class CompUnit::Repository::FileSystem does CompUnit::Repository::Locally does CompUnit::Repository {
     has %!loaded;
     has $!precomp;
+    has $!id;
 
     my %extensions =
       Perl6 => <pm6 pm>,
@@ -55,6 +56,25 @@ class CompUnit::Repository::FileSystem does CompUnit::Repository::Locally does C
 
     method !comp-unit-id($name) {
         nqp::sha1($name ~ self.id);
+    }
+
+    method id() {
+        unless ($!id) {
+            my @dirs = self.prefix;
+            $!id = "";
+            while @dirs {
+                for @dirs.pop.dir -> $file {
+                    if $file.d {
+                        push @dirs, $file unless $file.basename eq ".precomp";
+                    }
+                    else {
+                        $!id = nqp::sha1($!id ~ nqp::sha1($file.slurp(:enc<latin1>)));
+                    }
+                }
+            }
+            $!id = nqp::sha1($!id ~ self.next-repo.id) if self.next-repo;
+        }
+        $!id
     }
 
     method resolve(CompUnit::DependencySpecification $spec) returns CompUnit {

commit 7d804663db8fdd3313e01be9d02d0e4133fe3cd9

Fix loading modules when a FileSystem repo doesn't actually exist
Commit 848add944fe6de9090f0abc3b17e9a3a8e2cc42b broke module loading when a
FileSystem repo pointing to a non-existing directory was in the repo-chain,
i.e. perl6 -I/this/path/does/not/exist
Affected panda's bootstrap.

src/core/CompUnit/Repository/FileSystem.pm
@@ -60,19 +60,24 @@ class CompUnit::Repository::FileSystem does CompUnit::Repository::Locally does C
 
     method id() {
         unless ($!id) {
-            my @dirs = self.prefix;
-            $!id = "";
-            while @dirs {
-                for @dirs.pop.dir -> $file {
-                    if $file.d {
-                        push @dirs, $file unless $file.basename eq ".precomp";
-                    }
-                    else {
-                        $!id = nqp::sha1($!id ~ nqp::sha1($file.slurp(:enc<latin1>)));
+            if self.prefix.e {
+                my @dirs = self.prefix;
+                $!id = "";
+                while @dirs {
+                    for @dirs.pop.dir -> $file {
+                        if $file.d {
+                            push @dirs, $file unless $file.basename eq ".precomp";
+                        }
+                        else {
+                            $!id = nqp::sha1($!id ~ nqp::sha1($file.slurp(:enc<latin1>)));
+                        }
                     }
                 }
+                $!id = nqp::sha1($!id ~ self.next-repo.id) if self.next-repo;
             }
-            $!id = nqp::sha1($!id ~ self.next-repo.id) if self.next-repo;
+            else {
+                $!id = nqp::sha1('');
+            }
         }
         $!id
     }
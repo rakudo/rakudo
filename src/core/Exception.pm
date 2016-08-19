c732485e740b74d4268f3f60a7636a768d686f63
Zoffix Znet <cpan@zoffix.com>
2016-08-19 10:56:00 +0000

    Revert fix for empty 'Actually thrown at' message
    The fix for RT#127425 was added to prevent the 'Actually thrown..' message
    from being printed when the location was empty. The underlying cause has now
    been fixed [^1] on MoarVM level, making this fix un-needed.
    
    [1] https://github.com/MoarVM/MoarVM/pull/390

--- src/core/Exception.pm
+++ src/core/Exception.pm
@@ -321,9 +321,9 @@ do {
             if $e.is-compile-time || $e.backtrace && $e.backtrace.is-runtime {
                 nqp::printfh($err, $e.gist);
                 nqp::printfh($err, "\n");
-                if $v and $v.Str -> $location {
+                if $v {
                    nqp::printfh($err, "Actually thrown at:\n");
-                   nqp::printfh($err, $location);
+                   nqp::printfh($err, $v.Str);
                    nqp::printfh($err, "\n");
                 }
             }

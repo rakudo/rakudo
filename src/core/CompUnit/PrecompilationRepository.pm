c170ebe50365248b04beff9ab541dab084a73965
Stefan Seifert <nine@detonation.org>
2016-09-12 09:11:54 +0200

    Speed up loading deps by only running .modified checks when needed
    $*REPO.id represents the contents of all repositories. We already check if
    that changed since we precompiled a module. If it is still the same, there
    should be no reason to check the .modified of the source file. If it changed,
    we need to check it as a change in our source file could be exactly what
    caused the different checksum.
    Note that this relies on the immutability of installed distros which we
    haven't enforced so far.
    Brings GTK::Simple's load time back down to 227ms.

--- src/core/CompUnit/PrecompilationRepository.pm
+++ src/core/CompUnit/PrecompilationRepository.pm
@@ -108,15 +108,19 @@ class CompUnit::PrecompilationRepository::Default does CompUnit::PrecompilationR
         my @dependencies;
         for $precomp-unit.dependencies -> $dependency {
             $RMD("dependency: $dependency") if $RMD;
-                if $resolve {
-                    my $comp-unit = $repo.resolve($dependency.spec);
-                    $RMD("Old id: $dependency.id(), new id: {$comp-unit.repo-id}") if $RMD;
-                    return False unless $comp-unit and $comp-unit.repo-id eq $dependency.id;
-                }
-                my $file;
-                my $store = @precomp-stores.first({ $file = $_.path($compiler-id, $dependency.id); $file.e });
-                $RMD("Could not find $dependency.spec()") if $RMD and not $store;
-                return False unless $store;
+
+            if $resolve {
+                my $comp-unit = $repo.resolve($dependency.spec);
+                $RMD("Old id: $dependency.id(), new id: {$comp-unit.repo-id}") if $RMD;
+                return False unless $comp-unit and $comp-unit.repo-id eq $dependency.id;
+            }
+
+            my $file;
+            my $store = @precomp-stores.first({ $file = $_.path($compiler-id, $dependency.id); $file.e });
+            $RMD("Could not find $dependency.spec()") if $RMD and not $store;
+            return False unless $store;
+
+            if $resolve { # a repo changed, so maybe it's a change in our source file
                 my $modified = $file.modified;
                 $RMD("$file\nspec: $dependency.spec()\nmtime: $modified\nsince: $since")
                   if $RMD;
@@ -124,12 +128,13 @@ class CompUnit::PrecompilationRepository::Default does CompUnit::PrecompilationR
                 my $srcIO = CompUnit::RepositoryRegistry.file-for-spec($dependency.src) // $dependency.src.IO;
                 $RMD("source: $srcIO mtime: " ~ $srcIO.modified) if $RMD and $srcIO.e;
                 return False if not $srcIO.e or $modified < $srcIO.modified;
+            }
 
-                my $dependency-precomp = $store.load-unit($compiler-id, $dependency.id);
-                $RMD("dependency checksum $dependency.checksum() unit: $dependency-precomp.checksum()") if $RMD;
-                return False if $dependency-precomp.checksum ne $dependency.checksum;
+            my $dependency-precomp = $store.load-unit($compiler-id, $dependency.id);
+            $RMD("dependency checksum $dependency.checksum() unit: $dependency-precomp.checksum()") if $RMD;
+            return False if $dependency-precomp.checksum ne $dependency.checksum;
 
-                @dependencies.push: $dependency-precomp;
+            @dependencies.push: $dependency-precomp;
         }
 
         for @dependencies -> $dependency-precomp {

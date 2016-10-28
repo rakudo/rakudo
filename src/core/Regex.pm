rakudo/rakudo

commit e4dc8b6ed1f9b3b4846f0e371c9b8e0569b798c5

Make <foo bar baz> ~~ /foo/ about 2x faster
- rewrite using nqp ops
- don't create Match object until we found a match
- no need to -return- on succesful match

src/core/Regex.pm
@@ -34,15 +34,30 @@ my class Regex { # declared in BOOTSTRAP
     }
 
     multi method ACCEPTS(Regex:D \SELF: @a) {
-        my $dollar_slash := nqp::getlexrelcaller(
-            nqp::ctxcallerskipthunks(nqp::ctx()),
-            '$/');
-        for flat @a {
-            $dollar_slash = SELF.(Cursor.'!cursor_init'($_, :c(0))).MATCH_SAVE;
-            return $dollar_slash if $dollar_slash;
-        }
-        Nil;
+        nqp::decont(
+          nqp::getlexrelcaller(nqp::ctxcallerskipthunks(nqp::ctx()),'$/') =
+          nqp::stmts(
+            (my int $elems = @a.elems),  # reifies
+            (my int $i     = -1),
+            nqp::while(
+              nqp::islt_i(($i = nqp::add_i($i,1)),$elems)  # left to check?
+                && nqp::islt_i(                            # invalid match?
+                     nqp::getattr_i(
+                       (my \cursor := SELF.($cursor-init(Cursor,
+                         nqp::atpos(nqp::getattr(@a,List,'$!reified'),$i),
+                       :c(0)))),Cursor,'$!pos'),
+                   0),
+              nqp::null
+            ),
+            nqp::if(
+              nqp::iseq_i($i,$elems),
+              Nil,               # exhausted list
+              cursor.MATCH       # found it!
+            )
+          )
+        )
     }
+
     multi method ACCEPTS(Regex:D \SELF: %h) {
         my $dollar_slash := nqp::getlexrelcaller(
             nqp::ctxcallerskipthunks(nqp::ctx()),

commit 33eeb323373c7b01f2d791779a877838369a11e8

Make %h ~~ /foo/ 2x faster, fix @a ~~ /foo/
- make fast internal method using an iterator
- make %h case use that with %h.keys.iterator
- make @a case use that with @a.iterator

This also fixes

  "a"...* ~~ / z /

which was broken with e4dc8b6ed1f9b3b48

src/core/Regex.pm
@@ -34,39 +34,39 @@ my class Regex { # declared in BOOTSTRAP
     }
 
     multi method ACCEPTS(Regex:D \SELF: @a) {
-        nqp::decont(
-          nqp::getlexrelcaller(nqp::ctxcallerskipthunks(nqp::ctx()),'$/') =
-          nqp::stmts(
-            (my int $elems = @a.elems),  # reifies
-            (my int $i     = -1),
-            nqp::while(
-              nqp::islt_i(($i = nqp::add_i($i,1)),$elems)  # left to check?
-                && nqp::islt_i(                            # invalid match?
-                     nqp::getattr_i(
-                       (my \cursor := SELF.($cursor-init(Cursor,
-                         nqp::atpos(nqp::getattr(@a,List,'$!reified'),$i),
-                       :c(0)))),Cursor,'$!pos'),
-                   0),
-              nqp::null
-            ),
-            nqp::if(
-              nqp::iseq_i($i,$elems),
-              Nil,               # exhausted list
-              cursor.MATCH       # found it!
-            )
-          )
+        SELF!ACCEPT-ITERATOR(
+          nqp::getlexrelcaller(nqp::ctxcallerskipthunks(nqp::ctx()),'$/'),
+          @a.iterator
         )
     }
 
     multi method ACCEPTS(Regex:D \SELF: %h) {
-        my $dollar_slash := nqp::getlexrelcaller(
-            nqp::ctxcallerskipthunks(nqp::ctx()),
-            '$/');
-        for %h.keys {
-            $dollar_slash = SELF.(Cursor.'!cursor_init'($_, :c(0))).MATCH_SAVE;
-            return $dollar_slash if $dollar_slash;
-        }
-        Nil;
+        SELF!ACCEPT-ITERATOR(
+          nqp::getlexrelcaller(nqp::ctxcallerskipthunks(nqp::ctx()),'$/'),
+          %h.keys.iterator
+        )
+    }
+
+    method !ACCEPT-ITERATOR(Regex:D \SELF: \slash, Iterator:D \iter) {
+        nqp::decont(slash =
+          nqp::stmts(
+            nqp::until(
+              nqp::eqaddr(                                 # nothing to check?
+                (my $pulled := iter.pull-one),IterationEnd)
+                || nqp::isge_i(                            # valid match?
+                     nqp::getattr_i(
+                       (my \cursor := SELF.($cursor-init(Cursor,$pulled,:0c))),
+                       Cursor,'$!pos'),
+                   0),
+              nqp::null
+            ),
+            nqp::if(
+              nqp::eqaddr($pulled,IterationEnd),
+              Nil,               # no match found
+              cursor.MATCH       # found it!
+            )
+          )
+        )
     }
 
     multi method Bool(Regex:D:) {
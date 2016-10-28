1f76b17ee37ff666a8f0e0d81bdecbca1fb5d856
Elizabeth Mattijsen <liz@dijkmat.nl>
2016-10-28 14:38:35 +0200

    Streamline argument handling in Grammar
    - rewrite in nqp ops
    - don't use defaults in sigs when we don't need them
    - default case rule TOP better optimizable through literal call
    - lookup cursor initialization only once

--- src/core/Grammar.pm
+++ src/core/Grammar.pm
@@ -1,19 +1,71 @@
 my class Grammar is Cursor {
-    method parse($target, :$rule = 'TOP',  Capture() :$args = \(), Mu :$actions = Mu, *%opt) {
-        my $*ACTIONS = $actions;
-        my Mu $result :=
-            self."!cursor_init"($target, |%opt)."$rule"(|$args).MATCH;
-        $result := Nil unless $result.to == $target.chars;
-        nqp::getlexcaller('$/') = $result;
+
+    # cache cursor initialization lookup
+    my $cursor-init := Cursor.^can("!cursor_init").AT-POS(0);
+
+    method parse(\target, :$rule, Capture() :$args, Mu :$actions) {
+        nqp::decont(nqp::getlexdyn('$/') = nqp::stmts(
+          (my $*ACTIONS = $actions),
+          nqp::if(
+            (my $cursor := nqp::if(
+              $rule,
+              nqp::if(
+                $args,
+                self!cursor-init(target, %_)."$rule"(|$args),
+                self!cursor-init(target, %_)."$rule"()
+              ),
+              nqp::if(
+                $args,
+                self!cursor-init(target, %_).TOP(|$args),
+                self!cursor-init(target, %_).TOP()
+              ),
+            )),
+            nqp::if(
+              nqp::iseq_i(
+                nqp::getattr_i((my $match := $cursor.MATCH),Match,'$!to'),
+                target.chars
+              ),
+              $match,
+              Nil
+            ),
+            Nil
+          )
+        ))
     }
-    method subparse($target, :$rule = 'TOP', Capture() :$args = \(),  Mu :$actions = Mu, *%opt) {
-        my $*ACTIONS = $actions;
-        nqp::getlexcaller('$/') =
-            self."!cursor_init"($target, |%opt)."$rule"(|$args).MATCH;
+
+    method subparse(\target, :$rule, Capture() :$args, Mu :$actions) {
+        nqp::decont(nqp::getlexdyn('$/') = nqp::stmts(
+          (my $*ACTIONS = $actions),
+          nqp::if(
+            $rule,
+            nqp::if(
+              $args,
+              self!cursor-init(target, %_)."$rule"(|$args).MATCH,
+              self!cursor-init(target, %_)."$rule"().MATCH,
+            ),
+            nqp::if(
+              $args,
+              self!cursor-init(target, %_).TOP(|$args).MATCH,
+              self!cursor-init(target, %_).TOP().MATCH
+            ),
+          )
+        ))
     }
-    method parsefile(Str(Cool) $filename, :$enc, *%opts) {
-        my Mu $match := self.parse($filename.IO.slurp(:$enc), |%opts);
-        nqp::getlexcaller('$/') = $match;
+
+    method parsefile(Str(Cool) $filename, :$enc) {
+        nqp::decont(nqp::getlexdyn('$/') = nqp::if(
+          nqp::elems(nqp::getattr(%_,Map,'$!storage')),
+          self.parse($filename.IO.slurp(:$enc), |%_),
+          self.parse($filename.IO.slurp(:$enc))
+        ))
+    }
+
+    method !cursor-init(\target, %opts) is raw {
+        nqp::if(
+          nqp::elems(nqp::getattr(%opts,Map,'$!storage')),
+          $cursor-init(self, target, |%opts),
+          $cursor-init(self, target)
+        )
     }
 }
 

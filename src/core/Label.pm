rakudo/rakudo

commit 34f7f36f65ef0d52932b3d1986afe6c39db42ab8

Streamline Label.new a bit

src/core/Label.pm
@@ -6,11 +6,10 @@ my class Label {
     has Str $!postmatch;
     method new(:$name, :$line, :$prematch, :$postmatch) {
         # XXX Register in &?BLOCK.labels when we have &?BLOCK.
-        my $file := nqp::getlexdyn('$?FILES');
-        $file    := nqp::isnull($file) ?? '<unkown>' !! nqp::p6box_s($file);
-        my $obj  := nqp::create(self);
+        my $obj := nqp::create(self);
         nqp::bindattr($obj, Label, '$!name',      $name);
-        nqp::bindattr($obj, Label, '$!file',      $file);
+        nqp::bindattr($obj, Label, '$!file',
+          nqp::ifnull(nqp::getlexdyn('$?FILES'),'<unknown>'));
         nqp::bindattr($obj, Label, '$!line',      $line);
         nqp::bindattr($obj, Label, '$!prematch',  nqp::p6box_s($prematch));
         nqp::bindattr($obj, Label, '$!postmatch', nqp::p6box_s($postmatch));

commit 29db214274ce63121eac3cbe91d2fe54ae37aece

fix Label.gist, 34f7f36 broke it

src/core/Label.pm
@@ -9,7 +9,7 @@ my class Label {
         my $obj := nqp::create(self);
         nqp::bindattr($obj, Label, '$!name',      $name);
         nqp::bindattr($obj, Label, '$!file',
-          nqp::ifnull(nqp::getlexdyn('$?FILES'),'<unknown>'));
+          nqp::ifnull(nqp::p6box_s(nqp::getlexdyn('$?FILES')), '<unknown>'));
         nqp::bindattr($obj, Label, '$!line',      $line);
         nqp::bindattr($obj, Label, '$!prematch',  nqp::p6box_s($prematch));
         nqp::bindattr($obj, Label, '$!postmatch', nqp::p6box_s($postmatch));
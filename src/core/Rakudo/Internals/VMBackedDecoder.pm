e5e81e405187c9b10b56d966cdb039b07ec3be45
Zoffix Znet <cpan@zoffix.com>
2016-12-12 14:50:32 +0000

    Re-instate comment about why the `with` is in the code
    And expand on why it was removed/re-added

--- src/core/Rakudo/Internals/VMBackedDecoder.pm
+++ src/core/Rakudo/Internals/VMBackedDecoder.pm
@@ -28,6 +28,17 @@ augment class Rakudo::Internals {
                 my $available = $decoder.consume-available-chars();
                 emit $available if $available ne '';
                 LAST {
+                    # XXX The `with` is required due to a bug where the
+                    # LAST phaser is not properly scoped if we don't get
+                    # any bytes. Since that means there's nothing to emit
+                    # anyway, we'll not worry about this case for now.
+                    #
+                    # --- or at least that was the the idea before we fixed
+                    # that bug: https://irclog.perlgeek.de/perl6/2016-12-07#i_13698178
+                    # and tried removing the `with` in 58cdfd8, but then the error
+                    # `No such method 'consume-all-chars' for invocant of type 'Any`
+                    # started popping up on Proc::Async tests, so...
+                    # there may be some other bug affecting this?
                     with $decoder {
                         my $rest = .consume-all-chars();
                         emit $rest if $rest ne '';

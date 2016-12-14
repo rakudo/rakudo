a78d3304db856e5c0fc5d3e61819fa72e080cf5c
Jonathan Worthington <jnthn@jnthn.net>
2016-12-14 17:50:32 +0100

    Missing MVMROOT in late-bound type check.
    Needed because of mutex acquisition, and thus possible GC trigger,
    by lazy deserialization of HOW and method cache.

--- src/vm/moar/ops/container.c
+++ src/vm/moar/ops/container.c
@@ -112,9 +112,16 @@ static void rakudo_scalar_store(MVMThreadContext *tc, MVMObject *cont, MVMObject
             /* If we get here, need to call .^type_check on the value we're
              * checking, unless it's an accepts check. */
             if (!STABLE(obj)->type_check_cache || (mode & MVM_TYPE_CHECK_CACHE_THEN_METHOD)) {
-                MVMObject *HOW = MVM_6model_get_how_obj(tc, rcd->of);
-                MVMObject *meth = MVM_6model_find_method_cache_only(tc, HOW,
-                    tc->instance->str_consts.type_check);
+                MVMObject *HOW, *meth;
+                MVMROOT(tc, obj, {
+                MVMROOT(tc, rcd, {
+                    HOW = MVM_6model_get_how_obj(tc, rcd->of);
+                    MVMROOT(tc, HOW, {
+                        meth = MVM_6model_find_method_cache_only(tc, HOW,
+                            tc->instance->str_consts.type_check);
+                    });
+                });
+                });
                 if (meth) {
                     /* Set up the call, using a fake register in special return
                      * data as the target. */
@@ -137,9 +144,16 @@ static void rakudo_scalar_store(MVMThreadContext *tc, MVMObject *cont, MVMObject
 
             /* If the flag to call .accepts_type on the target value is set, do so. */
             if (mode & MVM_TYPE_CHECK_NEEDS_ACCEPTS) {
-                MVMObject *HOW = MVM_6model_get_how_obj(tc, rcd->of);
-                MVMObject *meth = MVM_6model_find_method_cache_only(tc, HOW,
-                    tc->instance->str_consts.accepts_type);
+                MVMObject *HOW, *meth;
+                MVMROOT(tc, obj, {
+                MVMROOT(tc, rcd, {
+                    HOW = MVM_6model_get_how_obj(tc, rcd->of);
+                    MVMROOT(tc, HOW, {
+                        meth = MVM_6model_find_method_cache_only(tc, HOW,
+                            tc->instance->str_consts.accepts_type);
+                    });
+                });
+                });
                 if (meth) {
                     /* Set up the call, using the result register as the target. */
                     MVMObject *code = MVM_frame_find_invokee(tc, meth, NULL);

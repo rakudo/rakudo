8eef234a5b441a3332a8e601d0eabcd1dd2b7386
Zoffix Znet <cpan@zoffix.com>
2016-12-14 14:49:05 +0000

    Fix broken .perl for itemized Slip
    The output of Slip.perl is nothing more than `slip` prepended to
    output of List.perl. This creates a problem where an itemized Slip's
    .perl ends up being `slip$("a", "b")`, which fails to .EVAL
    
    Fix by checking whether output of List.perl is itemized and outputting
    an itemized slip `$(slip("a", "b"))` in that case.

--- src/core/Slip.pm
+++ src/core/Slip.pm
@@ -4,7 +4,14 @@ my class Slip { # is List
     multi method Slip(Slip:D:) { self }
     method defined ()          { self.so }
     method CALL-ME (+args)     { args.Slip }
-    multi method perl(Slip:D:) { 'slip' ~ callsame }
+    multi method perl(Slip:D:) {
+        my $guts := callsame;
+        nqp::if(
+            nqp::eqat($guts, '$', 0), # we're itemized
+            nqp::concat('$(slip', nqp::concat(nqp::substr($guts, 1), ')')),
+            nqp::concat('slip', $guts),
+        )
+    }
 }
 
 # The slip(...) function creates a Slip.

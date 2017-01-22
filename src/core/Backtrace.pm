10bcec2865bff7b192d98fb8c44556f0c4e86b3e
usev6 <use_v6@aglaz.de>
2017-01-22 12:51:28 +0100

    Don't mention *CORE.setting in error message
    References to CORE.setting.moarvm where removed with 4ae08e0b24,
    but rakudo-j points to gen/jvm/CORE.setting (no .jar there).
    
    Fixes RT #130509 and RT #124767 on JVM

--- src/core/Backtrace.pm
+++ src/core/Backtrace.pm
@@ -61,6 +61,7 @@ my class Backtrace::Frame {
     method is-setting(Backtrace::Frame:D:) {
         $!file.starts-with("SETTING::")
           || $!file.ends-with("CORE.setting." ~ Rakudo::Internals.PRECOMP-EXT)
+          || $!file.ends-with("CORE.setting")
     }
 }
 

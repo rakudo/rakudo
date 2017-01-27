4aa8d70ebfde06520f3cb01808d25a09a1f594ab
Jonathan Worthington <jnthn@jnthn.net>
2017-01-27 18:39:52 +0100

    Make `react` also non-blocking in 6.d.PREVIEW.
    Or at least, make it that way if it's called in a `start` block.
    This means you can have many thousands of `start react { ... }`
    constructs running with just a handful of CPU threads; they will no
    longer block up a real thread.

--- src/core.d/await.pm
+++ src/core.d/await.pm
@@ -11,3 +11,38 @@ multi sub await(Any:D $x) {
 multi sub await(Awaitable:D \a) { $*AWAITER.await(a) }
 multi sub await(Iterable:D \i)  { $*AWAITER.await-all(i) }
 multi sub await(*@awaitables)   { $*AWAITER.await-all(@awaitables) }
+
+sub REACT(&block --> Nil) {
+    my class ReactAwaitable does Awaitable {
+        has $!handle;
+
+        method new($handle) {
+            self.CREATE!set-handle($handle)
+        }
+        method !set-handle($handle) {
+            $!handle = $handle;
+            self
+        }
+
+        method get-await-handle() { $!handle }
+    }
+    my class ReactAwaitHandle does Awaitable::Handle {
+        has &!react-block;
+
+        method not-ready(&react-block) {
+            self.CREATE!set-react-block(&react-block)
+        }
+        method !set-react-block(&react-block) {
+            &!react-block = &react-block;
+            self
+        }
+
+        method subscribe-awaiter(&subscriber) {
+            SUPPLY(&!react-block).tap:
+                { warn "Useless use of emit in react" },
+                done => { subscriber(True, Nil) },
+                quit => { subscriber(False, $_) };
+        }
+    }
+    $*AWAITER.await(ReactAwaitable.new(ReactAwaitHandle.not-ready(&block)));
+}

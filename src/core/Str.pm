6e9d08c8ba1c72fa599ddd3ccf53073285e35d37
Zoffix Znet <cpan@zoffix.com>
2017-02-02 19:17:35 -0500

    move Str.subst proto to be next to its methods
    No functional changes. The proto for Str.subst seems to be hundreds
    of lines away from Str.subst methods, tucked next to Str.substr-*
    
    Fix by moving the proto in the source code.

--- src/core/Str.pm
+++ src/core/Str.pm
@@ -144,10 +144,6 @@ my class Str does Stringy { # declared in BOOTSTRAP
         ))
     }
 
-    proto method subst(|) {
-        $/ := nqp::getlexdyn('$/');
-        {*}
-    }
     multi method substr-eq(Str:D: Str:D $needle) {
         nqp::p6bool(nqp::eqat($!value,nqp::getattr($needle,Str,'$!value'),0))
     }
@@ -1091,6 +1087,10 @@ my class Str does Stringy { # declared in BOOTSTRAP
         }
     }
 
+    proto method subst(|) {
+        $/ := nqp::getlexdyn('$/');
+        {*}
+    }
     multi method subst(Str:D: $matcher, $replacement, :global(:$g),
                        :ii(:$samecase), :ss(:$samespace), :mm(:$samemark),
                        *%options) {

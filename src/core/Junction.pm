b283e52bb2de9cf751a66868c0f9959f638c7845
Timo Paulssen <timonator@perpetuum-immobile.de>
2017-02-25 13:17:42 +0100

    since Mu has a sink method, Junction.sink don't need to .?sink

--- src/core/Junction.pm
+++ src/core/Junction.pm
@@ -227,7 +227,7 @@ my class Junction { # declared in BOOTSTRAP
     method sink(Junction:D: --> Nil) {
         my int $elems = nqp::elems($!storage);
         my int $i     = -1;
-        nqp::atpos($!storage,$i).?sink while nqp::islt_i(++$i,$elems);
+        nqp::atpos($!storage,$i).sink while nqp::islt_i(++$i,$elems);
     }
 
     method AUTOTHREAD(&call, |args) {

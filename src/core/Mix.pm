de983bc4ddf530d4797e2e63663140344598670a
Elizabeth Mattijsen <liz@dijkmat.nl>
2017-04-07 21:35:24 +0200

    Make Mix.MixHash coercion about 25x faster
    For a 10 element Mix.

--- src/core/Mix.pm
+++ src/core/Mix.pm
@@ -32,8 +32,33 @@ my class Mix does Mixy {
     }
 
 #--- coercion methods
-    method Mix { self }
-    method MixHash { MixHash.new-from-pairs(%!elems.values) }
+    method Mix() { self }
+    method MixHash() {
+        nqp::if(
+          (my $raw := nqp::getattr(%!elems,Map,'$!storage'))
+            && nqp::elems($raw),
+          nqp::stmts(                             # something to coerce
+            (my $elems := nqp::clone($raw)),
+            (my $iter := nqp::iterator($elems)),
+            nqp::while(
+              $iter,
+              nqp::bindkey(
+                $elems,
+                nqp::iterkey_s(my $tmp := nqp::shift($iter)),
+                nqp::p6bindattrinvres(
+                  nqp::clone(nqp::iterval($tmp)),
+                  Pair,
+                  '$!value',
+                  (nqp::p6scalarfromdesc(nqp::null) =
+                    nqp::getattr(nqp::iterval($tmp),Pair,'$!value'))
+                )
+              )
+            ),
+            nqp::create(MixHash).SET-SELF($elems)
+          ),
+          nqp::create(MixHash)                    # nothing to coerce
+        )
+    }
     method Bag     {     Bag.new-from-pairs(%!elems.values.grep(*.value > 0).map({.key => .value.Int})) }
     method BagHash { BagHash.new-from-pairs(%!elems.values.grep(*.value > 0).map({.key => .value.Int})) }
 

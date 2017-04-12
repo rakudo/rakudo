f1b4af7af4bd0f98a124a7d2c2fbcdd3da367351
Zoffix Znet <cpan@zoffix.com>
2017-04-12 17:44:13 +0000

    [io grant] Implement IO::Handle.slurp
    - Per IO plan, add .slurp as a replacement for .slurp-rest
        - Will add a DEPRECATED to .slurp-rest when we cut 6.d, as
            currently the compiler version for that is unknown
    - Leave .slurp-rest as is, but in new .slurp, toss :bin and :enc
        params. Use the handle's $!encoding attr. This aligns the routine
        with all the others we have and avoids
        switch-of-encoding-at-a-distance for the handle
    - There's a way to make the binary mode 16x faster by doing a tell,
        seek to end, tell, seek to original position and using the value
        as size for the binary read, but it's a bit iffy since some handles
        can't be .seek'ed (so we need a `try` there that falls back to
        slower method) and perhaps some can be .seeked forward but not back.
        So I left it out; the faster way *is* implemented in IO::Path.slurp

--- src/core/IO/Handle.pm
+++ src/core/IO/Handle.pm
@@ -793,6 +793,9 @@ my class IO::Handle {
 
     proto method slurp-rest(|) { * }
     multi method slurp-rest(IO::Handle:D: :$bin! where *.so, :$close --> Buf:D) {
+        # NOTE: THIS METHOD WILL BE DEPRECATED IN 6.d in favour of .slurp()
+        # Testing of it in roast master has been removed and only kept in 6.c
+        # If you're changing this code for whatever reason, test with 6.c-errata
         LEAVE self.close if $close;
         my $res := buf8.new;
         loop {
@@ -803,9 +806,29 @@ my class IO::Handle {
         }
     }
     multi method slurp-rest(IO::Handle:D: :$enc, :$bin, :$close --> Str:D) {
+        # NOTE: THIS METHOD WILL BE DEPRECATED IN 6.d in favour of .slurp()
+        # Testing of it in roast master has been removed and only kept in 6.c
+        # If you're changing this code for whatever reason, test with 6.c-errata
         LEAVE self.close if $close;
         self.encoding($enc) if $enc.defined;
         nqp::p6box_s(nqp::readallfh($!PIO));
+    }
+
+    method slurp(IO::Handle:D: :$close) {
+        my $res;
+        nqp::if(
+          nqp::iseq_s($!encoding, 'bin'),
+          nqp::stmts(
+            ($res := buf8.new),
+            nqp::while(
+              nqp::elems(my $buf := nqp::readfh($!PIO, buf8.new, 0x100000)),
+              $res.append($buf)
+            )
+          ),
+          ($res := nqp::p6box_s(nqp::readallfh($!PIO))),
+        );
+        self.close if $close;
+        $res
     }
 
     proto method spurt(|) { * }

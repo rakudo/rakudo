4a560aa746539bb093fac65185981868f6e758b8
Stefan Seifert <nine@detonation.org>
2017-04-18 18:33:19 +0200

    Fix "Cannot invoke this object (REPR: Null; VMNull)"
    A packaged Inline::Perl5 will throw the error about VMNull at runtime when
    trying to access the Proxy's FETCH method. Presumably the method closure did
    not survive serialization.
    Use a less elegant, but ultimately working approach instead.

--- src/core/Distribution.pm
+++ src/core/Distribution.pm
@@ -157,24 +157,12 @@ class Distribution::Resource {
     has $.repo-name;
     has $.dist-id;
     has $.key;
-    has $.IO handles <
-        Str gist perl
-        absolute is-absolute relative is-relative
-        parts volume dirname basename extension
-        open resolve slurp lines comb split words copy
-    >;
 
-    method BUILD(:$!repo, :$!repo-name, :$!dist-id, :$!key) {
-        my $self = self;
-        $!IO = Proxy.new(
-            FETCH => method () {
-                my $repo := $self.repo-name
-                    ?? CompUnit::RepositoryRegistry.repository-for-name($self.repo-name)
-                    !! CompUnit::RepositoryRegistry.repository-for-spec($self.repo);
-                $repo.resource($self.dist-id, "resources/$self.key()")
-            },
-            STORE => method ($new) { die "Attribute IO is read-only" },
-        );
+    method IO() {
+        my $repo := self.repo-name
+            ?? CompUnit::RepositoryRegistry.repository-for-name(self.repo-name)
+            !! CompUnit::RepositoryRegistry.repository-for-spec(self.repo);
+        $repo.resource(self.dist-id, "resources/$.key")
     }
 
     method platform-library-name() {
@@ -182,6 +170,68 @@ class Distribution::Resource {
         ($library ~~ /\.<.alpha>+$/ or $library ~~ /\.so(\.<.digit>+)+$/) #Already a full name?
             ??  $library
             !!  $*VM.platform-library-name($library)
+    }
+
+    # delegate appropriate IO::Path methods to the resource IO::Path object
+    method Str(|c) {
+        self.IO.Str(|c)
+    }
+    method gist(|c) {
+        self.IO.gist(|c)
+    }
+    method perl(|c) {
+        self.IO.perl(|c)
+    }
+    method absolute(|c) {
+        self.IO.absolute(|c)
+    }
+    method is-absolute(|c) {
+        self.IO.is-absolute(|c)
+    }
+    method relative(|c) {
+        self.IO.relative(|c)
+    }
+    method is-relative(|c) {
+        self.IO.is-relative(|c)
+    }
+    method parts(|c) {
+        self.IO.parts(|c)
+    }
+    method volume(|c) {
+        self.IO.volume(|c)
+    }
+    method dirname(|c) {
+        self.IO.dirname(|c)
+    }
+    method basename(|c) {
+        self.IO.basename(|c)
+    }
+    method extension(|c) {
+        self.IO.extension(|c)
+    }
+    method open(|c) {
+        self.IO.open(|c)
+    }
+    method resolve(|c) {
+        self.IO.resolve(|c)
+    }
+    method slurp(|c) {
+        self.IO.slurp(|c)
+    }
+    method lines(|c) {
+        self.IO.lines(|c)
+    }
+    method comb(|c) {
+        self.IO.comb(|c)
+    }
+    method split(|c) {
+        self.IO.split(|c)
+    }
+    method words(|c) {
+        self.IO.words(|c)
+    }
+    method copy(|c) {
+        self.IO.copy(|c)
     }
 }
 

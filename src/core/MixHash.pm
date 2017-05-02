0338ce46dcb4db23021ea8ca349706b5ccfcbaf0
Elizabeth Mattijsen <liz@dijkmat.nl>
2017-05-02 12:25:46 +0200

    Also fix .iterator/.pairs for RT #131241
    - abstract the Proxy logic in a proxy sub
    - make both .iterator (which feeds .pairs) and .values use that

--- src/core/MixHash.pm
+++ src/core/MixHash.pm
@@ -75,66 +75,88 @@ my class MixHash does Mixy {
     method MixHash() is nodal { self }
 
 #--- iterator methods
+    sub proxy(Mu \iter,Mu \storage) is raw {
+        # We are only sure that the key exists when the Proxy
+        # is made, but we cannot be sure of its existence when
+        # either the FETCH or STORE block is executed.  So we
+        # still need to check for existence, and handle the case
+        # where we need to (re-create) the key and value.  The
+        # logic is therefore basically the same as in AT-KEY,
+        # except for tests for allocated storage and .WHICH
+        # processing.
+        nqp::stmts(
+          (my $which := nqp::iterkey_s(iter)),
+          (my $object := nqp::getattr(          # recreation
+            nqp::decont(nqp::iterval(iter)),Pair,'$!key')),
+
+          Proxy.new(
+            FETCH => {
+                nqp::if(
+                  nqp::existskey(storage,$which),
+                  nqp::getattr(
+                    nqp::decont(nqp::atkey(storage,$which)),Pair,'$!value'
+                  ),
+                  0
+                )
+            },
+            STORE => -> $, Int() $value {
+                nqp::if(
+                  nqp::istype($value,Failure),  # RT 128927
+                  $value.throw,
+                  nqp::if(
+                    nqp::existskey(storage,$which),
+                    nqp::if(                    # existing element
+                      $value == 0,
+                      nqp::stmts(               # goodbye!
+                        nqp::deletekey(storage,$which),
+                        0
+                      ),
+                      (nqp::getattr(            # value ok
+                        nqp::decont(nqp::atkey(storage,$which)),
+                        Pair,
+                        '$!value'
+                      ) = $value)
+                    ),
+                    nqp::unless(                # where did it go?
+                      $value == 0,
+                      nqp::bindkey(storage,$which,Pair.new($object,$value))
+                    )
+                  )
+                )
+            }
+          )
+        )
+    }
+
+    multi method iterator(MixHash:D:) {    # also .pairs
+        class :: does Rakudo::Iterator::Mappy {
+            method pull-one() is raw {
+                nqp::if(
+                  $!iter,
+                  nqp::p6bindattrinvres(
+                    nqp::clone(nqp::iterval(nqp::shift($!iter))),
+                    Pair,
+                    '$!value',
+                    proxy($!iter,$!storage)
+                  ),
+                  IterationEnd
+                )
+            }
+            method push-all($target --> IterationEnd) {
+                nqp::while(  # doesn't sink
+                  $!iter,
+                  $target.push(nqp::iterval(nqp::shift($!iter)))
+                )
+            }
+        }.new(%!elems)
+    }
+
     multi method values(MixHash:D:) {
         Seq.new(class :: does Rakudo::Iterator::Mappy {
             method pull-one() is raw {
                 nqp::if(
                   $!iter,
-
-                  # We are only sure that the key exists when the Proxy
-                  # is made, but we cannot be sure of its existence when
-                  # either the FETCH or STORE block is executed.  So we
-                  # still need to check for existence, and handle the case
-                  # where we need to (re-create) the key and value.  The
-                  # logic is therefore basically the same as in AT-KEY,
-                  # except for tests for allocated storage and .WHICH
-                  # processing.
-                  nqp::stmts(
-                    (my $which := nqp::iterkey_s(nqp::shift($!iter))),
-                    (my $pair  := nqp::iterval($!iter)),  # recreation
-
-                    Proxy.new(
-                      FETCH => {
-                          nqp::if(
-                            nqp::existskey($!storage,$which),
-                            nqp::getattr(
-                              nqp::decont(nqp::atkey($!storage,$which)),
-                              Pair,
-                              '$!value'
-                            ),
-                            0
-                          )
-                      },
-                      STORE => -> $, Real() $value {
-                          nqp::if(
-                            nqp::istype($value,Failure),  # RT 128927
-                            $value.throw,
-                            nqp::if(
-                              nqp::existskey($!storage,$which),
-                              nqp::if(                    # existing element
-                                $value == 0,
-                                nqp::stmts(               # goodbye!
-                                  nqp::deletekey($!storage,$which),
-                                  0
-                                ),
-                                (nqp::getattr(            # value ok
-                                  nqp::decont(nqp::atkey($!storage,$which)),
-                                  Pair,
-                                  '$!value'
-                                ) = $value)
-                              ),
-                              nqp::unless(                # where did it go?
-                                $value == 0,
-                                nqp::stmts(               # ok to add (again)
-                                  (nqp::getattr($pair,Pair,'$!value') = $value),
-                                  nqp::bindkey($!storage,$which,$pair)
-                                )
-                              )
-                            )
-                          )
-                      }
-                    )
-                  ),
+                  proxy(nqp::shift($!iter),$!storage),
                   IterationEnd
                 )
             }
@@ -148,7 +170,7 @@ my class MixHash does Mixy {
                 )
             }
         }.new(%!elems))
-   }
+    }
 }
 
 # vim: ft=perl6 expandtab sw=4

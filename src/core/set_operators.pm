b1d83f9d6956673c8306c1c9e9edbd8ff5d46017
Elizabeth Mattijsen <liz@wenzperl.nl>
2017-05-23 20:35:13 +0200

    Give (<=) Baggy and Mixy semantics for Bags/Mixes
    This oddly enough does *not* break any spectests, even though before
    only Set semantics were used.  Given A and B:
    
    For Bags: a key in A must exist in B with val(A) <= val(B)
    For Mixes:
     - matching keys in A and B should have val(A) <= val(B)
     - missing keys in A should have val(B) >= 0
     - missing keys in B should have val(A) <= 0
     - since we don't have 0 values in Mixes, the <= can be <

--- src/core/set_operators.pm
+++ src/core/set_operators.pm
@@ -849,6 +849,107 @@ multi sub infix:<<(<=)>>(Setty:D $a, Setty:D $b --> Bool:D) {
       True
     )
 }
+multi sub infix:<<(<=)>>(Mixy:D $a, Baggy:D $b --> Bool:D) {
+    infix:<<(<=)>>($a, $b.Mix)
+}
+multi sub infix:<<(<=)>>(Baggy:D $a, Mixy:D $b --> Bool:D) {
+    infix:<<(<=)>>($a.Mix, $b)
+}
+multi sub infix:<<(<=)>>(Mixy:D $a, Mixy:D $b --> Bool:D) {
+    nqp::if(
+      nqp::eqaddr(nqp::decont($a),nqp::decont($b)),
+      True,                     # X is always a subset of itself
+      nqp::if(
+        (my $araw := $a.raw_hash) && nqp::elems($araw),
+        nqp::if(                # elems in A
+          (my $braw := $b.raw_hash) && nqp::elems($braw),
+          nqp::stmts(           # elems in A and B
+            (my $iter := nqp::iterator($araw)),
+            nqp::while(         # check all values in A with B
+              $iter,
+              nqp::unless(
+                nqp::getattr(nqp::iterval(nqp::shift($iter)),Pair,'$!value')
+                  <=            # value in A should be less or equal than B
+                nqp::getattr(
+                  nqp::ifnull(
+                    nqp::atkey($braw,nqp::iterkey_s($iter)),
+                    BEGIN       # provide virtual value 0
+                      nqp::p6bindattrinvres(nqp::create(Pair),Pair,'$!value',0)
+                  ),
+                  Pair,
+                  '$!value'
+                ),
+                return False
+              )
+            ),
+
+            ($iter := nqp::iterator($braw)),
+            nqp::while(         # check all values in B with A
+              $iter,
+              nqp::unless(
+                nqp::getattr(nqp::iterval(nqp::shift($iter)),Pair,'$!value')
+                  >=            # value in B should be more or equal than A
+                nqp::getattr(
+                  nqp::ifnull(
+                    nqp::atkey($araw,nqp::iterkey_s($iter)),
+                    BEGIN       # provide virtual value 0
+                      nqp::p6bindattrinvres(nqp::create(Pair),Pair,'$!value',0)
+                  ),
+                  Pair,
+                  '$!value'
+                ),
+                return False
+              )
+            ),
+            True                # all checks worked out, so ok
+          ),
+          # nothing in B, all elems in A should be < 0
+          Rakudo::QuantHash.MIX-ALL-NEGATIVE($araw)
+        ),
+        nqp::if(
+          ($braw := $b.raw_hash) && nqp::elems($braw),
+          # nothing in A, all elems in B should be >= 0
+          Rakudo::QuantHash.MIX-ALL-POSITIVE($braw),
+          False                 # nothing in A nor B
+        )
+      )
+    )
+}
+multi sub infix:<<(<=)>>(Baggy:D $a, Baggy:D $b --> Bool:D) {
+    nqp::stmts(
+      nqp::unless(
+        nqp::eqaddr(nqp::decont($a),nqp::decont($b)),
+        nqp::if(
+          (my $araw := $a.raw_hash)
+            && nqp::elems($araw),
+          nqp::if(                # number of elems in B *always* >= A
+            (my $braw := $b.raw_hash)
+              && nqp::isle_i(nqp::elems($araw),nqp::elems($braw))
+              && (my $iter := nqp::iterator($araw)),
+            nqp::while(           # number of elems in B >= A
+              $iter,
+              nqp::unless(
+                nqp::getattr(nqp::iterval(nqp::shift($iter)),Pair,'$!value')
+                  <=              # value in A should be less or equal than B
+                nqp::getattr(
+                  nqp::ifnull(
+                    nqp::atkey($braw,nqp::iterkey_s($iter)),
+                    BEGIN       # provide virtual value 0
+                      nqp::p6bindattrinvres(nqp::create(Pair),Pair,'$!value',0)
+                  ),
+                  Pair,
+                  '$!value'
+                ),
+                return False
+              )
+            ),
+            return False          # number of elems in B smaller than A
+          )
+        )
+      ),
+      True
+    )
+}
 multi sub infix:<<(<=)>>(Map:D $a, Map:D $b --> Bool:D) {
     # don't need to check for object hashes, just checking keys is ok
     nqp::stmts(

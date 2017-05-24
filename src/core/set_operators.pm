rakudo/rakudo

commit e65800a84bba1a5c3b5bf1cf80fa0797a601e351

Make Any (elem) Iterable:D between 1.3x and 110x faster
- based on a 1000 elem array
- 1.3x faster if matched on last array element or no match
- 110x faster if matched on first array element
- gain caused by not needing to coerce to Set, but check on the fly

src/core/set_operators.pm
@@ -2,6 +2,20 @@ proto sub infix:<(elem)>($, $ --> Bool:D) is pure {*}
 multi sub infix:<(elem)>(Str:D $a, Map:D $b --> Bool:D) {
     $b.AT-KEY($a).Bool;
 }
+multi sub infix:<(elem)>(Any $a, Iterable:D $b --> Bool:D) {
+    nqp::stmts(
+      (my str $needle = $a.WHICH),
+      (my $iterator := $b.iterator),
+      nqp::until(
+        nqp::eqaddr((my $pulled := $iterator.pull-one),IterationEnd),
+        nqp::if(
+          nqp::iseq_s($needle,$pulled.WHICH),
+          return True
+        )
+      ),
+      False
+    )
+}
 multi sub infix:<(elem)>(Any $a, QuantHash:D $b --> Bool:D) {
     nqp::p6bool(
       (my $elems := $b.raw_hash) && nqp::existskey($elems,$a.WHICH)

commit af895626cdacf94d55f681cdf548f5c6ebc216f5

Make sure we handle Any (elem) Map:D correctly
This can only work if the Map:D is actually an object hash, since
it can only work on normal hashes if the Any is a Str:D, and that
case is already handled in a separate candidate.

Fixes a regression caused by e65800a84bba1a5c3

src/core/set_operators.pm
@@ -2,6 +2,22 @@ proto sub infix:<(elem)>($, $ --> Bool:D) is pure {*}
 multi sub infix:<(elem)>(Str:D $a, Map:D $b --> Bool:D) {
     nqp::p6bool($b.AT-KEY($a))
 }
+multi sub infix:<(elem)>(Any $a, Map:D $b --> Bool:D) {
+    nqp::p6bool(
+      (my $storage := nqp::getattr(nqp::decont($b),Map,'$!storage'))
+        && nqp::elems($storage)                         # haz a haystack
+        && nqp::not_i(nqp::eqaddr($b.keyof,Str(Any)))   # is object hash
+        && nqp::getattr(
+             nqp::ifnull(
+               nqp::atkey($storage,$a.WHICH),           # exists
+               BEGIN   # provide virtual value False    # did not exist
+                 nqp::p6bindattrinvres(nqp::create(Pair),Pair,'$!value',False)
+             ),
+             Pair,
+             '$!value'
+           )
+    )
+}
 multi sub infix:<(elem)>(Any $a, Iterable:D $b --> Bool:D) {
     nqp::stmts(
       (my str $needle = $a.WHICH),
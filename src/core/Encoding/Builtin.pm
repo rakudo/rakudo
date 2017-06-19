da1b37a79abd17fd3c42245c14fb9401a47eccdf
Jonathan Worthington <jnthn@jnthn.net>
2017-06-19 16:48:59 +0200

    Add Encoding::Builtin; register built-ins.
    An implementation of `Encoding` for the built-in encodings. Register
    all of the built-in encodings using it.

---
+++ src/core/Encoding/Builtin.pm
@@ -0,0 +1,40 @@
+class Encoding::Builtin does Encoding {
+    has Str $.name;
+    has @.alternative-names;
+
+    method new() {
+        die "Do not create instnaces of this class directly; instead use Encoding::Registry"
+    }
+
+    method alternative-names() { @!alternative-names }
+
+    method decoder(--> Encoding::Decoder) {
+        die "NYI";
+    }
+
+    method encoder(--> Encoding::Encoder) {
+        die "NYI"
+    }
+}
+
+Encoding::Registry.register(Encoding::Builtin.bless(
+    :name<utf8>, :alternative-names<utf-8>
+));
+Encoding::Registry.register(Encoding::Builtin.bless(
+    :name<ascii>
+));
+Encoding::Registry.register(Encoding::Builtin.bless(
+    :name<iso-8859-1>, :alternative-names<
+        iso_8859-1:1987 iso_8859-1 iso-ir-100 latin1 latin-1 csisolatin1
+        l1 ibm819 cp819
+    >
+));
+Encoding::Registry.register(Encoding::Builtin.bless(
+    :name<windows-1252>
+));
+Encoding::Registry.register(Encoding::Builtin.bless(
+    :name<utf16>, :alternative-names<utf-16>
+));
+Encoding::Registry.register(Encoding::Builtin.bless(
+    :name<utf32>, :alternative-names<utf-32>
+));

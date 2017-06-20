79ce1a991f044e381ba4d17b4dd850fa0c9bbedc
Elizabeth Mattijsen <liz@wenzperl.nl>
2017-06-20 17:46:28 +0200

    Precompie Encoding::Builtin objects
    This was rather more involved than just prefixing with BEGIN, as .bless
    does not appear to work at that point in the setting yet.  And passing
    HLL Lists at that point, also doesn't work.  So use more primitive means
    to create the Encoding::Builtin objects.

--- src/core/Encoding/Builtin.pm
+++ src/core/Encoding/Builtin.pm
@@ -1,12 +1,20 @@
 class Encoding::Builtin does Encoding {
     has Str $.name;
-    has @.alternative-names;
+    has $!alternative-names;
 
     method new() {
         X::Cannot::New.new(class => self.WHAT).throw
     }
 
-    method alternative-names() { @!alternative-names }
+    method SET-SELF(\name, \alternatives) {
+        nqp::stmts(
+          ($!name := name),
+          ($!alternative-names := alternatives),
+          self
+        )
+    }
+
+    method alternative-names() { $!alternative-names }
 
     method decoder(*%options --> Encoding::Decoder) {
         Encoding::Decoder::Builtin.new($!name, |%options)
@@ -35,27 +43,49 @@ class Encoding::Builtin does Encoding {
     }
 }
 
-Encoding::Registry.register(Encoding::Builtin.bless(
-    :name<utf8>, :alternative-names<utf-8>
-));
-Encoding::Registry.register(Encoding::Builtin.bless(
-    :name<utf8-c8>, :alternative-names<utf-8-c8>
-));
-Encoding::Registry.register(Encoding::Builtin.bless(
-    :name<ascii>
-));
-Encoding::Registry.register(Encoding::Builtin.bless(
-    :name<iso-8859-1>, :alternative-names<
-        iso_8859-1:1987 iso_8859-1 iso-ir-100 latin1 latin-1 csisolatin1
-        l1 ibm819 cp819
-    >
-));
-Encoding::Registry.register(Encoding::Builtin.bless(
-    :name<windows-1252>
-));
-Encoding::Registry.register(Encoding::Builtin.bless(
-    :name<utf16>, :alternative-names<utf-16>
-));
-Encoding::Registry.register(Encoding::Builtin.bless(
-    :name<utf32>, :alternative-names<utf-32>
-));
+Encoding::Registry.register(
+  BEGIN nqp::create(Encoding::Builtin).SET-SELF(
+    "utf8", nqp::list("utf-8")
+  )
+);
+Encoding::Registry.register(
+  BEGIN nqp::create(Encoding::Builtin).SET-SELF(
+    "utf8-c8", nqp::list("utf-8-c8")
+  )
+);
+Encoding::Registry.register(
+  BEGIN nqp::create(Encoding::Builtin).SET-SELF(
+    "ascii", nqp::list
+  )
+);
+Encoding::Registry.register(
+  BEGIN nqp::create(Encoding::Builtin).SET-SELF(
+    "iso-8859-1",
+    nqp::list(
+      "iso_8859-1:1987",
+      "iso_8859-1",
+      "iso-ir-100",
+      "latin1",
+      "latin-1",
+      "csisolatin1:",
+      "l1", 
+      "ibm819",
+      "cp819"
+    )
+  )
+);
+Encoding::Registry.register(
+  BEGIN nqp::create(Encoding::Builtin).SET-SELF(
+    "windows-1252", nqp::list
+  )
+);
+Encoding::Registry.register(
+  BEGIN nqp::create(Encoding::Builtin).SET-SELF(
+    "utf16", nqp::list("utf-16")
+  )
+);
+Encoding::Registry.register(
+  BEGIN nqp::create(Encoding::Builtin).SET-SELF(
+    "utf32", nqp::list("utf-32")
+  )
+);

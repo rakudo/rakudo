9a2127f2b5a411e011b548a47b945bba7f2d51a4
Timo Paulssen <timonator@perpetuum-immobile.de>
2017-06-28 00:42:58 +0200

    give nil methods a much more efficient "take any args" signature

--- src/core/Nil.pm
+++ src/core/Nil.pm
@@ -5,22 +5,22 @@ my class Nil is Cool { # declared in BOOTSTRAP
     multi method gist(Nil:)   { 'Nil' }
     method Numeric() { warn "Use of {self.gist} in numeric context"; 0 }
     method Str() { warn "Use of {self.gist} in string context"; '' }
-    method sink(*@ --> Nil)   { }     # required by RESTRICTED setting
+    method sink(| --> Nil)   { }     # required by RESTRICTED setting
 
-    method AT-POS(*@ --> Nil) { }
-    method AT-KEY(*@ --> Nil) { }
+    method AT-POS(| --> Nil) { }
+    method AT-KEY(| --> Nil) { }
 #    method ACCEPTS(*@ --> Nil) { }  # XXX spec says Nil, but makes spectest hang
 
-    method BIND-POS(*@)   { die "Attempted to BIND-POS to {self.gist}." }
-    method BIND-KEY(*@)   { Failure.new(X::Bind.new(target => self.gist)) }
-    method ASSIGN-POS(*@) { die "Attempted to ASSIGN-POS to {self.gist}." }
-    method ASSIGN-KEY(*@) { die "Attempted to ASSIGN-KEY to {self.gist}." }
-    method STORE(*@)      { X::Assignment::RO.new(:value<Nil>).throw }
-    method push(*@)    is nodal { die "Attempted to push to {self.gist}." }
-    method append(*@)  is nodal { die "Attempted to append to {self.gist}." }
-    method unshift(*@) is nodal { die "Attempted to unshift to {self.gist}." }
-    method prepend(*@) is nodal { die "Attempted to prepend to {self.gist}." }
-    method FALLBACK(*@ --> Nil) { }
+    method BIND-POS(|)   { die "Attempted to BIND-POS to {self.gist}." }
+    method BIND-KEY(|)   { Failure.new(X::Bind.new(target => self.gist)) }
+    method ASSIGN-POS(|) { die "Attempted to ASSIGN-POS to {self.gist}." }
+    method ASSIGN-KEY(|) { die "Attempted to ASSIGN-KEY to {self.gist}." }
+    method STORE(|)      { X::Assignment::RO.new(:value<Nil>).throw }
+    method push(|)    is nodal { die "Attempted to push to {self.gist}." }
+    method append(|)  is nodal { die "Attempted to append to {self.gist}." }
+    method unshift(|) is nodal { die "Attempted to unshift to {self.gist}." }
+    method prepend(|) is nodal { die "Attempted to prepend to {self.gist}." }
+    method FALLBACK(| --> Nil) { }
 
     # These suggest using Nil.new if they fall through, which is LTA
     method ords() { self.Str.ords }

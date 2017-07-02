d4436e18ce1e49eb74733ef551a55c728586b4ca
Elizabeth Mattijsen <liz@wenzperl.nl>
2017-07-02 14:56:43 +0200

    Revert to Setty semantics for (<)
    Because we also did this for (<=).

--- src/core/set_proper_subset.pm
+++ src/core/set_proper_subset.pm
@@ -7,7 +7,7 @@
 #   ⊅     is NOT a proper superset of
 
 proto sub infix:<<(<)>>($, $ --> Bool:D) is pure {*}
-multi sub infix:<<(<)>>(Setty:D $a, Setty:D $b --> Bool:D) {
+multi sub infix:<<(<)>>(QuantHash:D $a, QuantHash:D $b --> Bool:D) {
     nqp::if(
       nqp::eqaddr(nqp::decont($a),nqp::decont($b)),
       False,                    # X is never a true subset of itself
@@ -31,111 +31,6 @@ multi sub infix:<<(<)>>(Setty:D $a, Setty:D $b --> Bool:D) {
             False               # number of elems in B smaller or equal to A
           ),
           True                  # no elems in A, and elems in B
-        ),
-        False                   # can never have fewer elems in A than in B
-      )
-    )
-}
-multi sub infix:<<(<)>>(Mixy:D $a, Baggy:D $b --> Bool:D) {
-    infix:<<(<)>>($a, $b.Mix)
-}
-multi sub infix:<<(<)>>(Baggy:D $a, Mixy:D $b --> Bool:D) {
-    infix:<<(<)>>($a.Mix, $b)
-}
-multi sub infix:<<(<)>>(Mixy:D $a, Mixy:D $b --> Bool:D) {
-    nqp::if(
-      nqp::eqaddr(nqp::decont($a),nqp::decont($b)),
-      False,                    # X is never a true subset of itself
-      nqp::if(
-        (my $araw := $a.raw_hash) && nqp::elems($araw),
-        nqp::if(                # elems in A
-          (my $braw := $b.raw_hash) && nqp::elems($braw),
-          nqp::stmts(           # elems in A and B
-            (my $iter := nqp::iterator($araw)),
-            nqp::while(         # check all values in A with B
-              $iter,
-              nqp::unless(
-                nqp::getattr(nqp::iterval(nqp::shift($iter)),Pair,'$!value')
-                  <             # value in A should be less than (virtual) B
-                nqp::getattr(
-                  nqp::ifnull(
-                    nqp::atkey($braw,nqp::iterkey_s($iter)),
-                    BEGIN       # provide virtual value 0
-                      nqp::p6bindattrinvres(nqp::create(Pair),Pair,'$!value',0)
-                  ),
-                  Pair,
-                  '$!value'
-                ),
-                return False
-              )
-            ),
-
-            ($iter := nqp::iterator($braw)),
-            nqp::while(         # check all values in B with A
-              $iter,
-              nqp::unless(
-                nqp::getattr(nqp::iterval(nqp::shift($iter)),Pair,'$!value')
-                  >             # value in B should be more than (virtual) A
-                nqp::getattr(
-                  nqp::ifnull(
-                    nqp::atkey($araw,nqp::iterkey_s($iter)),
-                    BEGIN       # provide virtual value 0
-                      nqp::p6bindattrinvres(nqp::create(Pair),Pair,'$!value',0)
-                  ),
-                  Pair,
-                  '$!value'
-                ),
-                return False
-              )
-            ),
-            True                # all checks worked out, so ok
-          ),
-          # nothing in B, all elems in A should be < 0
-          Rakudo::QuantHash.MIX-ALL-NEGATIVE($araw)
-        ),
-        nqp::if(                # nothing in A
-          ($braw := $b.raw_hash) && nqp::elems($braw),
-          # something in B, all elems in B should be > 0
-          Rakudo::QuantHash.MIX-ALL-POSITIVE($braw),
-          False                 # nothing in A nor B
-        )
-      )
-    )
-}
-multi sub infix:<<(<)>>(Baggy:D $a, Baggy:D $b --> Bool:D) {
-    nqp::if(
-      nqp::eqaddr($a,$b),
-      False,                    # X is never a true subset of itself
-      nqp::if(
-        (my $braw := $b.raw_hash) && nqp::elems($braw),
-        nqp::if(
-          (my $araw := $a.raw_hash) && nqp::elems($araw),
-          nqp::if(
-            nqp::islt_i(nqp::elems($araw),nqp::elems($braw))
-              && (my $iter := nqp::iterator($araw)),
-            nqp::stmts(         # A has fewer elems than B
-              nqp::while(
-                $iter,
-                nqp::unless(
-                  nqp::getattr(nqp::iterval(nqp::shift($iter)),Pair,'$!value')
-                   <
-                  nqp::getattr(
-                    nqp::ifnull(
-                      nqp::atkey($braw,nqp::iterkey_s($iter)),
-                      BEGIN nqp::p6bindattrinvres(     # virtual 0
-                        nqp::create(Pair),Pair,'$!value',0)
-                    ),
-                    Pair,
-                    '$!value'
-                  ),
-                  return False  # elem in A not in B or same or more in B
-                )
-              ),
-              True              # all elems in A exist in B and are less
-            ),
-            False               # number of elems in B smaller or equal to A
-          ),
-          True                  # elems in B, no elems in A
         ),
         False                   # can never have fewer elems in A than in B
       )

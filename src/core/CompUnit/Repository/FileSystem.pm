31eefb0d00e440acc073ffc2b323c5b321403d38
TimToady <larry@wall.org>
2017-07-04 10:12:52 -0700

    Don't use .<<$key>> where .{$key} is intended

--- src/core/CompUnit/Repository/FileSystem.pm
+++ src/core/CompUnit/Repository/FileSystem.pm
@@ -194,8 +194,8 @@ class CompUnit::Repository::FileSystem does CompUnit::Repository::Locally does C
         # but we also want to root any path request to the CUR's resources directory
 
         # When $.prefix points at a directory containing a meta file (eg. -I.)
-        return $.prefix.add( %!meta<files><<$key>> )
-            if %!meta<files> && %!meta<files><<$key>>;
+        return $.prefix.add( %!meta<files>{$key} )
+            if %!meta<files> && %!meta<files>{$key};
         return $.prefix.add( $key )
             if %!meta<resources> && %!meta<resources>.first({ $_ eq $key.subst(/^resources\//, "") });
 

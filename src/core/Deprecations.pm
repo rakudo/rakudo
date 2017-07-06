1b6d048bdf3d2442f736c936fbf04adf1406f37c
Elizabeth Mattijsen <liz@wenzperl.nl>
2017-07-06 13:14:03 +0200

    Handle case where "removed" is set and "from" isn't

--- src/core/Deprecations.pm
+++ src/core/Deprecations.pm
@@ -37,11 +37,12 @@ class Deprecation {
         for %.callsites.kv -> $file, $lines {
             $message ~=
               "  $file, line{ 's' if +$lines > 1 } {$lines.keys.sort.join(',')}\n";
-            $message ~=
-              "Deprecated since v$.from, will be removed {$.removed
-                ?? 'with release v' ~ $.removed ~ '!'
-                !! 'sometime in the future'
-              }\n" if $.from;
+            $message ~= $.from
+              ?? "Deprecated since v$.from, will be removed"
+              !! "Will be removed";
+            $message ~= $.removed
+              ?? " with release v$.removed!\n"
+              !! " sometime in the future\n";
         }
         $message ~= "Please use $.alternative instead.\n";
         $message;

c63c57e9a823303e74c06afdee10a99d6cb137ad
Elizabeth Mattijsen <liz@wenzperl.nl>
2017-07-31 15:12:15 +0200

    Use "is default" for IO::Handle atttributes

--- src/core/IO/Handle.pm
+++ src/core/IO/Handle.pm
@@ -5,9 +5,9 @@ my class Proc { ... }
 my class IO::Handle {
     has $.path;
     has $!PIO;
-    has $.chomp is rw = Bool::True;
-    has $.nl-in = ["\x0A", "\r\n"];
-    has Str:D $.nl-out is rw = "\n";
+    has $.chomp is rw is default(True);
+    has $.nl-in is default(["\x0A", "\r\n"]);
+    has Str:D $.nl-out is rw is default("\n");
     has Str $.encoding;
     has Encoding::Decoder $!decoder;
     has Encoding::Encoder $!encoder;

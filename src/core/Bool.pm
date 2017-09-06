e7a588060907e93fc197118a173427505a444caf
Elizabeth Mattijsen <liz@wenzperl.nl>
2017-09-06 23:33:35 +0200

    Make Bool.enums consistent with Enumeration.enums

--- src/core/Bool.pm
+++ src/core/Bool.pm
@@ -34,7 +34,7 @@ BEGIN {
     Bool.^add_method('pred',  my method pred() { Bool::False });
     Bool.^add_method('succ',  my method succ() { Bool::True });
 
-    Bool.^add_method('enums', my method enums() { self.^enum_values });
+    Bool.^add_method('enums', my method enums() { self.^enum_values.Map });
 
     Bool.^compose;
 }

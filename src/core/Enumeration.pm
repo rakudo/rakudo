43e41ec6833d1e36df8cd598c1380a5e4c28b37d
Zoffix Znet <cpan@zoffix.com>
2017-09-15 17:39:02 +0000

    Apply lizmat++'s improvements
    From https://irclog.perlgeek.de/perl6-dev/2017-09-15#i_15169665
    
    Makes .pred 2.8x faster still and .succ 80% faster

--- src/core/Enumeration.pm
+++ src/core/Enumeration.pm
@@ -46,34 +46,32 @@ my role Enumeration {
 
     method pred(::?CLASS:D:) {
         nqp::stmts(
-          (my $values := self.^enum_value_list),
+          (my $values := nqp::getattr(self.^enum_value_list, List, '$!reified')),
           # We find ourselves in $values and give previous value, or self if we are the first one
           nqp::if(
-            nqp::isle_i((my int $els = $values.elems), 1),
+            nqp::isle_i((my int $els = nqp::elems($values)), 1),
             self, # short-curcuit; there's only us in the list; avoids --$i'ing past end later
             nqp::stmts(
-              ($values := nqp::getattr($values, List, '$!reified')),
               (my int $i = $els),
               nqp::while(
                 nqp::isgt_i(($i = nqp::sub_i($i, 1)), 1) # >1 because we subtract one after the loop
-                && nqp::isfalse(self === nqp::atpos($values, $i)),
+                && nqp::isfalse(nqp::eqaddr(self, nqp::atpos($values, $i))),
                 nqp::null),
               nqp::atpos($values, nqp::sub_i($i,1)))))
     }
     method succ(::?CLASS:D:) {
         nqp::stmts(
-          (my $values := self.^enum_value_list),
+          (my $values := nqp::getattr(self.^enum_value_list, List, '$!reified')),
           # We find ourselves in $values and give next value, or self if we are the last one
           nqp::if(
-            nqp::isle_i((my int $els = nqp::sub_i($values.elems, 2)), -1),
+            nqp::isle_i((my int $els = nqp::sub_i(nqp::elems($values), 2)), -1),
                   # $els - 2 because we add 1 after the loop
             self, # short-curcuit; there's only us in the list; avoids ++$i'ing past end later
             nqp::stmts(
-              ($values := nqp::getattr($values, List, '$!reified')),
-              (my int $i   = -1),
+              (my int $i = -1),
               nqp::while(
                 nqp::islt_i(($i = nqp::add_i($i, 1)), $els)
-                && nqp::isfalse(self === nqp::atpos($values, $i)),
+                && nqp::isfalse(nqp::eqaddr(self, nqp::atpos($values, $i))),
                 nqp::null),
               nqp::atpos($values, nqp::add_i($i,1)))))
     }
@@ -127,11 +125,7 @@ Metamodel::EnumHOW.set_composalizer(-> $type, $name, %enum_values {
 # that simply unboxes the values. That's no good for us, since two different
 # Enumertaion:Ds could have the same Int:D value.
 multi infix:<===> (Enumeration:D \a, Enumeration:D \b) {
-    nqp::p6bool(
-      nqp::eqaddr(a,b)
-      || (nqp::eqaddr(a.WHAT,b.WHAT)
-           && nqp::iseq_s(nqp::unbox_s(a.WHICH), nqp::unbox_s(b.WHICH)))
-    )
+    nqp::p6bool(nqp::eqaddr(nqp::decont(a), nqp::decont(b)))
 }
 
 # vim: ft=perl6 expandtab sw=4

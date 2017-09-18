48406db639183f850bfd7b6d22de5fb4c794fdb8
Elizabeth Mattijsen <liz@wenzperl.nl>
2017-09-18 12:01:19 +0200

    Streamline registering an encoding
    Not much for something that only needs to be done once, you might
    argue.  But we're doing *7* of these at *every* perl6 startup.
    This shaves 1-2 milliseconds off of each perl6 startup.

--- src/core/Encoding/Registry.pm
+++ src/core/Encoding/Registry.pm
@@ -3,24 +3,39 @@ my class X::Encoding::AlreadyRegistered { ... }
 
 my class Encoding::Registry {
     my $lock := Lock.new;
-    my %lookup;
+    my $lookup := nqp::hash;
 
     method register(Encoding $enc --> Nil) {
-        my @names = ($enc.name, $enc.alternative-names).flat.map(*.fc);
         $lock.protect: {
-            if %lookup{@names}:k -> @bad {
-                X::Encoding::AlreadyRegistered.new(name => @bad[0]).throw;
-            }
-            %lookup{@names} = $enc xx *;
+            nqp::stmts(
+              nqp::if(
+                nqp::existskey($lookup,(my str $key = $enc.name.fc)),
+                X::Encoding::AlreadyRegistered.new(name => $enc.name).throw,
+                nqp::bindkey($lookup,$key,$enc)
+              ),
+              (my $names :=
+                nqp::getattr($enc.alternative-names,List,'$!reified')),
+              (my int $elems = nqp::elems($names)),
+              (my int $i = -1),
+              nqp::while(
+                nqp::islt_i(($i = nqp::add_i($i,1)),$elems),
+                nqp::if(
+                  nqp::existskey($lookup,($key = nqp::atpos($names,$i).fc)),
+                  X::Encoding::AlreadyRegistered.new(
+                    name => nqp::atpos($names,$i)).throw,
+                  nqp::bindkey($lookup,$key,$enc)
+                )
+              )
+            )
         }
     }
 
     method find(Str() $name) {
         $lock.protect: {
-            my $fname = $name.fc;
-            %lookup{$fname}:exists
-                ?? %lookup{$fname}
-                !! X::Encoding::Unknown.new(:$name).throw
+            nqp::ifnull(
+              nqp::atkey($lookup,$name.fc),
+              X::Encoding::Unknown.new(:$name).throw
+            )
         }
     }
 }

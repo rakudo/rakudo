a19fa494464e5a4b357eadad96a53cb5f5483683
Zoffix Znet <cpan@zoffix.com>
2017-10-04 17:57:40 +0000

    Improve error on Inf.base
    Fixes RT#125818: https://rt.perl.org/Ticket/Display.html?id=125818

--- src/core/Real.pm
+++ src/core/Real.pm
@@ -89,7 +89,7 @@ my role Real does Numeric {
                 :range<0..1073741824>
             ) if $digits.defined and $digits < 0;
         my $prec = $digits // 1e8.log($base.Num).Int;
-        my Int $int_part = self.Int;
+        my Int $int_part = self.Int.self; # .self blows up Failures
         my $frac = abs(self - $int_part);
         my @frac_digits;
         my @conversion := <0 1 2 3 4 5 6 7 8 9

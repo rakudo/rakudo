c73221225f623e1937d5bd5ddfb2d11ed100ef0e
Zoffix Znet <cpan@zoffix.com>
2017-10-05 15:49:07 +0000

    Move &EVAL/&EVALFILE further down the setting
    I need it to know about `Blob` and adding stubs into
    src/core/control.pm is unweildy because all the role
    mixins and the stubs for the roles need to be added as well

--- src/core/ForeignCode.pm
+++ src/core/ForeignCode.pm
@@ -18,4 +18,85 @@ my class ForeignCode does Callable { # declared in BOOTSTRAP
     multi method Str(ForeignCode:D:) { self.name }
 }
 
+my class Rakudo::Internals::EvalIdSource {
+    my Int $count = 0;
+    my Lock $lock = Lock.new;
+    method next-id() {
+        $lock.protect: { $count++ }
+    }
+}
+proto sub EVAL(Cool $code, Str() :$lang = 'perl6', PseudoStash :$context, *%n) {
+    # First look in compiler registry.
+    my $compiler := nqp::getcomp($lang);
+    if nqp::isnull($compiler) {
+        # Try a multi-dispatch to another EVAL candidate. If that fails to
+        # dispatch, map it to a typed exception.
+        CATCH {
+            when X::Multi::NoMatch {
+                X::Eval::NoSuchLang.new(:$lang).throw
+            }
+        }
+        return {*};
+    }
+    $context := CALLER:: unless nqp::defined($context);
+    my $eval_ctx := nqp::getattr(nqp::decont($context), PseudoStash, '$!ctx');
+    my $?FILES   := 'EVAL_' ~ Rakudo::Internals::EvalIdSource.next-id;
+    my \mast_frames := nqp::hash();
+    my $*CTXSAVE; # make sure we don't use the EVAL's MAIN context for the currently compiling compilation unit
+    my $compiled;
+    my $LANG := $context<%?LANG>;
+    if !$LANG {
+        $LANG := CALLERS::<%?LANG>;
+    }
+    if $LANG {
+        # XXX
+        my $grammar := $LANG<MAIN>;
+        my $actions := $LANG<MAIN-actions>;
+        $compiled := $compiler.compile(
+            $code.Stringy,
+            :outer_ctx($eval_ctx),
+            :global(GLOBAL),
+            :mast_frames(mast_frames),
+            :grammar($grammar),
+            :actions($actions),
+        );
+    }
+    else {
+        $compiled := $compiler.compile(
+            $code.Stringy,
+            :outer_ctx($eval_ctx),
+            :global(GLOBAL),
+            :mast_frames(mast_frames),
+        );
+    }
+    if $*W and $*W.is_precompilation_mode() { # we are still compiling
+        $*W.add_additional_frames(mast_frames);
+    }
+    nqp::forceouterctx(nqp::getattr($compiled, ForeignCode, '$!do'), $eval_ctx);
+    $compiled();
+}
+
+multi sub EVAL(Cool $code, Str :$lang where { ($lang // '') eq 'Perl5' }, PseudoStash :$context) {
+    my $eval_ctx := nqp::getattr(nqp::decont($context // CALLER::), PseudoStash, '$!ctx');
+    my $?FILES   := 'EVAL_' ~ (state $no)++;
+    state $p5;
+    unless $p5 {
+        {
+            my $compunit := $*REPO.need(CompUnit::DependencySpecification.new(:short-name<Inline::Perl5>));
+            GLOBAL.WHO.merge-symbols($compunit.handle.globalish-package);
+            CATCH {
+                #X::Eval::NoSuchLang.new(:$lang).throw;
+                note $_;
+            }
+        }
+        $p5 = ::("Inline::Perl5").default_perl5;
+    }
+    $p5.run($code);
+}
+
+proto sub EVALFILE($, *%) {*}
+multi sub EVALFILE($filename, :$lang = 'perl6') {
+    EVAL slurp($filename), :$lang, :context(CALLER::);
+}
+
 # vim: ft=perl6 expandtab sw=4

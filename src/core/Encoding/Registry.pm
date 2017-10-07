598fd9a763143b867c86471374e608f230d34caa
Elizabeth Mattijsen <liz@wenzperl.nl>
2017-10-07 22:32:52 +0200

    Set up Builtin encodings at setting compile time
    - so we don't need to do this each time on startup
    - only saves maybe 1 msec of startup
    - but it makes future optimizations opening STDIN/OUT/ERR easier

--- src/core/Encoding/Registry.pm
+++ src/core/Encoding/Registry.pm
@@ -3,7 +3,33 @@ my class X::Encoding::AlreadyRegistered { ... }
 
 my class Encoding::Registry {
     my $lock := Lock.new;
-    my $lookup := nqp::hash;
+    my %lookup;  # access for registering builtins at compile time
+    my $lookup := nqp::getattr(%lookup,Map,'$!storage');  # access for runtime
+
+    BEGIN {
+        my $lookup := nqp::bindattr(%lookup,Map,'$!storage',nqp::hash);
+        my $encodings := nqp::list(
+          nqp::list("ascii"),
+          nqp::list("iso-8859-1","iso_8859-1:1987","iso_8859-1","iso-ir-100",
+            "latin1","latin-1","csisolatin1","l1","ibm819","cp819"),
+          nqp::list("utf8","utf-8"),
+          nqp::list("utf8-c8","utf-8-c8"),
+          nqp::list("utf16","utf-16"),
+          nqp::list("utf32","utf-32"),
+          nqp::list("windows-1252")
+        );
+        my int $i = -1;
+        my int $elems = nqp::elems($encodings);
+        while nqp::islt_i(($i = nqp::add_i($i,1)),$elems) {
+            my $names := nqp::atpos($encodings,$i);
+            my $builtin := nqp::create(Encoding::Builtin).SET-SELF(
+              nqp::shift($names),nqp::clone($names));
+            nqp::bindkey($lookup,$builtin.name,$builtin);
+            while nqp::elems($names) {
+                nqp::bindkey($lookup,nqp::shift($names),$builtin);
+            }
+        }
+    }
 
     method register(Encoding $enc --> Nil) {
         $lock.protect: {

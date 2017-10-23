e4a5bb17c9b16a6e2b2e7ec973c09bc8918a41e0
Zoffix Znet <cpan@zoffix.com>
2017-10-23 17:32:47 +0000

    Improve/Fix Int.new
    - Don't use p6bindattrinvres with `bigint` attributes; it's
        questionable if it should work and fails on JVM (part of #1201)
    - Make Int.new: int candidate properly give a new object instead
        of a cached constant

--- src/core/Int.pm
+++ src/core/Int.pm
@@ -25,11 +25,14 @@ my class Int does Real { # declared in BOOTSTRAP
     }
 
     proto method new(|) {*}
-    multi method new(      \value) { self.new: value.Int         }
-    multi method new(int   \value) { nqp::box_i(value,self.WHAT) }
+    multi method new(      \value) { self.new: value.Int }
+    multi method new(int   \value) {
+        # rebox the value, so we get rid of any potential mixins
+        nqp::div_I(nqp::decont(value), 1, self)
+    }
     multi method new(Int:D \value = 0) {
-        nqp::p6bindattrinvres(self.bless, Int,'$!value',
-          nqp::getattr(nqp::decont(value),Int,'$!value'))
+        # rebox the value, so we get rid of any potential mixins
+        nqp::div_I(nqp::decont(value), 1, self)
     }
 
     multi method perl(Int:D:) {

273168d7b2fa6afad99a194deaa6985eb5936a50
Elizabeth Mattijsen <liz@wenzperl.nl>
2017-10-29 00:18:07 +0200

    Usage -> Telemetry + more changes
    - now also includes wallclock information
    - Telemetry class is just a class with "standard" methods
      - cpu, cpu-user, cpu-sys, wallclock
      - can not be created with parameters, .new snapshots current state
    - Telemetry::Period is just a class with the same standard methods
      - same methods as Telemetry
      - but *can* be created with given values for cpu-user,cpu-sys,wallclock
    - Telemetry.Period is a special method which:
      - stores new Telemetry object if called on a containerized type object
      - snapshots the current state again on instantiated object
      - returns a Telemetry::Period object with the difference
    - added .gist/.Str/.perl methods for convenience

---
+++ src/core/Telemetry.pm
@@ -0,0 +1,122 @@
+# An attempt at providing an API to nqp::getrusage.
+
+class Telemetry::Period { ... }
+
+class Telemetry {
+    has int $!cpu-user;
+    has int $!cpu-sys;
+    has int $!wallclock;
+
+    multi method new(Telemetry:) { nqp::create(self).SET-SELF }
+
+    method SET-SELF() {
+        my \rusage = nqp::getrusage;
+        $!cpu-user = nqp::atpos_i(rusage, nqp::const::RUSAGE_UTIME_SEC) * 1000000
+          + nqp::atpos_i(rusage, nqp::const::RUSAGE_UTIME_MSEC);
+        $!cpu-sys  = nqp::atpos_i(rusage, nqp::const::RUSAGE_STIME_SEC) * 1000000
+          + nqp::atpos_i(rusage, nqp::const::RUSAGE_STIME_MSEC);
+        $!wallclock = nqp::fromnum_I(1000000 * nqp::time_n,Int);
+        self
+    }
+
+    proto method cpu() { * }
+    multi method cpu(Telemetry:U:) is raw {
+        my \rusage = nqp::getrusage;
+        nqp::atpos_i(rusage, nqp::const::RUSAGE_UTIME_SEC) * 1000000
+          + nqp::atpos_i(rusage, nqp::const::RUSAGE_UTIME_MSEC)
+          + nqp::atpos_i(rusage, nqp::const::RUSAGE_STIME_SEC) * 1000000
+          + nqp::atpos_i(rusage, nqp::const::RUSAGE_STIME_MSEC)
+    }
+    multi method cpu(Telemetry:D:) is raw {
+        nqp::add_i($!cpu-user,$!cpu-sys)
+    }
+
+    proto method cpu-user() { * }
+    multi method cpu-user(Telemetry:U:) is raw {
+        my \rusage = nqp::getrusage;
+        nqp::atpos_i(rusage, nqp::const::RUSAGE_UTIME_SEC) * 1000000
+          + nqp::atpos_i(rusage, nqp::const::RUSAGE_UTIME_MSEC)
+    }
+    multi method cpu-user(Telemetry:D:) is raw { $!cpu-user }
+
+    proto method cpu-sys() { * }
+    multi method cpu-sys(Telemetry:U:) is raw {
+        my \rusage = nqp::getrusage;
+        nqp::atpos_i(rusage, nqp::const::RUSAGE_STIME_SEC) * 1000000
+          + nqp::atpos_i(rusage, nqp::const::RUSAGE_STIME_MSEC)
+    }
+    multi method cpu-sys(Telemetry:D:) is raw { $!cpu-sys }
+
+    proto method wallclock() { * }
+    multi method wallclock(Telemetry:U:) is raw {
+        nqp::fromnum_I(1000000 * nqp::time_n,Int)
+    }
+    multi method wallclock(Telemetry:D:) is raw { $!wallclock }
+
+    proto method Period() { * }
+    multi method Period(Telemetry:U \SELF:) is raw {
+        if nqp::iscont(SELF) {
+            SELF = SELF.new;
+            nqp::create(Telemetry::Period)
+        }
+        else {
+            die "Must use container of type Telemetry"
+        }
+    }
+    multi method Period(Telemetry:D:) is raw {
+        my int $cpu-user  = $!cpu-user;
+        my int $cpu-sys   = $!cpu-sys;
+        my int $wallclock = $!wallclock;
+        self.SET-SELF;
+
+        Telemetry::Period.new(
+          nqp::sub_i($!cpu-user,$cpu-user),
+          nqp::sub_i($!cpu-sys,$cpu-sys),
+          nqp::sub_i($!wallclock,$wallclock)
+        )
+    }
+
+    multi method Str(Telemetry:D:) {
+        $!wallclock ?? "$.cpu / $!wallclock" !! "cpu / wallclock"
+    }
+    multi method gist(Telemetry:D:) {
+        $!wallclock ?? "$.cpu / $!wallclock" !! "cpu / wallclock"
+    }
+}
+
+class Telemetry::Period is Telemetry {
+    multi method new(Telemetry::Period:
+      int :$cpu-user,
+      int :$cpu-sys,
+      int :$wallclock
+    ) {
+        self.new($cpu-user, $cpu-sys, $wallclock)
+    }
+    multi method new(Telemetry::Period:
+      int $cpu-user,
+      int $cpu-sys,
+      int $wallclock
+    ) {
+        my $period := nqp::create(Telemetry::Period);
+        nqp::bindattr_i($period,Telemetry,'$!cpu-user', $cpu-user);
+        nqp::bindattr_i($period,Telemetry,'$!cpu-sys',  $cpu-sys);
+        nqp::bindattr_i($period,Telemetry,'$!wallclock',$wallclock);
+        $period
+    }
+
+    multi method perl(Telemetry::Period:) {
+        "Telemetry::Period.new(:cpu-user({
+          nqp::getattr_i(self,Telemetry,'$!cpu-user')
+        }), :cpu-sys({
+          nqp::getattr_i(self,Telemetry,'$!cpu-sys')
+        }), :wallclock({
+          nqp::getattr_i(self,Telemetry,'$!wallclock')
+        }))"
+    }
+}
+
+multi sub infix:<->(Telemetry $a, Telemetry $b) {
+    $a.cpu - $b.cpu
+}
+
+# vim: ft=perl6 expandtab sw=4

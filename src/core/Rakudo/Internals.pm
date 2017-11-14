fff43fd709cd0ed9693a85d06242f0fcd9f55ba3
Elizabeth Mattijsen <liz@wenzperl.nl>
2017-11-14 22:16:52 +0100

    Oops, fix typo in message, MasterDuke17++

--- src/core/Rakudo/Internals.pm
+++ src/core/Rakudo/Internals.pm
@@ -1551,7 +1551,7 @@ my constant $?BITS = nqp::isgt_i(nqp::add_i(2147483648, 1), 0) ?? 64 !! 32;
                     )(IO::Handle);
 #?endif
                     if @exceptions {
-                        note "Some exceptions where thrown in END blocks:";
+                        note "Some exceptions were thrown in END blocks:";
                         note "  $_.^name(): $_.message()" for @exceptions;
                     }
                     nqp::not_i(($the-end-is-done = 1)); # we're really done now

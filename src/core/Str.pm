2a8287cf89966c4cd77e6889706a7abac170da01
Zoffix Znet <cpan@zoffix.com>
2017-12-21 19:28:05 -0500

    Rename Str.parse-names to Str.uniparse
    The original name was chosen to align with `.parse-base` that parses out
    base-X numbers out strings. However, we have a whole block of more closely
    related routines all named in `.uni*` format, so it makes sense for this
    routine's naming to align with those. They are: `infix:<unicmp>`,
    `unimatch`, `uniname`, `uninames`, `unival`, `univals`, `uniprop`,
    `uniprop-bool`, `uniprop-int`, `uniprop-str`, `uniprops`.
    
    Refs:
    https://github.com/perl6/6.d-prep/commit/efa24f68eddc8091a4d5da41bfeeeb4
    https://irclog.perlgeek.de/perl6-dev/2017-12-21#i_15609653

--- src/core/Str.pm
+++ src/core/Str.pm
@@ -1410,24 +1410,6 @@ my class Str does Stringy { # declared in BOOTSTRAP
         }
     }
 
-    method parse-names(Str:D:) {
-        my     \names := nqp::split(',', self);
-        my int $elems  = nqp::elems(names);
-        my int $i      = -1;
-        my str $res    = '';
-        nqp::while(
-            nqp::islt_i( ($i = nqp::add_i($i,1)), $elems ),
-            ($res = nqp::concat($res,
-                nqp::unless(
-                    nqp::getstrfromname(nqp::atpos(names, $i).trim),
-                    X::Str::InvalidCharName.new(
-                        :name(nqp::atpos(names, $i).trim)
-                    ).fail
-            ))),
-        );
-        $res
-    }
-
     multi method split(Str:D: Regex:D $pat, $limit is copy = Inf;;
       :$v is copy, :$k, :$kv, :$p, :$skip-empty) {
 
@@ -2607,6 +2589,29 @@ my class Str does Stringy { # declared in BOOTSTRAP
             LSM.new(self,$substitutions,$squash,$complement).result;
         }
     }
+
+    method parse-names(Str:D:) {
+        # XXX TODO: issue deprecation warning in 6.d; remove in 6.e
+        self.uniparse
+    }
+    method uniparse(Str:D:) {
+        my     \names := nqp::split(',', self);
+        my int $elems  = nqp::elems(names);
+        my int $i      = -1;
+        my str $res    = '';
+        nqp::while(
+            nqp::islt_i( ($i = nqp::add_i($i,1)), $elems ),
+            ($res = nqp::concat($res,
+                nqp::unless(
+                    nqp::getstrfromname(nqp::atpos(names, $i).trim),
+                    X::Str::InvalidCharName.new(
+                        :name(nqp::atpos(names, $i).trim)
+                    ).fail
+            ))),
+        );
+        $res
+    }
+
     proto method indent($) {*}
     # Zero indent does nothing
     multi method indent(Int() $steps where { $_ == 0 }) {
@@ -3020,8 +3025,6 @@ sub chrs(*@c --> Str:D) {
 proto sub parse-base(|) {*}
 multi sub parse-base(Str:D $str, Int:D $radix) { $str.parse-base($radix) }
 
-sub parse-names(Str:D $str) { $str.parse-names }
-
 proto sub substr(|) {*}
 multi sub substr(Str:D \what, Int:D \start) {
     my str $str  = nqp::unbox_s(what);
@@ -3110,5 +3113,11 @@ multi sub infix:<eqv>(Str:D \a, Str:D \b) {
 
 proto sub samemark(|) {*}
 multi sub samemark($s, $pat) { $s.samemark($pat) }
+
+sub parse-names(Str:D \names) {
+    # XXX TODO: issue deprecation warning in 6.d; remove in 6.e
+    names.uniparse
+}
+sub uniparse (Str:D \names) { names.uniparse }
 
 # vim: ft=perl6 expandtab sw=4

d8ef93f232f68d5d2679c6171c13e8c1bf72d2b3
Jeremy Studer <studerj1.mail@gmail.com>
2018-03-18 14:39:30 -0400

    [p6stateinit] Move initial declaration from loop
    Not supported in C89, which is the standard MSVC supports.

--- src/vm/moar/ops/perl6_ops.c
+++ src/vm/moar/ops/perl6_ops.c
@@ -476,7 +476,8 @@ static void p6stateinit(MVMThreadContext *tc, MVMuint8 *cur_op) {
     /* Find num of lexical, so that we can mark it as HLL inited */
     MVMObject   *var    = GET_REG(tc, 2).o;
     MVMCodeBody *crbody = &((MVMCode *)tc->cur_frame->code_ref)->body;
-    for (MVMuint32 i = 0; i < tc->cur_frame->static_info->body.num_lexicals; i++) {
+    MVMuint32 i;
+    for (i = 0; i < tc->cur_frame->static_info->body.num_lexicals; i++) {
         MVMRegister *env_val     = &tc->cur_frame->env[i];
         MVMuint8    *is_hll_init = &crbody->state_vars_is_hll_init[i];
         if (env_val && var == env_val->o) {

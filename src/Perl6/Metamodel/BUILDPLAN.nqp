rakudo/rakudo

commit 39227d5456723bcfc2eae526716932e19afda9be

Make build of BOOTSTRAP.moarvm reproducible
The build plan ends up in the compiled code and therefor should not depend
on random hash order.
Many thanks to timotimo++ for helping uncover this.

src/Perl6/Metamodel/BUILDPLAN.nqp
@@ -113,8 +113,10 @@ role Perl6::Metamodel::BUILDPLAN {
         }
 
         # Add vivify instructions.
-        for %attrs_untouched {
-            nqp::push(@plan,[10, $obj, $_.key]);
+        for @attrs { # iterate over the array to get a consistent order
+            if %attrs_untouched{$_.name} {
+                nqp::push(@plan,[10, $obj, $_.name]);
+            }
         }
 
         # Does it have a TWEAK?

commit 56c03572a0e1041ae087a8d9f3474b697833a654

Fix brown paper bag regression by commit 39227d5456723bcfc2eae526716932e19afda9be
The hash is used as a set without values, so an existskey is the right check.

src/Perl6/Metamodel/BUILDPLAN.nqp
@@ -114,7 +114,7 @@ role Perl6::Metamodel::BUILDPLAN {
 
         # Add vivify instructions.
         for @attrs { # iterate over the array to get a consistent order
-            if %attrs_untouched{$_.name} {
+            if nqp::existskey(%attrs_untouched, $_.name) {
                 nqp::push(@plan,[10, $obj, $_.name]);
             }
         }
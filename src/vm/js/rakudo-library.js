f460ad1a9c88e3fc332b64af28ffd74fade434cb
Paweł Murias <pawelmurias@gmail.com>
2018-11-01 18:19:58 +0100

    [js] Fix indentation

--- src/vm/js/rakudo-library.js
+++ src/vm/js/rakudo-library.js
@@ -14,10 +14,10 @@ nqp.run = function(code) {
 let passedArgs;
 
 nqp.args = function(calledFrom) {
-    if (calledFrom.parent === module) {
-        return passedArgs.map(arg => new nqp.NativeStrArg(arg));
-    }
-    return oldArgs(calledFrom);
+  if (calledFrom.parent === module) {
+      return passedArgs.map(arg => new nqp.NativeStrArg(arg));
+  }
+  return oldArgs(calledFrom);
 };
 
 const code = require('./rakudo.js');
@@ -25,32 +25,32 @@ const code = require('./rakudo.js');
 const core = require('nqp-runtime/core.js');
 
 module.exports = function(source) {
-    const tmpFile = tmp.tmpNameSync();
+  const tmpFile = tmp.tmpNameSync();
 
-    passedArgs = ['perl6-js', '--output', tmpFile, '--target=js', source];
+  passedArgs = ['perl6-js', '--output', tmpFile, '--target=js', source];
 
-    const oldWritefh = nqp.op.getstdout().constructor.prototype.$$writefh;
-    const output = [];
-    nqp.op.getstdout().constructor.prototype.$$writefh = function(buf) {
-      output.push(core.toRawBuffer(buf));
+  const oldWritefh = nqp.op.getstdout().constructor.prototype.$$writefh;
+  const output = [];
+  nqp.op.getstdout().constructor.prototype.$$writefh = function(buf) {
+    output.push(core.toRawBuffer(buf));
+  }
+
+  code();
+
+  nqp.op.getstdout().constructor.prototype.$$writefh = oldWritefh;
+  const lines = Buffer.concat(output).toString().split(/\n/);
+
+  const loaded = [];
+
+  for (const line of lines) {
+    let match;
+    if (/^[A-Z0-9]{40}\0/.test(line)) {
+    } else if (match = line.match(/^load-unit: (.+)/)) {
+      loaded.push(match[1]);
+    } else {
     }
-
-    code();
-
-    nqp.op.getstdout().constructor.prototype.$$writefh = oldWritefh;
-    const lines = Buffer.concat(output).toString().split(/\n/);
-
-    const loaded = [];
-
-    for (const line of lines) {
-      let match;
-      if (/^[A-Z0-9]{40}\0/.test(line)) {
-      } else if (match = line.match(/^load-unit: (.+)/)) {
-        loaded.push(match[1]);
-      } else {
-      }
-    }
+  }
 
 
-    return {js: fs.readFileSync(tmpFile, 'utf8'), loaded: loaded};
+  return {js: fs.readFileSync(tmpFile, 'utf8'), loaded: loaded};
 };

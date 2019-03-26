cdd2a7fdbd89f5bdb7817f3020b388bb3ad1ddf8
Vadim Belman <vrurg@lflat.org>
2019-03-26 15:24:04 -0400

    Started filling in with macros

--- tools/lib/NQP/Configure/Macros.pm
+++ tools/lib/NQP/Configure/Macros.pm
@@ -2,6 +2,7 @@
 package NQP::Configure::Macros;
 use v5.10;
 use NQP::Versions;
+use File::Spec;
 
 sub new {
     my $class = shift;
@@ -34,6 +35,9 @@ sub execute {
 sub expand {
     my $self = shift;
     my $text = shift;
+
+    return $text if index($text, '@') < 0;
+
     my %params = @_;
 
     my $config = $self->{config};
@@ -90,15 +94,64 @@ sub expand {
     return $text_out;
 }
 
-sub _m_test {
-    #my $self = shift;
-    return "*this is a test* ($_[1])";
+sub split_spaced {
+    my $self = shift;
+
+    return split /(?<!\\)\s/s, shift;
 }
 
+# include(file1 file2)
+# Include a file. Parameter is expanded first, then the result is used a the
+# file name. File content is been expanded.
+# Multiple filenames are split by spaces. If file path contains a space in it it
+# must be quoted with \
+sub _m_include {
+    my $self = shift;
+    my @filenames = $self->split_spaced( $self->expand(shift) );
+    my $text = "";
+
+    for my $file ( map { $self->_m_unescape($_) } @filenames ) {
+        next unless $file; # Split may result in some empty items.
+        die "Circular dependency detected on including $file" 
+            if $self->{'-including'}{$file};
+        $self->{'-including'}{$file} = 1;
+        $text .= "\n## Included from $file\n";
+        $text .= $self->expand( NQP::Configure::slurp( $file ) );
+        $text .= "\n## End of included $file\n"; 
+        delete $self->{'-including'}{$file};
+    }
+    return $text;
+}
+
+# sp_escape(a string)
+# Escapes all spaces in a string with \
+# Implicitly called by @@ macros
 sub _m_sp_escape {
-    my $str = $_[1];
+    my $self = shift;
+    my $str = self->expand($_[1]);
     $str =~ s{ }{\\ }g;
     $str;
+}
+
+# unescape(a\ st\ring)
+# Simlpe unescaping from backslashes. Replaces any \<char> sequence with <char>
+sub _m_unescape {
+    my $self = shift;
+    my $str = shift;
+    $str =~ s/\\(.)/$1/g;
+    return $str;
+}
+
+# nfs(dir/file)
+# Normalizes a Unix-style file name for the current OS.
+sub _m_nfn {
+    my $self = shift;
+    my ($vol, $dirs, $file) = File::Spec->splitpath(shift);
+    return File::Spec->catpath(
+        $vol,
+        File::Spec->catdir( File::Spec->splitdir( $dirs ) ),
+        $file
+    );
 }
 
 1;

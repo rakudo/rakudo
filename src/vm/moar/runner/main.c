829f4ccbac07efe1d0ad4ab508643fb3643c51fc
Daniel Green <ddgreen@gmail.com>
2020-01-03 16:07:18 -0500

    Variables should also be set under Windows

--- src/vm/moar/runner/main.c
+++ src/vm/moar/runner/main.c
@@ -281,7 +281,7 @@ int wmain(int argc, wchar_t *wargv[])
     /* The +1 is the trailing \0 terminating the string. */
     exec_dir_path_temp = (char*)malloc(exec_path_size + 1);
     memcpy(exec_dir_path_temp, exec_path, exec_path_size + 1);
-#if !(defined(STATIC_NQP_HOME) && defined(STATIC_RAKUDO_HOME))
+#if !(defined(STATIC_NQP_HOME) && defined(STATIC_RAKUDO_HOME)) || defined(_WIN32)
 #ifdef _WIN32
     PathRemoveFileSpecA(exec_dir_path_temp);
     exec_dir_path_size = strlen(exec_dir_path_temp);

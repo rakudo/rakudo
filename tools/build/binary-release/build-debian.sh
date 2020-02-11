8c995febda33deab614917517741f04c929a8a68
Patrick Böker <patzim@users.noreply.github.com>
2020-02-11 19:14:12 +0100

    Add a binary release build script that works on Debian
    This can / will be used when building for ARM architectures. The decision
    fell on Debian 9, as it is the distro with the oldest glibc, while being
    mainstream and still supported on armhf / arm64 architecture.

---
+++ tools/build/binary-release/build-debian.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+# This script should be run in a debian installation (a container will do
+# just fine).
+
+set -o errexit
+set -o pipefail
+
+# Update Debian
+apt-get update
+
+# Install dependencies
+apt-get install -y curl git perl gcc make
+
+# Download release file
+curl -o rakudo.tgz $RELEASE_URL
+tar -xzf rakudo.tgz
+cd rakudo-*
+
+# Build Rakudo
+perl Configure.pl --gen-moar --gen-nqp --backends=moar --moar-option='--toolchain=gnu' --relocatable
+make
+make install
+
+# Test the build
+make test
+
+# Build Zef
+git clone https://github.com/ugexe/zef.git
+pushd zef
+../install/bin/raku -I. bin/zef install .
+popd
+
+# Prepare the package
+cp -r tools/build/binary-release/Linux/* install
+cp LICENSE install
+mv install rakudo-$VERSION
+tar -zcv --owner=0 --group=0 --numeric-owner -f ../rakudo-linux.tar.gz rakudo-$VERSION
+

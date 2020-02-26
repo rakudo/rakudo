rakudo/rakudo

commit 8ac2eecc239c1c9c7eba71becf68e8e7e4087cd4

De-hllize the loaded cache

src/core.c/CompUnit/PrecompilationStore/File.pm6
@@ -119,7 +119,7 @@ class CompUnit::PrecompilationStore::File
     has IO::Handle $!lock;
     has int $!wont-lock;
     has int $!lock-count;
-    has %!loaded;
+    has $!loaded;
     has %!compiler-cache;
     has %!dir-cache;
     has Lock $!update-lock;
@@ -129,6 +129,7 @@ class CompUnit::PrecompilationStore::File
         if $*W -> $World {
             $!wont-lock = 1 if $World.is_precompilation_mode;
         }
+        $!loaded := nqp::hash;
     }
 
     method new-unit(|c) {
@@ -186,12 +187,18 @@ class CompUnit::PrecompilationStore::File
       CompUnit::PrecompilationId:D $precomp-id
     ) {
         $!update-lock.protect: {
-            %!loaded{$precomp-id} //= do {
-                my $path := self.path($compiler-id, $precomp-id);
-                $path.e
-                    ?? CompUnit::PrecompilationUnit::File.new(:id($precomp-id), :$path, :store(self))
-                    !! Nil
-            }
+            my str $key = $precomp-id.Str;
+            nqp::ifnull(
+              nqp::atkey($!loaded,$key),
+              nqp::bindkey($!loaded,$key,
+                do {
+                    my $path := self.path($compiler-id, $precomp-id);
+                    $path.e
+                      ?? CompUnit::PrecompilationUnit::File.new(:id($precomp-id), :$path, :store(self))
+                      !! Nil
+                }
+              )
+            )
         }
     }
 
@@ -207,7 +214,7 @@ class CompUnit::PrecompilationStore::File
 
     method remove-from-cache(CompUnit::PrecompilationId:D $precomp-id) {
         $!update-lock.protect: {
-            %!loaded{$precomp-id}:delete
+            nqp::deletekey($!loaded,$precomp-id.Str);
         }
     }
 

commit 6c7ffbdbbf967cbff78a46589e58a18be57b3341

Potentially fix problem
nine++ reported that 8ac2eecc239c1c9c7eba71becf68e8e7e4087cd4 broke
t/04-nativecall/00-misc.t , which I cannot reproduce.  However,
that commit did change the semantics slightly: instead of an Any,
a Nil would be returned for a non-existing path.  But since the
check used //= , any subsequent attemopt would run the same code
again, as //= does not differentiate between Nil and Any.

This patch does not bind to the loaded hash if the path does not
exist, effectively still having the same semantics as a non-existing
key returns nqp::null.

src/core.c/CompUnit/PrecompilationStore/File.pm6
@@ -202,14 +202,14 @@ class CompUnit::PrecompilationStore::File
             my str $key = $precomp-id.Str;
             nqp::ifnull(
               nqp::atkey($!loaded,$key),
-              nqp::bindkey($!loaded,$key,
-                do {
-                    my $path := self.path($compiler-id, $precomp-id);
-                    $path.e
-                      ?? CompUnit::PrecompilationUnit::File.new(:id($precomp-id), :$path, :store(self))
-                      !! Nil
-                }
-              )
+              do {
+                  my $path := self.path($compiler-id, $precomp-id);
+                  $path.e
+                    ?? nqp::bindkey($!loaded,$key,
+                         CompUnit::PrecompilationUnit::File.new(
+                           :id($precomp-id), :$path, :store(self)))
+                    !! Nil
+              }
             )
         }
     }
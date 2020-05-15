rakudo/rakudo

commit a0e58412a04253ce0321a30587eda6eb75db2b30

Make "42".Numeric about 4.3x as fast
- no change for "+42"
- "42.35" about 10% slower

The speed increase is caused by (ab)using the implicit conversion
when assigning a native string to a native int if the string consists
of only numeric characters and have 18 or fewer characters.

src/core.c/Str.pm6
@@ -773,21 +773,44 @@ my class Str does Stringy { # declared in BOOTSTRAP
           !! self
     }
 
+    method !combiners() {
+        Failure.new(
+          X::Str::Numeric.new(
+            source => self,
+            pos    => 1,
+            reason => "combining codepoints found at"
+         )
+        )
+    }
     multi method Numeric(Str:D: --> Numeric:D) {
-        nqp::chars(self)
-          ?? nqp::index(self,'.') == -1    # is there no period?
-               && nqp::atpos(              # no period, so parse as Int
-                    (my $n := nqp::radix_I(10,self,0,0b10,Int)),
-                    2
-               ) == nqp::chars(self)
-            ?? nqp::atpos($n,0)            # fast path Int ok
-            !! nqp::findnotcclass(         # any non-whitespace?
+#?if !jvm
+        # check for any combining characters
+        nqp::isne_i(nqp::chars(self),nqp::codes(self))
+          ?? self!combiners
+          !!
+#?endif
+#?if jvm
+            # https://github.com/Raku/old-issue-tracker/issues/5418
+            # Needs Str.codes impl that doesn't just return chars
+#?endif
+        nqp::iseq_i(                              # all numeric?
+          nqp::findnotcclass(
+            nqp::const::CCLASS_NUMERIC,self,0,nqp::chars(self)),
+          nqp::chars(self)
+        ) ?? nqp::isle_i(nqp::chars($!value),18)  # upto 18 digits can be native
+            ?? (my int $ = $!value)               # quick conversion, "" also ok
+            !! nqp::atpos(nqp::radix_I(10,self,0,0b10,Int),0)
+          !! nqp::atpos(                          # no period, so parse as Int
+               (my $n := nqp::radix_I(10,self,0,0b10,Int)),
+               2
+             ) == nqp::chars(self)
+            ?? nqp::atpos($n,0)                   # fast path Int ok
+            !! nqp::findnotcclass(                # any non-whitespace?
                  nqp::const::CCLASS_WHITESPACE,
                  self,0,nqp::chars(self)
                ) == nqp::chars(self)
-              ?? 0                         # just spaces
-              !! val(self, :val-or-fail)   # take the slow route
-          !! 0                             # empty string
+              ?? 0                                # just spaces
+              !! val(self, :val-or-fail)          # take the slow route
     }
 
     multi method gist(Str:D:) { self }

commit c2e8b22f7b55d0a5b419056e846d75c22eaf424b

Fix "١٢٣".Numeric and "١٢٣".Int
- a0e5841 broke this, apparently MoarVM only uses 0..9 for implicit conversion
- makes "123".Numeric.Int about 1.7x **slower**
- perhaps we need a CCLASS_DECIMAL class to quickly check for 0..9
  This would allow the speedup to be brought back again
- fixes R#3694

src/core.c/Str.pm6
@@ -784,9 +784,9 @@ my class Str does Stringy { # declared in BOOTSTRAP
             nqp::const::CCLASS_NUMERIC,self,0,nqp::chars(self)),
           nqp::chars(self)
         ) ?? nqp::isle_i(nqp::chars($!value),18)  # upto 18 digits can be native
-            ?? (my int $ = $!value)               # quick conversion, "" also ok
-            !! nqp::atpos(nqp::radix_I(10,self,0,0b10,Int),0)
-          !! nqp::atpos(                          # no period, so parse as Int
+            ?? nqp::atpos(nqp::radix(10,self,0,0),0) # quick conversion, "" also
+            !! nqp::atpos(nqp::radix_I(10,self,0,0,Int),0)
+          !! nqp::atpos(                          # try parsing as an Int
                (my $n := nqp::radix_I(10,self,0,0b10,Int)),
                2
              ) == nqp::chars(self)
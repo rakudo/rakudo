rakudo/rakudo

commit 88d98225a4df61473ff5b935561719c56b04d1c2

Normalizing IO::Handle.say/put/print/note, part 2
The &say sub now performs all of the .gist + newline logic and then
dispatches to .print.  The advantage of this is that we do not need
any additional logic / checks for modules that pretend to be $*OUT.

This also removes the IO::Handle.say(Str:D) candidate: even though a
Str may be passed to IO::Handle.say, it should *still* call the .gist
method on it, as it can have a non noop .gist method mixed in (roast
actually tests for this in the &sub case).

This commit also creates a single proto say for all of Rakudo.

This commit also adds Mu.say candidates to take care of those classes
that pretend to do the $*OUT API and which do not provide their own
.say method.

Finally, this adds two Rakudo::Internals methods for quickly generating
a native str array from a given list, which each element .gist or .Str'd.

src/core.c/Mu.pm6
@@ -670,8 +670,36 @@ Perhaps it can be found at https://docs.raku.org/type/$name"
 
     method item(Mu \item:) is raw { item }
 
-    # basic output operations
-    method say()   { say(self)   }
+    proto method say(|) {*}
+
+    # Handle the typical "foo.say"
+    multi method say() {
+
+        # no own print method, so use $*OUT.print
+        if nqp::eqaddr(self.^find_method("print").package,Mu) {
+            $_ := $*OUT;
+            .print(nqp::concat(self.gist,.nl-out))
+        }
+
+        # has its own .print, let it handle the empty case
+        else {
+            self.print(self.nl-out)
+        }
+    }
+
+    # Fallback for classes that act as $*OUT / $*ERR, but which do not have
+    # a .say method themselves.
+    multi method say(\x) {
+        self.print: nqp::concat(x.gist,self.nl-out)
+    }
+    multi method say(|) {
+        my \args := nqp::p6argvmarray;
+        nqp::shift(args);  # lose self
+        my $parts := Rakudo::Internals.GistList2list_s(args);
+        nqp::push_s($parts,self.nl-out);
+        self.print: nqp::join("",$parts)
+    }
+
     method print() { print(self) }
     method put()   { put(self)   }
     method note()  { note(self)  }

src/core.c/IO/CatHandle.pm6
@@ -417,7 +417,6 @@ my class IO::CatHandle is IO::Handle {
     multi method print-nl   (|) { X::NYI.new(:feature<print-nl>).throw   }
     proto method put        (|) {*}
     multi method put        (|) { X::NYI.new(:feature<put>).throw        }
-    proto method say        (|) {*}
     multi method say        (|) { X::NYI.new(:feature<say>).throw        }
     proto method write      (|) {*}
     multi method write      (|) { X::NYI.new(:feature<write>).throw      }

src/core.c/IO/Handle.pm6
@@ -684,11 +684,6 @@ my class IO::Handle {
           ?? self.WRITE($!encoder.encode-chars($!nl-out))
           !! X::IO::BinaryMode.new(:trying<say>).throw
     }
-    multi method say(IO::Handle:D: Str:D $x --> True) {
-        $!decoder
-          ?? self.WRITE($!encoder.encode-chars(nqp::concat($x,$!nl-out)))
-          !! X::IO::BinaryMode.new(:trying<say>).throw
-    }
     multi method say(IO::Handle:D: \x --> True) {
         $!decoder
           ?? self.WRITE($!encoder.encode-chars(nqp::concat(x.gist,$!nl-out)))
@@ -698,11 +693,7 @@ my class IO::Handle {
         if $!decoder {
             my Mu $args := nqp::p6argvmarray;
             nqp::shift($args);
-            my $parts := nqp::list_s;
-            nqp::while(
-              nqp::elems($args),
-              nqp::push_s($parts,nqp::shift($args).gist)
-            );
+            my $parts := Rakudo::Internals.GistList2list_s($args);
             nqp::push_s($parts,$!nl-out);
             self.WRITE($!encoder.encode-chars(nqp::join("",$parts)))
         }

src/core.c/Rakudo/Internals.pm6
@@ -85,6 +85,26 @@ my class Rakudo::Internals {
         ($min, $max);
     }
 
+    method GistList2list_s(List:D \list) {
+        my \values := nqp::getattr(list,List,'$!reified');
+        my \parts  := nqp::list_s;
+        nqp::while(
+          nqp::elems(values),
+          nqp::push_s(parts,nqp::shift(values).gist)
+        );
+        parts
+    }
+
+    method StrList2list_s(List:D \list) {
+        my \values := nqp::getattr(list,List,'$!reified');
+        my \parts  := nqp::list_s;
+        nqp::while(
+          nqp::elems(values),
+          nqp::push_s(parts,nqp::shift(values).Str)
+        );
+        parts
+    }
+
     method SET_LEADING_DOCS($obj, $docs) {
         my $current_why := $obj.WHY;
 

src/core.c/io_operators.pm6
@@ -7,23 +7,23 @@ multi sub print(Str:D \x)       { $*OUT.print(x) }
 multi sub print(\x)             { $*OUT.print(x.Str) }
 multi sub print(**@args is raw) { $*OUT.print: @args.join }
 
+# To ensure that classes that mimic the $*OUT / $*ERR API (which are only
+# required to provide a ".print" method), all logic is done in the subs
+# here, and then passed on to the .print method.
 proto sub say(|) {*}
-multi sub say() { $*OUT.print-nl }
-multi sub say(\x) {
-    nqp::if(
-      nqp::istype((my $out := $*OUT),IO::Handle),
-      $out.say(x.gist),
-      $out.print(nqp::concat(nqp::unbox_s(x.gist),$out.nl-out))
-    )
+multi sub say() {
+    $_ := $*OUT;
+    .print: .nl-out
 }
-multi sub say(**@args is raw) {
-    my str $str;
-    my $iter := @args.iterator;
-    nqp::until(
-      nqp::eqaddr(($_ := $iter.pull-one), IterationEnd),
-      $str = nqp::concat($str, nqp::unbox_s(.gist)));
-    my $out := $*OUT;
-    $out.print(nqp::concat($str,$out.nl-out));
+multi sub say(\x) {
+    $_ := $*OUT;
+    .print: nqp::concat(x.gist,.nl-out)
+}
+multi sub say(|) {
+    my $parts := Rakudo::Internals.GistList2list_s(nqp::p6argvmarray);
+    $_ := $*OUT;
+    nqp::push_s($parts,.nl-out);
+    .print: nqp::join("",$parts)
 }
 
 proto sub put(|) {*}

commit 9f546ef6f91d642e9aa5e9b79f207b12bae49e20

Allow Mu.say/put/note handling objects with NQP methods
The Match object has a "print" method that lives in NQP.  Calling .say
(and .put and .note) on a Match object was broken with 88d98225a4df61 .
This commit fixes that.

src/core.c/Mu.pm6
@@ -684,8 +684,9 @@ Perhaps it can be found at https://docs.raku.org/type/$name"
     # Handle the typical "foo.say"
     multi method say() {
 
-        # no own print method, so use $*OUT.print
-        if nqp::eqaddr(self.^find_method("print").package,Mu) {
+        my $method := self.^find_method("print");
+        if nqp::not_i(nqp::istype($method,Mu))  # an NQP routine
+          || nqp::eqaddr($method.package,Mu) {  # no own print method, use $*OUT
             $_ := $*OUT;
             .print(nqp::concat(self.gist,.nl-out))
         }
@@ -712,8 +713,9 @@ Perhaps it can be found at https://docs.raku.org/type/$name"
     # Handle the typical "foo.put"
     multi method put() {
 
-        # no own print method, so use $*OUT.print
-        if nqp::eqaddr(self.^find_method("print").package,Mu) {
+        my $method := self.^find_method("print");
+        if nqp::not_i(nqp::istype($method,Mu))  # an NQP routine
+          || nqp::eqaddr($method.package,Mu) {  # no own print method, use $*OUT
             $_ := $*OUT;
             .print(nqp::concat(self.Str,.nl-out))
         }
@@ -740,8 +742,9 @@ Perhaps it can be found at https://docs.raku.org/type/$name"
     # Handle the typical "foo.note"
     multi method note() {
 
-        # no own print method, so use $*ERR.print
-        if nqp::eqaddr(self.^find_method("print").package,Mu) {
+        my $method := self.^find_method("print");
+        if nqp::not_i(nqp::istype($method,Mu))  # an NQP routine
+          || nqp::eqaddr($method.package,Mu) {  # no own print method, use $*ERR
             $_ := $*ERR;
             .print(nqp::concat(self.gist,.nl-out))
         }
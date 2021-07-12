rakudo/rakudo

commit bb09bbb8581364f7323ff1f17ab4d84dffbe798c

Improve performance of .grep(Callable) and cleanup
- about 1% for single arg Callable
- more than 2x as fast for 2 arg Callable
- a few percent for 2+ arg Callables

This gets rid of the CheatArity hack, that turned out to be only in
place to let "sequential-map" select the correct iterator to use.
Selection of the correct iterator now happens in !grep-callable itself,
so that we don't need the CheatArity hack anymore.

The 2 arg Callable's improvement is due to the fact that it no longer
needs to take a full capture, but instead operates on a Callable that
just takes 2 parameters.

src/core.c/Any-iterable-methods.pm6
@@ -1063,80 +1063,90 @@ Consider using a block if any of these are necessary for your mapping code."
             )
         }
 
-        if $test.count == 1 {
-            Seq.new: IterateOneWithoutPhasers.new(
-              {
-                  nqp::if(
-                    nqp::stmts(
-                      nqp::handle(
-                        (my $result := $test($_)),
-                        'LAST', nqp::if(
-                          judge(
-                            nqp::ifnull(nqp::getpayload(nqp::exception),False),
-                            $_
-                          ),
-                          (last $_),
-                          (last)
-                        ),
-                        'NEXT', nqp::if(
-                          judge(
-                            nqp::ifnull(nqp::getpayload(nqp::exception),False),
-                            $_
-                          ),
-                          (next $_),
-                          (next)
-                        )
-                      ),
-                      judge($result, $_)
-                    ),
-                    $_,
-                    Empty
-                  )
-              },
-              self.iterator,
-              Any
-            )
-        }
-        else {
-            my role CheatArity {
-                has $!arity;
-                has $!count;
+        my $count := $test.count;
 
-                method with-arity-count($new-arity, $new-count) {
-                    $!arity := $new-arity;
-                    $!count := $new-count;
-                    self
-                }
-
-                method arity(Code:D:) { $!arity }
-                method count(Code:D:) { $!count }
-            }
-            my &tester := -> |c {
-                my \params := c.list;
-                nqp::handle(
-                  (my $result := $test(|c)),
-                  'NEXT', nqp::if(
-                    judge(
-                      nqp::ifnull(nqp::getpayload(nqp::exception),False),
-                      params
-                    ),
-                    (next params),
-                    (next)
-                  ),
-                  'LAST', nqp::if(
-                    judge(
-                      nqp::ifnull(nqp::getpayload(nqp::exception),False),
-                      params
-                    ),
-                    (last params),
-                    (last)
-                  )
-                );
-                judge($result, params) ?? params !! Empty
-            } but CheatArity;
-
-            self.map(&tester.with-arity-count($test.arity, $test.count))
-        }
+        Seq.new: $count < 2 || $count == Inf
+          ?? IterateOneWithoutPhasers.new(
+               -> \a {
+                   nqp::handle(
+                     (my $result := $test(a)),
+                     'LAST', nqp::if(
+                       judge(
+                         nqp::ifnull(nqp::getpayload(nqp::exception),False),
+                         a
+                       ),
+                       (last a),
+                       (last)
+                     ),
+                     'NEXT', nqp::if(
+                       judge(
+                         nqp::ifnull(nqp::getpayload(nqp::exception),False),
+                         a
+                       ),
+                       (next a),
+                       (next)
+                     )
+                   );
+                   judge($result, a) ?? a !! Empty
+               },
+               self.iterator,
+               Any
+             )
+          !! $count == 2
+            ?? IterateTwoWithoutPhasers.new(
+                 -> \a, \b {
+                     my \params := (a, b);
+                     nqp::handle(
+                       (my $result := $test(a, b)),
+                       'NEXT', nqp::if(
+                         judge(
+                           nqp::ifnull(nqp::getpayload(nqp::exception),False),
+                           params
+                         ),
+                         (next params),
+                         (next)
+                       ),
+                       'LAST', nqp::if(
+                         judge(
+                           nqp::ifnull(nqp::getpayload(nqp::exception),False),
+                           params
+                         ),
+                         (last params),
+                         (last)
+                       )
+                     );
+                     judge($result, params) ?? params !! Empty
+                 },
+                 self.iterator,
+                 Any
+               )
+            !! IterateMoreWithPhasers.new(
+                 -> |c {
+                     my \params := c.list;
+                     nqp::handle(
+                       (my $result := $test(|c)),
+                       'NEXT', nqp::if(
+                         judge(
+                           nqp::ifnull(nqp::getpayload(nqp::exception),False),
+                           params
+                         ),
+                         (next params),
+                         (next)
+                       ),
+                       'LAST', nqp::if(
+                         judge(
+                           nqp::ifnull(nqp::getpayload(nqp::exception),False),
+                           params
+                         ),
+                         (last params),
+                         (last)
+                       )
+                     );
+                     judge($result, params) ?? params !! Empty
+                 },
+                 self.iterator,
+                 Any
+               )
     }
 
     # Create a braid and fail cursor that we can use with all the normal,

commit 71f6bada9ee46f8367d2cbe8aad85e36646d5b43

Make sure we don't sink values in .grep(Callable)
bb09bbb8581364f7323ff1f17a caused a regression because the value
returned from the Callable was being sunk, when it wasn't before.
This would cause Failures that were returned, to be thrown instead
of just being passed along.

raydiak++ for golfing

src/core.c/Any-iterable-methods.pm6
@@ -1064,26 +1064,28 @@ Consider using a block if any of these are necessary for your mapping code."
         Seq.new: $count < 2 || $count == Inf
           ?? IterateOneWithoutPhasers.new(
                -> \a {
-                   nqp::handle(
-                     (my $result := $test(a)),
-                     'LAST', nqp::if(
-                       judge(
-                         nqp::ifnull(nqp::getpayload(nqp::exception),False),
-                         a
+                   nqp::stmts(  # don't sink the result
+                     nqp::handle(
+                        (my $result := $test(a)),
+                       'LAST', nqp::if(
+                         judge(
+                           nqp::ifnull(nqp::getpayload(nqp::exception),False),
+                           a
+                         ),
+                         (last a),
+                         (last)
                        ),
-                       (last a),
-                       (last)
+                       'NEXT', nqp::if(
+                         judge(
+                           nqp::ifnull(nqp::getpayload(nqp::exception),False),
+                           a
+                         ),
+                         (next a),
+                         (next)
+                       )
                      ),
-                     'NEXT', nqp::if(
-                       judge(
-                         nqp::ifnull(nqp::getpayload(nqp::exception),False),
-                         a
-                       ),
-                       (next a),
-                       (next)
-                     )
-                   );
-                   judge($result, a) ?? a !! Empty
+                     nqp::if(judge($result, a),a,Empty)
+                   )
                },
                self.iterator,
                Any
rakudo/rakudo

commit 1b75e3fa0c2cc172a1dd10b6ccc015da35257db1

Streamline CompUnit::DependencySpecification logic
Specifically with regards to the xxx-matcher logic.  Before, one
had to explicitely specify True to indicate no interest in matching
that matcher (which would essentially match against True, and thus
always pass).  And if something *was* specified for "version" or
"api", then at several places a Version object was made to get the
proper matching semantics.  This logic has now moved into the
CompUnit::DependencySpecification logic.

This also caches the stringification of a CompUnit::DependencySpecification
object, as it is relatively expensive.

This also adapts all calls to CompUnit::DependencySpecification in
the core.

Also add some internal documentation as to the functioning of it all.

src/core.c/CompUnit/DependencySpecification.pm6
@@ -1,23 +1,62 @@
 class CompUnit::DependencySpecification {
     has str $.short-name  is built(:bind) is required;  # must be native
     has str $.from        is built(:bind) = 'Perl6';    # must be native
-    has $.version-matcher is built(:bind) = True;
-    has $.auth-matcher    is built(:bind) = True;
-    has $.api-matcher     is built(:bind) = True;
+    has str $!stringified;
 
-    # Oddly enough, the exact format of this method can make / break
-    # dependency testing.  This should probably depend on the .WHICH.
-    method Str(CompUnit::DependencySpecification:D: --> Str:D) {
+    # The matcher methods will return True if implicitely or explicitely
+    # initialized with an undefined value.  In the case of version and
+    # API, a Version object for the given defined value, will be returned
+    # if it was not already specified as a Version object.  This does
+    # *not* cause an issue with stringification, as the stringification
+    # of a Version object returns its original string.
+    has $.auth-matcher    is built(:bind);  # either True or smartmatchee
+    has $.version-matcher is built(:bind);  # either True or Version
+    has $.api-matcher     is built(:bind);  # either True or Version
+
+    # Sadly this is needed because we have a spectest that explicitely
+    # passes True for unspecified matchers.  Hopefully this can go when
+    # we accept that an undefined value indicates no interest in matching.
+    method TWEAK() {
+        $!version-matcher := Any if nqp::eqaddr($!version-matcher,True);
+        $!auth-matcher    := Any if nqp::eqaddr($!auth-matcher,True);
+        $!api-matcher     := Any if nqp::eqaddr($!api-matcher,True);
+    }
+
+    method auth-matcher() {
+        $!auth-matcher // True
+    }
+    method version-matcher() {
+        nqp::defined($!version-matcher)
+          ?? nqp::istype($!version-matcher,Version)
+            ?? $!version-matcher
+            !! ($!version-matcher := Version.new($!version-matcher))
+          !! True
+    }
+    method api-matcher() {
+        nqp::defined($!api-matcher)
+          ?? nqp::istype($!api-matcher,Version)
+            ?? $!api-matcher
+            !! ($!api-matcher := Version.new($!api-matcher))
+          !! True
+    }
+
+    method !stringify() {
         my $parts := nqp::list_s($!short-name);
         nqp::push_s($parts,":from<$!from>")
           if $!from ne 'Perl6';
         nqp::push_s($parts,":ver<$!version-matcher>")
-          unless nqp::eqaddr($!version-matcher,True);
+          if nqp::defined($!version-matcher);
         nqp::push_s($parts,":auth<$!auth-matcher>")
-          unless nqp::eqaddr($!auth-matcher,True);
+          if nqp::defined($!auth-matcher);
         nqp::push_s($parts,":api<$!api-matcher>")
-          unless nqp::eqaddr($!api-matcher,True);
-        nqp::join('',$parts)
+          if nqp::defined($!api-matcher);
+        $!stringified = nqp::join('',$parts)
+    }
+
+    # Oddly enough, the exact format of this method can make / break
+    # dependency testing.  This should probably depend on the .WHICH.
+    method Str(CompUnit::DependencySpecification:D: --> str) {
+        $!stringified ?? $!stringified !! self!stringify
     }
 
     multi method raku(CompUnit::DependencySpecification:D: --> Str:D) {
@@ -27,11 +66,11 @@ class CompUnit::DependencySpecification {
         nqp::push_s($parts,",:from<$!from>")
           if $!from ne 'Perl6';
         nqp::push_s($parts,",:version-matcher<$!version-matcher>")
-          unless nqp::eqaddr($!version-matcher,True);
+          if nqp::defined($!version-matcher);
         nqp::push_s($parts,",:auth-matcher<$!auth-matcher>")
-          unless nqp::eqaddr($!auth-matcher,True);
+          if nqp::defined($!auth-matcher);
         nqp::push_s($parts,",:api-matcher<$!api-matcher>")
-          unless nqp::eqaddr($!api-matcher,True);
+          if nqp::defined($!api-matcher);
         nqp::push_s($parts,')');
         nqp::join('',$parts)
     }

src/Perl6/World.nqp
@@ -1477,13 +1477,12 @@ class Perl6::World is HLL::World {
         $RMD("  Late loading '$module_name'") if $RMD;
 
         # Immediate loading.
-        my $true := self.find_single_symbol('True', :setting-only);
         my $spec := self.find_symbol(['CompUnit', 'DependencySpecification'], :setting-only).new(
             :short-name($module_name),
             :from(%opts<from> // 'Perl6'),
-            :auth-matcher(%opts<auth> // $true),
-            :api-matcher(%opts<api> // $true),
-            :version-matcher(%opts<ver> // $true),
+            :auth-matcher(%opts<auth>),
+            :api-matcher(%opts<api>),
+            :version-matcher(%opts<ver>),
         );
         self.add_object_if_no_sc($spec);
         my $registry := self.find_symbol(['CompUnit', 'RepositoryRegistry'], :setting-only);

src/core.c/CompUnit/Repository/FileSystem.pm6
@@ -162,9 +162,9 @@ class CompUnit::Repository::FileSystem
     multi method candidates(Str:D $name, :$auth, :$ver, :$api) {
         return samewith(CompUnit::DependencySpecification.new(
             short-name      => $name,
-            auth-matcher    => $auth // True,
-            version-matcher => $ver  // True,
-            api-matcher     => $api  // True,
+            auth-matcher    => $auth,
+            version-matcher => $ver,
+            api-matcher     => $api,
         ));
     }
     multi method candidates(CompUnit::DependencySpecification $spec) {
@@ -185,16 +185,9 @@ class CompUnit::Repository::FileSystem
             $distribution = self!distribution;
         }
 
-        my $version-matcher = ($spec.version-matcher ~~ Bool)
-            ?? $spec.version-matcher
-            !! Version.new($spec.version-matcher);
-        my $api-matcher = ($spec.api-matcher ~~ Bool)
-            ?? $spec.api-matcher
-            !! Version.new($spec.api-matcher);
-
         return Empty unless (($distribution.meta<auth> // '') eq '' || ($distribution.meta<auth> // '') ~~ $spec.auth-matcher)
-            and (($distribution.meta<ver> // '*') eq '*' || Version.new($distribution.meta<ver> // 0) ~~ $version-matcher)
-            and (($distribution.meta<api> // '*') eq '*' || Version.new($distribution.meta<api> // 0) ~~ $api-matcher);
+            and (($distribution.meta<ver> // '*') eq '*' || Version.new($distribution.meta<ver> // 0) ~~ $spec.version-matcher)
+            and (($distribution.meta<api> // '*') eq '*' || Version.new($distribution.meta<api> // 0) ~~ $spec.api-matcher);
 
         return ($distribution,);
     }
@@ -203,9 +196,9 @@ class CompUnit::Repository::FileSystem
     multi method files($file, Str:D :$name!, :$auth, :$ver, :$api) {
         my $spec = CompUnit::DependencySpecification.new(
             short-name      => $name,
-            auth-matcher    => $auth // True,
-            version-matcher => $ver  // True,
-            api-matcher     => $api  // True,
+            auth-matcher    => $auth,
+            version-matcher => $ver,
+            api-matcher     => $api,
         );
 
         with self.candidates($spec) {
@@ -223,9 +216,9 @@ class CompUnit::Repository::FileSystem
     multi method files($file, :$auth, :$ver, :$api) {
         my $spec = CompUnit::DependencySpecification.new(
             short-name      => $file,
-            auth-matcher    => $auth // True,
-            version-matcher => $ver  // True,
-            api-matcher     => $api  // True,
+            auth-matcher    => $auth,
+            version-matcher => $ver,
+            api-matcher     => $api,
         );
 
         with self.candidates($spec) {

src/core.c/CompUnit/Repository/Installation.pm6
@@ -363,9 +363,9 @@ sub MAIN(:$name, :$auth, :$ver, *@, *%) {
 
         my $spec = CompUnit::DependencySpecification.new(
             short-name      => $name,
-            auth-matcher    => $auth // True,
-            version-matcher => $ver  // True,
-            api-matcher     => $api  // True,
+            auth-matcher    => $auth,
+            version-matcher => $ver,
+            api-matcher     => $api,
         );
 
         with self.candidates($spec) {
@@ -385,9 +385,9 @@ sub MAIN(:$name, :$auth, :$ver, *@, *%) {
 
         my $spec = CompUnit::DependencySpecification.new(
             short-name      => $file,
-            auth-matcher    => $auth // True,
-            version-matcher => $ver  // True,
-            api-matcher     => $api  // True,
+            auth-matcher    => $auth,
+            version-matcher => $ver,
+            api-matcher     => $api,
         );
 
         with self.candidates($spec) {
@@ -405,9 +405,9 @@ sub MAIN(:$name, :$auth, :$ver, *@, *%) {
     multi method candidates(Str:D $name, :$auth, :$ver, :$api) {
         return samewith(CompUnit::DependencySpecification.new(
             short-name      => $name,
-            auth-matcher    => $auth // True,
-            version-matcher => $ver  // True,
-            api-matcher     => $api  // True,
+            auth-matcher    => $auth,
+            version-matcher => $ver,
+            api-matcher     => $api,
         ));
     }
     multi method candidates(CompUnit::DependencySpecification $spec) {
@@ -436,13 +436,9 @@ sub MAIN(:$name, :$auth, :$ver, *@, *%) {
                     })
             );
 
-        my $auth-matcher := $spec.auth-matcher;
-        my $version-matcher := ($spec.version-matcher ~~ Bool)
-            ?? $spec.version-matcher # fast path for matching Version.new(*)
-            !! Version.new($spec.version-matcher);
-        my $api-matcher := ($spec.api-matcher ~~ Bool)
-            ?? $spec.api-matcher
-            !! Version.new($spec.api-matcher);
+        my $auth-matcher    := $spec.auth-matcher;
+        my $version-matcher := $spec.version-matcher;
+        my $api-matcher     := $spec.api-matcher;
 
         # $metas has already been filtered by name via $lookup, so do
         # remaining filtering on fast lookup fields

commit ef68d4ad4c9a9aef3876a2db343c35ab64dbc579

Hopefully fix problem in JVM backend\
Apparently caused by 1b75e3fa0c .  If this fixes it, then maybe the
JVM backend cannot handle native str return constraints?

src/core.c/CompUnit/DependencySpecification.pm6
@@ -55,7 +55,7 @@ class CompUnit::DependencySpecification {
 
     # Oddly enough, the exact format of this method can make / break
     # dependency testing.  This should probably depend on the .WHICH.
-    method Str(CompUnit::DependencySpecification:D: --> str) {
+    method Str(CompUnit::DependencySpecification:D: --> Str:D) {
         $!stringified ?? $!stringified !! self!stringify
     }
 
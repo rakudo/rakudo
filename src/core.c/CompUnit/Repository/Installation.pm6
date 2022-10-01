rakudo/rakudo

commit 6b50a940af77211709ef872560b2ced35f137c11

Make sure $*REPO gets restored on exception
At the moment, if anything goes wrong with module installation, we
completely abort anyway.  But that may not be like that in the future.
Make sure that $*REPO gets restored on failure.  Also reduce the number
of $*REPO lookups.

src/core.c/CompUnit/Repository/Installation.pm6
@@ -327,9 +327,12 @@ sub MAIN(:$name, :$auth, :$ver, *@, *%) {
         $!id = Any;
 
         if $precompile {
-            my $head = $*REPO;
-            PROCESS::<$REPO> := self; # Precomp files should only depend on downstream repos
-            my $precomp = $*REPO.precomp-repository;
+            my $head := $*REPO;
+            CATCH { PROCESS::<$REPO> := $head }
+            # Precomp files should only depend on downstream repos
+            PROCESS::<$REPO> := self;
+
+            my $precomp = $head.precomp-repository;
             my $repo-prefix = self!repo-prefix;
             my $*DISTRIBUTION = CompUnit::Repository::Distribution.new($dist, :repo(self), :$dist-id);
             my $*RESOURCES = Distribution::Resources.new(:repo(self), :$dist-id);

commit cc7193d0a6d95bd580a4b2783795d082a56d8e6d

Fix module installation writing precomp files into the wrong precomp store
On installation we want to write to the installation target repository's
precomp store. An attempted optimization changed this to the head
repoository's, i.e. the default target. This caused issues with e.g.
system wide installations and packaging of modules.

Regression caused by commit 6b50a940af77211709ef872560b2ced35f137c11

Fixes GH #4907

src/core.c/CompUnit/Repository/Installation.pm6
@@ -332,7 +332,7 @@ sub MAIN(:$name, :$auth, :$ver, *@, *%) {
             # Precomp files should only depend on downstream repos
             PROCESS::<$REPO> := self;
 
-            my $precomp = $head.precomp-repository;
+            my $precomp = self.precomp-repository;
             my $repo-prefix = self!repo-prefix;
             my $*DISTRIBUTION = CompUnit::Repository::Distribution.new($dist, :repo(self), :$dist-id);
             my $*RESOURCES = Distribution::Resources.new(:repo(self), :$dist-id);
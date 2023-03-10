rakudo/rakudo

commit e8f419aefb716e2e89b098662cbed01fc3ec3bbd

RakuAST: fix colonpairs silently getting dropped from identifiers

src/Raku/ast/name.rakumod
@@ -72,6 +72,15 @@ class RakuAST::Name
         False
     }
 
+    method without-colonpair($key) {
+        my @parts := nqp::clone($!parts);
+        my $type := RakuAST::Name.new(|@parts);
+        for $!colonpairs {
+            $type.add-colonpair($_) if $_.key ne $key;
+        }
+        $type
+    }
+
     method visit-children(Code $visitor) {
         if nqp::isconcrete(self) {
             for $!parts {

src/Raku/Actions.nqp
@@ -1227,11 +1227,7 @@ class Raku::Actions is HLL::Actions does Raku::CommonActions {
         }
         else {
             if $*is-type {
-                my $type := self.r('Type', 'Simple').new($name);
-                if $<arglist> {
-                    $type := self.r('Type', 'Parameterized').new(:base-type($type), :args($<arglist>.ast));
-                }
-                self.attach: $/, $type;
+                self.attach: $/, self.type-for-name($/, $name);
             }
             else {
                 self.attach: $/, self.r('Term', 'Name').new($name);
@@ -2131,6 +2127,29 @@ class Raku::Actions is HLL::Actions does Raku::CommonActions {
     ## Types
     ##
 
+    method type-for-name($/, $base-name) {
+        my $type := self.r('Type', 'Simple').new($base-name.without-colonpair('_'));
+        if $base-name.has-colonpair('D') {
+            $type := self.r('Type', 'Simple').new($base-name.without-colonpair('D'));
+            $type := self.r('Type', 'Definedness').new(:base-type($type), :definite);
+        }
+        elsif $base-name.has-colonpair('U') {
+            $type := self.r('Type', 'Simple').new($base-name.without-colonpair('U'));
+            $type := self.r('Type', 'Definedness').new(:base-type($type), :!definite);
+        }
+        if $<arglist> {
+            $type := self.r('Type', 'Parameterized').new(:base-type($type), :args($<arglist>.ast));
+        }
+        if $<accept> {
+            $type := self.r('Type', 'Coercion').new(:base-type($type), :constraint($<accept>.ast));
+        }
+        elsif $<accept_any> {
+            my $Any := self.r('Type', 'Setting').new(RakuAST::Name.from-identifier('Any'));
+            $type := self.r('Type', 'Coercion').new(:base-type($type), :constraint($Any));
+        }
+        $type
+    }
+
     method typename($/) {
         my $base-name := $<longname>
             ?? $<longname>.ast
@@ -2149,31 +2168,14 @@ class Raku::Actions is HLL::Actions does Raku::CommonActions {
             if $str_longname eq '::' {
                 $/.panic("Cannot use :: as a type name");
             }
-            my $type-capture := self.r('Type', 'Capture').new($base-name);
+            my $type-capture := self.r('Type', 'Capture').new($base-name.without-colonpair('D').without-colonpair('U').without-colonpair('_'));
             self.attach: $/, $type-capture;
 
             # Declare the lexical so it is available right away (e.g. for traits)
             $*R.declare-lexical($type-capture);
         }
         else {
-            my $type := self.r('Type', 'Simple').new($base-name);
-            if $base-name.has-colonpair('D') {
-                $type := self.r('Type', 'Definedness').new(:base-type($type), :definite);
-            }
-            elsif $base-name.has-colonpair('U') {
-                $type := self.r('Type', 'Definedness').new(:base-type($type), :!definite);
-            }
-            if $<arglist> {
-                $type := self.r('Type', 'Parameterized').new(:base-type($type), :args($<arglist>.ast));
-            }
-            if $<accept> {
-                $type := self.r('Type', 'Coercion').new(:base-type($type), :constraint($<accept>.ast));
-            }
-            elsif $<accept_any> {
-                my $Any := self.r('Type', 'Setting').new(RakuAST::Name.from-identifier('Any'));
-                $type := self.r('Type', 'Coercion').new(:base-type($type), :constraint($Any));
-            }
-            self.attach: $/, $type;
+            self.attach: $/, self.type-for-name($/, $base-name);
         }
     }
 

src/Raku/Grammar.nqp
@@ -1747,9 +1747,13 @@ grammar Raku::Grammar is HLL::Grammar does Raku::Common {
     token term:sym<name> {
         :my $*is-type;
         <longname>
+        :my $base-name;
         [
-        || <?{ nqp::eqat($<longname>.Str, '::', 0) || $*R.is-name-known($<longname>.ast) }>
-            { $*is-type := $*R.is-name-type($<longname>.ast) }
+        || <?{
+                $base-name := $<longname>.ast.without-colonpair('D').without-colonpair('U').without-colonpair('_');
+                nqp::eqat($<longname>.Str, '::', 0) || $*R.is-name-known($base-name)
+            }>
+            { $*is-type := $*R.is-name-type($base-name) }
             [
                 <?[[]> <?{ $*is-type }>
                 :dba('type parameter') '[' ~ ']' <arglist>
@@ -2601,7 +2605,7 @@ grammar Raku::Grammar is HLL::Grammar does Raku::Common {
         | <longname>
           <?{
             # ::T introduces a type, so always is one
-            nqp::eqat(~$<longname>, '::', 0) || $*R.is-name-known($<longname>.ast)
+            nqp::eqat(~$<longname>, '::', 0) || $*R.is-name-known($<longname>.ast.without-colonpair('D').without-colonpair('U').without-colonpair('_'))
           }>
         ]
         # parametric/coercion type?

src/Raku/ast/resolver.rakumod
@@ -155,7 +155,7 @@ class RakuAST::Resolver {
         if $name.is-identifier {
             return self.global-package() if $name.canonicalize eq 'GLOBAL';
             # Single-part name, so look lexically.
-            my str $bare-name := $name.IMPL-UNWRAP-LIST($name.parts)[0].name;
+            my str $bare-name := $name.canonicalize;
             my str $lexical-name := $sigil ?? $sigil ~ $bare-name !! $bare-name;
             $found := self.resolve-lexical($lexical-name)
         }
@@ -206,7 +206,7 @@ class RakuAST::Resolver {
 
     method IMPL-RESOLVE-NAME-CONSTANT(RakuAST::Name $name, Bool :$setting, str :$sigil) {
         if $name.is-identifier {
-            my str $identifier := $name.IMPL-UNWRAP-LIST($name.parts)[0].name;
+            my str $identifier := $name.canonicalize;
             return self.global-package() if $identifier eq 'GLOBAL';
             $setting
                 ?? self.resolve-lexical-constant-in-setting($identifier)
@@ -267,7 +267,7 @@ class RakuAST::Resolver {
 
     method IMPL-PARTIALLY-RESOLVE-NAME-CONSTANT(RakuAST::Name $name, Bool :$setting, str :$sigil) {
         if $name.is-identifier {
-            my str $identifier := $name.IMPL-UNWRAP-LIST($name.parts)[0].name;
+            my str $identifier := $name.canonicalize;
             my $constant := $setting
                 ?? self.resolve-lexical-constant-in-setting($identifier)
                 !! self.resolve-lexical-constant($identifier);

commit ac217e866fa1ebb73a2a2123d8a3bc25918e5c0c

RakuAST: fix without-colonpairs stumbling over QuotedString
Fixes regression caused by e8f419aefb716e2e89b098662cbed01fc3ec3bbd

src/Raku/ast/name.rakumod
@@ -76,7 +76,7 @@ class RakuAST::Name
         my @parts := nqp::clone($!parts);
         my $type := RakuAST::Name.new(|@parts);
         for $!colonpairs {
-            $type.add-colonpair($_) if $_.key ne $key;
+            $type.add-colonpair($_) if !nqp::istype($_, RakuAST::ColonPair) || $_.key ne $key;
         }
         $type
     }
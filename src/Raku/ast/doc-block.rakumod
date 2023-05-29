rakudo/rakudo

commit 213bacbb14fa6b6ca5f7d109e7713dcc802a82fc

RakuAST: some children in RakuAST::Doc:: *should* be visited

src/Raku/ast/doc-block.rakumod
@@ -45,8 +45,13 @@ class RakuAST::Doc::Paragraph
         $obj
     }
 
-    # nothing to do
-    method visit-children(Code $visitor) { }
+    method visit-children(Code $visitor) {
+        for $!atoms {
+            if nqp::istype($_,RakuAST::Node) {
+                $visitor($_);
+            }
+        }
+    }
 
     method add-atom($atom) { nqp::push($!atoms, $atom) }
     method atoms() { self.IMPL-WRAP-LIST($!atoms) }
@@ -125,7 +130,11 @@ class RakuAST::Doc::Block
     method paragraphs() { self.IMPL-WRAP-LIST($!paragraphs) }
 
     method visit-children(Code $visitor) {
-        # no serviceable parts inside
+        for $!paragraphs {
+            if nqp::istype($_,RakuAST::Node) {
+                $visitor($_);
+            }
+        }
     }
 
     method PERFORM-CHECK(
@@ -169,10 +178,14 @@ class RakuAST::Doc::Markup
 
     method visit-children(Code $visitor) {
         for $!atoms {
-            $visitor($_);
+            if nqp::istype($_,RakuAST::Node) {
+                $visitor($_);
+            }
         }
         for $!meta {
-            $visitor($_);
+            if nqp::istype($_,RakuAST::Node) {
+                $visitor($_);
+            }
         }
     }
 

commit 1fd990fb015af825ef2af6708dc75aaed4c153f4

RakuAST: don't let ::Doc::Block descend into ::Doc::Blocks
To fix an issue with production of $=pod that was introduced with
213bacbb14fa6b6ca5f7d109e7713dcc802a82fc .  Still not sure whether
this is the correct solution, as this feels icky.  But it will
unbreak the spectests that were broken for now.

src/Raku/ast/doc-block.rakumod
@@ -131,7 +131,10 @@ class RakuAST::Doc::Block
 
     method visit-children(Code $visitor) {
         for $!paragraphs {
-            if nqp::istype($_,RakuAST::Node) {
+            if nqp::istype($_,RakuAST::Doc::Block) {
+                # no action
+            }
+            elsif nqp::istype($_,RakuAST::Node) {
                 $visitor($_);
             }
         }
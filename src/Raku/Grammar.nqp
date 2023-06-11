rakudo/rakudo

commit 54fee5e6641840661d3d9bd1afbd0fad21e2b446

RakuAST: fix parser stumbling over backslashes in names of categoricals
Fixes: sub infix:«~~>\»» { }; say &[~~>»].name

src/Raku/Grammar.nqp
@@ -2456,7 +2456,7 @@ grammar Raku::Grammar is HLL::Grammar does Raku::Common {
                 my $category := $<deflongname><name>.Str;
 
                 my $opname := $cf<circumfix>
-                    ?? $cf<circumfix><nibble> // $cf<circumfix><semilist>
+                    ?? $cf<circumfix><nibble>.ast.literal-value // $cf<circumfix><semilist>
                     !! '';
                 my $canname := $category ~ ':sym' ~ self.actions.r('ColonPairish').IMPL-QUOTE-VALUE($opname.Str);
 

commit f05cd03336afb243b2fb0a835217613413cf2cec

RakuAST: fix regression when parsing categoricals
Broken by 54fee5e6641840661d3d9bd1afbd0fad21e2b446

src/Raku/Grammar.nqp
@@ -2456,7 +2456,9 @@ grammar Raku::Grammar is HLL::Grammar does Raku::Common {
                 my $category := $<deflongname><name>.Str;
 
                 my $opname := $cf<circumfix>
-                    ?? $cf<circumfix><nibble>.ast.literal-value // $cf<circumfix><semilist>
+                    ?? $cf<circumfix><nibble>
+                        ?? $cf<circumfix><nibble>.ast.literal-value // ~$cf<circumfix><nibble>
+                        !! $cf<circumfix><semilist>
                     !! '';
                 my $canname := $category ~ ':sym' ~ self.actions.r('ColonPairish').IMPL-QUOTE-VALUE($opname.Str);
 
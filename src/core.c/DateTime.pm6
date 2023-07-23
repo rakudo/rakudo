rakudo/rakudo

commit e6c0b5e91932b0c709af5ef567b9b7d1a9031cb4

Make DateTime.truncated-to a lot faster
- upto 2x as fast for truncating to week
- upto 5.5x as fast for truncating on second
- upto 19x as fast for truncating on other units

src/core.c/DateTime.pm6
@@ -481,20 +481,56 @@ my class DateTime does Dateish {
         }
     }
 
-    method truncated-to(DateTime:D: Cool $unit --> DateTime:D) {
-        my %parts;
-        given self!VALID-UNIT($unit) {
-            %parts<second> = self.whole-second;
-            when 'second' | 'seconds' {}
-            %parts<second> = 0;
-            when 'minute' | 'minutes' {}
-            %parts<minute> = 0;
-            when 'hour'   | 'hours'   {}
-            %parts<hour> = 0;
-            when 'day'    | 'days'    {}
-            %parts = self!truncate-ymd($unit, %parts);
-        }
-        self!clone-without-validating(|%parts);
+    method truncated-to(DateTime:D: str $unit --> DateTime:D) {
+        my $truncated := nqp::clone(self);
+        my $what      := self.WHAT;
+        nqp::if(
+          nqp::eqat($unit,'second',0),
+          nqp::bindattr($truncated,$what,'$!second',$!second.Int),
+          nqp::stmts(
+            nqp::bindattr($truncated,$what,'$!second',0),
+            nqp::unless(
+              nqp::eqat($unit,'minute',0),
+              nqp::stmts(
+                nqp::bindattr_i($truncated,$what,'$!minute',0),
+                nqp::unless(
+                  nqp::eqat($unit,'hour',0),
+                  nqp::stmts(
+                    nqp::bindattr_i($truncated,$what,'$!hour',0),
+                    nqp::unless(
+                      nqp::eqat($unit,'day',0),
+                      nqp::stmts(
+                        nqp::bindattr_i($truncated,$what,'$!daycount',0),
+                        nqp::if(
+                          nqp::eqat($unit,'week',0),
+                          ($truncated := $truncated.move-by-unit(
+                            'day',
+                            nqp::sub_i(1,$truncated.day-of-week)
+                          )),
+                          nqp::stmts(
+                            nqp::bindattr_i($truncated,$what,'$!day',1),
+                            nqp::unless(
+                              nqp::eqat($unit,'month',0),
+                              nqp::stmts(
+                                nqp::bindattr_i($truncated,$what,'$!month',1),
+                                nqp::unless(
+                                  nqp::eqat($unit,'year',0),
+                                  die "Cannot truncate {self.^name} object to '$unit'"
+                                )
+                              )
+                            )
+                          )
+                        )
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        );
+
+        $truncated
     }
     method whole-second(DateTime:D: --> Int:D) { $!second.Int }
 

commit b58bf6db30a05af5098a666a27c5c3e0a6630925

Fix thinko introduced in e6c0b5e919
Accessing attributes in Subclassed objects of DateTime should refer
to the original class, **not** to the subclass type.

Xliff++ for spotting the problem with the Interval module.

src/core.c/DateTime.pm6
@@ -611,24 +611,23 @@ my class DateTime does Dateish {
 
     method truncated-to(DateTime:D: str $unit --> DateTime:D) {
         my $truncated := nqp::clone(self);
-        my $what      := self.WHAT;
         nqp::if(
           nqp::eqat($unit,'second',0),
-          nqp::bindattr($truncated,$what,'$!second',$!second.Int),
+          nqp::bindattr($truncated,DateTime,'$!second',$!second.Int),
           nqp::stmts(
-            nqp::bindattr($truncated,$what,'$!second',0),
+            nqp::bindattr($truncated,DateTime,'$!second',0),
             nqp::unless(
               nqp::eqat($unit,'minute',0),
               nqp::stmts(
-                nqp::bindattr_i($truncated,$what,'$!minute',0),
+                nqp::bindattr_i($truncated,DateTime,'$!minute',0),
                 nqp::unless(
                   nqp::eqat($unit,'hour',0),
                   nqp::stmts(
-                    nqp::bindattr_i($truncated,$what,'$!hour',0),
+                    nqp::bindattr_i($truncated,DateTime,'$!hour',0),
                     nqp::unless(
                       nqp::eqat($unit,'day',0),
                       nqp::stmts(
-                        nqp::bindattr_i($truncated,$what,'$!daycount',0),
+                        nqp::bindattr_i($truncated,DateTime,'$!daycount',0),
                         nqp::if(
                           nqp::eqat($unit,'week',0),
                           ($truncated := $truncated.move-by-unit(
@@ -636,11 +635,11 @@ my class DateTime does Dateish {
                             nqp::sub_i(1,$truncated.day-of-week)
                           )),
                           nqp::stmts(
-                            nqp::bindattr_i($truncated,$what,'$!day',1),
+                            nqp::bindattr_i($truncated,DateTime,'$!day',1),
                             nqp::unless(
                               nqp::eqat($unit,'month',0),
                               nqp::stmts(
-                                nqp::bindattr_i($truncated,$what,'$!month',1),
+                                nqp::bindattr_i($truncated,DateTime,'$!month',1),
                                 nqp::unless(
                                   nqp::eqat($unit,'year',0),
                                   die "Cannot truncate {self.^name} object to '$unit'"
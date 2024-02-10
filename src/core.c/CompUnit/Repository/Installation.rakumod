rakudo/rakudo

commit 7ca96bbd90f79bfa9d51d80b93e62506eb2e44c5

Properly leave the install scope
Check for there actually being a concrete lock, before trying to
unlock and close.

src/core.c/CompUnit/Repository/Installation.rakumod
@@ -231,9 +231,7 @@ sub MAIN(*@, *%) {
         my @*MODULES;
         my $path   = self!writeable-path or die "No writeable path found, $.prefix not writeable";
         my $lock = $.prefix.add('repo.lock').open(:create, :w);
-        LEAVE $lock.close;
-        $lock.lock;
-        LEAVE $lock.unlock;
+        LEAVE .unlock, .close with $lock;
 
         my $version = self!repository-version;
         self.upgrade-repository unless $version == 2;

commit a723c387cef2dfe9d62dd99903852a080aa1c9b9

Fix removal of file locking during installation
7ca96bb accidentally removed the logic to lock the file. This reintroduces the necessary locking.

src/core.c/CompUnit/Repository/Installation.rakumod
@@ -231,6 +231,7 @@ sub MAIN(*@, *%) {
         my @*MODULES;
         my $path   = self!writeable-path or die "No writeable path found, $.prefix not writeable";
         my $lock = $.prefix.add('repo.lock').open(:create, :w);
+        $lock.lock;
         LEAVE .unlock, .close with $lock;
 
         my $version = self!repository-version;
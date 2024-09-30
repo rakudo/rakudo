rakudo/rakudo

commit a186dd05a49f26b3c01214c3f330559ebac56692

RakuAST: no trailing whitespace in translatable tokens
Significant or not: removed mostly because the localizations are
generated that way as well.

src/Raku/Grammar.nqp
@@ -1,4 +1,4 @@
-use NQPP6QRegex;
+NQPP6QRegex;
 use NQPP5QRegex;
 use Raku::Actions;
 
@@ -762,181 +762,181 @@ grammar Raku::Grammar is HLL::Grammar does Raku::Common {
 
     # These tokens replace bare strings so that they can be localized by
     # mixing in tokens with translated localizations.
-    token block-default  { default }
-    token block-else     { else    }
-    token block-elsif    { elsif   }
-    token block-for      { for     }
-    token block-given    { given   }
-    token block-if       { if      }
-    token block-loop     { loop    }
-    token block-orwith   { orwith  }
-    token block-repeat   { repeat  }
-    token block-unless   { unless  }
-    token block-until    { until   }
-    token block-when     { when    }
-    token block-whenever { whenever }
-    token block-while    { while   }
-    token block-with     { with    }
-    token block-without  { without }
+    token block-default  { default}
+    token block-else     { else}
+    token block-elsif    { elsif}
+    token block-for      { for}
+    token block-given    { given}
+    token block-if       { if}
+    token block-loop     { loop}
+    token block-orwith   { orwith}
+    token block-repeat   { repeat}
+    token block-unless   { unless}
+    token block-until    { until}
+    token block-when     { when}
+    token block-whenever { whenever}
+    token block-while    { while}
+    token block-with     { with}
+    token block-without  { without}
 
-    token constraint-where { where }
+    token constraint-where { where}
 
-    token infix-after      { after      }
-    token infix-and        { and        }
-    token infix-andthen    { andthen    }
-    token infix-before     { before     }
-    token infix-but        { but        }
-    token infix-cmp        { cmp        }
-    token infix-coll       { coll       }
-    token infix-pcontp     { '(cont)'   }
-    token infix-div        { div        }
-    token infix-does       { does       }
-    token infix-pelemp     { '(elem)'   }
-    token infix-eq         { eq         }
-    token infix-eqv        { eqv        }
-    token infix-ff         { ff         }
-    token infix-cff        { '^ff'      }
-    token infix-ffc        { 'ff^'      }
-    token infix-cffc       { '^ff^'     }
-    token infix-fff        { fff        }
-    token infix-cfff       { '^fff'     }
-    token infix-fffc       { 'fff^'     }
-    token infix-cfffc      { '^fff^'    }
-    token infix-gcd        { gcd        }
-    token infix-ge         { ge         }
-    token infix-gt         { gt         }
-    token infix-lcm        { lcm        }
-    token infix-le         { le         }
-    token infix-leg        { leg        }
-    token infix-lt         { lt         }
-    token infix-max        { max        }
-    token infix-min        { min        }
-    token infix-minmax     { minmax     }
-    token infix-mod        { mod        }
-    token infix-ne         { ne         }
-    token infix-notandthen { notandthen }
-    token infix-or         { or         }
-    token infix-orelse     { orelse     }
-    token infix-unicmp     { unicmp     }
-    token infix-x          { x          }
-    token infix-xor        { xor        }
-    token infix-xx         { xx         }
+    token infix-after      { after}
+    token infix-and        { and}
+    token infix-andthen    { andthen}
+    token infix-before     { before}
+    token infix-but        { but}
+    token infix-cmp        { cmp}
+    token infix-coll       { coll}
+    token infix-pcontp     { '(cont)'}
+    token infix-div        { div}
+    token infix-does       { does}
+    token infix-pelemp     { '(elem)'}
+    token infix-eq         { eq}
+    token infix-eqv        { eqv}
+    token infix-ff         { ff}
+    token infix-cff        { '^ff'}
+    token infix-ffc        { 'ff^'}
+    token infix-cffc       { '^ff^'}
+    token infix-fff        { fff}
+    token infix-cfff       { '^fff'}
+    token infix-fffc       { 'fff^'}
+    token infix-cfffc      { '^fff^'}
+    token infix-gcd        { gcd}
+    token infix-ge         { ge}
+    token infix-gt         { gt}
+    token infix-lcm        { lcm}
+    token infix-le         { le}
+    token infix-leg        { leg}
+    token infix-lt         { lt}
+    token infix-max        { max}
+    token infix-min        { min}
+    token infix-minmax     { minmax}
+    token infix-mod        { mod}
+    token infix-ne         { ne}
+    token infix-notandthen { notandthen}
+    token infix-or         { or}
+    token infix-orelse     { orelse}
+    token infix-unicmp     { unicmp}
+    token infix-x          { x}
+    token infix-xor        { xor}
+    token infix-xx         { xx}
 
-    token meta-R { R }
-    token meta-X { X }
-    token meta-Z { Z }
+    token meta-R { R}
+    token meta-X { X}
+    token meta-Z { Z}
 
-    token modifier-for     { for     }
-    token modifier-given   { given   }
-    token modifier-if      { if      }
-    token modifier-unless  { unless  }
-    token modifier-until   { until   }
-    token modifier-when    { when    }
-    token modifier-while   { while   }
-    token modifier-with    { with    }
-    token modifier-without { without }
+    token modifier-for     { for}
+    token modifier-given   { given}
+    token modifier-if      { if}
+    token modifier-unless  { unless}
+    token modifier-until   { until}
+    token modifier-when    { when}
+    token modifier-while   { while}
+    token modifier-with    { with}
+    token modifier-without { without}
 
-    token multi-multi { multi }
-    token multi-only  { only  }
-    token multi-proto { proto }
+    token multi-multi { multi}
+    token multi-only  { only}
+    token multi-proto { proto}
 
-    token package-class   { class   }
-    token package-grammar { grammar }
-    token package-knowhow { knowhow }
-    token package-module  { module  }
-    token package-native  { native  }
-    token package-package { package }
-    token package-role    { role    }
+    token package-class   { class}
+    token package-grammar { grammar}
+    token package-knowhow { knowhow}
+    token package-module  { module}
+    token package-native  { native}
+    token package-package { package}
+    token package-role    { role}
 
-    token phaser-BEGIN   { BEGIN   }
-    token phaser-CATCH   { CATCH   }
-    token phaser-CHECK   { CHECK   }
-    token phaser-CLOSE   { CLOSE   }
-    token phaser-CONTROL { CONTROL }
-    token phaser-DOC     { DOC     }
-    token phaser-END     { END     }
-    token phaser-ENTER   { ENTER   }
-    token phaser-FIRST   { FIRST   }
-    token phaser-INIT    { INIT    }
-    token phaser-KEEP    { KEEP    }
-    token phaser-LAST    { LAST    }
-    token phaser-LEAVE   { LEAVE   }
-    token phaser-NEXT    { NEXT    }
-    token phaser-POST    { POST    }
-    token phaser-PRE     { PRE     }
-    token phaser-QUIT    { QUIT    }
-    token phaser-UNDO    { UNDO    }
+    token phaser-BEGIN   { BEGIN}
+    token phaser-CATCH   { CATCH}
+    token phaser-CHECK   { CHECK}
+    token phaser-CLOSE   { CLOSE}
+    token phaser-CONTROL { CONTROL}
+    token phaser-DOC     { DOC}
+    token phaser-END     { END}
+    token phaser-ENTER   { ENTER}
+    token phaser-FIRST   { FIRST}
+    token phaser-INIT    { INIT}
+    token phaser-KEEP    { KEEP}
+    token phaser-LAST    { LAST}
+    token phaser-LEAVE   { LEAVE}
+    token phaser-NEXT    { NEXT}
+    token phaser-POST    { POST}
+    token phaser-PRE     { PRE}
+    token phaser-QUIT    { QUIT}
+    token phaser-UNDO    { UNDO}
 
-    token prefix-let  { let  }
-    token prefix-not  { not  }
-    token prefix-so   { so   }
-    token prefix-temp { temp }
+    token prefix-let  { let}
+    token prefix-not  { not}
+    token prefix-so   { so}
+    token prefix-temp { temp}
 
-    token quote-lang-m  { m  }
-    token quote-lang-ms { ms }
-    token quote-lang-q  { q  }
-    token quote-lang-Q  { Q  }
-    token quote-lang-qq { qq }
-    token quote-lang-rx { rx }
-    token quote-lang-s  { s  }
-    token quote-lang-S  { S  }
-    token quote-lang-ss { ss }
-    token quote-lang-Ss { Ss }
+    token quote-lang-m  { m}
+    token quote-lang-ms { ms}
+    token quote-lang-q  { q}
+    token quote-lang-Q  { Q}
+    token quote-lang-qq { qq}
+    token quote-lang-rx { rx}
+    token quote-lang-s  { s}
+    token quote-lang-S  { S}
+    token quote-lang-ss { ss}
+    token quote-lang-Ss { Ss}
 
-    token routine-method    { method    }
-    token routine-sub       { sub       }
-    token routine-regex     { regex     }
-    token routine-rule      { rule      }
-    token routine-submethod { submethod }
-    token routine-token     { token     }
+    token routine-method    { method}
+    token routine-sub       { sub}
+    token routine-regex     { regex}
+    token routine-rule      { rule}
+    token routine-submethod { submethod}
+    token routine-token     { token}
 
-    token scope-anon      { anon      }
-    token scope-augment   { augment   }
-    token scope-constant  { constant  }
-    token scope-has       { has       }
-    token scope-HAS       { HAS       }
-    token scope-my        { my        }
-    token scope-our       { our       }
-    token scope-state     { state     }
-    token scope-supersede { supersede }
-    token scope-unit      { unit      }
+    token scope-anon      { anon}
+    token scope-augment   { augment}
+    token scope-constant  { constant}
+    token scope-has       { has}
+    token scope-HAS       { HAS}
+    token scope-my        { my}
+    token scope-our       { our}
+    token scope-state     { state}
+    token scope-supersede { supersede}
+    token scope-unit      { unit}
 
-    token stmt-prefix-also    { also    }
-    token stmt-prefix-do      { do      }
-    token stmt-prefix-eager   { eager   }
-    token stmt-prefix-gather  { gather  }
-    token stmt-prefix-hyper   { hyper   }
-    token stmt-prefix-lazy    { lazy    }
-    token stmt-prefix-once    { once    }
-    token stmt-prefix-quietly { quietly }
-    token stmt-prefix-race    { race    }
-    token stmt-prefix-react   { react   }
-    token stmt-prefix-sink    { sink    }
-    token stmt-prefix-start   { start   }
-    token stmt-prefix-supply  { supply  }
-    token stmt-prefix-try     { try     }
+    token stmt-prefix-also    { also}
+    token stmt-prefix-do      { do}
+    token stmt-prefix-eager   { eager}
+    token stmt-prefix-gather  { gather}
+    token stmt-prefix-hyper   { hyper}
+    token stmt-prefix-lazy    { lazy}
+    token stmt-prefix-once    { once}
+    token stmt-prefix-quietly { quietly}
+    token stmt-prefix-race    { race}
+    token stmt-prefix-react   { react}
+    token stmt-prefix-sink    { sink}
+    token stmt-prefix-start   { start}
+    token stmt-prefix-supply  { supply}
+    token stmt-prefix-try     { try}
 
-    token term-self { self }
-    token term-nano { nano }
-    token term-now  { now  }
-    token term-rand { rand }
-    token term-time { time }
+    token term-self { self}
+    token term-nano { nano}
+    token term-now  { now}
+    token term-rand { rand}
+    token term-time { time}
 
-    token traitmod-does    { does    }
-    token traitmod-handles { handles }
-    token traitmod-hides   { hides   }
-    token traitmod-is      { is      }
-    token traitmod-of      { of      }
-    token traitmod-returns { returns }
+    token traitmod-does    { does}
+    token traitmod-handles { handles}
+    token traitmod-hides   { hides}
+    token traitmod-is      { is}
+    token traitmod-of      { of}
+    token traitmod-returns { returns}
 
-    token typer-enum   { enum   }
-    token typer-subset { subset }
+    token typer-enum   { enum}
+    token typer-subset { subset}
 
-    token use-import  { import  }
-    token use-need    { need    }
-    token use-no      { no      }
-    token use-require { require }
-    token use-use     { use     }
+    token use-import  { import}
+    token use-need    { need}
+    token use-no      { no}
+    token use-require { require}
+    token use-use     { use}
 
     # Convert the invocant, a match that is expected to have a RakuAST::Name
     # object as its ".ast" (or to have no ".ast" at all), to a RakuAST::Name

commit 702559fb33357a57f3ac9d81326ca47cb2d2bc81

RakuAST: remove dead code
a186dd05a49f26b3 accidentally removed the "use" before this line,
thus creating a symbol in void context that wouldn't resolve, and
as such became an nqp::null.

Since this did not have any adverse effects, we can remove the rest
of that statement as well.

src/Raku/Grammar.nqp
@@ -1,4 +1,3 @@
-NQPP6QRegex;
 use NQPP5QRegex;
 use Raku::Actions;
 
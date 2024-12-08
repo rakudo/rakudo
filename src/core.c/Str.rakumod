rakudo/rakudo

commit 8365a5deb41a5469f8c05065746d0d3b19d7c48d

Make .trans(Regex => Str) about 3 as fast
As that case can be simplified to .split(Regex).join(Str)

Part of #5488

src/core.c/Str.rakumod
@@ -3012,16 +3012,28 @@ my class Str does Stringy { # declared in BOOTSTRAP
         my $slash := nqp::getlexcaller('$/');
         $/ := $slash if nqp::iscont($slash);
 
-        return self.trans(($what,), |%_)
-          if nqp::not_i(nqp::istype($from,Str))  # from not a string
-          || nqp::not_i(nqp::istype($to,Str))    # or to not a string
-          || %_.Bool;                            # or any named params passed
+        if nqp::istype($to,Str) {
 
-        # from 1 char
-        return nqp::box_s(
-          Rakudo::Internals.TRANSPOSE(self, $from, $to.substr(0,1)),
-          self
-        ) if $from.chars == 1;
+            # Fast path regex to string: ccan be simplified to split/join
+            return self.split($from).join($to)
+              if nqp::istype($from,Regex);
+
+            # Slow path for none strings or nameds
+            return self.trans(($what,), |%_)
+              if nqp::not_i(nqp::istype($from,Str))
+              || %_.Bool;
+
+            # Fast path single char to single other char
+            return nqp::box_s(
+              Rakudo::Internals.TRANSPOSE(self, $from, $to.substr(0,1)),
+              self
+            ) if $from.chars == 1;
+        }
+
+        # Slow path, the "to" is not a string
+        else {
+            return self.trans(($what,), |%_)
+        }
 
         my str $sfrom  = nqp::join("",expand-literal-range($from));
         my str $str    = self;

commit 4082d2c707cebcf496b0b974204b238436bcaa35

Fix issue introduced with 8365a5deb4
Only .trans(Regex => Str) without named arguments can fast path.

Rearrange various tests so that case is also covered.

Part of #5488

src/core.c/Str.rakumod
@@ -3009,19 +3009,28 @@ my class Str does Stringy { # declared in BOOTSTRAP
           unless (my $from := $what.key).defined
               && (my $to   := $what.value).defined;
 
+        # Save caller's $/ now for when it's needed
         my $slash := nqp::getlexcaller('$/');
-        $/ := $slash if nqp::iscont($slash);
 
-        if nqp::istype($to,Str) {
+        # Slow path handler for DRYness
+        my &slow-path = {
+
+            # Make sure slow path will see caller's $/
+            $/ := $slash if nqp::iscont($slash);
+            return self.trans(($what,), |%_);
+        }
+
+        # Slow path for any nameds
+        if %_ {
+            slow-path
+        }
+        elsif nqp::istype($to,Str) {
 
             # Fast path regex to string: ccan be simplified to split/join
-            return self.split($from).join($to)
-              if nqp::istype($from,Regex);
+            return self.split($from).join($to) if nqp::istype($from,Regex);
 
-            # Slow path for none strings or nameds
-            return self.trans(($what,), |%_)
-              if nqp::not_i(nqp::istype($from,Str))
-              || %_.Bool;
+            # Slow path for
+            slow-path unless nqp::istype($from,Str);
 
             # Fast path single char to single other char
             return nqp::box_s(
@@ -3032,7 +3041,7 @@ my class Str does Stringy { # declared in BOOTSTRAP
 
         # Slow path, the "to" is not a string
         else {
-            return self.trans(($what,), |%_)
+            slow-path;
         }
 
         my str $sfrom  = nqp::join("",expand-literal-range($from));
rakudo/rakudo

commit 7fcbfd657cdbde316c1fced6c0947c83f83f9153

RakuAST: add localization of internal regex modifiers
- hide token metachar:sym<mod> from NQP with a local implementation
- create a local token metachar:sym<dba> for handling :dba()
- use regex adverb lookup to do internal modifier localization

This removes support for the undocumented (and untested) :42i and :i(42)
features.  Although technically one could possibly want something like
:i($flag), this is not possible in RakuAST.  And in the legacy grammar
it will silently ignore the value of $flag.

Sadly this appears to have broken support for :dba.  But this does not
break any (further) tests, so leaving that for sometime in the hopefully
near future to fix.

src/Raku/Grammar.nqp
@@ -5841,6 +5841,23 @@ grammar Raku::RegexGrammar is QRegex::P6Regex::Grammar does Raku::Common {
         <.obsbrace>
     }
 
+    token metachar:sym<mod> {
+        ':'
+        $<negated>  = '!'?
+        $<modifier> = \w+
+
+        :my $*NEGATED;
+        :my $*MODIFIER;
+        {
+            $*NEGATED  := ?~$<negated>;
+            $*MODIFIER := self.slangs<MAIN>.adverb-rx2str(~$<modifier>);
+        }
+    }
+
+    token metachar:sym<dba> {
+        ':dba(' <-[)]>* ')'
+    }
+
     token backslash:sym<1> {
         <.[\d] - [0]>\d*
         {}

src/Raku/Actions.nqp
@@ -3664,8 +3664,28 @@ class Raku::RegexActions is HLL::Actions does Raku::CommonActions {
     }
 
     method metachar:sym<mod>($/) {
-        self.attach: $/, $<mod_internal>.ast;
+        my constant CLASS := nqp::hash(
+          'i',          'IgnoreCase',
+          'ignorecase', 'IgnoreCase',
+          'm',          'IgnoreMark',
+          'ignoremark', 'IgnoreMark',
+          'r',          'Ratchet',
+          'ratchet',    'Ratchet',
+          's',          'Sigspace',
+          'sigspace',   'Sigspace'
+        );
+        if CLASS{$*MODIFIER} -> $class {
+            self.attach: $/, Nodify('Regex','InternalModifier',$class).new(
+              negated => $*NEGATED
+            );
+        }
+        else {
+            $/.typed-panic: 'X::Syntax::Regex::UnrecognizedModifier',
+              modifier => $*MODIFIER;
+        }
     }
+
+    method metachar:sym<dba>($/) { Nil }
 
     method metachar:sym<assert>($/) {
         self.attach: $/, $<assertion>.ast;
@@ -3977,24 +3997,6 @@ class Raku::RegexActions is HLL::Actions does Raku::CommonActions {
     method cclass_backslash:sym<any>($/) {
         self.attach: $/,
           Nodify('Regex','CharClassEnumerationElement','Character').new(~$/)
-    }
-
-    method mod_internal($/) {
-        my constant CLASS := nqp::hash(
-          'i', 'IgnoreCase',
-          'm', 'IgnoreMark',
-          'r', 'Ratchet',
-          's', 'Sigspace'
-        );
-        if CLASS{$<mod_ident><sym>} -> $class {
-            my str $n := $<n> ?? ~$<n>[0] !! '';
-            self.attach: $/, Nodify('Regex','InternalModifier',$class).new(
-              negated => $n eq '!' || ($n && +$n == 0)
-            );
-        }
-        else {
-            nqp::die('Unimplemented internal modifier ' ~ $<sym>);
-        }
     }
 
     method codeblock($/) {

commit 8b60dc26087cb7b07c38f373ccd280a7848f0c39

RakuAST: reinstate parsing of numeric args to internal regex modifiers
Fixes regression caused by commit 7fcbfd657cdbde316c1fced6c0947c83f83f9153

src/Raku/Grammar.nqp
@@ -6293,13 +6293,16 @@ grammar Raku::RegexGrammar is QRegex::P6Regex::Grammar does Raku::Common {
 
     token metachar:sym<mod> {
         ':'
-        $<negated>  = '!'?
-        $<modifier> = \w+
+        [
+        || <?before '!'> $<n>=('!')**1  $<modifier> = \w+ »
+        || <?before \d>  $<n>=(\d+)**1  $<modifier> = \w+ »
+        || $<modifier> = \w+
+        ]
 
         :my $*NEGATED;
         :my $*MODIFIER;
         {
-            $*NEGATED  := ?~$<negated>;
+            $*NEGATED := $<negated>[0] gt '' ?? ($<n>[0] eq '!' ?? 1 !! !+$<n>[0]) !! 0;
             $*MODIFIER := self.slangs<MAIN>.adverb-rx2str(~$<modifier>);
         }
     }
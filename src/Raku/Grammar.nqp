rakudo/rakudo

commit 8b60dc26087cb7b07c38f373ccd280a7848f0c39

RakuAST: reinstate parsing of numeric args to internal regex modifiers
Fixes regression caused by commit 7fcbfd657cdbde316c1fced6c0947c83f83f9153

src/Raku/Grammar.nqp
@@ -6293,13 +6293,16 @@ grammar Raku::RegexGrammar is QRegex::P6Regex::Grammar does Raku::Common {
 
     token metachar:sym<mod> {
         ':'
-        $<negated>  = '!'?
-        $<modifier> = \w+
+        [
+        || <?before '!'> $<n>=('!')**1  $<modifier> = \w+ »
+        || <?before \d>  $<n>=(\d+)**1  $<modifier> = \w+ »
+        || $<modifier> = \w+
+        ]
 
         :my $*NEGATED;
         :my $*MODIFIER;
         {
-            $*NEGATED  := ?~$<negated>;
+            $*NEGATED := $<negated>[0] gt '' ?? ($<n>[0] eq '!' ?? 1 !! !+$<n>[0]) !! 0;
             $*MODIFIER := self.slangs<MAIN>.adverb-rx2str(~$<modifier>);
         }
     }

commit 243e2939c16ea254dd0e9d4fb84e7f8fab4a3d13

RakuAST: fix accidental dropping ! from internal regex modifiers
Fixes regression caused by 8b60dc26087cb7b07c38f373ccd280a7848f0c39

src/Raku/Grammar.nqp
@@ -6302,7 +6302,7 @@ grammar Raku::RegexGrammar is QRegex::P6Regex::Grammar does Raku::Common {
         :my $*NEGATED;
         :my $*MODIFIER;
         {
-            $*NEGATED := $<negated>[0] gt '' ?? ($<n>[0] eq '!' ?? 1 !! !+$<n>[0]) !! 0;
+            $*NEGATED := $<n>[0] gt '' ?? ($<n>[0] eq '!' ?? 1 !! !+$<n>[0]) !! 0;
             $*MODIFIER := self.slangs<MAIN>.adverb-rx2str(~$<modifier>);
         }
     }
rakudo/rakudo

commit bc05703131b667160808974f99fd8b3546fa2607

Expand Hash parameterization
This allows one to specify a default value for any containers as
well.  By default, this is the same as the "of" type, as it was
before.

So the standard hash "parameterization" is now Hash[Mu,Str(Any),Any]
(as it was before, but now you can actually explicitely parameterize
a hash that way).

This commit does not actually change anything in the way object
hashes are created.  That will be next.

This breaks two .assuming tests in t/spec/S06-currying/positional.t
that will need fixing.

src/core.c/Hash.rakumod
@@ -465,9 +465,16 @@ my class Hash { # declared in BOOTSTRAP
             !! self.clone
     }
 
-    method ^parameterize(Mu:U \hash, Mu \of, Mu \keyof = Str(Any)) {
+    method ^parameterize(
+      Mu:U \hash,
+      Mu \of,
+      Mu \keyof = Str(Any),
+      Mu \default = of
+    ) {
         # fast path
-        if nqp::eqaddr(of,Mu) && nqp::eqaddr(keyof,Str(Any)) {
+        if nqp::eqaddr(of,Mu)
+          && nqp::eqaddr(keyof,Str(Any))
+          && nqp::eqaddr(default,Any) {
             hash
         }
 
@@ -478,10 +485,12 @@ my class Hash { # declared in BOOTSTRAP
 
         # only constraint on type
         elsif nqp::eqaddr(keyof,Str(Any)) {
-            my $what := hash.^mixin(Hash::Typed[of]);
+            my $what := hash.^mixin(Hash::Typed[of, default]);
              # needs to be done in COMPOSE phaser when that works
-            $what.^set_name:
-              hash.^name ~ '[' ~ of.^name ~ ']';
+            my $name = hash.^name ~ '[' ~ of.^name;
+            $name ~= (',Str(Any),' ~ default.^name)
+              unless nqp::eqaddr(default,of);
+            $what.^set_name: "$name]";
             $what
         }
 
@@ -499,10 +508,11 @@ my class Hash { # declared in BOOTSTRAP
 
         # a true object hash
         else {
-            my $what := hash.^mixin(Hash::Object[of, keyof]);
+            my $what := hash.^mixin(Hash::Object[of, keyof, default]);
             # needs to be done in COMPOSE phaser when that works
-            $what.^set_name:
-              hash.^name ~ '[' ~ of.^name ~ ',' ~ keyof.^name ~ ']';
+            my $name = hash.^name ~ '[' ~ of.^name ~ ',' ~ keyof.^name;
+            $name ~= (',' ~ default.^name) unless nqp::eqaddr(default,of);
+            $what.^set_name: "$name]";
             $what
         }
     }

src/core.c/Hash/Typed.rakumod
@@ -1,10 +1,10 @@
-my role Hash::Typed[::TValue] does Associative[TValue] {
+my role Hash::Typed[::TValue, ::TDefault = TValue] does Associative[TValue] {
 
     # make sure we get the right descriptor
     multi method new(::?CLASS:) {
         nqp::p6bindattrinvres(
           nqp::create(self),Hash,'$!descriptor',
-          ContainerDescriptor.new(:of(TValue), :default(TValue))
+          ContainerDescriptor.new(:of(TValue), :default(TDefault))
         )
     }
 

src/core.c/Hash/Object.rakumod
@@ -1,10 +1,11 @@
-my role Hash::Object[::TValue, ::TKey] does Associative[TValue] {
+my role Hash::Object[::TValue, ::TKey, ::TDefault = TValue]
+  does Associative[TValue] {
 
     # make sure we get the right descriptor
     multi method new(::?CLASS:) {
         nqp::p6bindattrinvres(
           nqp::create(self),Hash,'$!descriptor',
-          ContainerDescriptor.new(:of(TValue), :default(TValue))
+          ContainerDescriptor.new(:of(TValue), :default(TDefault))
         )
     }
     method keyof () { TKey }

commit 9fe52a1d58c2596889318454bafa546222e3c244

Make sure Hash/::Typed/::Object take same parameterization
Not entirely sure exactly what is going on here, but .assuming got
confused by the fact that Hash::Typed only took 2 paremeterization
arguments, and Hash and Hash::Object take 3.

Code should just use the Hash[...] parameterization.  But internally
this parameterization is used to parameterize one of two roles:
if the parameterization for key is Str(Any), then Hash::Typed will
be parameterized and mixed into Hash.  Otherwise, Hash::Object will
be parameterized and mixed in.

What was happening, is that .assuming tried to parameterize
Hash::Type with the arguments it got for Hash[...], which did the
wrong thing.  By making sure the positional parameters are in the
same order and have the same meaning, we're working around this
issue.

I guess in the future, there's something to be said for moving to
named arguments for "of", "keyof" and "default" in parameterization.

This fixes the two failures in t/spec/S06-currying/positional.t
introduced by bc05703131b6671608

src/core.c/Hash.rakumod
@@ -485,7 +485,7 @@ my class Hash { # declared in BOOTSTRAP
 
         # only constraint on type
         elsif nqp::eqaddr(keyof,Str(Any)) {
-            my $what := hash.^mixin(Hash::Typed[of, default]);
+            my $what := hash.^mixin(Hash::Typed[of, Str(Any), default]);
              # needs to be done in COMPOSE phaser when that works
             my $name = hash.^name ~ '[' ~ of.^name;
             $name ~= (',Str(Any),' ~ default.^name)

src/core.c/Hash/Typed.rakumod
@@ -1,4 +1,4 @@
-my role Hash::Typed[::TValue, ::TDefault = TValue] does Associative[TValue] {
+my role Hash::Typed[::TValue, ::TKey, ::TDefault = TValue] does Associative[TValue] {
 
     # make sure we get the right descriptor
     multi method new(::?CLASS:) {
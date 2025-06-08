rakudo/rakudo

commit 283f6bf60376f54a5a941ab79dd7b90ae5e7d239

Remove a lot of uses of IMPL-WRAP-LIST
In UPPERCASE methods we can make assumptions we can't about lowercase
methods.

"[Lower case names are] a public API that can be implemented in HLL
code and therefore potentially returns a Raku List instead of a
VMArray."
 - https://irclogs.raku.org/raku-dev/2025-06-01.html#17:34

src/Raku/ast/variable-access.rakumod
@@ -294,10 +294,10 @@ class RakuAST::Var::Attribute
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Term::Self.new,
             RakuAST::Var::Compiler::Lookup.new('$?CLASS'),
-        ])
+        ]
     }
 
     method IMPL-QAST-PACKAGE-LOOKUP(RakuAST::Impl::QASTContext $context) {
@@ -400,7 +400,7 @@ class RakuAST::Var::Attribute::Public
     }
 
     method args() {
-        $!expression.operand.postfix.args
+        self.IMPL-WRAP-LIST($!expression.operand.postfix.args)
     }
 
     method creates-block() {
@@ -552,9 +552,9 @@ class RakuAST::Var::Compiler::Resources
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier-parts('Distribution', 'Resources')),
-        ])
+        ]
     }
 
     method IMPL-EXPR-QAST(RakuAST::IMPL::QASTContext $context) {
@@ -587,9 +587,9 @@ class RakuAST::Var::Compiler::Distribution
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier-parts('CompUnit', 'Repository', 'Distribution')),
-        ])
+        ]
     }
 
     method IMPL-EXPR-QAST(RakuAST::IMPL::QASTContext $context) {
@@ -669,10 +669,10 @@ class RakuAST::Var::PositionalCapture
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Var::Lexical.new('&postcircumfix:<[ ]>'),
             RakuAST::Var::Lexical.new('$/'),
-        ])
+        ]
     }
 
     method IMPL-EXPR-QAST(RakuAST::IMPL::QASTContext $context) {
@@ -702,10 +702,10 @@ class RakuAST::Var::NamedCapture
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Var::Lexical.new('&postcircumfix:<{ }>'),
             RakuAST::Var::Lexical.new('$/'),
-        ])
+        ]
     }
 
     method IMPL-EXPR-QAST(RakuAST::IMPL::QASTContext $context) {
@@ -836,9 +836,9 @@ class RakuAST::Var::Slang
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('Slang')),
-        ])
+        ]
     }
 
     method sigil() { '$' }

src/Raku/Actions.nqp
@@ -2126,9 +2126,7 @@ class Raku::Actions is HLL::Actions does Raku::CommonActions {
         $<capvar>
           ?? self.attach($/, Nodify('Var', 'NamedCapture').new(
                Nodify('QuotedString').new(
-                 :segments(Nodify('QuotedString').IMPL-WRAP-LIST([
-                   Nodify('StrLiteral').new(~$<desigilname>)
-                 ]))
+                 :segments([Nodify('StrLiteral').new(~$<desigilname>)])
                )
              ))
           !! self.simple-variable($/);
@@ -2152,9 +2150,7 @@ class Raku::Actions is HLL::Actions does Raku::CommonActions {
             my $name := Nodify('Name').from-identifier('infix');
             $name.add-colonpair(
                 Nodify('QuotedString').new(
-                    :segments($name.IMPL-WRAP-LIST([
-                        Nodify('StrLiteral').new(~$<infixish>)
-                    ]))
+                    :segments([Nodify('StrLiteral').new(~$<infixish>)])
                 )
             );
             self.compile-variable-access($/, '&', '', $name);

src/Raku/ast/call.rakumod
@@ -397,11 +397,10 @@ class RakuAST::Call::Name
                         }
 
                         my $protoguilt := @ct_result_multi && $ct_result == -1 ?? True !! False;
-                        my $signature := self.IMPL-WRAP-LIST(
+                        my $signature :=
                             nqp::can($routine, 'is_dispatcher') && $routine.is_dispatcher && !$protoguilt
                                 ?? self.IMPL-MULTI-SIG-LIST($routine)
-                                !! [try $routine.signature.gist]
-                        );
+                                !! [try $routine.signature.gist];
 
                         self.add-sorry(
                             $resolver.build-exception: 'X::TypeCheck::Argument',
@@ -637,7 +636,7 @@ class RakuAST::Call::Method
                     RakuAST::Type::Simple.new: RakuAST::Name.new: |@parts;
             }
         }
-        self.IMPL-WRAP-LIST(@lookups)
+        @lookups
     }
 
     method IMPL-SPECIAL-OP(str $name) {
@@ -1030,9 +1029,9 @@ class RakuAST::Call::PrivateMethod
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Var::Lexical::Constant.new('::?CLASS'),
-        ]);
+        ]
     }
 
     method IMPL-POSTFIX-QAST(RakuAST::IMPL::QASTContext $context, Mu $invocant-qast) {
@@ -1312,11 +1311,11 @@ class RakuAST::Stub
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Simple.new(
               RakuAST::Name.from-identifier-parts('X','StubCode')
             )
-        ])
+        ]
     }
 
     method IMPL-TO-QAST(RakuAST::IMPL::QASTContext $context) {

src/Raku/ast/code.rakumod
@@ -1371,14 +1371,14 @@ class RakuAST::Block
         if $!fresh-exception {
             nqp::push(@implicit, RakuAST::VarDeclaration::Implicit::Special.new(:name('$!')));
         }
-        self.IMPL-WRAP-LIST(@implicit)
+        @implicit
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('Code')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&FATALIZE')),
-        ])
+        ]
     }
 
     method IMPL-FATALIZE() {
@@ -1713,10 +1713,10 @@ class RakuAST::PointyBlock
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('Callable')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&FATALIZE')),
-        ])
+        ]
     }
 
     method IMPL-FATALIZE() {
@@ -1876,10 +1876,10 @@ class RakuAST::Routine
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('Callable')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&FATALIZE')),
-        ])
+        ]
     }
 
     method IMPL-FATALIZE() {
@@ -2025,11 +2025,11 @@ class RakuAST::Routine
                         :scope<my>,
                         :name(self.name),
                         :signature(RakuAST::Signature.new(
-                            :parameters(self.IMPL-WRAP-LIST([
+                            :parameters([
                                 RakuAST::Parameter.new(
                                     :slurpy(RakuAST::Parameter::Slurpy::Capture),
                                 )
-                            ])),
+                            ]),
                         )),
                         :body(RakuAST::OnlyStar.new),
                         :multiness<proto>,
@@ -2114,7 +2114,7 @@ class RakuAST::Routine
         nqp::push(@declarations, RakuAST::VarDeclaration::Implicit::Special.new(:name('$_'))) if $underscore;
         nqp::push(@declarations, RakuAST::VarDeclaration::Implicit::Routine.new()) if $!need-routine-variable;
         nqp::push(@declarations, RakuAST::VarDeclaration::Implicit::Cursor.new()); #TODO maybe we can rule out cases where we don't need it
-        self.IMPL-WRAP-LIST(@declarations)
+        @declarations
     }
 
     method IMPL-HAS-PARAMETER(Str $name) {
@@ -2852,7 +2852,7 @@ class RakuAST::RegexDeclaration
         ];
         nqp::push(@declarations, RakuAST::VarDeclaration::Implicit::Routine.new())
             if nqp::getattr(self, RakuAST::Routine, '$!need-routine-variable');
-        self.IMPL-WRAP-LIST(@declarations)
+        @declarations
     }
 
     method IMPL-COMPILE-BODY(RakuAST::IMPL::QASTContext $context) {
@@ -2975,7 +2975,7 @@ class RakuAST::QuotedMatchConstruct
             }
         }
         nqp::bindattr(self, RakuAST::QuotedMatchConstruct, '$!adverbs',
-            self.IMPL-WRAP-LIST(@checked-adverbs));
+            @checked-adverbs);
         Nil
     }
 
@@ -3082,13 +3082,13 @@ class RakuAST::QuotedRegex
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Var::Lexical.new('$_'),
             RakuAST::Var::Lexical.new('$/'),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier-parts(
                 'Rakudo', 'Internals', 'RegexBoolification6cMarker'
             ))
-        ])
+        ]
     }
 
     method IMPL-IS-IMMEDIATE-MATCH-ADVERB(str $norm-adverb) {
@@ -3229,11 +3229,11 @@ class RakuAST::Substitution
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Var::Lexical.new('$_'),
             RakuAST::Var::Lexical.new('$/'),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('Positional')),
-        ])
+        ]
     }
 
     method IMPL-IS-SUBST-MATCH-ADVERB(str $norm-adverb) {
@@ -3492,10 +3492,10 @@ class RakuAST::Transliteration
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('Pair')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('StrDistance')),
-        ])
+        ]
     }
 
     method IMPL-EXPR-QAST(RakuAST::IMPL::QASTContext $context) {
@@ -3618,9 +3618,9 @@ class RakuAST::CurryThunk
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('WhateverCode'))
-        ])
+        ]
     }
 
     method IMPL-NUM-PARAMS() {
@@ -3657,10 +3657,10 @@ class RakuAST::BlockThunk
     }
 
     method PRODUCE-IMPLICIT-DECLARATIONS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::VarDeclaration::Implicit::BlockTopic.new:
                 parameter => self.signature ?? False !! True
-        ]);
+        ];
     }
 
     method IMPL-THUNK-OBJECT-TYPE() {

src/Raku/ast/compunit.rakumod
@@ -369,7 +369,7 @@ class RakuAST::CompUnit
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier-parts(
                 'CompUnit', 'RepositoryRegistry',
             )),
@@ -378,7 +378,7 @@ class RakuAST::CompUnit
                 'X', 'Comp', 'BeginTime'
             )),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&COMP_EXCEPTION')),
-        ])
+        ]
     }
 
     method IMPL-FATALIZE {
@@ -433,7 +433,7 @@ class RakuAST::CompUnit
             name => '!EVAL_MARKER', value => 1
         )) if $!is-eval;
 
-        self.IMPL-WRAP-LIST(@decls)
+        @decls
     }
 
     method IMPL-TO-QAST-COMP-UNIT(*%options) {

src/Raku/ast/expressions.rakumod
@@ -289,7 +289,7 @@ class RakuAST::Infix
         nqp::push(@lookups,
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('Nil')))
             if $!operator eq '^^' || $!operator eq 'xor';
-        self.IMPL-WRAP-LIST(@lookups)
+        @lookups
     }
 
     method PERFORM-PARSE(RakuAST::Resolver $resolver, RakuAST::IMPL::QASTContext $context) {
@@ -935,13 +935,13 @@ class RakuAST::FlipFlop
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('Nil')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('True')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('False')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('Int')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('Whatever'))
-        ])
+        ]
     }
 
     method PERFORM-BEGIN(RakuAST::Resolver $resolver, RakuAST::IMPL::QASTContext $context) {
@@ -1221,7 +1221,7 @@ class RakuAST::MetaInfix
   is RakuAST::CheckTime
 {
     method IMPL-HOP-INFIX() {
-        self.get-implicit-lookups().AT-POS(0).resolution.compile-time-value()(
+        self.get-implicit-lookups()[0].resolution.compile-time-value()(
             self.infix.IMPL-HOP-INFIX
         )
     }
@@ -1309,9 +1309,9 @@ class RakuAST::MetaInfix::Assign
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier(self.IMPL-OPERATOR-NAME(1))),
-        ])
+        ]
     }
 
     method IMPL-OPERATOR() {
@@ -1407,9 +1407,9 @@ class RakuAST::MetaInfix::Negate
     method reducer-name() { $!infix.reducer-name }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&METAOP_NEGATE')),
-        ])
+        ]
     }
 
     method IMPL-OPERATOR() {
@@ -1468,9 +1468,9 @@ class RakuAST::MetaInfix::Reverse
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&METAOP_REVERSE')),
-        ])
+        ]
     }
 
     method IMPL-OPERATOR() {
@@ -1529,8 +1529,7 @@ class RakuAST::MetaInfix::Sequence
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
-        ])
+        []
     }
 
     method IMPL-OPERATOR() {
@@ -1584,10 +1583,10 @@ class RakuAST::MetaInfix::Cross
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&METAOP_CROSS')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier($!infix.reducer-name)),
-        ])
+        ]
     }
 
     method IMPL-OPERATOR() {
@@ -1611,9 +1610,9 @@ class RakuAST::MetaInfix::Cross
 
     method IMPL-HOP-INFIX() {
         my $lookups := self.get-implicit-lookups;
-        $lookups.AT-POS(0).resolution.compile-time-value()(
+        $lookups[0].resolution.compile-time-value()(
             self.infix.IMPL-OPERATOR,
-            $lookups.AT-POS(1).resolution.compile-time-value,
+            $lookups[1].resolution.compile-time-value,
         )
     }
 
@@ -1676,10 +1675,10 @@ class RakuAST::MetaInfix::Zip
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&METAOP_ZIP')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier($!infix.reducer-name)),
-        ])
+        ]
     }
 
     method IMPL-OPERATOR() {
@@ -1703,9 +1702,9 @@ class RakuAST::MetaInfix::Zip
 
     method IMPL-HOP-INFIX() {
         my $lookups := self.get-implicit-lookups;
-        $lookups.AT-POS(0).resolution.compile-time-value()(
+        $lookups[0].resolution.compile-time-value()(
             self.infix.IMPL-OPERATOR,
-            $lookups.AT-POS(1).resolution.compile-time-value,
+            $lookups[1].resolution.compile-time-value,
         )
     }
 
@@ -1770,9 +1769,9 @@ class RakuAST::MetaInfix::Hyper
     method reducer-name() { $!infix.reducer-name }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&METAOP_HYPER')),
-        ])
+        ]
     }
 
     method IMPL-OPERATOR() {
@@ -1809,7 +1808,7 @@ class RakuAST::MetaInfix::Hyper
     }
 
     method IMPL-HOP-INFIX() {
-        self.get-implicit-lookups().AT-POS(0).resolution.compile-time-value()(
+        self.get-implicit-lookups()[0].resolution.compile-time-value()(
             self.infix.resolution.compile-time-value,
             :dwim-left($!dwim-left),
             :dwim-right($!dwim-right)
@@ -1993,7 +1992,7 @@ class RakuAST::ApplyInfix
         while nqp::isconcrete($!args.arg-at-pos($i)) {
             @colonpairs.push($!args.arg-at-pos($i++));
         }
-        self.IMPL-WRAP-LIST(@colonpairs)
+        @colonpairs
     }
 
     method operands() { $!args.IMPL-UNWRAP-LIST($!args.args) }
@@ -2517,13 +2516,13 @@ class RakuAST::MetaPrefix::Hyper
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&METAOP_HYPER_PREFIX')),
-        ])
+        ]
     }
 
     method IMPL-HOP-INFIX() {
-        self.get-implicit-lookups().AT-POS(0).resolution.compile-time-value()(
+        self.get-implicit-lookups()[0].resolution.compile-time-value()(
             self.prefix.IMPL-HOP-PREFIX
         )
     }
@@ -3218,13 +3217,13 @@ class RakuAST::MetaPostfix::Hyper
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&METAOP_HYPER_POSTFIX')),
-        ])
+        ]
     }
 
     method IMPL-HOP-INFIX() {
-        self.get-implicit-lookups().AT-POS(0).resolution.compile-time-value()(
+        self.get-implicit-lookups()[0].resolution.compile-time-value()(
             self.postfix.IMPL-HOP-POSTFIX
         )
     }

src/Raku/ast/literals.rakumod
@@ -216,7 +216,7 @@ class RakuAST::QuotedString
                 }
             }
         }
-        self.IMPL-WRAP-LIST(@needed)
+        @needed
     }
 
     method type-name() { 'string' }

src/Raku/ast/name.rakumod
@@ -298,10 +298,10 @@ class RakuAST::Name
     }
 
     method PRODUCE-IMPLICIT-LOOKUPS() {
-        self.IMPL-WRAP-LIST([
+        [
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('&INDIRECT_NAME_LOOKUP')),
             RakuAST::Type::Setting.new(RakuAST::Name.from-identifier('PseudoStash')),
-        ])
+        ]
     }
 
     method IMPL-LOOKUP-PARTS() {

commit d8486be9c5976e5a20a6af475989b3b2b833501b

Fix "This type (RakuAST::ArgList) does not support elems" in make test
postfix.args is an ArgList object, not an array, so no wrapping
necessary and indeed wrapping would create a broken List object.

Fixes regression caused by commit 283f6bf60376f54a5a941ab79dd7b90ae5e7d239

src/Raku/ast/variable-access.rakumod
@@ -400,7 +400,7 @@ class RakuAST::Var::Attribute::Public
     }
 
     method args() {
-        self.IMPL-WRAP-LIST($!expression.operand.postfix.args)
+        $!expression.operand.postfix.args
     }
 
     method creates-block() {
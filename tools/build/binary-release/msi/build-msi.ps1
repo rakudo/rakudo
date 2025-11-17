635279c8f9ce21661f4ec844db9078ee6acde88f
Patrick Böker <patrick.boeker@posteo.de>
2025-11-17 14:24:01 +0100

    Win MSI: Remove stale command
    Forgot to remove this during the last refactor

--- tools/build/binary-release/msi/build-msi.ps1
+++ tools/build/binary-release/msi/build-msi.ps1
@@ -16,7 +16,6 @@ Copy-Item -Path "$asset_dir\scripts\*" -Destination "$work_rakudo_release_dir\sc
 
 &"$wix_dir\heat.exe" dir $work_rakudo_release_dir -dr RAKUDO_BIN_DIR_REFS -cg FileComponents -gg -g1 -sfrag -srd -suid -ke -sw5150 -var "var.RAKUDO_BIN_FILES_DIR" -out "$work_dir\rakudo_bin_files.wxs"
 &"$wix_dir\candle.exe" "$work_dir\rakudo_bin_files.wxs" -arch x64 -dRAKUDO_BIN_FILES_DIR="$work_rakudo_release_dir" -out "$work_dir\rakudo_bin_files.wxsobj"
-&"$wix_dir\candle.exe" "$work_dir\asset_files.wxs" -arch x64 -dASSET_FILES_DIR="$asset_dir" -out "$work_dir\asset_files.wxsobj"
 
 &"$wix_dir\candle.exe" "$msi_dir\rakudo.wxs" -ext WixUtilExtension -dVERSION="$rakudo_version" -dASSET_FILE_DIR="$asset_dir" -dINSTALLER_ASSET_FILE_DIR="$installer_asset_dir" -out "$work_dir\rakudo.wxsobj"
 

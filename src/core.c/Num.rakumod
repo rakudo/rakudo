rakudo/rakudo

commit 1b46f7135b7776e7795f240a5e539f055776d6a7

Add infix + - / * candidates for mixed int / num (#6040)
* Add infix + - / * candidates for mixed int / num

Inspired by the benchmark mentioned at
  https://irclogs.raku.org/raku/2025-12-11.html#22:53

When using native int and num in an expression with these infixes,
both the natives where being upgraded, which makes it a *lot* slower.

Since the underlying nqp::xxx_n() ops accept native ints (these probably
get a nqp::coerce_in inserted at MAST time, I guess), the only
thing needed was to add candidates for the mixed cases (assuming
the fact that a native "num" was used being enough infectuous to
have a native num returned, instead of a Rat).

Makes these infix operations, such as:

  my int $b = 42;
  my num $a = 1e0;
  my num $c = $a * $b for ^10000000;
  say now - INIT now;

between 4x and 7x as fast.

* Some more native infix fixes

- add implementation-detail method "divzero" that produces a division
  by 0 error for its invocant
- call this method when attempting to divide by zero for / and %
  hereby reducing bytecode size of methods, therefore making them
  easier to inline.
- add mixed native num/int candidates for infix % as well
- remove --> num return signatures, as they will bomb in an unuseful
  way with the legacy grammar if a Failure is returned
- use nqp ops to see if the divisor is 0: this should prevent needing
  to HLLize the value to be able to call the Bool method on it, and
  thus save a bit on performance (although it looks like spesh all
  optimized the HLLizing away eventually, so this is only a boon for
  short running programs

* Address japhb++'s thinko catches

src/core.c/Num.rakumod
@@ -286,6 +286,10 @@ my class Num does Real { # declared in BOOTSTRAP
     method UPGRADE-RAT(\nu, \de) is raw {
         nqp::p6box_n(nqp::div_In(nu,de))  # downgrade to float
     }
+
+    method divzero() is implementation-detail {
+        X::Numeric::DivideByZero.new(:using</>, :numerator(self)).Failure
+    }
 }
 
 my constant tau = 6.28318_53071_79586_476e0;
@@ -352,44 +356,48 @@ multi sub abs(num   $a --> num)   { nqp::abs_n($a)               }
 multi sub infix:<+>(Num:D $a, Num:D $b) {
     nqp::p6box_n(nqp::add_n($a,$b))
 }
-multi sub infix:<+>(num $a, num $b --> num) {
-    nqp::add_n($a, $b)
-}
+multi sub infix:<+>(num $a, num $b --> num) { nqp::add_n($a, $b) }
+multi sub infix:<+>(int $a, num $b --> num) { nqp::add_n($a, $b) }
+multi sub infix:<+>(num $a, int $b --> num) { nqp::add_n($a, $b) }
 
 multi sub infix:<->(Num:D $a, Num:D $b) {
     nqp::p6box_n(nqp::sub_n($a,$b))
 }
-multi sub infix:<->(num $a, num $b --> num) {
-    nqp::sub_n($a, $b)
-}
+multi sub infix:<->(num $a, num $b --> num) { nqp::sub_n($a, $b) }
+multi sub infix:<->(int $a, num $b --> num) { nqp::sub_n($a, $b) }
+multi sub infix:<->(num $a, int $b --> num) { nqp::sub_n($a, $b) }
 
 multi sub infix:<*>(Num:D $a, Num:D $b) {
     nqp::p6box_n(nqp::mul_n($a,$b))
 }
-multi sub infix:<*>(num $a, num $b --> num) {
-    nqp::mul_n($a, $b)
-}
+multi sub infix:<*>(num $a, num $b --> num) { nqp::mul_n($a, $b) }
+multi sub infix:<*>(int $a, num $b --> num) { nqp::mul_n($a, $b) }
+multi sub infix:<*>(num $a, int $b --> num) { nqp::mul_n($a, $b) }
 
 multi sub infix:</>(Num:D $a, Num:D $b) {
-    $b
-      ?? nqp::p6box_n(nqp::div_n($a,$b))
-      !! X::Numeric::DivideByZero.new(:using</>, :numerator($a)).Failure
+    nqp::iseq_n($b,0e0) ?? $a.divzero !!  nqp::p6box_n(nqp::div_n($a,$b))
 }
-multi sub infix:</>(num $a, num $b --> num) {
-    $b
-      ?? nqp::div_n($a, $b)
-      !! fail X::Numeric::DivideByZero.new(:using</>, :numerator($a))
+multi sub infix:</>(num $a, num $b) {
+    nqp::iseq_n($b,0e0) ?? $a.divzero !! nqp::div_n($a, $b)
+}
+multi sub infix:</>(num $a, int $b) {
+    nqp::iseq_i($b,0) ?? $a.divzero !! nqp::div_n($a, $b)
+}
+multi sub infix:</>(int $a, num $b) {
+    nqp::iseq_n($b,0e0) ?? nqp::coerce_in($a).divzero !! nqp::div_n($a, $b)
 }
 
 multi sub infix:<%>(Num:D $a, Num:D $b) {
-    $b
-      ?? nqp::p6box_n(nqp::mod_n($a,$b))
-      !! X::Numeric::DivideByZero.new(:using<%>, :numerator($a)).Failure
+    nqp::iseq_n($b,0e0) ?? $a.divzero !! nqp::p6box_n(nqp::mod_n($a,$b))
 }
-multi sub infix:<%>(num $a, num $b --> num) {
-    $b
-      ?? nqp::mod_n($a, $b)
-      !! fail X::Numeric::DivideByZero.new(:using<%>, :numerator($a))
+multi sub infix:<%>(num $a, num $b) {
+    nqp::iseq_n($b,0e0) ?? $a.divzero !! nqp::mod_n($a,$b)
+}
+multi sub infix:<%>(num $a, int $b) {
+    nqp::iseq_i($b,0) ?? $a.divzero !! nqp::mod_n($a,$b)
+}
+multi sub infix:<%>(int $a, num $b) {
+    nqp::iseq_n($b,0e0) ?? nqp::coerce_in($a).divzero !! nqp::mod_n($a,$b)
 }
 
 # (If we get 0 here, must be underflow, since floating overflow provides Inf.)
@@ -399,11 +407,11 @@ multi sub infix:<**>(Num:D $a, Num:D $b) {
         ?? 0e0
         !! X::Numeric::Underflow.new.Failure
 }
-multi sub infix:<**>(num $a, num $b --> num) {
+multi sub infix:<**>(num $a, num $b) {
     nqp::pow_n($a, $b)
       or $a == 0e0 || $b.abs == Inf
         ?? 0e0
-        !! fail X::Numeric::Underflow.new
+        !! X::Numeric::Underflow.new.Failure
 }
 
 # Here we sort NaN in with string "NaN"

commit fbb967b32acb2c63cd1d5fe4c7c8f7ebb7ec919b

Fix roast test failure caused by 1b46f7135b7776e779

src/core.c/Num.rakumod
@@ -290,6 +290,9 @@ my class Num does Real { # declared in BOOTSTRAP
     method divzero() is implementation-detail {
         X::Numeric::DivideByZero.new(:using</>, :numerator(self)).Failure
     }
+    method modzero() is implementation-detail {
+        X::Numeric::DivideByZero.new(:using<%>, :numerator(self)).Failure
+    }
 }
 
 my constant tau = 6.28318_53071_79586_476e0;
@@ -388,16 +391,16 @@ multi sub infix:</>(int $a, num $b) {
 }
 
 multi sub infix:<%>(Num:D $a, Num:D $b) {
-    nqp::iseq_n($b,0e0) ?? $a.divzero !! nqp::p6box_n(nqp::mod_n($a,$b))
+    nqp::iseq_n($b,0e0) ?? $a.modzero !! nqp::p6box_n(nqp::mod_n($a,$b))
 }
 multi sub infix:<%>(num $a, num $b) {
-    nqp::iseq_n($b,0e0) ?? $a.divzero !! nqp::mod_n($a,$b)
+    nqp::iseq_n($b,0e0) ?? $a.modzero !! nqp::mod_n($a,$b)
 }
 multi sub infix:<%>(num $a, int $b) {
-    nqp::iseq_i($b,0) ?? $a.divzero !! nqp::mod_n($a,$b)
+    nqp::iseq_i($b,0) ?? $a.modzero !! nqp::mod_n($a,$b)
 }
 multi sub infix:<%>(int $a, num $b) {
-    nqp::iseq_n($b,0e0) ?? nqp::coerce_in($a).divzero !! nqp::mod_n($a,$b)
+    nqp::iseq_n($b,0e0) ?? nqp::coerce_in($a).modzero !! nqp::mod_n($a,$b)
 }
 
 # (If we get 0 here, must be underflow, since floating overflow provides Inf.)
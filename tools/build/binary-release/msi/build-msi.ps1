b05880342e983581fd6164ca209d099e15b09e71
Patrick Böker <patrick.boeker@posteo.de>
2026-01-27 12:57:01 +0100

    fix Windows MSI build

--- tools/build/binary-release/msi/build-msi.ps1
+++ tools/build/binary-release/msi/build-msi.ps1
@@ -1,4 +1,4 @@
-param ([string]$rakudo_version, [string]$rakudo_release_dir, [string]$out_file)
+param ([string]$rakudo_version, [string]$product_version, [string]$rakudo_release_dir, [string]$out_file)
 
 $msi_dir = split-path -parent $MyInvocation.MyCommand.Definition
 
@@ -17,6 +17,6 @@ Copy-Item -Path "$asset_dir\scripts\*" -Destination "$work_rakudo_release_dir\sc
 &"$wix_dir\heat.exe" dir $work_rakudo_release_dir -dr RAKUDO_BIN_DIR_REFS -cg FileComponents -gg -g1 -sfrag -srd -suid -ke -sw5150 -var "var.RAKUDO_BIN_FILES_DIR" -out "$work_dir\rakudo_bin_files.wxs"
 &"$wix_dir\candle.exe" "$work_dir\rakudo_bin_files.wxs" -arch x64 -dRAKUDO_BIN_FILES_DIR="$work_rakudo_release_dir" -out "$work_dir\rakudo_bin_files.wxsobj"
 
-&"$wix_dir\candle.exe" "$msi_dir\rakudo.wxs" -ext WixUtilExtension -dVERSION="$rakudo_version" -dASSET_FILE_DIR="$asset_dir" -dINSTALLER_ASSET_FILE_DIR="$installer_asset_dir" -out "$work_dir\rakudo.wxsobj"
+&"$wix_dir\candle.exe" "$msi_dir\rakudo.wxs" -ext WixUtilExtension -dVERSION="$rakudo_version" -dPRODUCT_VERSION="$product_version" -dASSET_FILE_DIR="$asset_dir" -dINSTALLER_ASSET_FILE_DIR="$installer_asset_dir" -out "$work_dir\rakudo.wxsobj"
 
 &"$wix_dir\light.exe" -ext WixUIExtension -ext WixUtilExtension "$work_dir\rakudo_bin_files.wxsobj" "$work_dir\rakudo.wxsobj" -sw1076 -o $out_file

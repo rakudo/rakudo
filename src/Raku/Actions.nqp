rakudo/rakudo

commit 6d2fa67e535f3dee79847d0821766cd323562d3d

RakuAST: make =document a directive
As requested in https://github.com/Raku/RakuDoc-v2/issues/94

src/Raku/Actions.nqp
@@ -4014,28 +4014,33 @@ class Raku::Actions is HLL::Actions does Raku::CommonActions {
         }
     }
 
-    method doc-block:sym<column>($/) {
+    # helper method for simple directives
+    method add-as-directive($/, $type) {
         unless $*FROM-SEEN{$/.from}++ {
             self.doc-origin: $/, Nodify('Doc','Block').new:
-              :directive, :margin(~$<margin>), :type<column>,
+              :directive, :margin(~$<margin>), :$type,
               :config(self.extract-config($/))
         }
+    }
+
+    method doc-block:sym<column>($/) {
+        self.add-as-directive($/, 'column');
+    }
+
+    method doc-block:sym<counter>($/) {
+        self.add-as-directive($/, 'counter');
+    }
+
+    method doc-block:sym<document>($/) {
+        self.add-as-directive($/, 'counter');
     }
 
     method doc-block:sym<row>($/) {
-        unless $*FROM-SEEN{$/.from}++ {
-            self.doc-origin: $/, Nodify('Doc','Block').new:
-              :directive, :margin(~$<margin>), :type<row>,
-              :config(self.extract-config($/))
-        }
+        self.add-as-directive($/, 'row');
     }
 
     method doc-block:sym<place>($/) {
-        unless $*FROM-SEEN{$/.from}++ {
-            self.doc-origin: $/, Nodify('Doc','Block').new:
-              :directive, :margin(~$<margin>), :type<place>,
-              :config(self.extract-config($/))
-        }
+        self.add-as-directive($/, 'place');
     }
 
     method doc-block:sym<formula>($/) {
@@ -4051,14 +4056,6 @@ class Raku::Actions is HLL::Actions does Raku::CommonActions {
         unless $*FROM-SEEN{$/.from}++ {
             self.doc-origin: $/, Nodify('Doc','Block').from-config:
               :directive, :margin(~$<margin>), :type<config>,
-              :config(self.extract-config($/)), :key(~$<doc-identifier>)
-        }
-    }
-
-    method doc-block:sym<counter>($/) {
-        unless $*FROM-SEEN{$/.from}++ {
-            self.doc-origin: $/, Nodify('Doc','Block').from-config:
-              :directive, :margin(~$<margin>), :type<counter>,
               :config(self.extract-config($/)), :key(~$<doc-identifier>)
         }
     }

src/Raku/Grammar.nqp
@@ -5744,7 +5744,7 @@ Rakudo significantly on *every* run."
     # directives that may not be used as block names
     token rakudoc-directives {
         [
-        alias | begin | column | config | counter | end | finish | for | place | row
+          alias  | begin | column | config | counter | document | end | finish | for | place | row
         ] >>
     }
 
@@ -5853,6 +5853,20 @@ Rakudo significantly on *every* run."
         [\h+ $<doc-identifier>=[ <.doc-identifier> | '*' ] ]?
 
         # fetch any counter configuration
+        [ [\n $<margin> '=']? \h+ <colonpair> ]*
+
+        # should now be at end of line
+        <.doc-newline>
+    }
+
+    # handle =document
+    token doc-block:sym<document> {
+
+        ^^ $<margin>=[ \h* ] '=document'
+
+        [\h+ $<doc-identifier>=[ <.doc-identifier> | '*' ] ]?
+
+        # fetch any document configuration
         [ [\n $<margin> '=']? \h+ <colonpair> ]*
 
         # should now be at end of line

commit ddd5a97caeb5a23010239b305a9a0a99309fd1ed

RakuAST: restore =counter behaviour
That was broken with 6d2fa67e535f3dee79 finanalyst++ for spotting

src/Raku/Actions.nqp
@@ -4027,10 +4027,6 @@ class Raku::Actions is HLL::Actions does Raku::CommonActions {
         self.add-as-directive($/, 'column');
     }
 
-    method doc-block:sym<counter>($/) {
-        self.add-as-directive($/, 'counter');
-    }
-
     method doc-block:sym<document>($/) {
         self.add-as-directive($/, 'document');
     }
@@ -4041,6 +4037,14 @@ class Raku::Actions is HLL::Actions does Raku::CommonActions {
 
     method doc-block:sym<place>($/) {
         self.add-as-directive($/, 'place');
+    }
+
+    method doc-block:sym<counter>($/) {
+        unless $*FROM-SEEN{$/.from}++ {
+            self.doc-origin: $/, Nodify('Doc','Block').from-config:
+              :directive, :margin(~$<margin>), :type<counter>,
+              :config(self.extract-config($/)), :key(~$<doc-identifier>)
+        }
     }
 
     method doc-block:sym<formula>($/) {
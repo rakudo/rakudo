rakudo/rakudo

commit 1cd333d857736f23e741f0995868b63bc6b8db68

RakuAST: ::Regex::Statement doesn't need an additional ;
As spotted by dwarring++.  Fixes #5991

src/core.c/RakuAST/Deparse.rakumod
@@ -1998,7 +1998,7 @@ CODE
     }
 
     multi method deparse(RakuAST::Regex::Statement:D $ast --> Str:D) {
-        ':' ~ self.deparse($ast.statement) ~ '; '
+        ':' ~ self.deparse($ast.statement).chomp
     }
 
 #- Regex::W --------------------------------------------------------------------

commit 2efcd85522233a5b656ee08c521e956d0dedfa14

RakuAST: tweak deparsing of ::Regex::Statement
Apparently 1cd333d857736f23e broke some cases of deparsing in the
test suite

src/core.c/RakuAST/Deparse.rakumod
@@ -1998,7 +1998,8 @@ CODE
     }
 
     multi method deparse(RakuAST::Regex::Statement:D $ast --> Str:D) {
-        ':' ~ self.deparse($ast.statement).chomp
+        my $deparsed := self.deparse($ast.statement).chomp;
+        ':' ~ ($deparsed.ends-with(';') ?? $deparsed !! $deparsed ~ '; ')
     }
 
 #- Regex::W --------------------------------------------------------------------
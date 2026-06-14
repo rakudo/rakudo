rakudo/rakudo

commit bd750d4fae5c17b643fc1c95a6f42215048750f9

Disable experimental macro usage in 6.e and higher
As a result of the discussion in:
  https://github.com/Raku/problem-solving/issues/520
and the decision by the Raku Steering Council.

This effectively bars one from specifying "use experimental :macros"
in 6.e and higher.  The idea being that true macro support will
evolve in the 6.f version, or will become a user lang slang.

The Raku Steering Council would like to thank all the people that
worked on getting macro support in the Raku Programming Language.
Macro support of the future will stand on the shoulders of the
giants that brought it this far!

lib/experimental.rakumod
@@ -23,7 +23,11 @@ package EXPORT::cached {
 }
 
 package EXPORT::macros {
-    OUR::<EXPERIMENTAL-MACROS> := True;
+    nqp::getcomp('Raku').language_revision < 3
+      ?? (OUR::<EXPERIMENTAL-MACROS> := True)
+      !! die "Experimental macro support not available in Raku {
+           Raku.version
+         }.";
 }
 
 package EXPORT::smallnatives {

commit e93a9dfac1f44bfbe42ee19ca50ede97ad877527

Disable macros in 6.e
This is a slightly other approach to disabling macros in 6.e.

In this commit, the internal flag to mark macros allowed is set to
False if "use experimental :macros" is specified in 6.e.

Then *if* a macro is being used, the X::Experimental error will be
thrown with an appropriate message for that situation:
  Experimental macros are no longer supported in Raku 6.e.
rather than:
  Use of macros is experimental; please 'use experimental :macros;

The approach of throwing an error on the use of "use experimental
:macros" itself is actually impossible without going into the depths
of the legacy grammar.  Which felt dirty.

The problem is really that "lib/experimental.rakumod" runs all of
the mainlines in the source on "use experimental".  Only later, the
core decides to selectively import the things set in the packages
respective EXPORT:: namespaces.

So https://github.com/rakudo/rakudo/commit/bd750d4fae caused *any*
use of "use experimental" to fail with the message that macros are
no longer supported  :-(

TIL

lib/experimental.rakumod
@@ -23,7 +23,7 @@ package EXPORT::cached {
 }
 
 package EXPORT::macros {
-    OUR::<EXPERIMENTAL-MACROS> := True;
+    OUR::<EXPERIMENTAL-MACROS> := nqp::getcomp('Raku').language_revision < 3;
 }
 
 package EXPORT::smallnatives {

src/core.c/Exception.rakumod
@@ -1110,7 +1110,12 @@ my class X::NYI::BigInt is Exception {  # XXX not used in core
 my class X::Experimental does X::Comp {
     has $.feature;
     has $.use = $!feature;
-    method message() { "Use of $.feature is experimental; please 'use experimental :$.use'" }
+    method message() {
+        my $version := ~Raku.version;
+        $version eq '6.e' && $.use eq 'macros'
+          ?? "Experimental $.feature are no longer supported in Raku $version."
+          !! "Use of $.feature is experimental; please 'use experimental :$.use;'"
+    }
 }
 
 my class X::Worry is Exception { }